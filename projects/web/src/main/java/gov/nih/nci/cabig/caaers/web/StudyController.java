f5a3a0150603c9641465c2ccd7e130b725eae643
Sujith Vellat <vtsujith@gmail.com>
2006-12-21 16:31:08 +0000

    Initial check-in of Controller Class for the Study(Protocol). This will be enhanced later.
    SVN-Revision: 33

---
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/StudyController.java
@@ -0,0 +1,45 @@
+package gov.nih.nci.cabig.caaers.web;
+
+import gov.nih.nci.cabig.caaers.dao.StudyDao;
+import gov.nih.nci.cabig.caaers.domain.Study;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.validation.BindException;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Controller class for the Study module.
+ * This handles the Create/Update/List flows for the Study.
+ * @author Sujith Vellat Thayyilthodi
+ * @see gov.nih.nci.cabig.caaers.dao.StudyDao
+ * @since 1.0
+ * */
+public final class StudyController extends CaaersAbstractFormController {
+
+	private StudyDao studyDao;
+
+	public StudyController() {
+		setCommandClass(Study.class);
+	}
+
+	public StudyDao getStudyDao() {
+		return studyDao;
+	}
+
+	public void setStudyDao(StudyDao studyDao) {
+		this.studyDao = studyDao;
+	}
+
+	protected ModelAndView onSubmit(HttpServletRequest request,
+			HttpServletResponse response, Object oCommand, BindException errors)
+			throws Exception {
+		Study study = (Study) oCommand;
+		studyDao.save(study);
+		ModelAndView modelAndView = new ModelAndView(getSuccessView());
+		modelAndView.addObject("study", study);
+		modelAndView.addAllObjects(errors.getModel());
+		return modelAndView;
+	}
+}
