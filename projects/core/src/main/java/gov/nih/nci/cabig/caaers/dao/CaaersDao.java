744685f3a9ff81ee2e332d98649101e2ddbf96f7
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-01-02 15:18:34 +0000

    Add protected log instance for subclass use (fix SiteDao not compiling)
    SVN-Revision: 90

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/CaaersDao.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/CaaersDao.java
@@ -1,15 +1,17 @@
 package gov.nih.nci.cabig.caaers.dao;
 
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import gov.nih.nci.cabig.caaers.domain.DomainObject;
-
-import java.lang.annotation.ElementType;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 /**
  * @author Rhett Sutphin
  */
 /* TODO: much of this class is shared with PSC.  Refactor into a shared library. */
 public abstract class CaaersDao<T extends DomainObject> extends HibernateDaoSupport {
+    protected final Log log = LogFactory.getLog(getClass());
+
     @SuppressWarnings("unchecked")
     public T getById(int id) {
         return (T) getHibernateTemplate().get(domainClass(), id);
