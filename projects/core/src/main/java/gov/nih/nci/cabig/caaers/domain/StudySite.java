56e92d72850d584da88c25f1706be42684788f76
Kulasekaran Sethumadhavan <kulasekaran@gmail.com>
2007-01-19 04:37:48 +0000

    added new attributes
    SVN-Revision: 211

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/StudySite.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/StudySite.java
@@ -6,17 +6,19 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
  * @author Krikor Krumlian
- *
+ * @author Kulasekaran
  */
 @Entity
 @Table (name = "study_sites")
@@ -25,30 +27,35 @@ import java.util.List;
         @Parameter(name="sequence", value="study_sites_id_seq")
     }
 )
-public class StudySite extends AbstractDomainObject {
-    private Site site;
-    private Study study;
+public class StudySite extends AbstractDomainObject {	
+	private Site site;
+    private Study study;    
+	private Date irbApprovalDate;
+    private String roleCode;
+    private String statusCode;
+    private Date startDate;
+    private Date endDate;
     private List<StudyParticipantAssignment> studyParticipantAssignments = new ArrayList<StudyParticipantAssignment>();
-
-    ////// LOGIC
+    
+    //////LOGIC
 
     public void addAssignment(StudyParticipantAssignment assignment) {
         getStudyParticipantAssignments().add(assignment);
         assignment.setStudySite(this);
     }
-
-    /** Are there any assignments using this relationship? */
+    
+	/** Are there any assignments using this relationship? */
     @Transient
     public boolean isUsed() {
         return getStudyParticipantAssignments().size() > 0;
     }
-    
+
     @Transient
     public String getSiteStudyNames() {
         return study.getShortTitle() + " : " + site.getName();
     }
-
-    ////// BEAN PROPERTIES
+    
+    /// BEAN PROPERTIES
 
     public void setSite(Site site) {
         this.site = site;
@@ -59,7 +66,7 @@ public class StudySite extends AbstractDomainObject {
     public Site getSite() {
         return site;
     }
-
+        
     public void setStudy(Study study) {
         this.study = study;
     }
@@ -78,9 +85,49 @@ public class StudySite extends AbstractDomainObject {
     @Cascade (value = { CascadeType.ALL, CascadeType.DELETE_ORPHAN })
     public List<StudyParticipantAssignment> getStudyParticipantAssignments() {
         return studyParticipantAssignments;
+    }    
+
+    public void setIrbApprovalDate(Date irbApprovalDate) {
+        this.irbApprovalDate = irbApprovalDate;
     }
 
-    ////// OBJECT METHODS
+    public Date getIrbApprovalDate() {
+        return irbApprovalDate;
+    }
+
+    public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getRoleCode() {
+		return roleCode;
+	}
+
+	public void setRoleCode(String roleCode) {
+		this.roleCode = roleCode;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}	
+
+	//////OBJECT METHODS
 
     public boolean equals(Object obj) {
         if (this == obj) return true;
