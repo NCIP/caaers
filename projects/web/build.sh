b71ead20ebdbbddd1c75730802a98e26b0b39823
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-01-19 07:13:47 +0000

    Build script; principal value is that it will use growlnotify to indicate build completion, if present
    SVN-Revision: 213

---
+++ projects/web/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -z "$ANT" ]; then
+    ANT=ant
+fi
+
+echo "JAVA_HOME: $JAVA_HOME"
+echo "ANT: $ANT"
+echo "ANT_OPTS: $ANT_OPTS"
+echo "@: $@"
+$ANT "$@"
+ANT_STATUS="$?"
+
+# Use growl to indicate build completion if available
+GROWL=`which growlnotify`
+if [ -e "$GROWL" ]; then
+    if [ "$ANT_STATUS" -eq 0 ]; then
+        TITLE="Study calendar build successful"
+    else
+        TITLE="Study calendar build failed"
+    fi
+    echo "
+    `pwd`
+    $@" | $GROWL $TITLE
+fi
