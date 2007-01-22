27373dc1e41dcf646056f279dd9e72c58f257e99
Sujith Vellat <vtsujith@gmail.com>
2007-01-22 06:01:35 +0000

    Integrating grid identifier with caaers.
    SVN-Revision: 269

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/utils/GridIdentifierCreator.java
@@ -0,0 +1,14 @@
+package gov.nih.nci.cabig.caaers.utils;
+
+/**
+ * @author Rhett Sutphin
+ */
+public interface GridIdentifierCreator {
+
+	/**
+	 * @param identification is a string that is used by
+	 * the caAERS application to store the uniquely identifyable
+	 * information for that resource.
+	 */
+	String getGridIdentifier(String identification);
+}
---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/utils/LocalGridIdentifierCreator.java
@@ -0,0 +1,18 @@
+package gov.nih.nci.cabig.caaers.utils;
+
+import java.util.UUID;
+
+
+/**
+ * A local implementation of GridIdentifierCreator, suitable for use in environments where
+ * there is no external identifier service.  The each identifier returned is the string version of a
+ * call to {UUID#randomUUID}.
+ *  
+ * @see {UUID}
+ * @author Rhett Sutphin
+ */
+public class LocalGridIdentifierCreator implements GridIdentifierCreator {
+    public String getGridIdentifier(String id) {
+        return UUID.randomUUID().toString();
+    }
+}
