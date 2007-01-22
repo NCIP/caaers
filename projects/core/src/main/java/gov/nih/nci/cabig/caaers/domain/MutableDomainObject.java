26c2fd618a6bff054bade4c351eb299d00762c6b
Sujith Vellat <vtsujith@gmail.com>
2007-01-22 05:58:32 +0000

    Added gridId
    SVN-Revision: 268

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/MutableDomainObject.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/MutableDomainObject.java
@@ -2,17 +2,30 @@ package gov.nih.nci.cabig.caaers.domain;
 
 /**
  * @author Rhett Sutphin
+ * @author Sujith Vellat Thayyilthodi
  */
 public interface MutableDomainObject extends DomainObject {
-    /**
-     * @return the optimistic locking version value for this object.
-     */
-    Integer getVersion();
+	/**
+	 * @return the optimistic locking version value for this object.
+	 */
+	Integer getVersion();
 
-    /**
-     * Set the optimistic locking version value for this object.  In practice this should not be
-     * called by application code -- just the persistence mechanism.
-     * @param version
-     */
-    void setVersion(Integer version);
+	/**
+	 * Set the optimistic locking version value for this object.  In practice this should not be
+	 * called by application code -- just the persistence mechanism.
+	 * @param version
+	 */
+	void setVersion(Integer version);
+
+	/**
+	 * @return the grid-scoped unique identifier for this object
+	 */
+	String getGridId();
+
+	/**
+	 * Specify the grid-scoped unique identifier for this object
+	 * @param gridId
+	 */
+	void setGridId(String bigId);
+
 }
