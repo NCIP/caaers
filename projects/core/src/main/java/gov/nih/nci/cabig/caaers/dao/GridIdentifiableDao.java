2bc16f8e37dda0fefa500f0eb8f6444db2511cce
Sujith Vellat <vtsujith@gmail.com>
2007-01-23 14:13:27 +0000

    getByGridId updated. Hard coding of Study in fetch by exampe is removed.
    SVN-Revision: 309

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/GridIdentifiableDao.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/GridIdentifiableDao.java
@@ -1,10 +1,12 @@
 package gov.nih.nci.cabig.caaers.dao;
 
 
+import org.apache.commons.beanutils.PropertyUtils;
+
 import edu.nwu.bioinformatics.commons.CollectionUtils;
+import gov.nih.nci.cabig.caaers.CaaersSystemException;
 import gov.nih.nci.cabig.caaers.domain.DomainObject;
 import gov.nih.nci.cabig.caaers.domain.GridIdentifiable;
-import gov.nih.nci.cabig.caaers.domain.Study;
 
 /**
  * 
@@ -12,13 +14,36 @@ import gov.nih.nci.cabig.caaers.domain.Study;
  * */
 public abstract class GridIdentifiableDao<T extends DomainObject & GridIdentifiable> extends CaaersDao<T>{
     
-	public T getByGridId(String gridId) {
+/*	
+	//Implementation taken from Study Calendar code. Needs to know why Study was hardcoded.
+    public T getByGridId(String gridId) {
         Study example = new Study();
         example.setGridId(gridId);
         return (T) CollectionUtils.firstElement(getHibernateTemplate().findByExample(example));
+    }*/
+
+    @SuppressWarnings("unchecked")
+	public T getByGridId(T template) {
+    	return (T) CollectionUtils.firstElement(getHibernateTemplate().findByExample(template));
     }
 
-    public T getByGridId(T template) {
-        return getByGridId(template.getGridId());
+/*    
+  	//Fetches the object using example object if a grid id is specified..
+  	public T getByGridId(String gridId) {
+    	try {
+    		Object obj = domainClass().newInstance();
+    		PropertyUtils.setProperty(obj, "gridId", gridId);
+    		return getByGridId((T)obj);
+    	} catch (Exception e) {
+    		throw new CaaersSystemException(e.getMessage(), e);
+    	}
     }
+*/
+    public T getByGridId(String gridId) {
+        StringBuilder query = new StringBuilder("from ")
+        .append(domainClass().getName()).append(" o where gridId = ?");
+        Object[] params = {gridId};
+        return (T) CollectionUtils.firstElement(getHibernateTemplate().find(query.toString(), params));
+    }    
+    
 }
