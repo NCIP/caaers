f710cfc2fc552fe8373bfd221ab4fb0aba1076d5
Kulasekaran Sethumadhavan <kulasekaran@gmail.com>
2007-01-23 15:14:20 +0000

    SVN-Revision: 312

--- projects/web/src/main/webapp/js/CalendarPopup.js
+++ projects/web/src/main/webapp/js/CalendarPopup.js
@@ -1,29 +1,8 @@
-// ===================================================================
-// Author: Matt Kruse <matt@mattkruse.com>
-// WWW: http://www.mattkruse.com/
-//
-// NOTICE: You may use this code for any purpose, commercial or
-// private, without any further permission from the author. You may
-// remove this notice from your final code if you wish, however it is
-// appreciated by the author if at least my web site address is kept.
-//
-// You may *NOT* re-distribute this code in any way except through its
-// use. That means, you can include it in your product, or your web
-// site, or any other form where the code is actually being used. You
-// may not put the plain javascript up on your site for download or
-// include it in your javascript libraries for download. 
-// If you wish to share this code with others, please just point them
-// to the URL instead.
-// Please DO NOT link directly to my .js files from your site. Copy
-// the files to your server and use them there. Thank you.
-// ===================================================================
-
 
 /* SOURCE FILE: AnchorPosition.js */
 
 /* 
 AnchorPosition.js
-Author: Matt Kruse
 Last modified: 10/11/02
 
 DESCRIPTION: These functions find the position of an <A> tag in a document,
@@ -471,7 +450,6 @@ function parseDate(val) {
 
 /* 
 PopupWindow.js
-Author: Matt Kruse
 Last modified: 02/16/04
 
 DESCRIPTION: This object allows you to easily and quickly popup a window
