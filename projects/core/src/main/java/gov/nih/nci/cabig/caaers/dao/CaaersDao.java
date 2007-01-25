ac939e1484b1af58cee10008acb914c3f741a8a3
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-01-25 22:06:43 +0000

    javadoc for findBySubname
    SVN-Revision: 430

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/CaaersDao.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/CaaersDao.java
@@ -21,12 +21,29 @@ public abstract class CaaersDao<T extends DomainObject> extends HibernateDaoSupp
         return (T) getHibernateTemplate().get(domainClass(), id);
     }
 
+    /**
+     * A variation of {@link #findBySubname} that does not allow for extra conditions
+     */
     protected List<T> findBySubname(
         String[] subnames, List<String> substringMatchProperties, List<String> exactMatchProperties
     ) {
         return findBySubname(subnames, null, null, substringMatchProperties, exactMatchProperties);
     }
 
+    /**
+     * A query builder for use by subclass DAOs.  It makes it easy to match a fragment of a name
+     * or identifier against multiple properties.  This is intended for use in implementing
+     * user-friendly dynamic searches; e.g., autocompleters.
+     *
+     * @param subnames the name fragments to search on
+     * @param extraConditions custom HQL conditions with which to constrain the fragment matches
+     * @param extraParameters parameters for the custom conditions
+     * @param substringMatchProperties a list of properties of the implementing object which should
+     *          be matched as case-insensitive substrings
+     * @param exactMatchProperties a list of properties which should be matched as case-insensitive
+     *          full strings
+     * @return a list of matching domain object instances
+     */
     @SuppressWarnings("unchecked")
     protected List<T> findBySubname(
         String[] subnames, String extraConditions, List<Object> extraParameters,
@@ -74,8 +91,8 @@ public abstract class CaaersDao<T extends DomainObject> extends HibernateDaoSupp
         query.append(')');
     }
 
-    private boolean hasAny(List<String> substringMatchProperties) {
-        return substringMatchProperties != null && substringMatchProperties.size() > 0;
+    private boolean hasAny(List<String> properties) {
+        return properties != null && properties.size() > 0;
     }
 
     public abstract Class<T> domainClass();
