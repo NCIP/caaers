1bf910f487a9f13e27e24b825610f32c359b183a
Sujith Vellat <vtsujith@gmail.com>
2007-01-26 05:19:22 +0000

    SVN-Revision: 446

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/service/InteroperationServiceImpl.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/service/InteroperationServiceImpl.java
@@ -1,5 +1,9 @@
 package gov.nih.nci.cabig.caaers.service;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import com.semanticbits.aenotification.AENotification;
 
 import gov.nih.nci.cabig.caaers.CaaersSystemException;
@@ -15,6 +19,8 @@ public class InteroperationServiceImpl implements InteroperationService {
 
 	private MessageBroadcastService messageBroadcastService;
 	
+	private String certificateLocation = "/sample.txt";
+	
 	public void pushToStudyCalendar(AdverseEventReport aeReport) throws CaaersSystemException {
 		AENotification aeNotification = new AENotification();
 		aeNotification.setRegistrationGridId(
@@ -22,7 +28,21 @@ public class InteroperationServiceImpl implements InteroperationService {
 		aeNotification.setDetectionDate(
 				new java.sql.Date(aeReport.getPrimaryAdverseEvent().getDetectionDate().getTime()));
         aeNotification.setDescription(aeReport.getNotificationMessage());
+		//getMessageBroadcastService().broadcast(secure(XMLUtil.getXML(aeNotification)));
 		getMessageBroadcastService().broadcast(XMLUtil.getXML(aeNotification));
+	}
+	
+	private String secure(String message) {
+		StringBuffer secureMessage = new StringBuffer();
+		secureMessage.append("<message>");
+		secureMessage.append("<credentials>");
+		secureMessage.append("<proxy>").append(getCertificate()).append("</proxy>");
+		secureMessage.append("</credentials>");
+		secureMessage.append("<content>");
+		secureMessage.append(message);
+		secureMessage.append("</content>");
+		secureMessage.append("</message>");		
+		return secureMessage.toString();
 	}
 
 	public MessageBroadcastService getMessageBroadcastService() {
@@ -32,6 +52,37 @@ public class InteroperationServiceImpl implements InteroperationService {
 	public void setMessageBroadcastService(
 			MessageBroadcastService messageBroadcastService) {
 		this.messageBroadcastService = messageBroadcastService;
+	}
+	
+	private String getCertificate() {
+		StringBuffer certificate = new StringBuffer();
+		InputStream inputStream = InteroperationServiceImpl.class.getResourceAsStream(getCertificateLocation());
+		DataInputStream dataInputStream = new DataInputStream(inputStream);
+		String line = "";
+		try {
+			while((line= dataInputStream.readLine()) != null){
+				certificate.append(line);
+			}
+		} catch (IOException e) {
+			throw new CaaersSystemException(e.getMessage(), e);
+		} finally{
+			try {
+				inputStream.close();
+			} catch (IOException ioException) {
+				throw new CaaersSystemException(ioException.getMessage(), ioException);
+			}
+		}
+		
+		return certificate.toString();
+
+	}
+
+	public String getCertificateLocation() {
+		return certificateLocation;
+	}
+
+	public void setCertificateLocation(String certificateLocation) {
+		this.certificateLocation = certificateLocation;
 	}
 
 }
