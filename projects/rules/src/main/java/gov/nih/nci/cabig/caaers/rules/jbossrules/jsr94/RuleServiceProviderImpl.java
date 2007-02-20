6688574b3c11b2a48eed4e7f42c0a6db70530f71
Sujith Vellat <vtsujith@gmail.com>
2007-02-20 21:58:11 +0000

    SVN-Revision: 567

--- projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/jbossrules/jsr94/RuleServiceProviderImpl.java
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/jbossrules/jsr94/RuleServiceProviderImpl.java
@@ -97,7 +97,7 @@ public class RuleServiceProviderImpl extends RuleServiceProvider implements java
             this.ruleRuntime = new RuleRuntimeImpl( getRepository() );
         }
         
-        return this.ruleRuntime;       
+        return this.ruleRuntime;
     }
 
     /**
