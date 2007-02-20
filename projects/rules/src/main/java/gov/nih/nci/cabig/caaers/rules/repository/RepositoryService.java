31d1df2b95ece6be6a7e28fa0e28729dd0c5dcc9
Sujith Vellat <vtsujith@gmail.com>
2007-02-20 22:53:25 +0000

    SVN-Revision: 573

---
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/repository/RepositoryService.java
@@ -0,0 +1,70 @@
+package gov.nih.nci.cabig.caaers.rules.repository;
+
+import gov.nih.nci.cabig.caaers.rules.brxml.Category;
+import gov.nih.nci.cabig.caaers.rules.brxml.Rule;
+import gov.nih.nci.cabig.caaers.rules.brxml.RuleSet;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * 
+ * @author Sujith Vellat Thayyilthodi
+ * */
+public interface RepositoryService extends Remote {
+
+	/**
+     * This will create a new category at the specified path.
+     */
+    public Boolean createCategory(Category category) throws RemoteException;
+    
+    /**
+     * Creates a brand new rule with the initial category.
+     * Return the UUID of the item created.
+     * String ruleName, String description, String initialCategory, String initialPackage, String format
+     */
+    public String createRule(Rule rule) throws RemoteException;
+    
+    
+    /**
+     * Move the rule from one Ruleset/package to another
+     * @throws RemoteException 
+     *  
+     * */
+    public void moveRule(String newRuleSetName, String ruleId) throws RemoteException;
+    
+    /**
+     * 
+     * */
+    public String createRuleSet(RuleSet ruleSet) throws RemoteException;
+    
+    /**
+     * This returns a list of packages where rules may be added.
+     */
+    public RuleSet[] listRuleSets() throws RemoteException;
+
+    
+    /**
+     * Loads a package by its name (NOT UUID !).
+     * @param name The name of the package (NOT THE UUID !).
+     * @return Well, its pretty obvious if you think about it for a minute. Really.
+     */
+    public RuleSet getRuleSet(String name);
+    
+    /**
+     * This loads up all the stuff for a 
+     * rule asset based on the UUID (always latest and editable version).
+     * @param ruleId UUID
+     */
+    public Rule getRule(String ruleId) throws RemoteException;    
+    
+    /**
+     * This checks in a new version of an asset. 
+     * @return the UUID of the asset you are checking in, 
+     * null if there was some problem (and an exception was not thrown).
+     */
+    public String checkinVersion(Rule rule) throws RemoteException;
+
+
+
+}
