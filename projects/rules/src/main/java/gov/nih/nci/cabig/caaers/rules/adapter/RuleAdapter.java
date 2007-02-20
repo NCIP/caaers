4e01f471c50ded347651395e85fbe549f133507e
Sujith Vellat <vtsujith@gmail.com>
2007-02-20 22:37:46 +0000

    SVN-Revision: 570

--- projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/adapter/RuleAdapter.java
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/adapter/RuleAdapter.java
@@ -1,6 +1,6 @@
 package gov.nih.nci.cabig.caaers.rules.adapter;
 
-import gov.nih.nci.cabig.caaers.rules.v1_0.RuleSet;
+import gov.nih.nci.cabig.caaers.rules.brxml.RuleSet;
 
 public interface RuleAdapter {
 
