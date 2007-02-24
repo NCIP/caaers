4596a22463a3b099ed9eb4c0ab100a464a8e0dca
Sujith Vellat <vtsujith@gmail.com>
2007-02-24 05:43:48 +0000

    SVN-Revision: 606

--- projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/deploy/RuleDeploymentService.java
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/deploy/RuleDeploymentService.java
@@ -1,9 +1,49 @@
 package gov.nih.nci.cabig.caaers.rules.deploy;
 
+import gov.nih.nci.cabig.caaers.rules.deploy.sxml.RepositoryConfiguration;
+import gov.nih.nci.cabig.caaers.rules.deploy.sxml.RuleSetInfo;
+
+/**
+ * The Service Interface for deploying the Business Rules.
+ * 
+ * @author Sujith Vellat Thayyilthodi
+ * */
 public interface RuleDeploymentService {
 
+	/**
+	 * Yes! You need to login...but later..i let u go thru now since i am under busy schedule...!!!
+	 * 
+	 * @param userName
+	 * @param password
+	 * */
+	public abstract void login(String userName, String password);
+	
+	/**
+	 * Specify and Configure the repository which Rule Deployment Service should make use of.
+	 *  
+	 * @param repositoryConfiguration Configuration information for the Jackrabbit. 
+	 * */
+	public abstract void configureRepository(RepositoryConfiguration repositoryConfiguration);
+	
+	/**
+	 * Register the Rule Set. I mean deploy a drools package if you are a JBoss Rules fan...
+	 * 
+	 * @param bindUri
+	 * @param ruleSetName
+	 * */
 	public abstract void registerRuleSet(String bindUri, String ruleSetName);
 	
+	
+	/**
+	 * De-Register the Rule Set. I mean UN-deploy a drools package if you are a JBoss Rules fan...
+	 * 
+	 * @param bindUri
+	 * */
 	public abstract void deregisterRuleSet(String bindUri);
+	
+	/**
+	 * List the information of all RuleSets currently deployed in the Server.
+	 * */
+	public RuleSetInfo[] listRegistrations();
 
 }
--- projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/deploy/RuleDeploymentServiceImpl.java
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/deploy/RuleDeploymentServiceImpl.java
@@ -4,6 +4,8 @@ import gov.nih.nci.cabig.caaers.RuleException;
 import gov.nih.nci.cabig.caaers.rules.brxml.RuleSet;
 import gov.nih.nci.cabig.caaers.rules.common.RuleServiceContext;
 import gov.nih.nci.cabig.caaers.rules.common.adapter.RuleAdapter;
+import gov.nih.nci.cabig.caaers.rules.deploy.sxml.RepositoryConfiguration;
+import gov.nih.nci.cabig.caaers.rules.deploy.sxml.RuleSetInfo;
 import gov.nih.nci.cabig.caaers.rules.repository.RepositoryService;
 import gov.nih.nci.cabig.caaers.rules.repository.jbossrules.RepositoryServiceImpl;
 
@@ -18,15 +20,13 @@ import javax.rules.admin.RuleExecutionSetRegisterException;
 
 public class RuleDeploymentServiceImpl implements java.rmi.Remote, RuleDeploymentService {
 
-	
 	private RepositoryService repositoryService;
 	
-
 	public RuleDeploymentServiceImpl() {
 		super();
 		this.repositoryService = (RepositoryServiceImpl)RuleServiceContext.getInstance().applicationContext.getBean("jcrService");
 	}
-	
+	 
 	
 	/* (non-Javadoc)
 	 * @see gov.nih.nci.cabig.caaers.rules.runtime.RuleDeploymentService#registerPackage(java.lang.String, java.lang.String)
@@ -34,10 +34,17 @@ public class RuleDeploymentServiceImpl implements java.rmi.Remote, RuleDeploymen
 	public void registerRuleSet(String bindUri, String ruleSetName) {
 		final Map<String, Object> properties = new HashMap<String, Object>();
 		properties.put("source", "xml");
+		
+		//The repository configurations can be passed in AS  PROPERTIES
+		
 		RuleSet ruleSet = this.repositoryService.getRuleSet(ruleSetName);
 
 		try {
 			RuleAdapter ruleAdapter  = (RuleAdapter)Class.forName("gov.nih.nci.cabig.caaers.rules.adapter.JBossXSLTRuleAdapter").newInstance();
+			Object ruleSetObj = ruleAdapter.adapt(ruleSet);
+			RuleSetInfo ruleSetInfo = new RuleSetInfo();
+			ruleSetInfo.setContent(ruleSetObj);
+			this.repositoryService.registerRuleSet(ruleSetName, ruleSetInfo);
 			//Please note that we can only pass the Package here to the RuleExecution set.
 			//Since we still use drools implementation of LocalRuleExecutionSetProvider
 			final RuleExecutionSet ruleExecutionSet = RuleServiceContext.getInstance().ruleSetProvider
@@ -68,5 +75,25 @@ public class RuleDeploymentServiceImpl implements java.rmi.Remote, RuleDeploymen
 			throw new RuleException(e.getMessage(), e);
 		}
 	}
+
+	/**
+	 * Read this configuration and make sure we have a JCR Repository implementation to support that.
+	 * */
+	public void configureRepository(RepositoryConfiguration repositoryConfiguration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void login(String userName, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public RuleSetInfo[] listRegistrations() {
+		return this.repositoryService.listRegistrations();
+	}
+	
+
 
 }
