50ce0c1b552d1e567948c885c8b893394756be88
Kulasekaran Sethumadhavan <kulasekaran@gmail.com>
2007-03-01 21:12:53 +0000

    SVN-Revision: 686

---
+++ projects/web/src/test/java/gov/nih/nci/cabig/caaers/web/study/SearchStudyControllerTest.java
@@ -0,0 +1,38 @@
+package gov.nih.nci.cabig.caaers.web.study;
+
+import gov.nih.nci.cabig.caaers.service.StudyService;
+import gov.nih.nci.cabig.caaers.web.ListValues;
+import gov.nih.nci.cabig.caaers.web.SearchStudyController;
+import gov.nih.nci.cabig.caaers.web.WebTestCase;
+
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Kulasekaran
+ */
+public class SearchStudyControllerTest extends WebTestCase {
+	
+	private SearchStudyController controller = new SearchStudyController();
+    private StudyService studyService;	    
+    
+    protected void setUp() throws Exception {
+        super.setUp();
+        studyService = registerMockFor(StudyService.class);               
+        controller.setStudyService(studyService);
+        controller.setListValues(new ListValues());
+    }
+    
+    public void testViewOnGet() throws Exception {
+        request.setMethod("GET");        
+        ModelAndView mv = controller.handleRequest(request, response);
+        assertEquals("study_search", mv.getViewName());
+    }
+
+    public void testViewOnAddSearch() throws Exception {    	
+        request.setParameter("_selected", "-1");
+        controller.handleRequest(request, response);
+        ModelAndView mv = controller.handleRequest(request, response);
+        assertEquals("study_search", mv.getViewName());
+    }   
+   
+}
