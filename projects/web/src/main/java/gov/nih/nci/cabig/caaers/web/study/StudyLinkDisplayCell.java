ae14e9e98f2278cf9d56cc9c347019a42773d15d
Kulasekaran Sethumadhavan <kulasekaran@gmail.com>
2007-03-01 20:24:21 +0000

    SVN-Revision: 683

---
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/study/StudyLinkDisplayCell.java
@@ -0,0 +1,24 @@
+package gov.nih.nci.cabig.caaers.web.study;
+
+import gov.nih.nci.cabig.caaers.domain.Study;
+
+import org.extremecomponents.table.bean.Column;
+import org.extremecomponents.table.cell.AbstractCell;
+import org.extremecomponents.table.core.TableModel;
+
+public class StudyLinkDisplayCell extends AbstractCell {
+
+	private static final String LINK = "edit?studyId=";
+
+	protected String getCellValue(TableModel model, Column column) {
+		Study study = (Study) model.getCurrentRowBean();
+		String cellValue = column.getValueAsString();
+
+		if (study != null) {
+			cellValue = "<a href=\"" + LINK + study.getId().toString() + "\">"
+					+ cellValue + "</a>";
+		}
+
+		return cellValue;
+	}
+}
