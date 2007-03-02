f3ec159d5527b237335258a29aa3cb50c923b6c5
Joshua Phillips <joshua.phillips@semanticbits.com>
2007-03-02 11:07:11 +0000

    Added
    SVN-Revision: 699

---
+++ projects/acegi/acegi-csm/toinstall/install.bat
@@ -0,0 +1,2 @@
+mvn install:install-file -DgroupId=gov.nih.nci.security -DartifactId=csm -Dversion=pre3.2 -Dpackaging=jar -Dfile=csm/csmapi-pre3.2.jar
+mvn install:install-file -DgroupId=gov.nih.nci.security -DartifactId=clm -Dversion=pre3.2 -Dpackaging=jar -Dfile=csm/clm-pre3.2.jar
