2098ec3d839f4f1a0bab40a9388b748f3f283aca
Joshua Phillips <joshua.phillips@semanticbits.com>
2007-03-02 11:01:02 +0000

    Updated tests
    SVN-Revision: 696

--- projects/acegi/acegi-acl-dao/src/test/java/gov/nih/nci/security/acegi/acls/test/ACLTest.java
+++ projects/acegi/acegi-acl-dao/src/test/java/gov/nih/nci/security/acegi/acls/test/ACLTest.java
@@ -21,215 +21,228 @@ import org.acegisecurity.acls.sid.PrincipalSid;
 import org.acegisecurity.acls.sid.Sid;
 import org.acegisecurity.context.SecurityContextHolder;
 import org.acegisecurity.providers.TestingAuthenticationToken;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.orm.hibernate3.LocalSessionFactoryBean;
+import org.springframework.orm.hibernate3.SessionFactoryUtils;
 import org.springframework.test.AbstractTransactionalDataSourceSpringContextTests;
 
-//public class ACLTest extends AbstractTransactionalDataSourceSpringContextTests {
-public class ACLTest extends TestCase{
+public class ACLTest extends AbstractTransactionalDataSourceSpringContextTests {
 
+    
+    
+    
+    public ACLTest() {
+        init();
+    }
 
-	public ACLTest() {
-	}
+    public ACLTest(String name) {
+        super(name);
+        init();
+    }
 
-	public ACLTest(String name) {
-		super(name);
-	}
+    public void init() {
+        
+        
+        ApplicationContext appCtx = new ClassPathXmlApplicationContext(getConfigLocations());
+        HibernateTransactionManager txMgr = (HibernateTransactionManager) appCtx
+                        .getBean("transactionManager");
 
-	protected String[] getConfigLocations() {
+        Session session = SessionFactoryUtils.getSession(txMgr.getSessionFactory(), true);
 
-		return new String[] { "classpath:/applicationContext-acls-dao-test.xml" };
-	}
+        if (session != null) {
+            session.clear();
+        }
 
+        LocalSessionFactoryBean sessionFactoryBean = (LocalSessionFactoryBean) appCtx
+                        .getBean("&sessionFactory");
 
-	public static void main(String[] args) {
-		junit.textui.TestRunner.run(suite());
-	}
+        sessionFactoryBean.createDatabaseSchema();
+        
+    }
+    
 
-	public static Test suite() {
-		TestSuite suite = new TestSuite();
-		suite.addTest(new ACLTest("testLifecycle"));
-		return suite;
-	}
+    protected String[] getConfigLocations() {
+        return new String[] { "classpath:/applicationContext-acls-dao-test.xml" };
+    }
 
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
 
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        suite.addTest(new ACLTest("testLifecycle"));
+        return suite;
+    }
 
-	public void testLifecycle() {
-//		setComplete();
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
-				new String[] {
-						"classpath:/applicationContext-acls-dao-test.xml"});
-		MutableAclService aclService = (MutableAclService) applicationContext
-				.getBean("aclService");
+    public void testLifecycle() {
+        
+        
 
-		Authentication auth = new TestingAuthenticationToken("joshua",
-				"ignored", new GrantedAuthority[] { new GrantedAuthorityImpl(
-						"ROLE_ADMINISTRATOR") });
-		auth.setAuthenticated(true);
-		SecurityContextHolder.getContext().setAuthentication(auth);
+        MutableAclService aclService = (MutableAclService) applicationContext
+                        .getBean("mutableAclDao");
 
-		ObjectIdentity topParentOid = new ObjectIdentityImpl("gov.nih.nci.security.acegi.acls.test.SomeBean",
-				new Long(100));
-		ObjectIdentity middleParentOid = new ObjectIdentityImpl(
-				"gov.nih.nci.security.acegi.acls.test.SomeBean", new Long(101));
-		ObjectIdentity childOid = new ObjectIdentityImpl("gov.nih.nci.security.acegi.acls.test.SomeBean",
-				new Long(102));
+        Authentication auth = new TestingAuthenticationToken("joshua", "ignored",
+                        new GrantedAuthority[] { new GrantedAuthorityImpl("ROLE_ADMINISTRATOR") });
+        auth.setAuthenticated(true);
+        SecurityContextHolder.getContext().setAuthentication(auth);
 
-		assertNotNull("No ACLService", aclService);
-		MutableAcl topParent = aclService.createAcl(topParentOid);
-		MutableAcl middleParent = aclService.createAcl(middleParentOid);
-		MutableAcl child = aclService.createAcl(childOid);
+        ObjectIdentity topParentOid = new ObjectIdentityImpl(
+                        "gov.nih.nci.security.acegi.acls.test.SomeBean", new Long(100));
+        ObjectIdentity middleParentOid = new ObjectIdentityImpl(
+                        "gov.nih.nci.security.acegi.acls.test.SomeBean", new Long(101));
+        ObjectIdentity childOid = new ObjectIdentityImpl(
+                        "gov.nih.nci.security.acegi.acls.test.SomeBean", new Long(102));
 
-		// Specify the inheritence hierarchy
-		middleParent.setParent(topParent);
-		child.setParent(middleParent);
+        assertNotNull("No ACLService", aclService);
+        MutableAcl topParent = aclService.createAcl(topParentOid);
+        MutableAcl middleParent = aclService.createAcl(middleParentOid);
+        MutableAcl child = aclService.createAcl(childOid);
 
-		// Now let's add a couple of permissions
-		topParent.insertAce(null, BasePermission.READ, new PrincipalSid(auth),
-				true);
-		topParent.insertAce(null, BasePermission.WRITE, new PrincipalSid(auth),
-				false);
-		middleParent.insertAce(null, BasePermission.DELETE, new PrincipalSid(
-				auth), true);
-		child.insertAce(null, BasePermission.DELETE, new PrincipalSid(auth),
-				false);
+        // Specify the inheritence hierarchy
+        middleParent.setParent(topParent);
+        child.setParent(middleParent);
 
-		// Explictly save the changed ACL
-		aclService.updateAcl(topParent);
-		aclService.updateAcl(middleParent);
-		aclService.updateAcl(child);
+        // Now let's add a couple of permissions
+        topParent.insertAce(null, BasePermission.READ, new PrincipalSid(auth), true);
+        topParent.insertAce(null, BasePermission.WRITE, new PrincipalSid(auth), false);
+        middleParent.insertAce(null, BasePermission.DELETE, new PrincipalSid(auth), true);
+        child.insertAce(null, BasePermission.DELETE, new PrincipalSid(auth), false);
 
-		// Let's check if we can read them back correctly
-		Map map = aclService.readAclsById(new ObjectIdentity[] { topParentOid,
-				middleParentOid, childOid });
-		assertEquals(3, map.size());
+        // Explictly save the changed ACL
+        aclService.updateAcl(topParent);
+        aclService.updateAcl(middleParent);
+        aclService.updateAcl(child);
 
-		// Replace our current objects with their retrieved versions
-		topParent = (MutableAcl) map.get(topParentOid);
-		middleParent = (MutableAcl) map.get(middleParentOid);
-		child = (MutableAcl) map.get(childOid);
+        // Let's check if we can read them back correctly
+        Map map = aclService.readAclsById(new ObjectIdentity[] { topParentOid, middleParentOid,
+                childOid });
+        assertEquals(3, map.size());
 
-		// Check the retrieved versions has IDs
-		assertNotNull(topParent.getId());
-		assertNotNull(middleParent.getId());
-		assertNotNull(child.getId());
+        // Replace our current objects with their retrieved versions
+        topParent = (MutableAcl) map.get(topParentOid);
+        middleParent = (MutableAcl) map.get(middleParentOid);
+        child = (MutableAcl) map.get(childOid);
 
-		// Check their parents were correctly persisted
-		assertNull(topParent.getParentAcl());
-		assertEquals(topParentOid, middleParent.getParentAcl()
-				.getObjectIdentity());
-		assertEquals(middleParentOid, child.getParentAcl().getObjectIdentity());
+        // Check the retrieved versions has IDs
+        assertNotNull(topParent.getId());
+        assertNotNull(middleParent.getId());
+        assertNotNull(child.getId());
 
-		// Check their ACEs were correctly persisted
-		assertEquals(2, topParent.getEntries().length);
-		assertEquals(1, middleParent.getEntries().length);
-		assertEquals(1, child.getEntries().length);
+        // Check their parents were correctly persisted
+        assertNull(topParent.getParentAcl());
+        assertEquals(topParentOid, middleParent.getParentAcl().getObjectIdentity());
+        assertEquals(middleParentOid, child.getParentAcl().getObjectIdentity());
 
-		// Check the retrieved rights are correct
-		assertTrue(topParent.isGranted(
-				new Permission[] { BasePermission.READ },
-				new Sid[] { new PrincipalSid(auth) }, false));
-		assertFalse(topParent.isGranted(
-				new Permission[] { BasePermission.WRITE },
-				new Sid[] { new PrincipalSid(auth) }, false));
-		assertTrue(middleParent.isGranted(
-				new Permission[] { BasePermission.DELETE },
-				new Sid[] { new PrincipalSid(auth) }, false));
-		assertFalse(child.isGranted(new Permission[] { BasePermission.DELETE },
-				new Sid[] { new PrincipalSid(auth) }, false));
+        // Check their ACEs were correctly persisted
+        assertEquals(2, topParent.getEntries().length);
+        assertEquals(1, middleParent.getEntries().length);
+        assertEquals(1, child.getEntries().length);
 
-		try {
-			child.isGranted(new Permission[] { BasePermission.ADMINISTRATION },
-					new Sid[] { new PrincipalSid(auth) }, false);
-			fail("Should have thrown NotFoundException");
-		} catch (NotFoundException expected) {
-			assertTrue(true);
-		}
+        // Check the retrieved rights are correct
+        assertTrue(topParent.isGranted(new Permission[] { BasePermission.READ },
+                        new Sid[] { new PrincipalSid(auth) }, false));
+        assertFalse(topParent.isGranted(new Permission[] { BasePermission.WRITE },
+                        new Sid[] { new PrincipalSid(auth) }, false));
+        assertTrue(middleParent.isGranted(new Permission[] { BasePermission.DELETE },
+                        new Sid[] { new PrincipalSid(auth) }, false));
+        assertFalse(child.isGranted(new Permission[] { BasePermission.DELETE },
+                        new Sid[] { new PrincipalSid(auth) }, false));
 
-		// Now check the inherited rights (when not explicitly overridden) also
-		// look OK
-		assertTrue(child.isGranted(new Permission[] { BasePermission.READ },
-				new Sid[] { new PrincipalSid(auth) }, false));
-		assertFalse(child.isGranted(new Permission[] { BasePermission.WRITE },
-				new Sid[] { new PrincipalSid(auth) }, false));
-		assertFalse(child.isGranted(new Permission[] { BasePermission.DELETE },
-				new Sid[] { new PrincipalSid(auth) }, false));
+        try {
+            child.isGranted(new Permission[] { BasePermission.ADMINISTRATION },
+                            new Sid[] { new PrincipalSid(auth) }, false);
+            fail("Should have thrown NotFoundException");
+        } catch (NotFoundException expected) {
+            assertTrue(true);
+        }
 
-		// Next change the child so it doesn't inherit permissions from above
-		child.setEntriesInheriting(false);
-		aclService.updateAcl(child);
+        // Now check the inherited rights (when not explicitly overridden) also
+        // look OK
+        assertTrue(child.isGranted(new Permission[] { BasePermission.READ },
+                        new Sid[] { new PrincipalSid(auth) }, false));
+        assertFalse(child.isGranted(new Permission[] { BasePermission.WRITE },
+                        new Sid[] { new PrincipalSid(auth) }, false));
+        assertFalse(child.isGranted(new Permission[] { BasePermission.DELETE },
+                        new Sid[] { new PrincipalSid(auth) }, false));
 
-		child = (MutableAcl) aclService.readAclById(childOid);
+        // Next change the child so it doesn't inherit permissions from above
+        child.setEntriesInheriting(false);
+        aclService.updateAcl(child);
 
-		assertFalse(child.isEntriesInheriting());
+        child = (MutableAcl) aclService.readAclById(childOid);
 
-		// Check the child permissions no longer inherit
-		assertFalse(child.isGranted(new Permission[] { BasePermission.DELETE },
-				new Sid[] { new PrincipalSid(auth) }, true));
+        assertFalse(child.isEntriesInheriting());
 
-		try {
-			child.isGranted(new Permission[] { BasePermission.READ },
-					new Sid[] { new PrincipalSid(auth) }, true);
-			fail("Should have thrown NotFoundException");
-		} catch (NotFoundException expected) {
-			assertTrue(true);
-		}
+        // Check the child permissions no longer inherit
+        assertFalse(child.isGranted(new Permission[] { BasePermission.DELETE },
+                        new Sid[] { new PrincipalSid(auth) }, true));
 
-		try {
-			child.isGranted(new Permission[] { BasePermission.WRITE },
-					new Sid[] { new PrincipalSid(auth) }, true);
-			fail("Should have thrown NotFoundException");
-		} catch (NotFoundException expected) {
-			assertTrue(true);
-		}
+        try {
+            child.isGranted(new Permission[] { BasePermission.READ }, new Sid[] { new PrincipalSid(
+                            auth) }, true);
+            fail("Should have thrown NotFoundException");
+        } catch (NotFoundException expected) {
+            assertTrue(true);
+        }
 
-		// Let's add an identical permission to the child, but it'll appear
-		// AFTER the current permission, so has no impact
-		child.insertAce(null, BasePermission.DELETE, new PrincipalSid(auth),
-				true);
+        try {
+            child.isGranted(new Permission[] { BasePermission.WRITE },
+                            new Sid[] { new PrincipalSid(auth) }, true);
+            fail("Should have thrown NotFoundException");
+        } catch (NotFoundException expected) {
+            assertTrue(true);
+        }
 
-		// Let's also add another permission to the child
-		child.insertAce(null, BasePermission.CREATE, new PrincipalSid(auth),
-				true);
+        // Let's add an identical permission to the child, but it'll appear
+        // AFTER the current permission, so has no impact
+        child.insertAce(null, BasePermission.DELETE, new PrincipalSid(auth), true);
 
-		// Save the changed child
-		aclService.updateAcl(child);
-		child = (MutableAcl) aclService.readAclById(childOid);
-		assertEquals(3, child.getEntries().length);
+        // Let's also add another permission to the child
+        child.insertAce(null, BasePermission.CREATE, new PrincipalSid(auth), true);
 
-		// Output permissions
-		for (int i = 0; i < child.getEntries().length; i++) {
-			System.out.println(child.getEntries()[i]);
-		}
+        // Save the changed child
+        aclService.updateAcl(child);
+        child = (MutableAcl) aclService.readAclById(childOid);
+        assertEquals(3, child.getEntries().length);
 
-		// Check the permissions are as they should be
-		assertFalse(child.isGranted(new Permission[] { BasePermission.DELETE },
-				new Sid[] { new PrincipalSid(auth) }, true)); // as earlier
-		// permission
-		// overrode
-		assertTrue(child.isGranted(new Permission[] { BasePermission.CREATE },
-				new Sid[] { new PrincipalSid(auth) }, true));
+        // Output permissions
+        for (int i = 0; i < child.getEntries().length; i++) {
+            System.out.println(child.getEntries()[i]);
+        }
 
-		// Now check the first ACE (index 0) really is DELETE for our Sid and is
-		// non-granting
-		AccessControlEntry entry = child.getEntries()[0];
-		assertEquals(BasePermission.DELETE.getMask(), entry.getPermission()
-				.getMask());
-		assertEquals(new PrincipalSid(auth), entry.getSid());
-		assertFalse(entry.isGranting());
-		assertNotNull(entry.getId());
+        // Check the permissions are as they should be
+        assertFalse(child.isGranted(new Permission[] { BasePermission.DELETE },
+                        new Sid[] { new PrincipalSid(auth) }, true)); // as earlier
+        // permission
+        // overrode
+        assertTrue(child.isGranted(new Permission[] { BasePermission.CREATE },
+                        new Sid[] { new PrincipalSid(auth) }, true));
 
-		// Now delete that first ACE
-		child.deleteAce(entry.getId());
+        // Now check the first ACE (index 0) really is DELETE for our Sid and is
+        // non-granting
+        AccessControlEntry entry = child.getEntries()[0];
+        assertEquals(BasePermission.DELETE.getMask(), entry.getPermission().getMask());
+        assertEquals(new PrincipalSid(auth), entry.getSid());
+        assertFalse(entry.isGranting());
+        assertNotNull(entry.getId());
 
-		// Save and check it worked
-		child = aclService.updateAcl(child);
-		assertEquals(2, child.getEntries().length);
-		assertTrue(child.isGranted(new Permission[] { BasePermission.DELETE },
-				new Sid[] { new PrincipalSid(auth) }, false));
+        // Now delete that first ACE
+        child.deleteAce(entry.getId());
 
-		SecurityContextHolder.clearContext();
+        // Save and check it worked
+        child = aclService.updateAcl(child);
+        assertEquals(2, child.getEntries().length);
+        assertTrue(child.isGranted(new Permission[] { BasePermission.DELETE },
+                        new Sid[] { new PrincipalSid(auth) }, false));
 
-	}
+        SecurityContextHolder.clearContext();
+
+    }
 
 }
