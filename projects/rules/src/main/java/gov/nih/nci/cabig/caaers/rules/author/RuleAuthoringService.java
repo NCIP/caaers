66f564be7581807ae64f9ef88bfc301826dddf30
Sujith Vellat <vtsujith@gmail.com>
2007-03-13 03:38:35 +0000

    SVN-Revision: 849

--- projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/author/RuleAuthoringService.java
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/author/RuleAuthoringService.java
@@ -5,12 +5,17 @@ import gov.nih.nci.cabig.caaers.rules.brxml.RuleSet;
 
 import java.rmi.RemoteException;
 
+import javax.jws.WebService;
+
 /**
  * Remote interface for Rule Provisioning. 
  * This interface can be exposed as a webservice.
  * 
  * @author Sujith Vellat Thayyilthodi
  * */
+@WebService(
+        serviceName = "RuleAuthoringService", endpointInterface = "gov.nih.nci.cabig.caaers.rules.author.RuleAuthoringService"
+)
 public interface RuleAuthoringService extends java.rmi.Remote {
 
 	public void createRuleSet(RuleSet ruleSet) throws RemoteException;
