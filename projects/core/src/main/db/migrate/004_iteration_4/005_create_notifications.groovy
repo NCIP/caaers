8e0efb121b4ba3d82880cbb1639a7286b6f22414
Sujith Vellat <vtsujith@gmail.com>
2007-03-27 13:32:38 +0000

    text chaged to string
    SVN-Revision: 1088

--- projects/core/src/main/db/migrate/004_iteration_4/005_create_notifications.groovy
+++ projects/core/src/main/db/migrate/004_iteration_4/005_create_notifications.groovy
@@ -1,10 +1,10 @@
 class CreateStudyParticipantAssignments extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
         createTable('notifications') { t ->
-            t.addColumn('content', 'text', nullable:true)
-            t.addColumn('email', 'text', nullable:true)
-            t.addColumn('subject', 'text', nullable:true)
-            t.addColumn('name', 'text', nullable:true)
+            t.addColumn('content', 'string', nullable:true)
+            t.addColumn('email', 'string', nullable:true)
+            t.addColumn('subject', 'string', nullable:true)
+            t.addColumn('name', 'string', nullable:true)
             t.addVersionColumn()
             t.addColumn('grid_id' , 'string' , nullable:true);
         }
