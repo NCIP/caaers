2fda82d89481a7c36bc5006c46a55dc49667206b
Sujith Vellat <vtsujith@gmail.com>
2007-03-28 14:33:30 +0000

    SVN-Revision: 1089

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/NotificationDao.java
@@ -0,0 +1,25 @@
+package gov.nih.nci.cabig.caaers.dao;
+
+import gov.nih.nci.cabig.caaers.domain.Notification;
+
+import java.util.List;
+
+/**
+ * @author Sujith Vellat Thayyilthodi
+ * */
+public class NotificationDao extends CaaersDao<Notification> {
+
+	@Override
+	public Class<Notification> domainClass() {
+		return Notification.class;
+	}
+
+    @SuppressWarnings("unchecked")
+    public List<Notification> getAll() {
+        return getHibernateTemplate().find("from Notification");
+    }
+	
+    public void save(Notification notification) {
+        getHibernateTemplate().saveOrUpdate(notification);
+    }    
+}
