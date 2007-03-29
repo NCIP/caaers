a4064a862cbda40fb8b32e6b016f296312671a88
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-03-29 20:00:15 +0000

    Need to qualify column names in order for data to be preserved
    SVN-Revision: 1117

--- projects/core/src/main/db/migrate/004_iteration_4/008_one_date_per_report.groovy
+++ projects/core/src/main/db/migrate/004_iteration_4/008_one_date_per_report.groovy
@@ -3,7 +3,7 @@ class OneDatePerReport extends edu.northwestern.bioinformatics.bering.Migration 
         addColumn("ae_reports", "detection_date", "date")
         execute("UPDATE ae_reports\n" +
                 "   SET detection_date=\n" +
-                "       (SELECT ae.detection_date FROM adverse_events ae WHERE ae.report_id=id)")
+                "       (SELECT ae.detection_date FROM adverse_events ae WHERE ae.report_id=ae_reports.id)")
         dropColumn("adverse_events", "detection_date")
         setNullable("ae_reports", "detection_date", false)
     }
@@ -12,7 +12,7 @@ class OneDatePerReport extends edu.northwestern.bioinformatics.bering.Migration 
         addColumn("adverse_events", "detection_date", "date")
         execute("UPDATE adverse_events\n" +
                 "   SET detection_date=\n" +
-                "       (SELECT aer.detection_date FROM ae_reports aer WHERE report_id=aer.id)")
+                "       (SELECT aer.detection_date FROM ae_reports aer WHERE adverse_events.report_id=aer.id)")
         dropColumn("ae_reports", "detection_date")
         setNullable("adverse_events", "detection_date", false)
     }
