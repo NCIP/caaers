062d32b6b596996c30b655327ef6532889c24253
Kulasekaran Sethumadhavan <kulasekaran@gmail.com>
2007-04-02 01:33:32 +0000

    SVN-Revision: 1178

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/StudyPersonnel.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/StudyPersonnel.java
@@ -1,5 +1,6 @@
 package gov.nih.nci.cabig.caaers.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -19,7 +20,9 @@ import org.hibernate.annotations.Parameter;
     }
 )
 public class StudyPersonnel extends AbstractDomainObject {
-		
+
+	private String roleCode;
+	private String statusCode;	
 	private ResearchStaff researchStaff;
 	private StudySite studySite;
 			
@@ -41,5 +44,23 @@ public class StudyPersonnel extends AbstractDomainObject {
 
 	public void setStudySite(StudySite studySite) {
 		this.studySite = studySite;
+	}
+
+	@Column(name = "role_code")
+	public String getRoleCode() {
+		return roleCode;
+	}
+
+	public void setRoleCode(String roleCode) {
+		this.roleCode = roleCode;
+	}
+
+	@Column(name = "status_code")
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
 	}
 }
