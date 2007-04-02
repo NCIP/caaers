01e6b9228b5fcee5b15075131e67e6186b77e0d9
Kulasekaran Sethumadhavan <kulasekaran@gmail.com>
2007-04-02 06:19:33 +0000

    SVN-Revision: 1183

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/ResearchStaffDao.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/ResearchStaffDao.java
@@ -1,13 +1,26 @@
 package gov.nih.nci.cabig.caaers.dao;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import gov.nih.nci.cabig.caaers.domain.ResearchStaff;
+import gov.nih.nci.cabig.caaers.domain.SiteInvestigator;
 
 /**
  * @author Kulasekaran
  * 
  */
 public class ResearchStaffDao extends GridIdentifiableDao<ResearchStaff> {
-    
+
+	private static final List<String> SUBSTRING_MATCH_PROPERTIES
+			= Arrays.asList("firstName", "lastName");
+	private static final List<String> EXACT_MATCH_PROPERTIES
+			= Collections.emptyList();
+	private static final List<Object> EXTRA_PARAMS
+			= Collections.emptyList();
+
+
     @Override
     public Class<ResearchStaff> domainClass() {
         return ResearchStaff.class;
@@ -16,4 +29,9 @@ public class ResearchStaffDao extends GridIdentifiableDao<ResearchStaff> {
     public void save(ResearchStaff researchStaff) {
         getHibernateTemplate().saveOrUpdate(researchStaff);                        
     }     
+    
+    public List<ResearchStaff> getBySubnames(String[] subnames) {
+        return findBySubname(subnames,null,EXTRA_PARAMS,
+            SUBSTRING_MATCH_PROPERTIES, EXACT_MATCH_PROPERTIES);
+	}
 }
