0be35c3fb7a903a0de187c3701ac0f5a9c476854
Kulasekaran Sethumadhavan <kulasekaran@gmail.com>
2007-04-19 18:12:16 +0000

    SVN-Revision: 1317

--- projects/core/src/main/db/migrate/005_iteration_5/001_create_reporters.groovy
+++ projects/core/src/main/db/migrate/005_iteration_5/001_create_reporters.groovy
@@ -4,7 +4,7 @@ class CreateReporters extends edu.northwestern.bioinformatics.bering.Migration {
             t.addColumn('first_name', 'string', nullable:false)
             t.addColumn('last_name', 'string', nullable:false)
             t.addColumn('middle_name', 'string', nullable:true)
-            
+            t.addColumn('maiden_name', 'string', nullable:true)
             t.addColumn('grid_id', 'string', nullable:true)
             t.addVersionColumn()
         }        
