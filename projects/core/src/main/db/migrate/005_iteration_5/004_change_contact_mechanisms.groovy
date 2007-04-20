6eaa494fdc5bfba238639de34e3724e9b57f581e
Kulasekaran Sethumadhavan <kulasekaran@gmail.com>
2007-04-20 20:05:12 +0000

    SVN-Revision: 1333

---
+++ projects/core/src/main/db/migrate/005_iteration_5/004_change_contact_mechanisms.groovy
@@ -0,0 +1,13 @@
+class ChangeContactMechanisms extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+    	addColumn('contact_mechanisms','reporter_id' , 'string' , nullable:true);
+    	addColumn('contact_mechanisms','physician_id' , 'string' , nullable:true); 
+    	removeColumn('contact_mechanisms','person_id');      
+    }
+
+    void down() {
+        removeColumn('contact_mechanisms','reporter_id');
+    	removeColumn('contact_mechanisms','physician_id');
+    	addColumn('contact_mechanisms','person_id' , 'string' , nullable:false); 
+    }
+}
