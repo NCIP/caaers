92fd505db88b968de0969049a579cd9eb1f53973
Kulasekaran Sethumadhavan <kulasekaran@gmail.com>
2007-04-22 22:52:22 +0000

    SVN-Revision: 1379

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/AnatomicSite.java
@@ -0,0 +1,40 @@
+package gov.nih.nci.cabig.caaers.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+/**
+ * @author Kulasekaran
+ * @version 1.0
+ */
+@Entity
+@Table (name="anatomic_sites")
+@GenericGenerator(name="id-generator", strategy = "native",
+    parameters = {
+        @Parameter(name="sequence", value="seq_anatomic_sites_id")
+    }
+)
+public class AnatomicSite extends AbstractIdentifiableDomainObject
+{			
+	private String name;
+	private String category;
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}	
+}
