5b44ae9033000ce683a0595b89c7e0f95fbd04cd
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-04-22 08:39:20 +0000

    Modify column creation in place instead of changing in later migration. This is because adding the not-null columns will fail on deployments where the ae_reports table already contains data.
    SVN-Revision: 1357

--- projects/core/src/main/db/migrate/005_iteration_5/005_change_ae_report.groovy
+++ projects/core/src/main/db/migrate/005_iteration_5/005_change_ae_report.groovy
@@ -1,11 +1,14 @@
 class ChangeAeReport extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-    	addColumn('ae_reports','reporter_id' , 'string' , nullable:false);
-    	addColumn('ae_reports','physician_id' , 'string' , nullable:false);     	
+        // these columns can't be created not-null -- the ae_reports table
+        // could already have rows in it.  Plus they shouldn't be not-null
+        // for business reasons.
+        addColumn('ae_reports', 'reporter_id', 'string');
+        addColumn('ae_reports', 'physician_id', 'string');
     }
 
     void down() {
         removeColumn('ae_reports','reporter_id');
-    	removeColumn('ae_reports','physician_id');    	
+        removeColumn('ae_reports','physician_id');
     }
 }
