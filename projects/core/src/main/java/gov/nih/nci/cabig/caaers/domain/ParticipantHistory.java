9e2cdbce9fd96615a3da2d8cee93c4530d61a39c
Kulasekaran Sethumadhavan <kulasekaran@gmail.com>
2007-04-22 00:30:43 +0000

    SVN-Revision: 1348

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/ParticipantHistory.java
@@ -0,0 +1,38 @@
+package gov.nih.nci.cabig.caaers.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+/**
+ * @author Kulasekaran
+ * @version 1.0
+ */
+@Entity
+@Table (name="participant_history")
+@GenericGenerator(name="id-generator", strategy = "native",
+    parameters = {
+        @Parameter(name="sequence", value="seq_participant_history_id")
+    }
+)
+public class ParticipantHistory extends AbstractIdentifiableDomainObject
+{			
+	private int height;
+    private int weight;
+
+    public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+}
+
