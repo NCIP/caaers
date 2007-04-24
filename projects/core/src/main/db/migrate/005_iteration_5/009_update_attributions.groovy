eb8edbb1c7d30a1a52cb81d1cb241f4da179c77f
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-04-24 18:14:10 +0000

    Add down method
    SVN-Revision: 1421

--- projects/core/src/main/db/migrate/005_iteration_5/009_update_attributions.groovy
+++ projects/core/src/main/db/migrate/005_iteration_5/009_update_attributions.groovy
@@ -9,5 +9,10 @@ class UpdateAttributions extends edu.northwestern.bioinformatics.bering.Migratio
     }
 
     void down() {
+        insert("ae_cause_types", [code: "SA", name: "study agent"], primaryKey: false)
+        execute("DELETE FROM ae_attributions WHERE cause_type='CA'")
+        execute("DELETE FROM ae_cause_types WHERE code='CA'")
+        execute("DELETE FROM ae_attributions WHERE cause_type='OC'")
+        execute("DELETE FROM ae_cause_types WHERE code='OC'")
     }
 }
