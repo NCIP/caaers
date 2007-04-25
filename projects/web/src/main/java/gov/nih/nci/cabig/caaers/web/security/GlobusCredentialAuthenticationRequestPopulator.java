07e9ecc817d19acf4452228bde53f478365d1cad
Joshua Phillips <joshua.phillips@semanticbits.com>
2007-04-25 10:55:05 +0000

    Toned down logging
    SVN-Revision: 1429

--- projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/security/GlobusCredentialAuthenticationRequestPopulator.java
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/security/GlobusCredentialAuthenticationRequestPopulator.java
@@ -35,24 +35,24 @@ public class GlobusCredentialAuthenticationRequestPopulator implements
 		
 		Authentication auth = null;
 	
-		logger.debug("Looking for grid proxy...");
+//		logger.debug("Looking for grid proxy...");
 		String gridProxyStr = Utils.findParameter(request, getGridProxyParameterName());
 		if(gridProxyStr == null){
-			logger.debug("...didn't find it.");
+//			logger.debug("...didn't find it.");
 			throw new BadCredentialsException("No grid proxy found in request.");
 		}else{
-			logger.debug("...found it.");
+//			logger.debug("...found it.");
 			try{
-				logger.debug("Creating GlobusCredential from gridProxy...");
+//				logger.debug("Creating GlobusCredential from gridProxy...");
 				GlobusCredential cred = new GlobusCredential(new ByteArrayInputStream(gridProxyStr.getBytes()));
 				auth = new GlobusCredentialAuthenticationToken(cred);
 			}catch(Exception ex){
-				logger.debug("...error encountered: " + ex.getMessage());
+//				logger.debug("...error encountered: " + ex.getMessage());
 				throw new BadCredentialsException("Error parsing grid proxy: " + ex.getMessage(), ex);
 			}
-			logger.debug("...GlobusCredential created.");
+//			logger.debug("...GlobusCredential created.");
 		}
-		logger.debug("Returning GlobusCredentialAuthenticationToken");
+//		logger.debug("Returning GlobusCredentialAuthenticationToken");
 		return auth;
 	}
 
