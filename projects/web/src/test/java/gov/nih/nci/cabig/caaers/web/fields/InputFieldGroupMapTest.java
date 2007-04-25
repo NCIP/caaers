e5abd6337c4273ea97aaac31ee2b7d3ac55c14f4
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-04-25 18:13:59 +0000

    Move a few tests that got left behind in the ae package
    SVN-Revision: 1447

---
+++ projects/web/src/test/java/gov/nih/nci/cabig/caaers/web/fields/InputFieldGroupMapTest.java
@@ -0,0 +1,36 @@
+package gov.nih.nci.cabig.caaers.web.fields;
+
+import gov.nih.nci.cabig.caaers.CaaersTestCase;
+import gov.nih.nci.cabig.caaers.web.fields.RepeatingFieldGroupFactory;
+import gov.nih.nci.cabig.caaers.web.fields.DefaultInputFieldGroup;
+import gov.nih.nci.cabig.caaers.web.fields.InputFieldGroupMap;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class InputFieldGroupMapTest extends CaaersTestCase {
+    @SuppressWarnings({ "MismatchedQueryAndUpdateOfCollection" })
+    private InputFieldGroupMap map = new InputFieldGroupMap();
+
+    public void testAddRepeatingGroupFactory() throws Exception {
+        map.addRepeatingFieldGroupFactory(new RepeatingFieldGroupFactory("foo", "dc"));
+        assertEquals(0, map.size());
+        assertNotNull(map.get("foo4"));
+        assertEquals(1, map.size());
+    }
+
+    public void testAddRepeatingGroupFactoryWithCount() throws Exception {
+        map.addRepeatingFieldGroupFactory(new RepeatingFieldGroupFactory("foo", "dc"), 3);
+        assertEquals(3, map.size());
+        assertNotNull(map.get("foo1"));
+        assertEquals(3, map.size());
+        assertNotNull(map.get("foo12"));
+        assertEquals(4, map.size());
+    }
+    
+    public void testAddInputFieldGroup() throws Exception {
+        map.addInputFieldGroup(new DefaultInputFieldGroup("bar"));
+        assertEquals(1, map.size());
+        assertNotNull(map.get("bar"));
+    }
+}
