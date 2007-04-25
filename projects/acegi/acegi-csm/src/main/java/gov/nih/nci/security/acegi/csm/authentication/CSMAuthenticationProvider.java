d4c7dda2c114488bff95f8da0d6f4e1564b8316e
Joshua Phillips <joshua.phillips@semanticbits.com>
2007-04-25 10:50:12 +0000

    Added some logging
    SVN-Revision: 1425

--- projects/acegi/acegi-csm/src/main/java/gov/nih/nci/security/acegi/csm/authentication/CSMAuthenticationProvider.java
+++ projects/acegi/acegi-csm/src/main/java/gov/nih/nci/security/acegi/csm/authentication/CSMAuthenticationProvider.java
@@ -1,29 +1,24 @@
 package gov.nih.nci.security.acegi.csm.authentication;
 
 import gov.nih.nci.security.AuthenticationManager;
-import gov.nih.nci.security.UserProvisioningManager;
-import gov.nih.nci.security.authorization.domainobjects.Group;
 import gov.nih.nci.security.exceptions.CSInsufficientAttributesException;
 import gov.nih.nci.security.exceptions.CSLoginException;
-import gov.nih.nci.security.exceptions.CSObjectNotFoundException;
-
-import java.util.Iterator;
-import java.util.Set;
 
 import org.acegisecurity.AuthenticationException;
 import org.acegisecurity.AuthenticationServiceException;
 import org.acegisecurity.BadCredentialsException;
-import org.acegisecurity.GrantedAuthority;
-import org.acegisecurity.GrantedAuthorityImpl;
 import org.acegisecurity.InsufficientAuthenticationException;
 import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
 import org.acegisecurity.providers.dao.AbstractUserDetailsAuthenticationProvider;
-import org.acegisecurity.userdetails.User;
 import org.acegisecurity.userdetails.UserDetails;
 import org.acegisecurity.userdetails.UserDetailsService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class CSMAuthenticationProvider extends
 		AbstractUserDetailsAuthenticationProvider {
+	
+	private static Log logger = LogFactory.getLog(CSMAuthenticationProvider.class);
 	
 	private AuthenticationManager csmAuthenticationManager;
 	private UserDetailsService userDetailsService;
@@ -40,16 +35,20 @@ public class CSMAuthenticationProvider extends
 	protected UserDetails retrieveUser(String userName,
 			UsernamePasswordAuthenticationToken token)
 			throws AuthenticationException {
-
+		logger.debug("Authenticating " + userName + "...");
 		try{
 			getCsmAuthenticationManager().authenticate(userName, (String)token.getCredentials());
 		}catch(CSInsufficientAttributesException ex){
+			logger.debug("authentication failed: insufficient attributes", ex);
 			throw new InsufficientAuthenticationException(ex.getMessage(), ex);
 		}catch(CSLoginException ex){
+			logger.debug("authentication failed: login exception", ex);
 			throw new BadCredentialsException(ex.getMessage(), ex);
 		}catch(Exception ex){
+			logger.debug("authentication failed: unknown", ex);
 			throw new AuthenticationServiceException("Error authenticating: " + ex.getMessage(), ex);
 		}
+		logger.debug("authentication succeeded");
 
 		return getUserDetailsService().loadUserByUsername(userName);
 	}
