54ab22e55057301d5cacbf3793ea5edb5d376bef
Priyatam Mudivarti <priyatam@gmail.com>
2007-05-18 18:48:17 +0000

    cleanup
    SVN-Revision: 1624

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/utils/ConfigProperty.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/utils/ConfigProperty.java
@@ -9,22 +9,12 @@ public class ConfigProperty {
 	// Temporary lov values to hold the data pertaining to CADsr
 	// TODO remove the lovMap once a CaDsr service is in place
 	private Map map  = new HashMap<String, List<Lov>>();
-	
-	// set the schema to be connected, although Oracle's username connects to corresponding
-	// schema, this property can ensure that your database connection will only be for one schema
-	// Currently used by Unit testing framework only
-	private String schema;
-	
+		
 	public Map getMap() {
 		return map;
 	}
 	public void setMap(Map map) {
 		this.map = map;
 	}
-	public String getSchema() {
-		return schema;
-	}
-	public void setSchema(String schema) {
-		this.schema = schema;
-	}
+
 }
