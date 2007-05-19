2b0c410a7fcd638d57fab1f6796db1aebc6129aa
Kumar Sambhuni <kumar.sambhuni@semanticbits.com>
2007-05-19 23:50:19 +0000

    SVN-Revision: 1657

---
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/runtime/BusinessRulesExecutionService.java
@@ -0,0 +1,8 @@
+package gov.nih.nci.cabig.caaers.rules.runtime;
+
+import java.util.List;
+
+public interface BusinessRulesExecutionService {
+ 
+	public List<Object> fireRules(String bindingURI, List<Object> objects);
+}
