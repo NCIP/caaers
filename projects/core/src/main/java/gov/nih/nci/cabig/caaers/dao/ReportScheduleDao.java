ae0b9d5d075fd91b8ba7febb2d3fa0224a836ab2
Biju Joseph <biju.joseph@semanticbits.com>
2007-05-23 21:25:57 +0000

    for Notification and Scheduler stuff
    SVN-Revision: 1747

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/ReportScheduleDao.java
@@ -0,0 +1,72 @@
+package gov.nih.nci.cabig.caaers.dao;
+
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.nwu.bioinformatics.commons.CollectionUtils;
+
+import gov.nih.nci.cabig.caaers.domain.notification.ReportSchedule;
+/**
+ * 
+ * 
+ * @author <a href="mailto:biju.joseph@semanticbits.com">Biju Joseph</a>
+ * Created-on : May 13, 2007
+ * @version     %I%, %G%
+ * @since       1.0
+ */
+@Transactional
+public class ReportScheduleDao extends GridIdentifiableDao<ReportSchedule>{
+
+	/* (non-Javadoc)
+	 * @see gov.nih.nci.cabig.caaers.dao.CaaersDao#domainClass()
+	 */
+	@Override
+	public Class<ReportSchedule> domainClass() {
+		return ReportSchedule.class;
+	}
+	
+	public void save(ReportSchedule rs){
+		getHibernateTemplate().saveOrUpdate(rs);
+	}
+	
+	public Session getHibernateSession(){
+		return super.getSession();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<ReportSchedule> getAll(){
+		return getHibernateTemplate().find("from ReportSchedule");
+	}
+	
+	@SuppressWarnings("unchecked")
+	public ReportSchedule getByName(String name){
+		return CollectionUtils.firstElement(
+				(List<ReportSchedule>) getHibernateTemplate().find(
+						"from ReportSchedule s where s.name=?", new String[]{name}
+						)
+				);
+	}
+	
+	public List<ReportSchedule> getAllByDueDate(Date dueDate){
+		return getByDate(2, dueDate);
+	}
+	
+	public List<ReportSchedule> getAllByCreatedDate(Date dueDate){
+		return getByDate(3, dueDate);
+	}
+	
+	public List<ReportSchedule> getAllBySubmittedDate(Date dueDate){
+		return getByDate(1, dueDate);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private List<ReportSchedule> getByDate(int dateType, Date d){
+		String column = (dateType == 1)? "submittedOn" : ((dateType == 2)? "dueOn" : "createdOn");
+		String hsql = "from ReportSchedule s where s." + column + "=?";
+		return getHibernateTemplate().find( hsql, new Object[]{d});
+	}
+}
