7eba1d559eee8f39a8de29007d9ca7408b1d8f0e
Biju Joseph <biju.joseph@semanticbits.com>
2007-05-25 20:49:44 +0000

    fix for NOT NULL issue.
    SVN-Revision: 1831

--- projects/core/src/main/db/migrate/006_construction_4/020_change_site_research_staffs.groovy
+++ projects/core/src/main/db/migrate/006_construction_4/020_change_site_research_staffs.groovy
@@ -1,6 +1,8 @@
 class ModifSiteResearchStaffs extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-   	  	addColumn('research_staffs','site_id',"integer", nullable:false);
+   	  	addColumn('research_staffs','site_id',"integer");
+   	  	execute("UPDATE research_staffs SET site_id=1 WHERE site_id IS NULL")  // site 1 is "default"
+   	  	setNullable('research_staffs', 'site_id', false)
      	execute("ALTER TABLE research_staffs ADD CONSTRAINT fk_res_stf_ste FOREIGN KEY (site_id) REFERENCES sites")
     }
     void down() {
