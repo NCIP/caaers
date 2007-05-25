190a24aa0b71cfef14e1de8a3c570ee091557bc4
Priyatam Mudivarti <priyatam@gmail.com>
2007-05-25 21:54:22 +0000

    removed unwanted attributes
    SVN-Revision: 1835

--- projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/study/DetailsTab.java
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/study/DetailsTab.java
@@ -1,10 +1,8 @@
 package gov.nih.nci.cabig.caaers.web.study;
 
-import gov.nih.nci.cabig.caaers.utils.Lov;
 import gov.nih.nci.cabig.caaers.domain.Study;
 
 import java.util.Map;
-import java.util.List;
 
 import org.springframework.validation.Errors;
 
@@ -19,16 +17,8 @@ class DetailsTab extends StudyTab {
     @Override
     public Map<String, Object> referenceData() {
         Map<String, Object> refdata = super.referenceData();
-        addConfigMapToRefdata(refdata, "diseaseCodeRefData");
-        addConfigMapToRefdata(refdata, "monitorCodeRefData");
         addConfigMapToRefdata(refdata, "phaseCodeRefData");
         addConfigMapToRefdata(refdata, "sponsorCodeRefData");
-        addConfigMapToRefdata(refdata, "statusRefData");
-        addConfigMapToRefdata(refdata, "typeRefData");
-        refdata.put("multiInstitutionIndicator", getConfigurationProperty().getMap().get("yesNo"));
-        refdata.put("randomizedIndicator",getConfigurationProperty().getMap().get("yesNo"));
-        refdata.put("blindedIndicator", getConfigurationProperty().getMap().get("yesNo"));
-        refdata.put("nciIdentifier", getConfigurationProperty().getMap().get("yesNo"));
         return refdata;
     }
 
