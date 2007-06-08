f07612903b1a3ae79079133755efed46613f8ebd
Kumar Sambhuni <kumar.sambhuni@semanticbits.com>
2007-06-08 15:55:18 +0000

    SVN-Revision: 1959

---
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/common/RuleType.java
@@ -0,0 +1,21 @@
+package gov.nih.nci.cabig.caaers.rules.common;
+
+public enum RuleType {
+	
+	AE_ASSESMENT_RULES("AE Assesment Rules","Rules regarding adverse event assesments"),
+	REPORT_SCHEDULING_RULES("Report Scheduling Rules","The rules regarding identifying the reporting periods");
+	private String name;
+	private String desc;
+	
+	RuleType(String name, String desc){
+		this.name=name;
+		this.desc=desc;
+	}
+	public String getName(){
+		return name;
+	}
+	public String getDescription(){
+		return desc;
+	}
+
+}
