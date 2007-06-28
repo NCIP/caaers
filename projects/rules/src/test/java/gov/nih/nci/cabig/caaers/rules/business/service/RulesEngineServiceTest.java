1cdb77a5369bdec82d85a1e54840d94901d17892
Srini Akkala <akkalas@gmail.com>
2007-06-28 20:18:07 +0000

    added tests from scheduling rules...
    SVN-Revision: 2131

--- projects/rules/src/test/java/gov/nih/nci/cabig/caaers/rules/business/service/RulesEngineServiceTest.java
+++ projects/rules/src/test/java/gov/nih/nci/cabig/caaers/rules/business/service/RulesEngineServiceTest.java
@@ -1,10 +1,13 @@
 	package gov.nih.nci.cabig.caaers.rules.business.service;
 
 import gov.nih.nci.cabig.caaers.domain.AdverseEvent;
+import gov.nih.nci.cabig.caaers.domain.ExpeditedAdverseEventReport;
 import gov.nih.nci.cabig.caaers.domain.Grade;
 import gov.nih.nci.cabig.caaers.domain.Hospitalization;
 import gov.nih.nci.cabig.caaers.domain.Site;
 import gov.nih.nci.cabig.caaers.domain.Study;
+import gov.nih.nci.cabig.caaers.domain.StudyParticipantAssignment;
+import gov.nih.nci.cabig.caaers.domain.StudySite;
 import gov.nih.nci.cabig.caaers.rules.brxml.Action;
 import gov.nih.nci.cabig.caaers.rules.brxml.Column;
 import gov.nih.nci.cabig.caaers.rules.brxml.Condition;
@@ -118,7 +121,157 @@ public class RulesEngineServiceTest extends TestCase {
 		createAdverseEvent3();
 		createAdverseEvent4();
 	}
+	
+	public void testInstitutionRuleFlowOnReportSchedulingRules() throws Exception {
+		RuleSet rs = this.createRulesForInstitute(3,RuleType.REPORT_SCHEDULING_RULES.getName());
+		RulesEngineService res = new RulesEngineServiceImpl();
+		//RulesEngineService res = new RulesEngineServiceImpl();
+		
+		res.saveRulesForInstitution(rs, "Wake Forest Comprehensive Cancer Center");
 
+		rs = res.getRuleSetForInstitution(rs.getDescription(),"Wake Forest Comprehensive Cancer Center");
+		
+		res.deployRuleSet(rs);
+		//createAdverseEvent3();
+		createAdverseEvent5();		
+		
+	}
+	
+	private void createAdverseEvent5() throws Exception {
+
+		// execute rules...
+		AdverseEvent ae1 = new AdverseEvent();
+		ae1.setGrade(Grade.MILD);
+
+		ae1.setHospitalization(Hospitalization.HOSPITALIZATION);
+
+		Site s = new Site();
+		s.setName("Wake Forest Comprehensive Cancer Center");
+		
+		ExpeditedAdverseEventReport exaer = new ExpeditedAdverseEventReport();
+		exaer.addAdverseEvent(ae1);
+		StudyParticipantAssignment spa = new StudyParticipantAssignment();
+		StudySite ss = new StudySite();
+		ss.setSite(s);
+		spa.setStudySite(ss);
+		
+		exaer.setAssignment(spa);
+		
+		//System.out.println(exaer.get);
+		
+
+		AdverseEventEvaluationServiceImpl aees = new AdverseEventEvaluationServiceImpl();
+		String msg = aees.evaluateSAEReportSchedule(exaer);
+
+		System.out.println(msg);
+		//assertEquals(msg, "SERIOUS_ADVERSE_EVENT");
+
+	}
+
+	private RuleSet createRulesForInstitute(int id,String ruleTypeDesc) {
+
+		RuleSet rs = new RuleSet();
+		rs.setDescription(ruleTypeDesc);
+
+		Rule r = makeRule(id,"24 Hour, 5 Calendar Days");
+		// inst based rules
+		// need to add inst name in the crieteria.
+		r.getCondition().getColumn().add(
+				this.createCriteriaForInstitute("Wake Forest Comprehensive Cancer Center"));
+
+		rs.getRule().add(r);
+		return rs;
+	
+	}
+
+	private Rule makeRule(int i,String actionStr) {
+		Rule rule1 = new Rule();
+		rule1.setMetaData(new MetaData());
+		rule1.getMetaData().setName("Rule" + i);
+		rule1.getMetaData().setDescription("Our test rule" + i);
+
+		Condition condition = new Condition();
+		// condition.getEval().add("adverseEvent.getGrade().getCode() <=
+		// Grade.MODERATE.getCode()");
+
+		Column column1 = new Column();
+		column1.setObjectType("AdverseEvent");
+		column1.setIdentifier("adverseEvent");
+		column1.setExpression("getGrade().getCode().intValue()");
+
+		FieldConstraint fieldConstraint1 = new FieldConstraint();
+		fieldConstraint1.setFieldName("grade");
+
+		LiteralRestriction literalRestriction1 = new LiteralRestriction();
+		literalRestriction1.setEvaluator("==");
+		List<String> values1 = new ArrayList<String>();
+
+		values1.add("1");
+
+		literalRestriction1.setValue(values1);
+
+		List<LiteralRestriction> lr1 = new ArrayList<LiteralRestriction>();
+		lr1.add(literalRestriction1);
+
+		fieldConstraint1.setLiteralRestriction(lr1);
+
+		ArrayList<FieldConstraint> fields1 = new ArrayList<FieldConstraint>();
+		fields1.add(fieldConstraint1);
+
+		column1.setFieldConstraint(fields1);
+
+		condition.getColumn().add(column1);
+
+		Column column2 = new Column();
+		column2.setObjectType("AdverseEvent");
+		column2.setIdentifier("adverseEvent");
+		column2.setExpression("getHospitalization().getCode().intValue()");
+
+		FieldConstraint fieldConstraint2 = new FieldConstraint();
+		fieldConstraint2.setFieldName("hospitalization");
+
+		LiteralRestriction literalRestriction2 = new LiteralRestriction();
+		literalRestriction2.setEvaluator("==");
+		List<String> values2 = new ArrayList<String>();
+
+		values2.add("1");
+		values2.add("2");
+
+		literalRestriction2.setValue(values2);
+
+		List<LiteralRestriction> lr2 = new ArrayList<LiteralRestriction>();
+		lr2.add(literalRestriction2);
+
+		fieldConstraint2.setLiteralRestriction(lr2);
+
+		ArrayList<FieldConstraint> fields2 = new ArrayList<FieldConstraint>();
+		fields2.add(fieldConstraint2);
+
+		column2.setFieldConstraint(fields2);
+
+		condition.getColumn().add(column2);
+
+		/**
+		 * Make it or break it
+		 */
+
+		// Column column_fixed = new Column();
+		// column_fixed.setObjectType("gov.nih.nci.cabig.caaers.rules.domain.AdverseEventEvaluationResult");
+		// column_fixed.setIdentifier("adverseEventEvaluationResult");
+		// condition.getColumn().add(column_fixed);
+		rule1.setCondition(condition);
+
+		// Notification action = new Notification();
+		// action.setActionId("ROUTINE_AE");
+
+		Action action = new Action();
+		action.setActionId(actionStr);
+
+		rule1.setAction(action);
+
+		return rule1;
+	}
+	
 	private void createAdverseEvent3() throws Exception {
 
 		// execute rules...
