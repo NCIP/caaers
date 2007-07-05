8542d92d7a7658e7c777839590dd0159e51cd7f4
Srini Akkala <akkalas@gmail.com>
2007-07-05 22:58:09 +0000

    sponsor defined and institution defined study level rules
    SVN-Revision: 2188

--- projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/common/CategoryConfiguration.java
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/common/CategoryConfiguration.java
@@ -12,9 +12,11 @@ public enum CategoryConfiguration {
 	CAAERS_BASE("CAAERS_BASE","CAAERS_BASE","gov.nih.nci.cabig.caaers.rules","The rule base for all caaers rules"),
 	SPONSOR_BASE("SPONSOR","/CAAERS_BASE/SPONSOR","gov.nih.nci.cabig.caaers.rules.sponsor",""),
 	INSTITUTION_BASE("INSTITUTION","/CAAERS_BASE/INSTITUTION","gov.nih.nci.cabig.caaers.rules.institution",""),
-	STUDY_BASE("STUDY","/CAAERS_BASE/STUDY","gov.nih.nci.cabig.caaers.rules.study","");
+	SPONSOR_DEFINED_STUDY_BASE("SPONSOR_DEFINED_STUDY","/CAAERS_BASE/SPONSOR_DEFINED_STUDY","gov.nih.nci.cabig.caaers.rules.sponsor.study",""),
+	INSTITUTION_DEFINED_STUDY_BASE("INSTITUTION_DEFINED_STUDY","/CAAERS_BASE/INSTITUTION_DEFINED_STUDY","gov.nih.nci.cabig.caaers.rules.institution.study","");
+	//STUDY_BASE("STUDY","/CAAERS_BASE/STUDY","gov.nih.nci.cabig.caaers.rules","");
 	
-	
+
 	private String name;
 	private String path;
 	private String packagePrefix;
