a566e47051d8d69f6f3f303d1e7cdff5d6fa02e0
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-07-11 23:44:16 +0000

    Correct wrong column name in delete (affected PostgreSQL and Oracle), plus a couple of oracle-specific problems
    SVN-Revision: 2297

--- projects/core/src/main/db/migrate/007_construction_5/019_refactor_disease_history.groovy
+++ projects/core/src/main/db/migrate/007_construction_5/019_refactor_disease_history.groovy
@@ -5,10 +5,12 @@ class RefactorDiseaseHistory extends edu.northwestern.bioinformatics.bering.Migr
         renameColumn('disease_histories', 'other_disease_site_code', 'other_primary_disease_site');
         renameColumn('disease_histories', 'anatomic_site_id', 'coded_primary_disease_site_id');
         // ensure that the new constraint will be applicable
-        execute("DELETE FROM disease_histories WHERE primary_disease_site_id IS NOT NULL AND primary_disease_site_id NOT IN (SELECT id FROM anatomic_sites)");
+        execute("DELETE FROM disease_histories WHERE coded_primary_disease_site_id IS NOT NULL AND coded_primary_disease_site_id NOT IN (SELECT id FROM anatomic_sites)");
         execute("ALTER TABLE disease_histories ADD CONSTRAINT fk_disease_primary FOREIGN KEY (coded_primary_disease_site_id) REFERENCES anatomic_sites");
 
-        renameTable('metastatic_disease_site', 'metastatic_disease_sites');
+        // pk: false here is a hack to prevent oracle from trying to rename the ID sequence
+        // -- for this table, the rename doesn't change the sequence name
+        renameTable('metastatic_disease_site', 'metastatic_disease_sites', primaryKey: false);
         renameColumn('metastatic_disease_sites', 'other_metastatic_disease_site', 'other_site');
         renameColumn('metastatic_disease_sites', 'anatomic_site_id', 'coded_site_id');
         // ensure that the new constraint will be applicable
@@ -20,12 +22,12 @@ class RefactorDiseaseHistory extends edu.northwestern.bioinformatics.bering.Migr
         execute("ALTER TABLE metastatic_disease_sites DROP CONSTRAINT fk_metastatic_anatomic");
         renameColumn('metastatic_disease_sites', 'other_site', 'other_metastatic_disease_site');
         renameColumn('metastatic_disease_sites', 'coded_site_id', 'anatomic_site_id');
-        renameTable('metastatic_disease_sites', 'metastatic_disease_site');
+        renameTable('metastatic_disease_sites', 'metastatic_disease_site', primaryKey: false);
 
-        execute("ALTER TABLE metastatic_disease_sites DROP CONSTRAINT fk_disease_primary");
+        execute("ALTER TABLE disease_histories DROP CONSTRAINT fk_disease_primary");
         renameColumn('disease_histories', 'other_primary_disease', 'other_disease_code');
         renameColumn('disease_histories', 'other_primary_disease_site', 'other_disease_site_code');
-        renameColumn('disease_histories', 'primary_disease_site_id', 'anatomic_site_id');
+        renameColumn('disease_histories', 'coded_primary_disease_site_id', 'anatomic_site_id');
         renameTable('disease_histories', 'disease_history');
     }
 }
