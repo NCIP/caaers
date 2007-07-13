e1254813d9bf8b66eac1a30ef47c88cf659c7680
Unknown <unknown@unknown.com>
2007-07-13 19:56:59 +0000

    Removed NetHelp folder, replaced by Help
    SVN-Revision: 2342

--- projects/web/src/main/webapp/NetHelp/_contextIds.js
+++
@@ -1,3 +0,0 @@
-
-var MAIN_HELP_PAGE = "default.htm";
-var g_d2hContextIds = new Array();
--- projects/web/src/main/webapp/NetHelp/urls.js
+++
@@ -1,4 +0,0 @@
-var g_ContentsURL = "caAERS-toc.htm";
-var g_IndexURL = "caAERS-index.htm";
-var g_SearchURL = "caAERS-search.htm";
-var g_FavoritesURL = "caAERS-favorites.htm";
