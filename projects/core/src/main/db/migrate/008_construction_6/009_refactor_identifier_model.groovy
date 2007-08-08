da31e56d116862bc30a417472dd49b79dc849b4e
Sanjeev Agarwal <iec_sanjeev@yahoo.com>
2007-08-08 16:42:50 +0000

    adding groovy scripts for data migration for identifiers.
    SVN-Revision: 2656

---
+++ projects/core/src/main/db/migrate/008_construction_6/009_refactor_identifier_model.groovy
@@ -0,0 +1,39 @@
+class RefactorIdentifierModel extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+    
+       		
+	    	setNullable('IDENTIFIERS', 'source', true)
+	    	setNullable('IDENTIFIERS', 'organization_id', true)
+	    	setNullable('IDENTIFIERS', 'system_name', true)
+	     	
+	     	
+	    	execute("update identifiers set organization_id = (select id from organizations where name like source)")
+	     	execute("update identifiers set discriminator_column = 1 where organization_id is not null")
+	     	execute("update identifiers set source ='' where organization_id is not null")
+	     	
+	     	dropColumn('IDENTIFIERS','system_name')
+	     	
+	     	renameColumn('IDENTIFIERS','source','system_name')
+	     	
+	     	
+	     			        	
+   	 	}
+
+    void down() {
+       
+    		renameColumn('IDENTIFIERS','system_name',"source")
+    		
+    		addColumn("identifiers","system_name", 'string')
+    		
+    		execute("update identifiers set source =(select name from organizations where  where id=organization_id")
+    		execute("update identifiers set discriminator_column = 2 where discriminator_column = 1")
+    		execute("update identifiers set organization_id =null where system_name=''")
+    		
+    		setNullable('IDENTIFIERS', 'system_name', false)		
+		setNullable('IDENTIFIERS', 'organization_id', false)	     	
+      		setNullable('IDENTIFIERS','source',false)
+      		
+
+	     	
+	    	}
+	    }
