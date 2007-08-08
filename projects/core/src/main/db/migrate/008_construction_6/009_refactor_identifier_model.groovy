0ec0aa2464e7979af2babf7b17ed5bfe336a9350
Biju Joseph <biju.joseph@semanticbits.com>
2007-08-08 18:56:39 +0000

    setNullable commented
    SVN-Revision: 2660

--- projects/core/src/main/db/migrate/008_construction_6/009_refactor_identifier_model.groovy
+++ projects/core/src/main/db/migrate/008_construction_6/009_refactor_identifier_model.groovy
@@ -2,38 +2,33 @@ class RefactorIdentifierModel extends edu.northwestern.bioinformatics.bering.Mig
     void up() {
     
        		
-	    	setNullable('IDENTIFIERS', 'source', true)
-	    	setNullable('IDENTIFIERS', 'organization_id', true)
-	    	setNullable('IDENTIFIERS', 'system_name', true)
+	    	//setNullable('identifiers', 'source', true);
+	    	//setNullable('IDENTIFIERS', 'organization_id', true);
+	    	//setNullable('IDENTIFIERS', 'system_name', true);
 	     	
 	     	
-	    	execute("update identifiers set organization_id = (select id from organizations where name like source)")
-	     	execute("update identifiers set discriminator_column = 1 where organization_id is not null")
-	     	execute("update identifiers set source ='' where organization_id is not null")
+	    	execute("update identifiers set organization_id = (select id from organizations where name like source)");
+	     	execute("update identifiers set discriminator_column = 1 where organization_id is not null");
+	     	execute("update identifiers set source ='' where organization_id is not null");
 	     	
-	     	dropColumn('IDENTIFIERS','system_name')
+	     	dropColumn('IDENTIFIERS','system_name');
 	     	
-	     	renameColumn('IDENTIFIERS','source','system_name')
-	     	
-	     	
-	     			        	
+	     	renameColumn('IDENTIFIERS','source','system_name');
    	 	}
 
     void down() {
        
-    		renameColumn('IDENTIFIERS','system_name',"source")
+    		renameColumn('IDENTIFIERS','system_name',"source");
     		
-    		addColumn("identifiers","system_name", 'string')
+    		addColumn("identifiers","system_name", 'string');
     		
-    		execute("update identifiers set source =(select name from organizations where  where id=organization_id")
-    		execute("update identifiers set discriminator_column = 2 where discriminator_column = 1")
-    		execute("update identifiers set organization_id =null where system_name=''")
+    		execute("update identifiers set source =(select name from organizations where  where id=organization_id");
+    		execute("update identifiers set discriminator_column = 2 where discriminator_column = 1");
+    		execute("update identifiers set organization_id =null where system_name=''");
     		
-    		setNullable('IDENTIFIERS', 'system_name', false)		
-		setNullable('IDENTIFIERS', 'organization_id', false)	     	
-      		setNullable('IDENTIFIERS','source',false)
+    		setNullable('IDENTIFIERS', 'system_name', false);
+			setNullable('IDENTIFIERS', 'organization_id', false);	     	
+      		setNullable('IDENTIFIERS','source',false);
       		
-
-	     	
 	    	}
 	    }
