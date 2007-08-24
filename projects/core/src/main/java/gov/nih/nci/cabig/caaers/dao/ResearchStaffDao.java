3181264adb25db716ba9ba0f39891b4207d3f671
Sanjeev Agarwal <iec_sanjeev@yahoo.com>
2007-08-24 18:37:04 +0000

    updating finder method to search based on organization id..
    SVN-Revision: 2853

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/ResearchStaffDao.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/ResearchStaffDao.java
@@ -1,43 +1,43 @@
 package gov.nih.nci.cabig.caaers.dao;
 
+import gov.nih.nci.cabig.caaers.domain.ResearchStaff;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import gov.nih.nci.cabig.caaers.domain.ResearchStaff;
-import gov.nih.nci.cabig.caaers.domain.Organization;
-import gov.nih.nci.cabig.caaers.domain.SiteInvestigator;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Kulasekaran
  * 
  */
-@Transactional(readOnly=true)
+@Transactional(readOnly = true)
 public class ResearchStaffDao extends GridIdentifiableDao<ResearchStaff> {
 
-	private static final List<String> SUBSTRING_MATCH_PROPERTIES
-			= Arrays.asList("firstName", "lastName");
-	private static final List<String> EXACT_MATCH_PROPERTIES
-			= Collections.emptyList();
-	private static final List<Object> EXTRA_PARAMS
-			= Collections.emptyList();
+	private static final List<String> SUBSTRING_MATCH_PROPERTIES = Arrays.asList("firstName", "lastName");
+
+	private static final List<String> EXACT_MATCH_PROPERTIES = Collections.emptyList();
+
+	private static final List<Object> EXTRA_PARAMS = Collections.emptyList();
 
 	public List<ResearchStaff> getAll() {
-        return getHibernateTemplate().find("from ResearchStaff");
-    }  
-	
-    public Class<ResearchStaff> domainClass() {
-        return ResearchStaff.class;
-    }
-    
-    @Transactional(readOnly=false)
-    public void save(ResearchStaff researchStaff) {
-        getHibernateTemplate().saveOrUpdate(researchStaff);                        
-    }     
-    
-    public List<ResearchStaff> getBySubnames(String[] subnames) {
-        return findBySubname(subnames,null,EXTRA_PARAMS,
-            SUBSTRING_MATCH_PROPERTIES, EXACT_MATCH_PROPERTIES);
+		return getHibernateTemplate().find("from ResearchStaff");
+	}
+
+	@Override
+	public Class<ResearchStaff> domainClass() {
+		return ResearchStaff.class;
+	}
+
+	@Transactional(readOnly = false)
+	public void save(ResearchStaff researchStaff) {
+		getHibernateTemplate().saveOrUpdate(researchStaff);
+	}
+
+	public List<ResearchStaff> getBySubnames(String[] subnames, int site) {
+
+		return findBySubname(subnames, "o.organization.id = '" + site + "'", EXTRA_PARAMS, SUBSTRING_MATCH_PROPERTIES,
+				EXACT_MATCH_PROPERTIES);
 	}
 }
