d5f4c611369609b44bb81f706a3b0d2de06a314d
Srini Akkala <akkalas@gmail.com>
2007-08-28 06:27:06 +0000

    SVN-Revision: 2905

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/api/AdverseEventReportSerializer.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/api/AdverseEventReportSerializer.java
@@ -109,7 +109,7 @@ public class AdverseEventReportSerializer {
 	    	aer.setParticipantHistory(getParticipantHistory(hibernateAdverseEventReport.getParticipantHistory()));
 
 	    	//build StudyParticipantAssignment
-//	    	aer.setAssignment(getStudyParticipantAssignment(hibernateAdverseEventReport.getAssignment()));
+	    	aer.setAssignment(getStudyParticipantAssignment(hibernateAdverseEventReport.getAssignment()));
 
 	    	//build treatment info
 	    	aer.setTreatmentInformation(getTreatmentInformation(hibernateAdverseEventReport.getTreatmentInformation()));
