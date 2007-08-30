7c8ac1b959da3c71357a8a233adc601285395fc0
Krikor Krumlian <krikor.krumlian@gmail.com>
2007-08-30 19:29:24 +0000

    Small text change
    SVN-Revision: 2960

--- projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/ae/CreateAdverseEventController.java
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/ae/CreateAdverseEventController.java
@@ -22,7 +22,7 @@ public class CreateAdverseEventController extends AbstractAdverseEventInputContr
     protected FlowFactory<ExpeditedAdverseEventInputCommand> createFlowFactory() {
         return new ExpeditedFlowFactory("Create expedited report") {
         	
-        	private String instructions = "In order to create or edit an expedited AE report, you need to first select a participant and a\n" +
+        	private String instructions = "In order to create an expedited AE report, you need to first select a participant and a\n" +
             "study. You may start with either one. Once you have selected one, the options\n" +
             "for the other will be automatically constrained.";
         	
