4811b9bd72f67ee419edf8c22bf0165df56048b3
Sanjeev Agarwal <iec_sanjeev@yahoo.com>
2007-09-13 14:40:07 +0000

    updating groovy scripts for research staff....
    SVN-Revision: 3043

--- projects/core/src/main/db/migrate/009_construction_7/003_update_research_staffs_add_nci_identifier.groovy
+++ projects/core/src/main/db/migrate/009_construction_7/003_update_research_staffs_add_nci_identifier.groovy
@@ -1,11 +1,13 @@
 class UpdateResearchStaffs extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
 
- 	
+ 	addColumn("research_staffs","nci_identifier", 'string');
+ 	execute("UPDATE research_staffs set nci_identifier='nci_id'")
+	setNullable("research_staffs", "nci_identifier", false);
     }
 
     void down() {
-        		
+        		dropColumn("research_staffs","nci_identifier");
     
     }
 }
