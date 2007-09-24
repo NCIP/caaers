019cf79a5eab821ec5613ae5ae85e4bbcd01ddc7
Biju Joseph <biju.joseph@semanticbits.com>
2007-09-24 23:52:12 +0000

    Fix for validation of collection type properties which are evaluated in a lazy fashion.
    SVN-Revision: 3085

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/expeditedfields/ExpeditedReportTree.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/expeditedfields/ExpeditedReportTree.java
@@ -217,18 +217,22 @@ public class ExpeditedReportTree extends PropertylessNode {
         List<UnsatisfiedProperty> unsatisfied = new LinkedList<UnsatisfiedProperty>();
         for (String propertyName : nodePropertyNames) {
             TreeNode node = find(propertyName);
+            //HACK - if there is a property mismatch, node will be null.
+            if(node == null) continue; //continue with next property.
+            PropertyValues values = node.getPropertyValuesFrom(report);
+            //if values is empty, then the mandatory 'propertyName' is not available.
+            if(values.isEmpty()){
+            	unsatisfied.add(new UnsatisfiedProperty(node, propertyName));
+            	continue;
+            }
 
-            if(node == null){
-            	//TODO: refactor this
-            	//System.out.println("Unable to find Node propertyName: " + propertyName);
-            }else{
-            	PropertyValues values = node.getPropertyValuesFrom(report);
-                for (PropertyValue pv : values.getPropertyValues()) {
-                    if (pv.getValue() == null) {
-                        unsatisfied.add(new UnsatisfiedProperty(node, pv.getName()));
-                    }
+            //check each property value.
+            for (PropertyValue pv : values.getPropertyValues()) {
+                if (pv.getValue() == null) {
+                    unsatisfied.add(new UnsatisfiedProperty(node, pv.getName()));
                 }
             }
+
 
         }
         return unsatisfied;
