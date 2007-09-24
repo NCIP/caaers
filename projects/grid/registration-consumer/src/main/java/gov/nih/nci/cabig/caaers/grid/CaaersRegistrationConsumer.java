a06fbe3266dc4a4ec8ff1d66905a4e31707a6b19
Kumar Sambhuni <kumar.sambhuni@semanticbits.com>
2007-09-24 20:38:16 +0000

    Updated the implementation to the latest XSD
    SVN-Revision: 3083

--- projects/grid/registration-consumer/src/main/java/gov/nih/nci/cabig/caaers/grid/CaaersRegistrationConsumer.java
+++ projects/grid/registration-consumer/src/main/java/gov/nih/nci/cabig/caaers/grid/CaaersRegistrationConsumer.java
@@ -13,13 +13,15 @@ import gov.nih.nci.cabig.caaers.domain.Participant;
 import gov.nih.nci.cabig.caaers.domain.Organization;
 import gov.nih.nci.cabig.caaers.domain.Study;
 import gov.nih.nci.cabig.caaers.domain.StudyParticipantAssignment;
-import gov.nih.nci.cabig.ctms.common.RegistrationConsumer;
-import gov.nih.nci.cabig.ctms.grid.IdentifierType;
-import gov.nih.nci.cabig.ctms.grid.ParticipantType;
-import gov.nih.nci.cabig.ctms.grid.RegistrationType;
-import gov.nih.nci.cabig.ctms.stubs.types.InvalidRegistration;
-import gov.nih.nci.cabig.ctms.stubs.types.RegistrationFailed;
+
 import gov.nih.nci.cagrid.metadata.security.ServiceSecurityMetadata;
+import gov.nih.nci.ccts.grid.IdentifierType;
+import gov.nih.nci.ccts.grid.ParticipantType;
+import gov.nih.nci.ccts.grid.Registration;
+import gov.nih.nci.ccts.grid.common.RegistrationConsumer;
+import gov.nih.nci.ccts.grid.stubs.service.RegistrationConsumerService;
+import gov.nih.nci.ccts.grid.stubs.types.InvalidRegistrationException;
+import gov.nih.nci.ccts.grid.stubs.types.RegistrationConsumptionException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -30,7 +32,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author <a href="mailto:joshua.phillips@semanticbits.com>Joshua Phillips</a>
  *
  */
-public class CaaersRegistrationConsumer implements RegistrationConsumer {
+public class CaaersRegistrationConsumer implements RegistrationConsumer{
     private static final Log logger = LogFactory.getLog(CaaersRegistrationConsumer.class);
 
     public static final String STUDY_SERVICE_BEAN_NAME = "studyServiceAPI";
@@ -50,26 +52,28 @@ public class CaaersRegistrationConsumer implements RegistrationConsumer {
      *
      * @see gov.nih.nci.cabig.ctms.common.RegistrationConsumer#createRegistration(gov.nih.nci.cabig.ctms.grid.RegistrationType)
      */
-    public void register(RegistrationType registration) throws RemoteException, InvalidRegistration, RegistrationFailed {
+    public Registration register(Registration registration) throws RemoteException, InvalidRegistrationException, RegistrationConsumptionException {
         Study study = new Study();
-        study.setGridId(registration.getStudyGridId());
+        study.setGridId(registration.getGridId());
 
         Participant participant = createParticipant(registration);
 
         Organization organization = new Organization();
-        organization.setGridId(registration.getHealthCareSiteGridId());
+        organization.setGridId(registration.getStudySite().getGridId());
 
-        String registrationGridId = registration.getStudyParticipantIdentifier();
+        //String registrationGridId = registration.getStudyParticipantIdentifier();
+        String registrationGridId = registration.getGridId();
 
         StudyService svc = (StudyService) getApplicationContext().getBean(STUDY_SERVICE_BEAN_NAME);
         StudyParticipantAssignment assignment = svc.assignParticipant(study, participant, organization, registrationGridId);
         logger.debug("Created assignment " + assignment.getGridId());
+        return registration;
     }
 
-    private Participant createParticipant(RegistrationType registration) {
+    private Participant createParticipant(Registration registration) {
         ParticipantType partBean = registration.getParticipant();
         Participant participant = new Participant();
-        participant.setGridId(partBean.getParticipantGridId());
+        participant.setGridId(partBean.getGridId());
         participant.setGender(partBean.getAdministrativeGenderCode());
         participant.setDateOfBirth(partBean.getBirthDate());
         participant.setEthnicity(partBean.getEthnicGroupCode());
@@ -81,7 +85,7 @@ public class CaaersRegistrationConsumer implements RegistrationConsumer {
             List<Identifier> identifiersList =  new ArrayList<Identifier>();
             for (IdentifierType identifierType : partBean.getIdentifier()) {
                 Identifier identifier = new Identifier();
-                identifier.setSource(identifierType.getSource());
+                identifier.setType(identifierType.getSource());
                 identifier.setType(identifierType.getType());
                 identifier.setValue(identifierType.getValue());
 
