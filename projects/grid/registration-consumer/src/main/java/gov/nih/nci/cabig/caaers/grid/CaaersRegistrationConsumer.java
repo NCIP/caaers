42baca6a43217b80c8061da9759bf4c77db4f628
Biju Joseph <biju.joseph@semanticbits.com>
2007-09-28 15:00:17 +0000

    logic of getting gird_id changed
    SVN-Revision: 3125

--- projects/grid/registration-consumer/src/main/java/gov/nih/nci/cabig/caaers/grid/CaaersRegistrationConsumer.java
+++ projects/grid/registration-consumer/src/main/java/gov/nih/nci/cabig/caaers/grid/CaaersRegistrationConsumer.java
@@ -3,30 +3,29 @@
  */
 package gov.nih.nci.cabig.caaers.grid;
 
-import gov.nih.nci.cabig.caaers.api.StudyService;
-import gov.nih.nci.cabig.caaers.domain.Identifier;
-import gov.nih.nci.cabig.caaers.domain.Organization;
-import gov.nih.nci.cabig.caaers.domain.Participant;
-import gov.nih.nci.cabig.caaers.domain.Study;
-import gov.nih.nci.cabig.caaers.domain.StudyParticipantAssignment;
-import gov.nih.nci.cagrid.metadata.security.ServiceSecurityMetadata;
-import gov.nih.nci.ccts.grid.IdentifierType;
-import gov.nih.nci.ccts.grid.ParticipantType;
-import gov.nih.nci.ccts.grid.Registration;
-import gov.nih.nci.ccts.grid.common.RegistrationConsumer;
-import gov.nih.nci.ccts.grid.stubs.types.InvalidRegistrationException;
-import gov.nih.nci.ccts.grid.stubs.types.RegistrationConsumptionException;
-import gov.nih.nci.security.acegi.csm.authorization.AuthorizationSwitch;
-
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.acegisecurity.Authentication;
-import org.acegisecurity.GrantedAuthority;
-import org.acegisecurity.GrantedAuthorityImpl;
-import org.acegisecurity.context.SecurityContextHolder;
-import org.acegisecurity.providers.TestingAuthenticationToken;
+import gov.nih.nci.cabig.caaers.api.StudyService;
+import gov.nih.nci.cabig.caaers.domain.Identifier;
+import gov.nih.nci.cabig.caaers.domain.OrganizationAssignedIdentifier;
+import gov.nih.nci.cabig.caaers.domain.Participant;
+import gov.nih.nci.cabig.caaers.domain.Organization;
+import gov.nih.nci.cabig.caaers.domain.Study;
+import gov.nih.nci.cabig.caaers.domain.StudyParticipantAssignment;
+import gov.nih.nci.cabig.caaers.domain.SystemAssignedIdentifier;
+
+import gov.nih.nci.cagrid.metadata.security.ServiceSecurityMetadata;
+import gov.nih.nci.ccts.grid.IdentifierType;
+import gov.nih.nci.ccts.grid.OrganizationAssignedIdentifierType;
+import gov.nih.nci.ccts.grid.ParticipantType;
+import gov.nih.nci.ccts.grid.Registration;
+import gov.nih.nci.ccts.grid.SystemAssignedIdentifierType;
+import gov.nih.nci.ccts.grid.common.RegistrationConsumer;
+import gov.nih.nci.ccts.grid.stubs.types.InvalidRegistrationException;
+import gov.nih.nci.ccts.grid.stubs.types.RegistrationConsumptionException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
@@ -47,27 +46,8 @@ public class CaaersRegistrationConsumer implements RegistrationConsumer{
         if (applicationContext == null) {
             applicationContext = new ClassPathXmlApplicationContext(
                 new String[] { "classpath*:gov/nih/nci/cabig/caaers/applicationContext-*.xml" });
-            //authorizationOnByDefault = SecurityTestUtils.enableAuthorization(false, applicationContext);
-            
-            AuthorizationSwitch sw = (AuthorizationSwitch) applicationContext.getBean("authorizationSwitch");
-            if (sw == null) throw new RuntimeException("Authorization switch not found");
-            boolean current = sw.isOn();
-            sw.setOn(false);
-            
-            
-            switchUser("test-default-user", "ROLE_caaers_super_user");
         }
         return applicationContext;
-    }
-    public static void switchUser(String userName, String... roles) {
-        GrantedAuthority[] authorities = new GrantedAuthority[roles.length];
-        for (int i = 0; i < roles.length; i++) {
-            authorities[i] = new GrantedAuthorityImpl(roles[i]);
-        }
-        Authentication auth = new TestingAuthenticationToken(userName,
-            "ignored", authorities);
-        auth.setAuthenticated(true);
-        SecurityContextHolder.getContext().setAuthentication(auth);
     }
 
     /*
@@ -76,14 +56,13 @@ public class CaaersRegistrationConsumer implements RegistrationConsumer{
      * @see gov.nih.nci.cabig.ctms.common.RegistrationConsumer#createRegistration(gov.nih.nci.cabig.ctms.grid.RegistrationType)
      */
     public Registration register(Registration registration) throws RemoteException, InvalidRegistrationException, RegistrationConsumptionException {
-        System.out.print("In register method ...");
-    	Study study = new Study();
+        Study study = new Study();
         study.setGridId(registration.getStudyRef().getGridId());
 
         Participant participant = createParticipant(registration);
 
         Organization organization = new Organization();
-        organization.setGridId(registration.getStudySite().getGridId());
+        organization.setGridId(registration.getStudySite().getHealthcareSite(0).getGridId());
 
         //String registrationGridId = registration.getStudyParticipantIdentifier();
         String registrationGridId = registration.getGridId();
@@ -107,8 +86,21 @@ public class CaaersRegistrationConsumer implements RegistrationConsumer{
         if (partBean.getIdentifier() != null) {
             List<Identifier> identifiersList =  new ArrayList<Identifier>();
             for (IdentifierType identifierType : partBean.getIdentifier()) {
-                Identifier identifier = new Identifier();
-                identifier.setType(identifierType.getSource());
+            	Identifier identifier = null;
+            	if(identifierType instanceof SystemAssignedIdentifierType){
+            		SystemAssignedIdentifierType sysIdentifierType = (SystemAssignedIdentifierType) identifierType;
+            		SystemAssignedIdentifier sysIdentifier = new SystemAssignedIdentifier();
+            		sysIdentifier.setSystemName(sysIdentifierType.getSystemName());
+            		identifier = sysIdentifier;
+            	}else{
+            		OrganizationAssignedIdentifierType orgIdentifierType = (OrganizationAssignedIdentifierType) identifierType;
+          
+            		OrganizationAssignedIdentifier orgIdentifier = new OrganizationAssignedIdentifier();
+            		Organization organization = new Organization();
+            		organization.setGridId(orgIdentifier.getOrganization().getGridId());
+            		orgIdentifier.setOrganization(organization);
+            		identifier = orgIdentifier;
+            	}
                 identifier.setType(identifierType.getType());
                 identifier.setValue(identifierType.getValue());
 
