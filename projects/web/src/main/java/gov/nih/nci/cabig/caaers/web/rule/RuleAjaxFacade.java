993408a7054f9d586268f741da2dabb9d06d3e60
Srini Akkala <akkalas@gmail.com>
2007-10-26 17:20:46 +0000

    SVN-Revision: 3432

--- projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/rule/RuleAjaxFacade.java
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/rule/RuleAjaxFacade.java
@@ -334,7 +334,7 @@ public class RuleAjaxFacade
     public Boolean removeRule(int ruleCount) throws Exception {
     	CreateRuleCommand createRuleCommand = getAuthorRuleCommand();
     	RuleSet ruleSet = (RuleSet)createRuleCommand.getRuleSet();
-    	//ruleSet.getRule().get(ruleCount).setMarkedDelete(true);
+    	ruleSet.getRule().get(ruleCount).setMarkedDelete(true);
     	
     	Rule r = ruleSet.getRule().get(ruleCount);
     	String rName = r.getMetaData().getName();
