e90f1c0c38e6cd7b766c462e4a666c9b68beb046
Biju Joseph <biju.joseph@semanticbits.com>
2007-10-30 23:03:18 +0000

    esb url related fix
    SVN-Revision: 3445

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/esb/client/impl/CaaersConnectionFactory.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/esb/client/impl/CaaersConnectionFactory.java
@@ -1,22 +1,32 @@
 package gov.nih.nci.cabig.caaers.esb.client.impl;
 
 import gov.nih.nci.cabig.caaers.tools.configuration.Configuration;
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.springframework.beans.factory.annotation.Required;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
 /**
- *
  *
  * @author Rhett Sutphin
  */
+
 public class CaaersConnectionFactory implements ConnectionFactory {
     private ConnectionFactory delegate;
     private Configuration configuration;
-
+    private static final Log log = LogFactory.getLog(CaaersConnectionFactory.class);
+    
+    public CaaersConnectionFactory(Configuration configuration) throws Exception{
+    	this.configuration = configuration;
+    	createConnection(null, null);
+    }
+    
     public Connection createConnection() throws JMSException {
         return createConnection(null, null);
     }
@@ -28,16 +38,14 @@ public class CaaersConnectionFactory implements ConnectionFactory {
         return delegate.createConnection(username, password);
     }
 
-    protected ConnectionFactory createDelegate(String url) {
+    protected ConnectionFactory createDelegate(final String url) {
+    	String newUrl = (StringUtils.isEmpty(url))? "tcp://10.10.10.2:61616" : url;
+    	log.info("ESB url :" + newUrl);
         ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
-        factory.setBrokerURL(url);
+        factory.setBrokerURL(newUrl);
         return factory;
     }
 
     ////// CONFIGURATION
 
-    @Required
-    public void setConfiguration(Configuration configuration) {
-        this.configuration = configuration;
-    }
 }
