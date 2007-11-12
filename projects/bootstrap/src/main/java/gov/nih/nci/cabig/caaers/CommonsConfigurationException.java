dea91ac42624278ff1e9a0e6f36b7178297f1e76
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-11-12 20:26:24 +0000

    Subclass depended on uncommitted changes
    SVN-Revision: 3547

--- projects/bootstrap/src/main/java/gov/nih/nci/cabig/caaers/CommonsConfigurationException.java
+++ projects/bootstrap/src/main/java/gov/nih/nci/cabig/caaers/CommonsConfigurationException.java
@@ -1,19 +1,20 @@
 package gov.nih.nci.cabig.caaers;
 
+import gov.nih.nci.cabig.ctms.CommonsSystemException;
+
 /**
  * @author Rhett Sutphin
  */
-public class CommonsConfigurationException extends RuntimeException {
-
-    public CommonsConfigurationException(String message, Throwable cause) {
-        super(message, cause);
+public class CommonsConfigurationException extends CommonsSystemException {
+    public CommonsConfigurationException(String message, Throwable cause, Object... substitutions) {
+        super(message, cause, substitutions);
     }
 
     public CommonsConfigurationException(Throwable cause) {
         super(cause);
     }
 
-    public CommonsConfigurationException(String message) {
-        super(message);
+    public CommonsConfigurationException(String message, Object... substitutions) {
+        super(message, substitutions);
     }
 }
