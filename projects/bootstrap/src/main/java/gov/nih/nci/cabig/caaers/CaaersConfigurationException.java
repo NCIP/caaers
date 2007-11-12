9e04127588238d44e251f92e148a36e63baa1a0c
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-11-12 20:22:59 +0000

    Replace
    SVN-Revision: 3546

---
+++ projects/bootstrap/src/main/java/gov/nih/nci/cabig/caaers/CaaersConfigurationException.java
@@ -0,0 +1,20 @@
+package gov.nih.nci.cabig.caaers;
+
+import gov.nih.nci.cabig.caaers.CommonsConfigurationException;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class CaaersConfigurationException extends CommonsConfigurationException {
+    public CaaersConfigurationException(String message, Throwable cause, Object... substitutions) {
+        super(message, cause, substitutions);
+    }
+
+    public CaaersConfigurationException(Throwable cause) {
+        super(cause);
+    }
+
+    public CaaersConfigurationException(String message, Object... substitutions) {
+        super(message, substitutions);
+    }
+}
