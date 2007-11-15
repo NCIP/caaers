f4388580addfc4a2a87f7e286d7616eecb56dd31
Srini Akkala <akkalas@gmail.com>
2007-11-15 16:59:48 +0000

    RulesCache
    SVN-Revision: 3562

---
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/jsr94/jbossrules/runtime/RuleSetModificationListener.java
@@ -0,0 +1,7 @@
+package gov.nih.nci.cabig.caaers.rules.jsr94.jbossrules.runtime;
+
+public interface RuleSetModificationListener {
+	
+	public void ruleSetModified(String uri);
+
+}
---
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/jsr94/jbossrules/runtime/RulesCache.java
@@ -0,0 +1,57 @@
+package gov.nih.nci.cabig.caaers.rules.jsr94.jbossrules.runtime;
+
+import gov.nih.nci.cabig.caaers.rules.brxml.RuleSet;
+
+import java.util.Hashtable;
+
+import javax.rules.admin.RuleExecutionSet;
+
+public class RulesCache implements RuleSetModificationListener,RuleExecutionSetModificationListener{
+	private static RulesCache instance = null;
+	private Hashtable<String,RuleSet> ruleSetCache = null;
+	private Hashtable<String,RuleExecutionSet> ruleExecutionSetCache = null;
+    
+	public static synchronized RulesCache getInstance(){
+		if(instance==null){
+			instance = new RulesCache();
+		}
+		return instance;
+	}
+	private RulesCache(){
+		ruleSetCache = new Hashtable<String,RuleSet>();
+		ruleExecutionSetCache = new Hashtable<String,RuleExecutionSet>();
+	}
+	public void ruleSetModified(String uri) {
+		// TODO Auto-generated method stub
+		this.removeRuleSet(uri);
+	}
+	public void putRuleSet(String uri, RuleSet ruleSet){
+		
+		ruleSetCache.put(uri, ruleSet);
+	}
+	
+	public RuleSet getRuleSet(String uri){
+		return ruleSetCache.get(uri);
+	}
+	
+	private void removeRuleSet(String uri){
+		if(ruleSetCache.containsKey(uri)){
+			ruleSetCache.remove(uri);
+		}
+	}
+	
+    public void putRuleExecutionSet(String uri, RuleExecutionSet ruleExecutionSet){
+    	ruleExecutionSetCache.put(uri, ruleExecutionSet);
+    }
+    public RuleExecutionSet getRuleExecutionSet(String uri){
+    	return ruleExecutionSetCache.get(uri);
+    }
+	public void ruleSetDeployed(String uri) {
+		// TODO Auto-generated method stub
+		if(ruleExecutionSetCache.containsKey(uri)){
+			ruleExecutionSetCache.remove(uri);
+		}
+		
+	}
+
+}
