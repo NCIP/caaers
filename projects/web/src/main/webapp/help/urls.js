99b3e65772f2ea7226b851e6777c75b75fe5da82
Krikor Krumlian <krikor.krumlian@gmail.com>
2007-11-28 15:14:53 +0000

    New Help Files
    SVN-Revision: 3627

---
+++ projects/web/src/main/webapp/help/urls.js
@@ -0,0 +1,4 @@
+var g_ContentsURL = "caaersSource_Full-toc.htm";
+var g_IndexURL = "caaersSource_Full-index.htm";
+var g_SearchURL = "caaersSource_Full-search.htm";
+var g_FavoritesURL = "caaersSource_Full-favorites.htm";
