17d011233947c29925e791fd2582b3127a24173a
Krikor Krumlian <krikor.krumlian@gmail.com>
2007-11-28 15:08:44 +0000

    Remove old help files
    SVN-Revision: 3626

--- projects/web/src/main/webapp/help/urls.js
+++
@@ -1,4 +0,0 @@
-var g_ContentsURL = "caaersSource_Full-toc.htm";
-var g_IndexURL = "caaersSource_Full-index.htm";
-var g_SearchURL = "caaersSource_Full-search.htm";
-var g_FavoritesURL = "caaersSource_Full-favorites.htm";
