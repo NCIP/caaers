26b479d5505dbd96f993de4cb2f9a4f674f2c33e
Srini Akkala <akkalas@gmail.com>
2007-12-06 21:36:04 +0000

    SVN-Revision: 3696

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/esb/client/impl/CaaersAdeersMessageBroadcastServiceImpl.java
@@ -0,0 +1,42 @@
+package gov.nih.nci.cabig.caaers.esb.client.impl;
+
+
+import gov.nih.nci.cabig.caaers.esb.client.BroadcastException;
+
+import java.util.Vector;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+
+/**
+ *
+ * @author Srini Akkala
+ */
+public class CaaersAdeersMessageBroadcastServiceImpl extends JmsServiceImpl {
+	public void broadcast(String message) throws BroadcastException {
+		// TODO Auto-generated method stub
+		if(!isProvider()){
+			System.out.println("no send queue provided ");
+		}else{
+			System.out.println("calling sendJms method...");
+	        sendJms(message);
+		}
+    }
+	
+	public Vector getBroadcastStatus() {
+		// TODO Auto-generated method stub
+		if(!isConsumer()){
+			System.out.println("no recieve queue provided ");
+		}
+		return messages;
+	}
+	public void setConnectionFactory(ConnectionFactory connectionFactory) {
+		this.connectionFactory = connectionFactory;
+	}
+	public void setRecvQueue(Destination recvQueue) {
+		this.recvQueue = recvQueue;
+	}
+	public void setSendQueue(Destination sendQueue) {
+		this.sendQueue = sendQueue;
+	}
+}
