bd986e022b6666524e31fa757dde5fc593e8c26f
Srini Akkala <akkalas@gmail.com>
2007-12-06 20:06:01 +0000

    SVN-Revision: 3693

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/esb/client/MessageNotificationService.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/esb/client/MessageNotificationService.java
@@ -1,42 +1,56 @@
 package gov.nih.nci.cabig.caaers.esb.client;
 
+import gov.nih.nci.cabig.caaers.dao.ExpeditedAdverseEventReportDao;
+import gov.nih.nci.cabig.caaers.domain.ExpeditedAdverseEventReport;
+import gov.nih.nci.cabig.caaers.domain.Reporter;
 import gov.nih.nci.cabig.caaers.tools.configuration.Configuration;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
 
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.orm.hibernate3.support.OpenSessionInViewInterceptor;
+import org.springframework.web.context.request.WebRequest;
 
 public class MessageNotificationService {
 	protected Configuration configuration;
-	//protected ExpeditedAdverseEventReportDao expeditedAdverseEventReportDao;
-	//protected org.springframework.orm.hibernate3.support.OpenSessionInViewInterceptor openSessionInViewInterceptor;
+	protected ExpeditedAdverseEventReportDao expeditedAdverseEventReportDao;
+	protected OpenSessionInViewInterceptor openSessionInViewInterceptor;
+	
+	private WebRequest preProcess(){
+		
+		WebRequest stubWebRequest = new StubWebRequest();
+		openSessionInViewInterceptor.preHandle(stubWebRequest);
+		return stubWebRequest;
+	}
+	
+	private void postProcess(WebRequest stubWebRequest){
+		openSessionInViewInterceptor.afterCompletion(stubWebRequest, null);
+	}
 	
 
 	public void sendNotificationToReporter(String messages, String aeReportId) throws Exception {
 		//get AEreport by using this id
+
 		
-		System.out.println("in send 000 .." );
-		/*
-		SessionFactory sf = openSessionInViewInterceptor.getSessionFactory();
-		expeditedAdverseEventReportDao.setSessionFactory(sf);
-		Session s = sf.openSession();
-		
-		System.out.println("is connected " + s.isConnected());
-		//Session s = hibernateTemplate.getSessionFactory().getCurrentSession();
-		//Session s = sessionFactory.getConfiguration().buildSessionFactory().getCurrentSession();
+		WebRequest stubWebRequest = preProcess();
 		ExpeditedAdverseEventReport aeReport = expeditedAdverseEventReportDao.getById(Integer.parseInt(aeReportId));
 		
 		//get submitter info
 		Reporter reporter = aeReport.getReporter();
 		Map contact = reporter.getContactMechanisms();
-		s.close();
-		*/
+		
+
 		//get email
-		//String email = contact.get(Reporter.EMAIL).toString();
-		String email = "akkals@gmail.com";
-		System.out.println("email  ....." + email);
+		String email = contact.get(Reporter.EMAIL).toString();
+		postProcess(stubWebRequest);
+		//String email = "akkals@gmail.com";
+
 
 //		send email .
 		sendMail(configuration.get(Configuration.SMTP_ADDRESS), configuration.get(Configuration.SMTP_USER), 
@@ -51,8 +65,8 @@ public class MessageNotificationService {
 		try {	
 			JavaMailSenderImpl sender = new JavaMailSenderImpl();
 			//sender.setHost("smtp.comcast.net");
-			//sender.setUsername(user);
-			//sender.setPassword(pwd);
+			sender.setUsername(user);
+			sender.setPassword(pwd);
 			System.out.println("host .." + mailHost );
 			sender.setHost(mailHost);
 			MimeMessage message = sender.createMimeMessage();
@@ -77,7 +91,7 @@ public class MessageNotificationService {
 		this.configuration = configuration;
 	}
 
-/*
+
 	public void setExpeditedAdverseEventReportDao(
 			ExpeditedAdverseEventReportDao expeditedAdverseEventReportDao) {
 		this.expeditedAdverseEventReportDao = expeditedAdverseEventReportDao;
@@ -88,9 +102,46 @@ public class MessageNotificationService {
 			org.springframework.orm.hibernate3.support.OpenSessionInViewInterceptor openSessionInViewInterceptor) {
 		this.openSessionInViewInterceptor = openSessionInViewInterceptor;
 	}
-*/
-	//public void setSessionFactory(AnnotationSessionFactoryBean sessionFactory) {
-	//	this.sessionFactory = sessionFactory;
-	//}
+
+
+	
+	private static class StubWebRequest implements WebRequest {
+		public String getParameter(final String paramName) {
+			return null;
+		}
+
+		public String[] getParameterValues(final String paramName) {
+			return null;
+		}
+
+		public Map getParameterMap() {
+			return Collections.emptyMap();
+		}
+
+		public Locale getLocale() {
+			return null;
+		}
+
+		public Object getAttribute(final String name, final int scope) {
+			return null;
+		}
+
+		public void setAttribute(final String name, final Object value, final int scope) {
+		}
+
+		public void removeAttribute(final String name, final int scope) {
+		}
+
+		public void registerDestructionCallback(final String name, final Runnable callback, final int scope) {
+		}
+
+		public String getSessionId() {
+			return null;
+		}
+
+		public Object getSessionMutex() {
+			return null;
+		}
+	}
 	
 }
