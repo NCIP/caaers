86fd88d33955f322627006e153386478758a4f01
Biju Joseph <biju.joseph@semanticbits.com>
2008-01-18 17:31:20 +0000

    Made NCI identifier an optional field.
    SVN-Revision: 4061

--- projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/admin/ResearchStaffTab.java
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/admin/ResearchStaffTab.java
@@ -84,7 +84,7 @@ public class ResearchStaffTab extends TabWithFields<ResearchStaff> {
 		InputFieldAttributes.setSize(lastNameField, 30);
 		researchStaffFieldGroup.getFields().add(lastNameField);
 
-		InputField ncidIdField = InputFieldFactory.createTextField("nciIdentifier", "NCI Identifier", true);
+		InputField ncidIdField = InputFieldFactory.createTextField("nciIdentifier", "NCI Identifier", false);
 		InputFieldAttributes.setSize(ncidIdField, 30);
 		researchStaffFieldGroup.getFields().add(ncidIdField);
 
