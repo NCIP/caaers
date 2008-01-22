c9f541ea1a2ccf6c0a8564b950fdbbf708126c78
Unknown <unknown@unknown.com>
2008-01-22 22:04:49 +0000

    SVN-Revision: 4072

--- projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/user/ResetPasswordController.java
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/user/ResetPasswordController.java
@@ -18,10 +18,12 @@ public class ResetPasswordController extends SimpleFormController {
 
     private PasswordManagerService passwordManagerService;
     private UserService userService;
-    
+    private String emailPretext, emailPosttext;
+
     public ResetPasswordController() {
 	setFormView("user/resetPassword");
 	setBindOnNewForm(true);
+	initEmailText();
     }
 
 
@@ -33,8 +35,29 @@ public class ResetPasswordController extends SimpleFormController {
     protected ModelAndView onSubmit(Object command, BindException errors) throws Exception {
 	UserName userName = (UserName) command;
 	String token = passwordManagerService.requestToken(userName.getUserName());
-	userService.sendUserEmail(userName.getUserName(), "Reset caAERS Password", userName.getURL() + "&token=" + token);
+	userService.sendUserEmail(userName.getUserName(), "Reset caAERS Password", emailPretext + userName.getURL() + "&token=" + token + emailPosttext);
 	return new ModelAndView("user/emailSent");
+    }
+
+    private void initEmailText() {
+	emailPretext = ""
+	    + "Did you forgot your password?\n"
+	    + "Do you want to select a new password?\n"
+	    + "\n"
+	    + "We cannot send you your password, because we don't know what it is. However, we can help you change your password to something new.\n"
+	    + "\n"
+	    + "\n"
+	    + "To change your password go to the Change Password page:\n"
+	    + "\n";
+	emailPosttext = "\n"
+	    + "\n"
+	    + "Enter a new password for yourself once you get there. After you choose a new password, you can log into caAERS using the new password.\n"
+	    + "\n"
+	    + "\n"
+	    + "Regards,\n"
+	    + "The caAERS Notification System.\n"
+	    + "\n"
+	    + "(Note: If you did not request that a new password, please disregard this message.)";
     }
 
     @Required
