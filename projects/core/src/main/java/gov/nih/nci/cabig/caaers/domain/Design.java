6dc6fccbd2ab8f81bd4aeeda634667e02b82b77e
Krikor Krumlian <krikor.krumlian@gmail.com>
2008-02-04 14:45:03 +0000

    DCP related change to study
    SVN-Revision: 4181

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/Design.java
@@ -0,0 +1,40 @@
+package gov.nih.nci.cabig.caaers.domain;
+
+import static gov.nih.nci.cabig.ctms.domain.CodedEnumHelper.getByClassAndCode;
+import static gov.nih.nci.cabig.ctms.domain.CodedEnumHelper.register;
+import static gov.nih.nci.cabig.ctms.domain.EnumHelper.sentenceCasedName;
+import gov.nih.nci.cabig.ctms.domain.CodedEnum;
+
+/**
+ * @author Krikor Krumlian
+ */
+public enum Design implements CodedEnum<Integer> {
+    BLIND(1, "Blind"),
+    OPEN_UNBLIND(2, "Open/Unblind"),
+    PARTIAL(3, "Partial Blind");
+
+    private int code;
+    private String displayName;
+
+    Design(int code) {
+        this(code, null);
+    }
+
+    Design(int code, String longName) {
+        this.code = code;
+        this.displayName = longName;
+        register(this);
+    }
+
+    public static Design getByCode(int code) {
+        return getByClassAndCode(Design.class, code);
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getDisplayName() {
+        return displayName == null ? sentenceCasedName(this) : displayName;
+    }
+}
