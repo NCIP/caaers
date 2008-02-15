6cb8871f977bd7747e533cbc86802ea3f02dbf51
Biju Joseph <biju.joseph@semanticbits.com>
2008-02-15 22:19:30 +0000

    tabbed flow _finish not working correctly.
    SVN-Revision: 4336

--- projects/web/src/main/webapp/js/tabbedflow.js
+++ projects/web/src/main/webapp/js/tabbedflow.js
@@ -3,7 +3,12 @@ AE.tabbedFlowUpdateTarget = function(evt) {
     var tabclass = Element.classNames(a).detect(function(cls) { return cls.slice(0, 3) == "tab" })
     var targetPage = tabclass.slice(3)
     $('_target').name = "_target" + targetPage
-    if ($('command')._finish) $('_finish').disable()
+    if(Prototype.Browser.IE){
+    	if ($('command')._finish) $('_finish').disable()
+    }else{
+    	if ($('command')._finish) $('command')._finish.disable()
+    }
+    
 }
 
 AE.tabbedFlowSelectAndSubmit = function(click) {
