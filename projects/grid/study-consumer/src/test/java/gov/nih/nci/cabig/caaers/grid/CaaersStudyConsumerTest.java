dad99d9755d0da7a4ebfb9b8fd15854ae244fd19
Biju Joseph <biju.joseph@semanticbits.com>
2008-02-27 18:42:05 +0000

    testcase fixed
    SVN-Revision: 4425

--- projects/grid/study-consumer/src/test/java/gov/nih/nci/cabig/caaers/grid/CaaersStudyConsumerTest.java
+++ projects/grid/study-consumer/src/test/java/gov/nih/nci/cabig/caaers/grid/CaaersStudyConsumerTest.java
@@ -1,16 +1,24 @@
 package gov.nih.nci.cabig.caaers.grid;
 
-import gov.nih.nci.cabig.caaers.domain.OrganizationAssignedIdentifier;
+import gov.nih.nci.cabig.caaers.CaaersTestCase;
+import gov.nih.nci.cabig.caaers.dao.OrganizationDao;
+import gov.nih.nci.cabig.caaers.dao.SiteInvestigatorDao;
+import gov.nih.nci.cabig.caaers.dao.StudyDao;
+import gov.nih.nci.cabig.caaers.utils.ConfigProperty;
+import gov.nih.nci.cabig.ctms.audit.dao.AuditHistoryRepository;
+import gov.nih.nci.ccts.grid.AddressType;
+import gov.nih.nci.ccts.grid.CoordinatingCenterStudyStatusType;
+import gov.nih.nci.ccts.grid.HealthcareSiteType;
+import gov.nih.nci.ccts.grid.OrganizationAssignedIdentifierType;
 import gov.nih.nci.ccts.grid.Study;
-import gov.nih.nci.ccts.grid.client.StudyConsumerClient;
+import gov.nih.nci.ccts.grid.StudyCoordinatingCenterType;
+import gov.nih.nci.ccts.grid.StudyDataEntryStatusType;
+import gov.nih.nci.ccts.grid.StudyFundingSponsorType;
+import gov.nih.nci.ccts.grid.StudyOrganizationType;
 
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.Reader;
+import java.math.BigInteger;
 
-import junit.framework.TestCase;
-
-public class CaaersStudyConsumerTest extends TestCase {
+public class CaaersStudyConsumerTest extends CaaersTestCase {
 	CaaersStudyConsumer studyConsumer;
     private String studyResourceName;
     private String serviceUrl;
@@ -20,111 +28,103 @@ public class CaaersStudyConsumerTest extends TestCase {
     
 	protected void setUp() throws Exception {
 	 super.setUp();
-	 this.configLoction = "C:/devtools/workspace/REF-StudyRegistration/src/gov/nih/nci/ccts/grid/client/client-config.wsdd";	
-     this.studyResourceName = "C:/devtools/workspace//REF-StudyRegistration/test/resources/SampleStudyMessage.xml";
-     //this.serviceUrl = "http://10.10.10.2:8015/wsrf/services/cagrid/StudyConsumer";
-     //this.serviceUrl = "http://localhost:8080/wsrf/services/cagrid/StudyConsumer";
+	 //this.configLoction="/client-config.wsdd";
+     //this.studyResourceName = "/study.xml";
+   
+	 this.configLoction="/client-config.wsdd";
+     this.studyResourceName = "/study.xml";
+    
      this.serviceUrl = "http://cbvapp-d1017.nci.nih.gov:18080/wsrf/services/cagrid/StudyConsumer";  
-     studyConsumer = new CaaersStudyConsumer();
+     studyConsumer = createStudyConsumer();
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
 
-	public void testCommitRemote() throws Exception {
+	public void testCreateStudyLocal() throws Exception{
 		try {
-			StudyConsumerClient studyClient = new StudyConsumerClient(serviceUrl);
 			Study study = obtainStudyDTO();
-			studyClient.commit(study);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
-
-	public void testRollbackRemote() throws Exception{
-		try {
-			StudyConsumerClient studyClient = new StudyConsumerClient(serviceUrl);
-			Study study = obtainStudyDTO();
-			studyClient.rollback(study);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
-
-	public void testCreateStudyRemote() throws Exception{
-		try {
-			StudyConsumerClient studyClient = new StudyConsumerClient(serviceUrl);
-			Study study = obtainStudyDTO();
-			studyClient.createStudy(study);
+			studyConsumer.createStudy(study);
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw e;
 		}
 	}
 	
-	public void testCreateDeleteInLoopRemote() throws Exception {
-		int cnt = 0;
-		for(int i = 0; i < 15 ; i++){
-			try {
-				StudyConsumerClient studyClient = new StudyConsumerClient(serviceUrl);
-				Study study = obtainStudyDTO();
-				String idValue = study.getIdentifier(0).getValue();
-				study.getIdentifier(0).setValue(idValue + i);
-				studyClient.createStudy(study);
-				studyClient.rollback(study);
-				
-				cnt++;
-			} catch (Exception e) {
-				e.printStackTrace();
-				throw e;
-			}	
+	public void testRollbackStudyLocal() throws Exception {
+		
+		try {
+			Study study = obtainStudyDTO();
+			studyConsumer.rollback(study);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
 		}
-		assertEquals("Count should be 14", 14, cnt); 
-	}
-
-	public void testFindCoordinatingCenterIdentifier() {
-		fail("Not yet implemented");
-	}
-
-	public void testPopulateStudyDetails() {
-		fail("Not yet implemented");
-	}
-
-	public void testPopulateIdentifiers() {
-		fail("Not yet implemented");
-	}
-
-	public void testPopulateStudyOrganizations() {
-		fail("Not yet implemented");
-	}
-
-	public void testPopulateInvestigators() {
-		fail("Not yet implemented");
-	}
-
-	public void testFetchOrganization() {
-		fail("Not yet implemented");
-	}
-
-	public void testFetchStudy() {
-     System.out.println(studyConsumer.fetchStudy("nci001", OrganizationAssignedIdentifier.COORDINATING_CENTER_IDENTIFIER_TYPE));
+		
 	}
 	
+	public CaaersStudyConsumer createStudyConsumer() {
+		CaaersStudyConsumer consumer = new CaaersStudyConsumer();
+		consumer.setConfigurationProperty((ConfigProperty) getDeployedApplicationContext().getBean("configurationProperty"));
+    	consumer.setOrganizationDao((OrganizationDao) getDeployedApplicationContext().getBean("organizationDao"));
+    	consumer.setStudyDao((StudyDao)getDeployedApplicationContext().getBean("studyDao"));
+    	consumer.setAuditHistoryRepository((AuditHistoryRepository) getDeployedApplicationContext().getBean("auditHistoryRepository"));
+    	consumer.setSiteInvestigatorDao((SiteInvestigatorDao) getDeployedApplicationContext().getBean("siteInvestigatorDao"));
+    	consumer.setStudyConsumerGridServiceUrl("/pages/task");
+    	consumer.setRollbackInterval(1);
+		return consumer;
+	}
 	public Study obtainStudyDTO() throws Exception{
-		Study studyDTO = null;
-		try {
-		Reader reader = new FileReader(studyResourceName);
-		FileInputStream fis = new FileInputStream(configLoction);
-			studyDTO = (Study) gov.nih.nci.cagrid.common.Utils.deserializeObject(reader, Study.class,fis);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			throw e;
-		}
-        return studyDTO;
+		Study s = new Study();
+		s.setBlindedIndicator("true");
+		s.setLongTitleText("Long");
+		s.setShortTitleText("short");
+		s.setMultiInstitutionIndicator("true");
+		s.setRandomizedIndicator("true");
+		s.setPhaseCode("Phase I/II Trial");
+		s.setDataEntryStatus(StudyDataEntryStatusType.INCOMPLETE);
+		s.setCoordinatingCenterStudyStatus(CoordinatingCenterStudyStatusType.PENDING);
+		s.setType("Genetic Non-therapeutic");
+		s.setDescriptionText("description");
+		s.setTargetAccrualNumber(BigInteger.ONE);
+		
+		//OrgIdentifiers
+		OrganizationAssignedIdentifierType[] idType = new OrganizationAssignedIdentifierType[2];
+		idType[0] = new OrganizationAssignedIdentifierType();
+		idType[0].setGridId("1111111111");
+		idType[0].setPrimaryIndicator(true);
+		idType[0].setType("Coordinating Center Identifier");
+		idType[0].setValue("abcd123");
+		HealthcareSiteType siteType = new HealthcareSiteType();
+		siteType.setAddress(new AddressType());
+		siteType.setDescriptionText("abce");
+		siteType.setNciInstituteCode("DUKE");
+		idType[0].setHealthcareSite(siteType);
+		
+		idType[1] = new OrganizationAssignedIdentifierType();
+		idType[1].setGridId("1113311111");
+		idType[1].setPrimaryIndicator(true);
+		idType[1].setType("Protocol Authority Identifier");
+		idType[1].setValue("x123");
+		idType[1].setHealthcareSite(siteType);
+		
+		s.setIdentifier(idType);
+		StudyOrganizationType[] orgType = new StudyOrganizationType[2];
+		StudyCoordinatingCenterType ccType = new StudyCoordinatingCenterType();
+		ccType.setGridId("11111");
+		HealthcareSiteType[] sites = new HealthcareSiteType[1];
+		sites[0] = siteType;
+		ccType.setHealthcareSite(sites);
+		orgType[0] = ccType;
+		
+		StudyFundingSponsorType sfType = new StudyFundingSponsorType();
+		sfType.setGridId("eeeee");
+		sfType.setHealthcareSite(sites);
+		orgType[1] = sfType;
+		s.setStudyOrganization(orgType);
+		
+		return s;
 	}
 	
 	
