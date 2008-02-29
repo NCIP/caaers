873ba3e9bea5ec4d73f74b5be384772db598135f
Biju Joseph <biju.joseph@semanticbits.com>
2008-02-29 15:59:04 +0000

    CAAERS-27 removing Organization.Access rights from all users. This group must be acccessible only by the organization group.
    SVN-Revision: 4447

---
+++ projects/core/src/main/db/migrate/010_construction_8/053_remove_accesss_organization_right_from_all_user.groovy
@@ -0,0 +1,40 @@
+class updateStudyDcpDesignColumn  extends edu.northwestern.bioinformatics.bering.Migration {
+
+
+    public void up(){
+
+     // Gives caaers_ae_cd group the
+                     //gov.nih.nci.cabig.caaers.domain.Study:READ role on
+                    //gov.nih.nci.cabig.caaers.domain.Study protection group
+                    		insert('csm_user_group_role_pg',
+                    			[
+                    				user_group_role_pg_id: -67,
+                    				group_id: -13, role_id: -3, protection_group_id: -1
+                    			],
+                    			primaryKey: false
+                    		)
+
+
+execute("DELETE FROM csm_user_group_role_pg WHERE user_group_role_pg_id=-57");
+
+   execute("DELETE FROM csm_user_group_role_pg WHERE user_group_role_pg_id=-58");
+
+execute("DELETE FROM csm_user_group_role_pg WHERE user_group_role_pg_id=-59");
+
+execute("DELETE FROM csm_user_group_role_pg WHERE user_group_role_pg_id=-60");
+
+
+
+
+
+
+    }
+
+
+    public void down(){
+    execute("DELETE FROM csm_user_group_role_pg WHERE user_group_role_pg_id=-67");
+
+
+    }
+
+  }
