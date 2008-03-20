993e94e7c4712e442c7aa2bf870922dcc74f0f34
Biju Joseph <biju.joseph@semanticbits.com>
2008-03-20 15:23:18 +0000

    updating groovy script.correcting date format for hsql db
    SVN-Revision: 4492

--- projects/core/src/main/db/migrate/010_construction_8/047_add_ccts_demo_user.groovy
+++ projects/core/src/main/db/migrate/010_construction_8/047_add_ccts_demo_user.groovy
@@ -1,16 +1,50 @@
 class UpdateCsmPrivileges extends edu.northwestern.bioinformatics.bering.Migration {
 	void up(){
 
+           if (databaseMatches('oracle')) {
+              insert('CSM_USER',
+                [
+                    USER_ID: -7,LOGIN_NAME: 'cctsdemo1@nci.nih.gov', FIRST_NAME: 'cctsdemo ', LAST_NAME: 'user',
+                       PASSWORD: 'yDq1c1nU4E7almNpMZNexg==',EMAIL_ID: 'cctsdemo1@nci.nih.gov', UPDATE_DATE: '27-JAN-2007'
 
-		insert('CSM_USER',
-			[
-				USER_ID: -7,LOGIN_NAME: 'cctsdemo1@nci.nih.gov', FIRST_NAME: 'cctsdemo ', LAST_NAME: 'user',
-                PASSWORD: 'yDq1c1nU4E7almNpMZNexg==',EMAIL_ID: 'cctsdemo1@nci.nih.gov', UPDATE_DATE: '27-JAN-2007'
+                ],
+                primaryKey: false
+            )
 
-			],
-			primaryKey: false
-		)
-            insert('CSM_USER_GROUP',
+        } else if (databaseMatches('postgresql')){
+             insert('CSM_USER',
+                        [
+                            USER_ID: -7,LOGIN_NAME: 'cctsdemo1@nci.nih.gov', FIRST_NAME: 'cctsdemo ', LAST_NAME: 'user',
+                             PASSWORD: 'yDq1c1nU4E7almNpMZNexg==',EMAIL_ID: 'cctsdemo1@nci.nih.gov', UPDATE_DATE: '27-JAN-2007'
+
+                        ],
+                        primaryKey: false
+                    )
+
+        } else if (databaseMatches('hsqldb')){
+        insert('CSM_USER',
+            			[
+            				USER_ID: -7,LOGIN_NAME: 'cctsdemo1@nci.nih.gov', FIRST_NAME: 'cctsdemo ', LAST_NAME: 'user',
+                            PASSWORD: 'yDq1c1nU4E7almNpMZNexg==',EMAIL_ID: 'cctsdemo1@nci.nih.gov', UPDATE_DATE: '2007-JAN-27'
+
+            			],
+            			primaryKey: false
+            		)
+
+
+       } else {
+        insert('CSM_USER',
+        			[
+        				USER_ID: -7,LOGIN_NAME: 'cctsdemo1@nci.nih.gov', FIRST_NAME: 'cctsdemo ', LAST_NAME: 'user',
+                        PASSWORD: 'yDq1c1nU4E7almNpMZNexg==',EMAIL_ID: 'cctsdemo1@nci.nih.gov', UPDATE_DATE: '27-JAN-2007'
+
+        			],
+        			primaryKey: false
+        		)
+                                 
+        }
+
+		   insert('CSM_USER_GROUP',
             			[USER_GROUP_ID: -11,group_id: -1, USER_ID: -7],primaryKey: false
             	   )
 
