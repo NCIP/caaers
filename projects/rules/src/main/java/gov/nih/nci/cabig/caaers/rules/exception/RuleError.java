5d15335896d8c5733e92ec1ccb3a46a34e06a23f
Srini Akkala <akkalas@gmail.com>
2008-04-02 13:43:04 +0000

    SVN-Revision: 4532

---
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/exception/RuleError.java
@@ -0,0 +1,14 @@
+package gov.nih.nci.cabig.caaers.rules.exception;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class RuleError extends Error {
+    public RuleError(String message) {
+        super(message);
+    }
+
+    public RuleError(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
