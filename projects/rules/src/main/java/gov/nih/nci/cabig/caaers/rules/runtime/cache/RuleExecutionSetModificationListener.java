e529aa41064dede7750ff88073ce244cee334a97
Srini Akkala <akkalas@gmail.com>
2008-04-03 00:58:43 +0000

    SVN-Revision: 4547

---
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/runtime/cache/RuleExecutionSetModificationListener.java
@@ -0,0 +1,7 @@
+package gov.nih.nci.cabig.caaers.rules.runtime.cache;
+
+public interface RuleExecutionSetModificationListener {
+
+    public void ruleSetDeployed(String uri);
+
+}
---
+++ projects/rules/src/main/java/gov/nih/nci/cabig/caaers/rules/runtime/cache/RuleSetModificationListener.java
@@ -0,0 +1,7 @@
+package gov.nih.nci.cabig.caaers.rules.runtime.cache;
+
+public interface RuleSetModificationListener {
+
+    public void ruleSetModified(String uri);
+
+}
