0852def2516f450eb992c0c3aa06772027b83ab3
Biju Joseph <biju.joseph@semanticbits.com>
2008-04-09 20:43:19 +0000

    adding report factory
    SVN-Revision: 4594

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/factory/ReportFactory.java
@@ -0,0 +1,137 @@
+package gov.nih.nci.cabig.caaers.domain.factory;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import gov.nih.nci.cabig.caaers.CaaersSystemException;
+import gov.nih.nci.cabig.caaers.domain.ExpeditedAdverseEventReport;
+import gov.nih.nci.cabig.caaers.domain.ReportStatus;
+import gov.nih.nci.cabig.caaers.domain.report.*;
+import gov.nih.nci.cabig.ctms.lang.NowFactory;
+import org.apache.commons.lang.StringUtils;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.*;
+
+/**
+ * @author Biju Joseph
+ */
+public class ReportFactory {
+
+    private NowFactory nowFactory;
+
+    public Report createReport(final ReportDefinition reportDefinition, final ExpeditedAdverseEventReport aeReport) {
+        assert reportDefinition != null : "ReportDefinition must be not null. Unable to create a Report";
+        assert aeReport != null : "ExpeditedAdverseEventReport should not be null. Unable to create a Report";
+
+        Date now = nowFactory.getNow();
+        Calendar cal = GregorianCalendar.getInstance();
+
+        Report report = reportDefinition.createReport();
+        report.setCreatedOn(now);
+
+        ReportVersion reportVersion = new ReportVersion();
+        reportVersion.setCreatedOn(now);
+        reportVersion.setReportStatus(ReportStatus.PENDING);
+        report.addReportVersion(reportVersion);
+
+        //attach the aeReport to report
+        aeReport.addReport(report);
+
+        //set the due date
+        cal.setTime(now);
+        cal.add(reportDefinition.getTimeScaleUnitType().getCalendarTypeCode(), reportDefinition.getDuration());
+        report.setDueOn(cal.getTime());
+        reportVersion.setDueOn(cal.getTime());
+
+        //populate the delivery definitions
+        if (reportDefinition.getDeliveryDefinitions() != null) {
+            for (ReportDeliveryDefinition reportDeliveryDefinition : reportDefinition.getDeliveryDefinitions()) {
+                //fetch the contact mechanism for role based entities.
+                if (reportDeliveryDefinition.getEndPointType().equals(ReportDeliveryDefinition.ENTITY_TYPE_ROLE)) {
+                    List<String> addresses = aeReport.findEmailAddress(reportDeliveryDefinition.getEndPoint());
+                    for (String address : addresses) {
+                        if (StringUtils.isNotEmpty(address)) {
+                            ReportDelivery reportDelivery = reportDeliveryDefinition.createReportDelivery();
+                            reportDelivery.setEndPoint(address);
+                            report.addReportDelivery(reportDelivery);
+                        }
+                    }
+                } else {
+                    if (StringUtils.isNotEmpty(reportDeliveryDefinition.getEndPoint())) {
+                        ReportDelivery rd = reportDeliveryDefinition.createReportDelivery();
+                        rd.setEndPoint(reportDeliveryDefinition.getEndPoint());
+                        report.addReportDelivery(rd);
+                    }
+                }
+
+            }//~for rdd
+        }//~if
+
+        //populate the scheduled notifications.
+        //Note:- ScheduledNotification is per Recipient. A PlannedNotificaiton has many recipients.
+        if (reportDefinition.getPlannedNotifications() != null) {
+
+            for (PlannedNotification plannedNotification : reportDefinition.getPlannedNotifications()) {
+                String subjectLine = null;
+                String bodyContent = null;
+
+                //obtain all valid recipient address
+                List<String> toAddressList = plannedNotification.findToAddressesForReport(report);
+                // for each recipient(address) create a ScheduledNotification.
+                for (String to : toAddressList) {
+
+                    ScheduledNotification scheduledNotification = null;
+                    // TODO: instanceof indicates an abstraction failure.  Could this be domain logic?
+                    if (plannedNotification instanceof PlannedEmailNotification) {
+                        PlannedEmailNotification penf = (PlannedEmailNotification) plannedNotification;
+                        ScheduledEmailNotification senf = penf.createScheduledNotification(to);
+                        scheduledNotification = senf;
+                        if (subjectLine == null) {
+                            subjectLine = applyRuntimeReplacementsForReport(penf.getSubjectLine(), report);
+                        }
+                        senf.setSubjectLine(subjectLine);
+                    }
+
+                    if (bodyContent == null) {
+                        bodyContent = applyRuntimeReplacementsForReport(plannedNotification.getNotificationBodyContent().getBody(), report);
+                    }
+                    scheduledNotification.setBody(bodyContent);
+
+                    // TODO: consider some or all of this domain logic, too
+                    scheduledNotification.setCreatedOn(now);
+                    scheduledNotification.setDeliveryStatus(DeliveryStatus.CREATED);
+                    cal.setTime(now);
+                    cal.add(reportDefinition.getTimeScaleUnitType().getCalendarTypeCode(), (plannedNotification.getIndexOnTimeScale()));
+                    scheduledNotification.setScheduledOn(cal.getTime());
+
+                    report.addScheduledNotification(scheduledNotification);
+
+                }//for each to
+            }//for each pnf
+        }//~if
+        return report;
+    }
+
+
+    private String applyRuntimeReplacementsForReport(String rawText, Report report) {
+        Configuration cfg = new Configuration();
+        try {
+            Template t = new Template("message", new StringReader(rawText), cfg);
+            Map<Object, Object> map = report.getContextVariables();
+            StringWriter writer = new StringWriter();
+            t.process(map, writer);
+            return writer.toString();
+        } catch (TemplateException e) {
+            throw new CaaersSystemException("Error while applying freemarker template [PlannedNotificatiton.body]", e);
+        } catch (IOException e) {
+            throw new CaaersSystemException("Error while applying freemarker template [PlannedNotificatiton.body]", e);
+        }
+    }
+
+    public void setNowFactory(final NowFactory nowFactory) {
+        this.nowFactory = nowFactory;
+    }
+}
---
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/domain/repository/ReportRepositoryTest.java
@@ -0,0 +1,233 @@
+package gov.nih.nci.cabig.caaers.domain.repository;
+
+import gov.nih.nci.cabig.caaers.CaaersTestCase;
+import gov.nih.nci.cabig.caaers.domain.*;
+import gov.nih.nci.cabig.caaers.domain.attribution.AdverseEventAttribution;
+import gov.nih.nci.cabig.caaers.domain.expeditedfields.ExpeditedReportSection;
+import static gov.nih.nci.cabig.caaers.domain.expeditedfields.ExpeditedReportSection.ATTRIBUTION_SECTION;
+import gov.nih.nci.cabig.caaers.domain.report.*;
+import gov.nih.nci.cabig.caaers.service.ReportSubmittability;
+import gov.nih.nci.cabig.ctms.domain.DomainObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class ReportRepositoryTest extends CaaersTestCase {
+    private static final Attribution[] SUFFICIENT_ATTRIBUTIONS = new Attribution[]{
+            Attribution.POSSIBLE, Attribution.PROBABLE, Attribution.DEFINITE};
+
+    private static final Attribution[] INSUFFICENT_ATTRIBUTIONS = new Attribution[]{
+            Attribution.UNLIKELY, Attribution.UNRELATED};
+
+    private static final String TERM = "Auralmonagem";
+
+    private ReportRepository reportRepository;
+
+    private ExpeditedAdverseEventReport expeditedData;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        reportRepository = new ReportRepositoryImpl();
+        expeditedData = new ExpeditedAdverseEventReport();
+        CtcTerm ctcTerm = new CtcTerm();
+        ctcTerm.setTerm(TERM);
+        expeditedData.getAdverseEvents().get(0).getAdverseEventCtcTerm().setCtcTerm(ctcTerm);
+    }
+
+//    public void testValidateChecksForNoAttribution() throws Exception {
+//        ReportSubmittability actual = validateForAttribution();
+//        assertInsuffientAttributionMessage("No attributions should not be sufficent", actual);
+//    }
+//
+//    public void testValidateAcceptsGrade3Through5ForOtherCause() throws Exception {
+//        OtherCause cause = new OtherCause();
+//        expeditedData.addOtherCause(cause);
+//        OtherCauseAttribution attr = createAttribution(cause, Attribution.POSSIBLE,
+//                        OtherCauseAttribution.class);
+//        expeditedData.getAdverseEvents().get(0).getOtherCauseAttributions().add(attr);
+//
+//        assertSufficientAttributionsAreSufficent(attr);
+//    }
+//
+//    public void testValidateAcceptsGrade3Through5ForCourseAgent() throws Exception {
+//        CourseAgent cause = new CourseAgent();
+//        expeditedData.getTreatmentInformation().addCourseAgent(cause);
+//        CourseAgentAttribution attr = createAttribution(cause, Attribution.POSSIBLE,
+//                        CourseAgentAttribution.class);
+//        expeditedData.getAdverseEvents().get(0).getCourseAgentAttributions().add(attr);
+//
+//        assertSufficientAttributionsAreSufficent(attr);
+//    }
+
+//    public void testValidateAcceptsGrade3Through5ForConcomitantMedication() throws Exception {
+//        ConcomitantMedication cause = new ConcomitantMedication();
+//        expeditedData.addConcomitantMedication(cause);
+//        ConcomitantMedicationAttribution attr = createAttribution(cause, Attribution.UNRELATED,
+//                ConcomitantMedicationAttribution.class);
+//        expeditedData.getAdverseEvents().get(0).getConcomitantMedicationAttributions().add(attr);
+//
+//        assertSufficientAttributionsAreSufficent(attr);
+//    }
+//
+//    public void testValidateAcceptsGrade3Through5ForDisease() throws Exception {
+//        expeditedData.getDiseaseHistory().setOtherPrimaryDisease("Hearing loss");
+//
+//        DiseaseAttribution attr = createAttribution(expeditedData.getDiseaseHistory(),
+//                Attribution.UNRELATED, DiseaseAttribution.class);
+//        expeditedData.getAdverseEvents().get(0).getDiseaseAttributions().add(attr);
+//
+//        assertSufficientAttributionsAreSufficent(attr);
+//    }
+//
+//    public void testValidateDoesNotAcceptGrade3Through5ForSurgery() throws Exception {
+//        SurgeryIntervention cause = new SurgeryIntervention();
+//        expeditedData.getSurgeryInterventions().add(cause);
+//
+//        SurgeryAttribution attr = createAttribution(cause, Attribution.UNRELATED,
+//                SurgeryAttribution.class);
+//        expeditedData.getAdverseEvents().get(0).getSurgeryAttributions().add(attr);
+//
+//        assertNoAttributionsAreSufficent(attr);
+//    }
+
+//    public void testValidateDoesNotAcceptGrade3Through5ForRadiation() throws Exception {
+//        RadiationIntervention cause = new RadiationIntervention();
+//        expeditedData.getRadiationInterventions().add(cause);
+//
+//        RadiationAttribution attr = createAttribution(cause, Attribution.UNRELATED,
+//                RadiationAttribution.class);
+//        expeditedData.getAdverseEvents().get(0).getRadiationAttributions().add(attr);
+//
+//        assertNoAttributionsAreSufficent(attr);
+//    }
+
+    //
+    public void testFindToAddress() {
+
+        StudyParticipantAssignment assignment = Fixtures.createAssignment();
+        StudySite site = assignment.getStudySite();
+        StudyInvestigator studyInvestigator = new StudyInvestigator();
+        studyInvestigator.setRoleCode("Site Principal Investigator");
+        SiteInvestigator siteInvestigator = new SiteInvestigator();
+        Investigator investigator = new Investigator();
+        investigator.setEmailAddress("biju@kk.com");
+        siteInvestigator.setInvestigator(investigator);
+        studyInvestigator.setSiteInvestigator(siteInvestigator);
+        site.addStudyInvestigators(studyInvestigator);
+        expeditedData.setAssignment(assignment);
+
+        PlannedEmailNotification penf = new PlannedEmailNotification();
+        RoleBasedRecipient recipient = new RoleBasedRecipient();
+        recipient.setRoleName("SPI");
+
+        List<Recipient> recipients = new ArrayList<Recipient>();
+        recipients.add(recipient);
+        penf.setRecipients(recipients);
+
+        ReportDefinition reportDef = Fixtures.createReportDefinition("test");
+        reportDef.addPlannedNotification(penf);
+
+        Report report = new Report();
+        report.setReportDefinition(reportDef);
+        report.setAeReport(expeditedData);
+
+        ReportRepositoryImpl impl = (ReportRepositoryImpl) reportRepository;
+        List<String> addresses = penf.findToAddressesForReport(report);
+        assertEquals(1, addresses.size());
+
+    }
+
+    public void testFindEmailAddress() {
+        StudyParticipantAssignment assignment = Fixtures.createAssignment();
+        StudySite site = assignment.getStudySite();
+        StudyInvestigator studyInvestigator = new StudyInvestigator();
+        studyInvestigator.setRoleCode("Site Principal Investigator");
+        SiteInvestigator siteInvestigator = new SiteInvestigator();
+        Investigator investigator = new Investigator();
+        investigator.setEmailAddress("biju@kk.com");
+        siteInvestigator.setInvestigator(investigator);
+        studyInvestigator.setSiteInvestigator(siteInvestigator);
+        site.addStudyInvestigators(studyInvestigator);
+
+        ResearchStaff staff = new ResearchStaff();
+        staff.setEmailAddress("aa@kk.com");
+        StudyPersonnel studyPersonnel = new StudyPersonnel();
+        studyPersonnel.setRoleCode("Participant Coordinator");
+        studyPersonnel.setResearchStaff(staff);
+        site.addStudyPersonnel(studyPersonnel);
+
+        expeditedData.setAssignment(assignment);
+
+        ReportRepository impl = (ReportRepository) reportRepository;
+//        List<String> addresses = expeditedData.findEmailAddress("SPI", impl);
+//        assertEquals(1, addresses.size());
+//        List<String> addresses2 = expeditedData.findEmailAddress("PC", impl);
+//        assertEquals(1, addresses2.size());
+    }
+
+    private void assertNoAttributionsAreSufficent(AdverseEventAttribution attr) {
+        for (Attribution attribution : Attribution.values()) {
+            attr.setAttribution(attribution);
+            ReportSubmittability actual = validateForAttribution();
+            assertInsuffientAttributionMessage(attribution + " should not be sufficient", actual);
+        }
+    }
+
+    private void assertSufficientAttributionsAreSufficent(AdverseEventAttribution attr) {
+        for (Attribution attribution : SUFFICIENT_ATTRIBUTIONS) {
+            attr.setAttribution(attribution);
+            ReportSubmittability actual = validateForAttribution();
+            assertTrue(attribution + " should be sufficent", actual.isSubmittable());
+        }
+
+        for (Attribution attribution : INSUFFICENT_ATTRIBUTIONS) {
+            attr.setAttribution(attribution);
+            ReportSubmittability actual = validateForAttribution();
+            assertInsuffientAttributionMessage(attribution + " should not be sufficent", actual);
+        }
+    }
+
+    private Report createAttributionMandatoryReport() {
+        Report report = new Report();
+        report.setReportDefinition(new ReportDefinition());
+        report.setAeReport(expeditedData);
+        report.getReportDefinition().setAttributionRequired(true);
+        // TODO:
+        // report.setAttributionMandatory(true);
+        return report;
+    }
+
+    private ReportSubmittability validateForAttribution() {
+        return reportRepository.validate(createAttributionMandatoryReport(), Collections
+                .<ExpeditedReportSection>emptySet());
+    }
+
+    private <C extends DomainObject, A extends AdverseEventAttribution<C>> A createAttribution(
+            C cause, Attribution level, Class<A> klass) throws IllegalAccessException,
+            InstantiationException {
+        A attr = klass.newInstance();
+        attr.setAttribution(level);
+        attr.setCause(cause);
+        return attr;
+    }
+
+    private void assertInsuffientAttributionMessage(String assertionMessage,
+                                                    ReportSubmittability container) {
+        assertTrue(assertionMessage + ": No attribution section messages", container.getMessages()
+                .containsKey(ATTRIBUTION_SECTION));
+        assertTrue(assertionMessage + ": No attribution section messages", container.getMessages()
+                .get(ATTRIBUTION_SECTION).size() > 0);
+        assertEquals(
+                assertionMessage + ": Wrong message",
+                "The adverse event "
+                        + TERM
+                        + " is not attributed to a cause. An attribution of possible or higher must be selected for at least one of the causes.",
+                container.getMessages().get(ATTRIBUTION_SECTION).get(0).getText());
+    }
+
+}
