6c7da33b153c3dd718e6fd579d16a5b05140b585
Biju Joseph <biju.joseph@semanticbits.com>
2008-05-22 16:09:55 +0000

    CAAERS-139
    Checked in by Sameer.
    The controller was modified to pass the list of the existing meddra versions already imported into the system.
    
    SVN-Revision: 4854

--- projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/admin/ImportMeddraController.java
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/admin/ImportMeddraController.java
@@ -1,6 +1,8 @@
 package gov.nih.nci.cabig.caaers.web.admin;
 
 // import Apache commons
+import gov.nih.nci.cabig.caaers.dao.MeddraVersionDao;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -15,6 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 public class ImportMeddraController extends ParameterizableViewController {
 
     private static Log log = LogFactory.getLog(ImportMeddraController.class);
+    
+    private MeddraVersionDao meddraVersionDao;
 
     public ImportMeddraController() {
         setViewName("admin/meddra_import");
@@ -25,8 +29,13 @@ public class ImportMeddraController extends ParameterizableViewController {
 
         setViewName("admin/meddra_import");
         ModelAndView mav = new ModelAndView("admin/meddra_import");
+        mav.addObject("meddraVersions", meddraVersionDao.getAll());
         log.debug("modelAndView" + mav.getViewName());
 
         return mav;
+    }
+    
+    public void setMeddraVersionDao(MeddraVersionDao meddraVersionDao) {
+        this.meddraVersionDao = meddraVersionDao;
     }
 }
