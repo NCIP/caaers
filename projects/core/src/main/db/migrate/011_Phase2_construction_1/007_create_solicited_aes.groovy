e952ad77207fcf804580cf296c42fb4d03faeefc
Arunkumar Kandregula <kandregula.arunkumar@gmail.com>
2008-05-28 22:36:46 +0000

    CAAERS-201
    SVN-Revision: 4884

--- projects/core/src/main/db/migrate/011_Phase2_construction_1/007_create_solicited_aes.groovy
+++ projects/core/src/main/db/migrate/011_Phase2_construction_1/007_create_solicited_aes.groovy
@@ -8,10 +8,10 @@ class CreateSolicitedAEs extends edu.northwestern.bioinformatics.bering.Migratio
             t.addColumn("grid_id" , "string" , nullable:true)
             
         }
-        execute("ALTER TABLE solicited_events ADD CONSTRAINT fk_solicited_adverse_event_arm_id FOREIGN KEY (arm_id) REFERENCES arms");
-        execute("ALTER TABLE solicited_events ADD CONSTRAINT fk_solicited_adverse_ctc_term_id FOREIGN KEY (ctc_term_id) REFERENCES ctc_terms");
-        execute("ALTER TABLE solicited_events ADD CONSTRAINT fk_solicited_adverse_lowlevel_term_id FOREIGN KEY (lowlevel_term_id) REFERENCES meddra_llt");
-        execute("ALTER TABLE solicited_events ADD CONSTRAINT ck_solicited_adverse_term_id_not_null CHECK( ctc_term_id is not null or lowlevel_term_id is not null )");
+        execute("ALTER TABLE solicited_events ADD CONSTRAINT fk_solicited_event_arm_id FOREIGN KEY (arm_id) REFERENCES arms");
+        execute("ALTER TABLE solicited_events ADD CONSTRAINT fk_solicited_ctc_term_id FOREIGN KEY (ctc_term_id) REFERENCES ctc_terms");
+        execute("ALTER TABLE solicited_events ADD CONSTRAINT fk_solicited_lowlevel_term_id FOREIGN KEY (lowlevel_term_id) REFERENCES meddra_llt");
+        execute("ALTER TABLE solicited_events ADD CONSTRAINT ck_solicited_term_id_not_null CHECK( ctc_term_id is not null or lowlevel_term_id is not null )");
         
     }
     
