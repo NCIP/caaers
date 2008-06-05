c127a8424743e773a47b54f9c3e6c97db00723d5
Biju Joseph <biju.joseph@semanticbits.com>
2008-06-05 12:58:06 +0000

    JIRA_ID : CAAERS-195  The solicitedAE tab is added in CreateStudyController
    SVN-Revision: 4910

---
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/study/SolicitedAdverseEventTab.java
@@ -0,0 +1,12 @@
+package gov.nih.nci.cabig.caaers.web.study;
+/**
+ * This tab captures the solicited adverse event details in the study flow.
+ * @author Biju Joseph
+ * @author Arun Kumar
+ *
+ */
+public class SolicitedAdverseEventTab extends StudyTab {
+	public SolicitedAdverseEventTab() {
+		 super("Solicited adverse events", "SolictedAEs", "study/solicited_ae");
+	}
+}
