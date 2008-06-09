1064cf6ff4404fd4ac293236b857dac462cfc7a6
Biju Joseph <biju.joseph@semanticbits.com>
2008-06-09 19:30:05 +0000

    caaers-tags.tld renaming
    SVN-Revision: 4937

---
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/tags/ValueTag.java
@@ -0,0 +1,30 @@
+package gov.nih.nci.cabig.caaers.web.tags;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspTagException;
+
+import org.springframework.web.servlet.tags.form.AbstractDataBoundFormElementTag;
+import org.springframework.web.servlet.tags.form.TagWriter;
+/**
+ * This tag will print the value of a path evaluated against the command object.
+ * &lt;caaersTag:value path=&quots;....&quots; /&gt;
+ * 
+ * @author Biju Joseph
+ * 
+ */
+public class ValueTag extends AbstractDataBoundFormElementTag {
+	
+	 @Override
+	protected int writeTagContent(TagWriter writer) throws JspException {
+		try {
+			pageContext.getOut().write(getDisplayString(getBoundValue(), getPropertyEditor()));
+		} catch (IOException e) {
+			throw new JspTagException(e.getMessage(),e);
+		}
+		return EVAL_PAGE;
+	}
+	
+
+}
