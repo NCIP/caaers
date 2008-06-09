5b759d9a5873d401aab2638234b91c3d09f522a4
Biju Joseph <biju.joseph@semanticbits.com>
2008-06-09 22:21:54 +0000

    CAAERS-230
    Checked in by Sameer.
    
    SVN-Revision: 4969

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/repository/AdverseEventReportingPeriodRepository.java
@@ -0,0 +1,27 @@
+package gov.nih.nci.cabig.caaers.domain.repository;
+
+import gov.nih.nci.cabig.caaers.dao.AdverseEventReportingPeriodDao;
+import gov.nih.nci.cabig.caaers.domain.AdverseEventReportingPeriod;
+import gov.nih.nci.cabig.caaers.domain.Study;
+
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Sameer Sawant
+ */
+@Transactional(readOnly = true)
+public class AdverseEventReportingPeriodRepository {
+	
+	AdverseEventReportingPeriodDao reportingPeriodDao;
+	 /**
+     * Saves a AdverseEventReportingPeriod object
+     *
+     * @param reportingPeriod the AdverseEventReportingPeriod object
+     * @throws Exception runtime exception object
+     */
+
+    @Transactional(readOnly = false)
+    public void save(AdverseEventReportingPeriod reportingPeriod) throws Exception {
+    	reportingPeriodDao.save(reportingPeriod);
+    }
+}
