6329ccb268d1e806209d5247714d55b330135218
Srini Akkala <akkalas@gmail.com>
2008-06-13 12:08:52 +0000

    SVN-Revision: 5020

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/api/InvestigatorMigratorService.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/api/InvestigatorMigratorService.java
@@ -1,5 +1,9 @@
 package gov.nih.nci.cabig.caaers.api;
 
-public interface InvestigatorMigratorService {
+import gov.nih.nci.cabig.caaers.integration.schema.investigator.Staff;
 
+import java.rmi.RemoteException;
+
+public interface InvestigatorMigratorService {
+	public void saveInvestigator(Staff staff) throws RemoteException;
 }
