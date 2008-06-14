bfadcc3eb2e6939982593aac16ca3bc62898f330
Biju Joseph <biju.joseph@semanticbits.com>
2008-06-14 01:22:25 +0000

    CAAERS-230
    Checked in by Sameer.
    
    SVN-Revision: 5036

---
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/fields/MultipleFieldGroupFactory.java
@@ -0,0 +1,32 @@
+package gov.nih.nci.cabig.caaers.web.fields;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class MultipleFieldGroupFactory extends RepeatingFieldGroupFactory{
+	private List<InputFieldGroup> inputFieldGroup = new LinkedList<InputFieldGroup>();
+
+    public MultipleFieldGroupFactory(String basename, String listPropertyName) {
+    	super(basename, listPropertyName);
+    }
+
+    
+    public void addFieldGroup(InputFieldGroup fieldGroup){
+    	inputFieldGroup.add(fieldGroup);
+    }
+    
+    public void clearFields(){
+    	baseFields = new LinkedList<InputField>();
+    }
+    
+    public List<InputFieldGroup> getInputFieldGroup() {
+		return inputFieldGroup;
+	}
+    
+    public void setInputFieldGroup(List<InputFieldGroup> inputFieldGroup) {
+		this.inputFieldGroup = inputFieldGroup;
+	}
+}
