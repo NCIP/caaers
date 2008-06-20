e1517a55cd7f7dcafc78da3836a65489a5f6311e
Monish Dombla <monish.dombla@semanticbits.com>
2008-06-20 16:27:13 +0000

    New Interface for Study WebService
    SVN-Revision: 5109

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/api/StudyProcessor.java
@@ -0,0 +1,37 @@
+package gov.nih.nci.cabig.caaers.api;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+/**
+ * Webservice Interface for Study Creation and Updation.
+ * @author Monish Dombla
+ *
+ */
+@WebService(name="StudyProcessorIntf",targetNamespace="http://webservice.caaers.cabig.nci.nih.gov")
+public interface StudyProcessor {
+	
+	/**
+	 * This operation will accept a Study which is a jaxb study and creates it.
+	 * @TODO
+	 * This operation should allow for response.
+	 * Need to modify schema for Response
+	 * @param xmlStudy
+	 */
+	@WebMethod
+	public void createStudy(@WebParam(name="Study") gov.nih.nci.cabig.caaers.webservice.Study xmlStudy);
+	
+	
+	
+	/**
+	 * This operation will accept a Study which is a jaxb Study and updates it.
+	 * @TODO
+	 * This operation should allow for response.
+	 * Need to modify schema for Response
+	 * @param xmlStudy
+	 */
+	@WebMethod
+	public void updateStudy(@WebParam(name="Study") gov.nih.nci.cabig.caaers.webservice.Study xmlStudy);
+
+}
