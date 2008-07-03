f932277faef27e16b6f8696144948c3165e76c4c
Srini Akkala <akkalas@gmail.com>
2008-07-03 15:13:20 +0000

    SVN-Revision: 5236

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/api/InvestigatorMigratorServiceTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/api/InvestigatorMigratorServiceTest.java
@@ -1,96 +1,108 @@
 package gov.nih.nci.cabig.caaers.api;
 
 import gov.nih.nci.cabig.caaers.CaaersDbTestCase;
-import gov.nih.nci.cabig.caaers.CaaersSystemException;
+import gov.nih.nci.cabig.caaers.dao.InvestigatorDao;
+import gov.nih.nci.cabig.caaers.dao.query.InvestigatorQuery;
+import gov.nih.nci.cabig.caaers.domain.Identifier;
+import gov.nih.nci.cabig.caaers.domain.Investigator;
+import gov.nih.nci.cabig.caaers.domain.Organization;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-
-import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Source;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public class InvestigatorMigratorServiceTest extends CaaersDbTestCase {
 
-    public String getTestDataFileName() {
-        String fileName = "testdata/InvestigatorMigratorServiceTest.xml";
-        return fileName;
-    }
+	private InvestigatorMigratorService svc = null;
+	private JAXBContext jaxbContext = null;
+	private Unmarshaller unmarshaller = null;
+	private gov.nih.nci.cabig.caaers.integration.schema.investigator.Staff staff = null;
+	private File xmlFile = null;
+	private InvestigatorDao investigatorDao = null;
+	Identifier identifier = null;
+	Organization organization = null;
+	Investigator updatedInvestigator = null;
 
-    private void validateAgainstSchema(File xmlFile, String xsdUrl) {
-        try {
-            // parse an XML document into a DOM tree
-            DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            Document document = parser.parse(xmlFile);
-
-            // create a SchemaFactory capable of understanding WXS schemas
-            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-
-            // load a WXS schema, represented by a Schema instance
-             Source schemaFile = new StreamSource(new File(xsdUrl));
-            
-            //Source schemaFile = new StreamSource(getApplicationContext().getResource(xsdUrl)
-              //              .getFile());
-            Schema schema = factory.newSchema(schemaFile);
-
-            // create a Validator instance, which can be used to validate an instance document
-            Validator validator = schema.newValidator();
-
-            // validate the DOM tree
-
-            validator.validate(new DOMSource(document));
-            // return xmlFile;
-        } catch (FileNotFoundException ex) {
-            throw new CaaersSystemException("File Not found Exception", ex);
-        } catch (IOException ioe) {
-            throw new CaaersSystemException(ioe);
-        } catch (SAXParseException spe) {
-        	spe.printStackTrace();
-        	throw new CaaersSystemException(spe);
-        } catch (SAXException e) {        	
-            throw new CaaersSystemException(e);
-        } catch (ParserConfigurationException pce) {
-            throw new CaaersSystemException("Parser configuration exception ", pce);
-        }
-        
-        System.out.println("VALIDATED - SCHEMA");
-    }
-
-    
-	public void testSaveInvestigator() {
-//		InvestigatorMigratorService svc = (InvestigatorMigratorService) getApplicationContext()
-//        .getBean("investigatorMigratorService");
-//
-//		try {
-//			File xmlFile = new File ("/Users/sakkala/tech/caaers/InvestigatorMigratorServiceTest0.xml");//getResources("/schema/integration/investigator.xml")[0].getFile();
-//
-//			validateAgainstSchema(xmlFile,"/Users/sakkala/tech-workspace/caaers12/core/src/main/resources/schema/integration/Investigator.xsd");
-//			/*
-//			JAXBContext jaxbContext = JAXBContext.newInstance("gov.nih.nci.cabig.caaers.integration.schema.investigator");
-//			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-//			Staff staff = (Staff)unmarshaller.unmarshal(xmlFile);
-//
-//			svc.saveInvestigator(staff);
-//			*/
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//			fail("Error running test: " + e.getMessage());
-//		}
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		jaxbContext = JAXBContext.newInstance("gov.nih.nci.cabig.caaers.integration.schema.investigator");
+		unmarshaller = jaxbContext.createUnmarshaller();
+		svc = (InvestigatorMigratorService)getDeployedApplicationContext().getBean("InvestigatorMigratorService");
+		investigatorDao = (InvestigatorDao)getDeployedApplicationContext().getBean("investigatorDao");
+		/*
+		updatedInvestigator = fetchInvestigator("nci_identifier");
+		if(updatedInvestigator != null){
+			investigatorDao.delete(updatedInvestigator);
+		}
+		createInvestigator();
+		*/
 	}
+
+	/**
+     * Fetches the research staff from the DB
+     * 
+     * @param nciCode
+     * @return
+     */
+	Investigator fetchInvestigator(String nciIdentifier) {
+    	InvestigatorQuery invQuery = new InvestigatorQuery();
+        if (StringUtils.isNotEmpty(nciIdentifier)) {
+        	invQuery.filterByNciIdentifier(nciIdentifier);
+        	
+        }
+        List<Investigator> rsList = investigatorDao.searchInvestigator(invQuery);
+        
+        if (rsList == null || rsList.isEmpty()) {
+            return null;
+        }
+        return rsList.get(0);
+    }
+	
+	public void testInvestigatorSave(){
+		try {
+			//Create or update , whatever it is new data will be populated ..
+			xmlFile = getResources("classpath*:gov/nih/nci/cabig/caaers/impl/studydata/CreateInvestigatorTest.xml")[0].getFile();
+			staff = (gov.nih.nci.cabig.caaers.integration.schema.investigator.Staff)unmarshaller.unmarshal(xmlFile);
+			svc.saveInvestigator(staff);	
+			
+			//update with modified data ..
+			xmlFile = getResources("classpath*:gov/nih/nci/cabig/caaers/impl/studydata/UpdateInvestigatorTest.xml")[0].getFile();
+			staff = (gov.nih.nci.cabig.caaers.integration.schema.investigator.Staff)unmarshaller.unmarshal(xmlFile);
+			svc.saveInvestigator(staff);
+			
+			updatedInvestigator = fetchInvestigator("sr-1");
+			
+			assertNotNull(updatedInvestigator);
+			
+			assertEquals("101-202-3333", updatedInvestigator.getFaxNumber());
+			assertEquals("101-202-4444", updatedInvestigator.getPhoneNumber());
+			
+			updatedInvestigator = fetchInvestigator("nci-id-2");
+			
+			assertNotNull(updatedInvestigator);
+			
+			assertEquals("870-098-0000", updatedInvestigator.getFaxNumber());
+			assertEquals("908-098-0000", updatedInvestigator.getPhoneNumber());
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Error running test: " + e.getMessage());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+			fail("Error running test: " + e.getMessage());
+		}		
+	}
+	
 	private static Resource[] getResources(String pattern) throws IOException {
         ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         Resource[] resources = resolver.getResources(pattern);
