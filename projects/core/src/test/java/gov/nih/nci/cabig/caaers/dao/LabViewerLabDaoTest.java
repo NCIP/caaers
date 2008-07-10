8e1ba94487d6644185ed987d3d09aea1092a04ac
Srini Akkala <akkalas@gmail.com>
2008-07-10 19:07:41 +0000

    SVN-Revision: 5300

---
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/LabViewerLabDaoTest.java
@@ -0,0 +1,71 @@
+package gov.nih.nci.cabig.caaers.dao;
+
+import gov.nih.nci.cabig.caaers.DaoTestCase;
+import gov.nih.nci.cabig.caaers.domain.Identifier;
+import gov.nih.nci.cabig.caaers.domain.LabViewerLab;
+import gov.nih.nci.cabig.caaers.domain.Participant;
+import gov.nih.nci.cabig.caaers.domain.Study;
+import gov.nih.nci.cabig.caaers.domain.StudyParticipantAssignment;
+
+public class LabViewerLabDaoTest extends DaoTestCase<LabViewerLabDao> {
+	
+	private LabViewerLabDao labViewerLabDao;
+	//private LabViewerLabRepository labViewerLabRepository;
+	private StudyParticipantAssignmentDao studyParticipantAssignmentDao;
+	private ParticipantDao participantDao;
+	private StudyDao studyDao;
+	
+	//private TransactionTemplate transactionTemplate;
+	
+	@Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        labViewerLabDao = getDao();
+
+        //transactionTemplate = (TransactionTemplate) getApplicationContext().getBean(
+          //              "transactionTemplate");
+        
+        studyParticipantAssignmentDao = (StudyParticipantAssignmentDao) getApplicationContext().getBean(
+                        "studyParticipantAssignmentDao");
+        participantDao = (ParticipantDao) getApplicationContext().getBean("participantDao");
+        
+        studyDao = (StudyDao)getApplicationContext().getBean("studyDao");
+        
+        //labViewerLabRepository = (LabViewerLabRepository)getApplicationContext().getBean("labViewerLabRepository");
+        //labViewerLabRepository
+        
+    }
+	
+	public void testSave() {
+
+		LabViewerLab toCreate = new LabViewerLab();
+		//expectCreate(toCreate);
+		toCreate.setName("TEST NAME 3");
+		toCreate.setResult("100.2");
+		toCreate.setUnits("cpq");
+		Identifier i = new Identifier();
+		i.setType("MRN");
+		i.setValue("mrn001");
+
+		Identifier i2 = new Identifier();
+		i2.setType("Protocol Authority Identifier");
+		i2.setValue("RTOG-0330");
+		
+		Participant p = participantDao.getByIdentifier(i);
+        Study s = studyDao.getByIdentifier(i2);
+        
+        System.out.println(p.getFirstName());
+        System.out.println(s.getShortTitle());
+		
+		StudyParticipantAssignment assignment = studyParticipantAssignmentDao.getAssignment(p,s);
+
+		toCreate.setAssignment(assignment);
+		labViewerLabDao.save(toCreate);
+		
+		
+
+        //assertNotNull("report id is null", toCreate.getId());
+        //assertNotNull("report version id is null", rs.getReportVersions().get(0).getId());
+    }
+
+}
