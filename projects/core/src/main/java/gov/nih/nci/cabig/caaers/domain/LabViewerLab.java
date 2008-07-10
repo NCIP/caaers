385203e033e986b62a03504c44e9567a7770e148
Srini Akkala <akkalas@gmail.com>
2008-07-10 19:04:59 +0000

    SVN-Revision: 5295

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/LabViewerLab.java
@@ -0,0 +1,120 @@
+package gov.nih.nci.cabig.caaers.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+/**
+ * This class represents the LabViewerLab domain object associated with the Adverse event report.
+ * 
+ * @author Srini Akkala
+ */
+@Entity
+@Table(name = "labviewer_labs")
+@GenericGenerator(name = "id-generator", strategy = "native", parameters = { @Parameter(name = "sequence", value = "seq_labviewer_labs_id") })
+public class LabViewerLab extends AbstractExpeditedReportCollectionElementChild {
+ 
+	private String name;
+
+    private String units; 
+
+    private String result;
+
+    private boolean  dismissed;
+
+    private Date labDate;
+    
+    private StudyParticipantAssignment assignment;
+
+    // //// BEAN PROPERTIES
+
+    public String getUnits() {
+        return units;
+    }
+
+    public void setUnits(String units) {
+        this.units = units;
+    }
+
+    public Date getLabDate() {
+        return labDate;
+    }
+
+    public void setLabDate(Date labDate) {
+        this.labDate = labDate;
+    }
+
+	public boolean isDismissed() {
+		return dismissed;
+	}
+
+	public void setDismissed(boolean dismissed) {
+		this.dismissed = dismissed;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+    public String getName() {
+		return name;
+	}
+    
+    @Transient
+    public Participant getParticipant() {
+        return getAssignment() == null ? null : getAssignment().getParticipant();
+    }
+
+    @Transient
+    public Study getStudy() {
+        StudySite ss = getAssignment() == null ? null : getAssignment().getStudySite();
+        return ss == null ? null : ss.getStudy();
+    }
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public StudyParticipantAssignment getAssignment() {
+        return assignment;
+    }
+    
+    public void setAssignment(StudyParticipantAssignment assignment) {
+        this.assignment = assignment;
+    }
+    
+	@Override
+    public int hashCode() {
+        return ("" + getName() ).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+
+        if (obj == null) return false;
+
+        if (!(obj instanceof Lab)) return false;
+
+        LabViewerLab l = (LabViewerLab) obj;
+        if (!StringUtils.equals(name, l.name)) return false;
+
+        if (l.units != null && !units.equals(l.units)) return false;
+        if (l.result != null && !result.equals(l.result)) return false;
+
+        return true;
+    }
+}
