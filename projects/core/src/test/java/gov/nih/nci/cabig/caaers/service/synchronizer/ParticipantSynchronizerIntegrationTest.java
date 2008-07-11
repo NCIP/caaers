10c71550a9b129f26decc41ea212ed8bda6c2a26
Monish Dombla <monish.dombla@semanticbits.com>
2008-07-11 15:06:11 +0000

    New Test Cases added
    SVN-Revision: 5317

---
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/service/synchronizer/ParticipantSynchronizerIntegrationTest.java
@@ -0,0 +1,127 @@
+package gov.nih.nci.cabig.caaers.service.synchronizer;
+
+import gov.nih.nci.cabig.caaers.AbstractTestCase;
+import gov.nih.nci.cabig.caaers.domain.DateValue;
+import gov.nih.nci.cabig.caaers.domain.Fixtures;
+import gov.nih.nci.cabig.caaers.domain.Identifier;
+import gov.nih.nci.cabig.caaers.domain.Organization;
+import gov.nih.nci.cabig.caaers.domain.OrganizationAssignedIdentifier;
+import gov.nih.nci.cabig.caaers.domain.Participant;
+import gov.nih.nci.cabig.caaers.domain.Study;
+import gov.nih.nci.cabig.caaers.domain.StudyParticipantAssignment;
+import gov.nih.nci.cabig.caaers.domain.StudySite;
+import gov.nih.nci.cabig.caaers.service.DomainObjectImportOutcome;
+import gov.nih.nci.cabig.caaers.service.migrator.Migrator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParticipantSynchronizerIntegrationTest extends AbstractTestCase {
+	
+	Participant dbParticipant,xmlParticipant;
+	StudyParticipantAssignmentSynchronizer studyParticipantAssignmentSynchronizer;
+	IdentifierSynchronizer<Participant> identifierSynchronizer;
+	ParticipantSynchronizer participantSynchronizer;
+	DomainObjectImportOutcome<Participant> outcome;
+	
+	StudyParticipantAssignment studyParticipantAssignment1,studyParticipantAssignment2,studyParticipantAssignment1a,
+							   studyParticipantAssignment2a,studyParticipantAssignment3a;
+	StudySite studySite1,studySite2,studySite1a,studySite2a;
+	Study study1,study2,study1a,study2a;
+	Identifier identifier1,identifier1a,identifier2a ;
+	Organization organization1,organization1a,organization2a;
+	OrganizationAssignedIdentifier orgIdentifier1,orgIdentifier1a,orgIdentifier2a;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		studyParticipantAssignmentSynchronizer = new StudyParticipantAssignmentSynchronizer();
+		identifierSynchronizer = new IdentifierSynchronizer<Participant>();
+		List<Migrator<Participant>> syncronizers = new ArrayList<Migrator<Participant>>();
+		syncronizers.add(identifierSynchronizer);
+		syncronizers.add(studyParticipantAssignmentSynchronizer);
+		participantSynchronizer = new ParticipantSynchronizer(syncronizers);
+		outcome = new DomainObjectImportOutcome<Participant>();
+	}
+	
+	public void testSync(){
+		study1 = Fixtures.createStudy("abc");
+		identifier1 = new Identifier();
+		identifier1.setType("type");
+		identifier1.setValue("value");
+		study1.addIdentifier(identifier1);
+		organization1 = Fixtures.createOrganization("organization");
+		studySite1 = new StudySite();
+		studySite1.setStudy(study1);
+		studySite1.setOrganization(organization1);
+		studySite1.setId(1);
+		orgIdentifier1 = Fixtures.createOrganizationAssignedIdentifier("1", organization1);
+		orgIdentifier1.setId(1);
+		dbParticipant = Fixtures.createParticipant("first", "last");
+		dbParticipant.setId(1);
+		dbParticipant.addIdentifier(orgIdentifier1);
+		studyParticipantAssignment1 = new StudyParticipantAssignment();
+		studyParticipantAssignment1.setStudySite(studySite1);
+		studyParticipantAssignment1.setId(1);
+		dbParticipant.addAssignment(studyParticipantAssignment1);
+		
+		List<StudyParticipantAssignment> assignmentList = new ArrayList<StudyParticipantAssignment>();
+		study1a = Fixtures.createStudy("abc");
+		identifier1a = new Identifier();
+		identifier1a.setType("type");
+		identifier1a.setValue("value");
+		study1a.addIdentifier(identifier1a);
+		organization1a = Fixtures.createOrganization("organization");
+		studySite1a = new StudySite();
+		studySite1a.setStudy(study1a);
+		studySite1a.setOrganization(organization1a);
+		orgIdentifier1a = Fixtures.createOrganizationAssignedIdentifier("1", organization1);
+		orgIdentifier2a = Fixtures.createOrganizationAssignedIdentifier("2", organization2a);
+		xmlParticipant = Fixtures.createParticipant("first", "last");
+		xmlParticipant.addIdentifier(orgIdentifier1a);
+		xmlParticipant.addIdentifier(orgIdentifier2a);
+		studyParticipantAssignment1a = new StudyParticipantAssignment();
+		studyParticipantAssignment1a.setStudySite(studySite1a);
+		assignmentList.add(studyParticipantAssignment1a);
+		
+		study2a = Fixtures.createStudy("abc2");
+		identifier2a = new Identifier();
+		identifier2a.setType("type1");
+		identifier2a.setValue("value1");
+		study2a.addIdentifier(identifier2a);
+		organization2a = Fixtures.createOrganization("organization2");
+		studySite2a = new StudySite();
+		studySite2a.setStudy(study2a);
+		studySite2a.setOrganization(organization2a);
+		studyParticipantAssignment2a = new StudyParticipantAssignment();
+		studyParticipantAssignment2a.setStudySite(studySite2a);
+		assignmentList.add(studyParticipantAssignment2a);
+		xmlParticipant.setAssignments(assignmentList);
+		
+		xmlParticipant.setMiddleName("MiddleName");
+		xmlParticipant.setMaidenName("MaidenName");
+		xmlParticipant.setGender("Male");
+		xmlParticipant.setEthnicity("Ethnicity");
+		xmlParticipant.setRace("Asian");
+		xmlParticipant.setDateOfBirth(new DateValue(11,11,1965));
+		
+		assertEquals(1, dbParticipant.getIdentifiers().size());
+		assertEquals(2, xmlParticipant.getIdentifiers().size());
+		
+		assertEquals(1, dbParticipant.getAssignments().size());
+		assertEquals(2, xmlParticipant.getAssignments().size());
+		
+		participantSynchronizer.migrate(dbParticipant, xmlParticipant, outcome);
+		
+		assertEquals("MiddleName", dbParticipant.getMiddleName());
+		assertEquals("MaidenName", dbParticipant.getMaidenName());
+		assertEquals("Male", dbParticipant.getGender());
+		assertEquals("Ethnicity", dbParticipant.getEthnicity());
+		assertEquals("Asian", dbParticipant.getRace());
+		assertEquals(new DateValue(11,11,1965), dbParticipant.getDateOfBirth());
+		assertEquals(2, dbParticipant.getIdentifiers().size());
+		assertEquals(2, dbParticipant.getAssignments().size());
+		
+	}
+
+}
---
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/service/synchronizer/ParticipantSynchronizerTest.java
@@ -0,0 +1,55 @@
+package gov.nih.nci.cabig.caaers.service.synchronizer;
+
+import gov.nih.nci.cabig.caaers.AbstractTestCase;
+import gov.nih.nci.cabig.caaers.domain.DateValue;
+import gov.nih.nci.cabig.caaers.domain.Fixtures;
+import gov.nih.nci.cabig.caaers.domain.Participant;
+import gov.nih.nci.cabig.caaers.service.DomainObjectImportOutcome;
+import gov.nih.nci.cabig.caaers.service.migrator.Migrator;
+
+import java.util.ArrayList;
+
+public class ParticipantSynchronizerTest extends AbstractTestCase {
+	
+	Participant dbParticipant;
+	Participant xmlParticipant;
+	ParticipantSynchronizer participantSynchronizer;
+	DomainObjectImportOutcome<Participant> outcome;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		participantSynchronizer = new ParticipantSynchronizer(new ArrayList<Migrator<Participant>>());
+		outcome = new DomainObjectImportOutcome<Participant>();
+	}
+	
+	public void testParticipantAttrUpdate(){
+		dbParticipant = Fixtures.createParticipant("FirstName", "LastName");
+		dbParticipant.setMiddleName("MiddleName");
+		dbParticipant.setMaidenName("MaidenName");
+		dbParticipant.setDateOfBirth(new DateValue(12,12,1970));
+		dbParticipant.setEthnicity("Ethnicity");
+		dbParticipant.setGender("Gender");
+		dbParticipant.setRace("Race");
+		
+		xmlParticipant = Fixtures.createParticipant("FirstName Updated", "LastName Updated");
+		xmlParticipant.setMiddleName("MiddleName Updated");
+		xmlParticipant.setMaidenName("MaidenName Updated");
+		xmlParticipant.setDateOfBirth(new DateValue(11,11,1971));
+		xmlParticipant.setEthnicity("Ethnicity Updated");
+		xmlParticipant.setGender("Gender Updated");
+		xmlParticipant.setRace("Race Updated");
+		
+		participantSynchronizer.migrate(dbParticipant, xmlParticipant, outcome);
+		
+		assertEquals("FirstName Updated", dbParticipant.getFirstName());
+		assertEquals("LastName Updated", dbParticipant.getLastName());
+		assertEquals("MiddleName Updated", dbParticipant.getMiddleName());
+		assertEquals("MaidenName Updated", dbParticipant.getMaidenName());
+		assertEquals("Ethnicity Updated", dbParticipant.getEthnicity());
+		assertEquals("Gender Updated", dbParticipant.getGender());
+		assertEquals("Race Updated", dbParticipant.getRace());
+		assertEquals(new DateValue(11,11,1971),dbParticipant.getDateOfBirth());
+	}
+	
+}
