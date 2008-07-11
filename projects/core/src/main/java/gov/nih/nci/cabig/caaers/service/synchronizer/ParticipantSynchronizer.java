6285495f860aca77821cd3f25470c32cc5bb290f
Monish Dombla <monish.dombla@semanticbits.com>
2008-07-11 15:05:11 +0000

    New class for participant Sync
    SVN-Revision: 5316

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/service/synchronizer/ParticipantSynchronizer.java
@@ -0,0 +1,30 @@
+package gov.nih.nci.cabig.caaers.service.synchronizer;
+
+import java.util.List;
+
+import gov.nih.nci.cabig.caaers.domain.Participant;
+import gov.nih.nci.cabig.caaers.service.DomainObjectImportOutcome;
+import gov.nih.nci.cabig.caaers.service.migrator.CompositeMigrator;
+import gov.nih.nci.cabig.caaers.service.migrator.Migrator;
+
+public class ParticipantSynchronizer extends CompositeMigrator<Participant>{
+
+	public ParticipantSynchronizer(List<Migrator<Participant>> synchronizers) {
+		super(synchronizers);
+	}
+	
+	@Override
+	public void preMigrate(Participant dbParticipant, Participant xmlParticipant,
+			DomainObjectImportOutcome<Participant> outcome) {
+		
+		dbParticipant.setFirstName(xmlParticipant.getFirstName());
+		dbParticipant.setLastName(xmlParticipant.getLastName());
+		dbParticipant.setMiddleName(xmlParticipant.getMiddleName());
+		dbParticipant.setMaidenName(xmlParticipant.getMaidenName());
+		dbParticipant.setDateOfBirth(xmlParticipant.getDateOfBirth());
+		dbParticipant.setGender(xmlParticipant.getGender());
+		dbParticipant.setEthnicity(xmlParticipant.getEthnicity());
+		dbParticipant.setRace(xmlParticipant.getRace());
+	}
+
+}
---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/service/synchronizer/StudyParticipantAssignmentSynchronizer.java
@@ -0,0 +1,102 @@
+package gov.nih.nci.cabig.caaers.service.synchronizer;
+
+import gov.nih.nci.cabig.caaers.domain.Identifier;
+import gov.nih.nci.cabig.caaers.domain.Participant;
+import gov.nih.nci.cabig.caaers.domain.StudyParticipantAssignment;
+import gov.nih.nci.cabig.caaers.service.DomainObjectImportOutcome;
+import gov.nih.nci.cabig.caaers.service.migrator.Migrator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudyParticipantAssignmentSynchronizer implements Migrator<Participant>{
+
+	public void migrate(Participant dbParticipant, Participant xmlParticipant,
+			DomainObjectImportOutcome<Participant> outcome) {
+		
+		
+		List<StudyParticipantAssignment> newStudyParticipantAssignmentList = new ArrayList<StudyParticipantAssignment>();
+		List<StudyParticipantAssignment> deleteStudyParticipantAssignmentList = new ArrayList<StudyParticipantAssignment>();
+		StudyParticipantAssignment remStudyParticipantAssignment = null;
+		
+		
+		//Identify New StudyParticipantAssignment .
+		for(StudyParticipantAssignment xmlStudyParticipantAssignment : xmlParticipant.getAssignments()){
+			for(StudyParticipantAssignment dbStudyParticipantAssignment : dbParticipant.getAssignments()){
+				xmlStudyParticipantAssignment.setId(dbStudyParticipantAssignment.getId());
+				List<Identifier> xmlIdentifiers = xmlStudyParticipantAssignment.getStudySite().getStudy().getIdentifiers();
+				List<Identifier> dbIdentifiers = dbStudyParticipantAssignment.getStudySite().getStudy().getIdentifiers();
+				boolean studyIdentifierMatchFound = matchIdentifiers(xmlIdentifiers, dbIdentifiers);
+				String xmlOrgName = xmlStudyParticipantAssignment.getStudySite().getOrganization().getName();
+				String dbOrgName = dbStudyParticipantAssignment.getStudySite().getOrganization().getName();
+				String xmlNciInstCode = xmlStudyParticipantAssignment.getStudySite().getOrganization().getNciInstituteCode();
+				String dbNciInstCode = dbStudyParticipantAssignment.getStudySite().getOrganization().getNciInstituteCode();
+				
+				if(studyIdentifierMatchFound && (xmlOrgName.equals(dbOrgName) || xmlNciInstCode.equals(dbNciInstCode))){
+					break;
+				}else{
+					xmlStudyParticipantAssignment.setId(null);
+				}
+			}
+			if(xmlStudyParticipantAssignment.getId() == null){
+				newStudyParticipantAssignmentList.add(xmlStudyParticipantAssignment);
+			}
+		}
+		
+		//Identify StudyParticipantAssignment to be Removed
+		for(StudyParticipantAssignment dbStudyParticipantAssignment : dbParticipant.getAssignments()){
+			for(StudyParticipantAssignment xmlStudyParticipantAssignment : xmlParticipant.getAssignments()){
+				remStudyParticipantAssignment = new StudyParticipantAssignment();
+				remStudyParticipantAssignment = dbStudyParticipantAssignment;
+				
+				List<Identifier> xmlIdentifiers = xmlStudyParticipantAssignment.getStudySite().getStudy().getIdentifiers();
+				List<Identifier> dbIdentifiers = remStudyParticipantAssignment.getStudySite().getStudy().getIdentifiers();
+				boolean studyIdentifierMatchFound = matchIdentifiers(xmlIdentifiers, dbIdentifiers);
+				String xmlOrgName = xmlStudyParticipantAssignment.getStudySite().getOrganization().getName();
+				String dbOrgName = remStudyParticipantAssignment.getStudySite().getOrganization().getName();
+				String xmlNciInstCode = xmlStudyParticipantAssignment.getStudySite().getOrganization().getNciInstituteCode();
+				String dbNciInstCode = remStudyParticipantAssignment.getStudySite().getOrganization().getNciInstituteCode();
+				
+				if(studyIdentifierMatchFound && (xmlOrgName.equals(dbOrgName) || xmlNciInstCode.equals(dbNciInstCode))){
+					remStudyParticipantAssignment = null;
+					break;
+				}
+			}
+			if(remStudyParticipantAssignment != null){
+				deleteStudyParticipantAssignmentList.add(remStudyParticipantAssignment);
+			}
+		}
+		
+		//Add New StudyParticipantAssignment
+		for(StudyParticipantAssignment newStudyParticipantAssignment : newStudyParticipantAssignmentList){
+			dbParticipant.getAssignments().add(newStudyParticipantAssignment);
+		}
+		//Remove StudyParticipantAssignment
+		for(StudyParticipantAssignment delStudyParticipantAssignment : deleteStudyParticipantAssignmentList){
+			dbParticipant.getAssignments().remove(delStudyParticipantAssignment);
+		}
+		
+		//Need to set the Study for the update to function
+		for(StudyParticipantAssignment studyParticipantAssignment : dbParticipant.getAssignments()){
+			studyParticipantAssignment.setParticipant(dbParticipant);
+		}
+	}
+	
+	public boolean matchIdentifiers(List<Identifier> xmlIdentifiers ,List<Identifier> dbIdentifiers){
+		boolean matchFound = false;
+		
+		for(Identifier xmlIdentifer : xmlIdentifiers){
+				for(Identifier dbIdentifer : dbIdentifiers){
+						xmlIdentifer.setId(dbIdentifer.getId());
+						if(xmlIdentifer.equals(dbIdentifer)){
+							matchFound = true;
+							return matchFound;
+						}else{
+							xmlIdentifer.setId(null);
+						}
+				}
+		}
+		return matchFound;
+	}
+
+}
