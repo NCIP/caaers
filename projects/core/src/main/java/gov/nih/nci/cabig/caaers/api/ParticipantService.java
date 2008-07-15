3b26491ad485fec340bf2ba8d4d2809b23fc96a9
Monish Dombla <monish.dombla@semanticbits.com>
2008-07-15 13:29:25 +0000

    Added for participant import
    SVN-Revision: 5331

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/api/ParticipantService.java
@@ -0,0 +1,32 @@
+package gov.nih.nci.cabig.caaers.api;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+/**
+ * Webservice Interface for Participant Creation and Updation.
+ * @author Monish Dombla
+ *
+ */
+@WebService(name="ParticipantServiceIntf",targetNamespace="http://webservice.caaers.cabig.nci.nih.gov/participant")
+public interface ParticipantService {
+	
+	
+	/**
+	 * This operation will accept a Participant which is a jaxb study and creates it.
+	 * @param xmlParticipant
+	 */
+	@WebMethod
+	public gov.nih.nci.cabig.caaers.webservice.participant.ParticipantServiceResponse createParticipant(@WebParam(name="ParticipantType") gov.nih.nci.cabig.caaers.webservice.participant.ParticipantType xmlParticipant);
+	
+	
+	
+	/**
+	 * This operation will accept a Participant which is a jaxb Participant and updates it.
+	 * @param xmlParticipant
+	 */
+	@WebMethod
+	public gov.nih.nci.cabig.caaers.webservice.participant.ParticipantServiceResponse updateParticipant(@WebParam(name="ParticipantType") gov.nih.nci.cabig.caaers.webservice.participant.ParticipantType xmlParticipant);
+
+}
