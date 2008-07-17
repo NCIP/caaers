6798a116d72f6f4959ed14bee2f1e7e61631ce20
Arunkumar Kandregula <kandregula.arunkumar@gmail.com>
2008-07-17 15:16:22 +0000

    CAAERS-208  ARUN KUMAR K
    SVN-Revision: 5375

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/Epoch.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/Epoch.java
@@ -24,7 +24,7 @@ public class Epoch  extends AbstractMutableDomainObject {
 	
 	private String name;
 	
-	private String descriptionText = "Add instructions here...";
+	private String descriptionText = "";
 	
 	private Integer epochOrder;
 	
