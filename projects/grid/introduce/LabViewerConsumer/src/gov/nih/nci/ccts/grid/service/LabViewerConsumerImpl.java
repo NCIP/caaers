3a6d0bcf1ca7c3fa432ae7b6c2eb0c10d521f7e1
Srini Akkala <akkalas@gmail.com>
2008-07-18 02:41:42 +0000

    SVN-Revision: 5389

---
+++ projects/grid/introduce/LabViewerConsumer/src/gov/nih/nci/ccts/grid/service/LabViewerConsumerImpl.java
@@ -0,0 +1,45 @@
+package gov.nih.nci.ccts.grid.service;
+
+import gov.nih.nci.ccts.grid.common.LabViewerConsumerI;
+
+import java.rmi.RemoteException;
+
+import org.globus.wsrf.config.ContainerConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/** 
+ * TODO:I am the service side implementation class.  IMPLEMENT AND DOCUMENT ME
+ * 
+ * @created by Introduce Toolkit version 1.2
+ * 
+ */
+public class LabViewerConsumerImpl extends LabViewerConsumerImplBase {
+
+	
+	private static final String SPRING_CLASSPATH_EXPRESSION = "springClasspathExpression";
+
+    private static final String DEFAULT_SPRING_CLASSPATH_EXPRESSION = "classpath:applicationContext-labviewer.xml";
+
+	private static final String LABVIEWER_CONSUMER_BEAN_NAME = "labViewerConsumerBeanName";
+
+	private static final String DEFAULT_LABVIEWER_CONSUMER_BEAN_NAME = "labViewerConsumer";
+	
+	private LabViewerConsumerI consumer;
+	    
+	public LabViewerConsumerImpl() throws RemoteException {
+		super();
+        String exp = ContainerConfig.getConfig().getOption(SPRING_CLASSPATH_EXPRESSION,DEFAULT_SPRING_CLASSPATH_EXPRESSION);
+        String bean = ContainerConfig.getConfig().getOption(LABVIEWER_CONSUMER_BEAN_NAME,DEFAULT_LABVIEWER_CONSUMER_BEAN_NAME);
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(exp);
+        consumer = (LabViewerConsumerI) ctx.getBean(bean);
+	}
+
+	public services.Acknowledgement loadLabs(services.LoadLabsRequest loadLabsRequest) throws RemoteException {
+		//System.out.println("calling Implementation .....");
+		return consumer.loadLabs(loadLabsRequest);
+
+	}
+
+}
+
