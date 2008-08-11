8e39bae049bdefb865d6c9cb92dcb1cb59afd0f2
Biju Joseph <biju.joseph@semanticbits.com>
2008-08-11 21:33:57 +0000

    Fixing the startdate/enddate issue on StudyParticipantConcomitantMedication
    SVN-Revision: 5808

--- projects/core/src/main/db/migrate/011_Phase2_construction_1/023_add_cols_concomitant_med.groovy
+++ projects/core/src/main/db/migrate/011_Phase2_construction_1/023_add_cols_concomitant_med.groovy
@@ -1,13 +1,29 @@
 class AddColsSPAConcomitantMedications extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
-		addColumn("spa_concomitant_medications", "start_date", "date");
-		addColumn("spa_concomitant_medications", "end_date", "date");
-		addColumn("spa_concomitant_medications", "still_taking_medications", "boolean");
+	      	addColumn("spa_concomitant_medications","start_date_day", "integer")
+      		addColumn("spa_concomitant_medications","start_date_month", "integer")
+      		addColumn("spa_concomitant_medications","start_date_year", "integer")
+      		addColumn("spa_concomitant_medications","start_date_zone", "integer", nullable: false, defaultValue: 0)
+      		
+	      	addColumn("spa_concomitant_medications","end_date_day", "integer")
+      		addColumn("spa_concomitant_medications","end_date_month", "integer")
+      		addColumn("spa_concomitant_medications","end_date_year", "integer")
+      		addColumn("spa_concomitant_medications","end_date_zone", "integer", nullable: false, defaultValue: 0)
+      		      		
+			addColumn("spa_concomitant_medications", "still_taking_medications", "boolean");
 	}
 	
 	void down() {
-		dropColumn("spa_concomitant_medications", "start_date");
-		dropColumn("spa_concomitant_medications", "end_date");
+		
+		dropColumn("spa_concomitant_medications", "start_date_day");
+		dropColumn("spa_concomitant_medications", "start_date_month");
+		dropColumn("spa_concomitant_medications", "start_date_year");
+		dropColumn("spa_concomitant_medications", "start_date_zone");
+		dropColumn("spa_concomitant_medications", "end_date_day");
+		
+		dropColumn("spa_concomitant_medications", "end_date_month");
+		dropColumn("spa_concomitant_medications", "end_date_year");
+		dropColumn("spa_concomitant_medications", "end_date_zone");
 		dropColumn("spa_concomitant_medications", "still_taking_medications");
 	}
 }
