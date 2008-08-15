0bdd8ad3dbcd5e2aa11e6ad3dd066533174701c9
Srini Akkala <akkalas@gmail.com>
2008-08-15 06:04:18 +0000

    added start date end date
    SVN-Revision: 5859

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/ConcomitantMedication.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/ConcomitantMedication.java
@@ -1,10 +1,12 @@
 package gov.nih.nci.cabig.caaers.domain;
 
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.Transient;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 /**
  * This class represents the ConcomitantMedication domain object associated with the Adverse event
@@ -16,10 +18,16 @@ import javax.persistence.Transient;
 @GenericGenerator(name = "id-generator", strategy = "native", parameters = { @Parameter(name = "sequence", value = "seq_concomitant_medications_id") })
 public class ConcomitantMedication extends AbstractExpeditedReportCollectionElementChild {
     private String agentName;
+    
+    private Date startDate;
+    
+    private Date endDate;
 
     // //// LOGIC
 
-    @Transient
+    
+
+	@Transient
     public String getName() {
         return agentName;
     }
@@ -33,4 +41,22 @@ public class ConcomitantMedication extends AbstractExpeditedReportCollectionElem
     public void setAgentName(String agentName) {
         this.agentName = agentName;
     }
+    // srini
+    @Transient
+    public Date getEndDate() {
+		return endDate;
+	}
+    @Transient
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+    @Transient
+	public Date getStartDate() {
+		return startDate;
+	}
+    @Transient
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	//srini
 }
