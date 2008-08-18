f188e4b32b381bbf11a42dd860c89d4faceb9436
Monish Dombla <monish.dombla@semanticbits.com>
2008-08-18 21:33:38 +0000

    New Files Added for caAERS Diagnostics
    SVN-Revision: 5891

---
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/admin/DiagnosticsCommand.java
@@ -0,0 +1,80 @@
+package gov.nih.nci.cabig.caaers.web.admin;
+
+import gov.nih.nci.cabig.caaers.tools.configuration.Configuration;
+import gov.nih.nci.cabig.caaers.tools.mail.CaaersJavaMailSender;
+
+
+public class DiagnosticsCommand extends ConfigurationCommand{
+	
+	private boolean smtpTestResult;
+	private String caExchangeHandShakeResult;
+	private String labViewerUrl;
+	private String pscUrl;
+	private String caExchangeUrl;
+	private String smtpHost;
+	private String smtpPort;
+	private Throwable smtpException;
+	private String smtpError;
+	
+	public DiagnosticsCommand(Configuration configuration){
+		super(configuration);
+	}
+
+	public boolean isSmtpTestResult() {
+		return smtpTestResult;
+	}
+
+	public void setSmtpTestResult(boolean smtpTestResult) {
+		this.smtpTestResult = smtpTestResult;
+	}
+
+
+	public String getCaExchangeHandShakeResult() {
+		return caExchangeHandShakeResult;
+	}
+
+	public void setCaExchangeHandShakeResult(String caExchangeHandShakeResult) {
+		this.caExchangeHandShakeResult = caExchangeHandShakeResult;
+	}
+	
+	public String getLabViewerUrl() {
+		labViewerUrl =  super.configuration.get(Configuration.LABVIEWER_BASE_URL);
+		return labViewerUrl;
+	}
+
+	public String getCaExchangeUrl() {
+		caExchangeUrl =  "";
+		return caExchangeUrl;
+	}
+	
+	public String getSmtpHost(){
+		smtpHost =  configuration.get(Configuration.SMTP_ADDRESS);
+		return smtpHost;
+	}
+	
+	public String getSmtpPort(){
+		smtpPort =  "" +configuration.get(Configuration.SMTP_PORT);
+		return smtpPort;
+	}
+
+	public String getPscUrl() {
+		pscUrl = configuration.get(Configuration.PSC_BASE_URL);
+		return pscUrl;
+	}
+
+	public Throwable getSmtpException() {
+		return smtpException;
+	}
+
+	public void setSmtpException(Throwable smtpException) {
+		this.smtpException = smtpException;
+	}
+
+	public String getSmtpError() {
+		if(smtpException != null){
+			smtpError = smtpException.getMessage();
+		}
+		return smtpError;
+	}
+	
+}
---
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/admin/DiagnosticsController.java
@@ -0,0 +1,60 @@
+package gov.nih.nci.cabig.caaers.web.admin;
+
+import gov.nih.nci.cabig.caaers.tools.configuration.Configuration;
+import gov.nih.nci.cabig.caaers.tools.mail.CaaersJavaMailSender;
+
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.servlet.mvc.SimpleFormController;
+
+public class DiagnosticsController extends SimpleFormController{
+	
+	private Configuration configuration;
+	protected CaaersJavaMailSender caaersJavaMailSender;
+	protected final Log log = LogFactory.getLog(getClass());
+	
+    public DiagnosticsController() {
+    	setCommandClass(DiagnosticsCommand.class);
+        setFormView("admin/happy");
+    }
+    
+    @Override
+    protected Object formBackingObject(HttpServletRequest request) throws Exception {
+    	DiagnosticsCommand diagnosticsCommand =  new DiagnosticsCommand(configuration);
+    	diagnosticsCommand.setSmtpTestResult(testSmtp(diagnosticsCommand));
+    	return diagnosticsCommand;
+    }
+
+    public void setConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
+    
+    public void setCaaersJavaMailSender(CaaersJavaMailSender caaersJavaMailSender) {
+		this.caaersJavaMailSender = caaersJavaMailSender;
+	}
+
+	private boolean testSmtp(DiagnosticsCommand diagnosticsCommand) {
+    	boolean testResult = false;
+	    try {
+		    MimeMessage message = caaersJavaMailSender.createMimeMessage();
+		    message.setSubject("Test mail from caAERS Diagnosis");
+		    message.setFrom(new InternetAddress("caaers.app@gmail.com"));
+		
+		    // use the true flag to indicate you need a multipart message
+		    MimeMessageHelper helper = new MimeMessageHelper(message, true);
+		    helper.setTo("admin@semanticbits.com");
+		    caaersJavaMailSender.send(message);
+		    testResult = true;
+		} catch (Exception e) {
+			//log.error(" Error in sending email , please check the confiuration " + e);
+			diagnosticsCommand.setSmtpException(e);
+			testResult = false;
+		}
+		return testResult;
+	}
+}
