076d872de0a801d45040d05ae42e96a52aa5b3f2
Biju Joseph <biju.joseph@semanticbits.com>
2008-08-26 16:28:25 +0000

    RenderDesitionmaker
    SVN-Revision: 6012

---
+++ projects/web/src/test/java/gov/nih/nci/cabig/caaers/web/tags/RenderDecisionManagerTagTest.java
@@ -0,0 +1,29 @@
+package gov.nih.nci.cabig.caaers.web.tags;
+
+import junit.framework.TestCase;
+
+public class RenderDecisionManagerTagTest extends TestCase {
+	RenderDecisionManagerTag tag; 
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		tag = new RenderDecisionManagerTag();
+	}
+	
+	public void testDoStartTag() throws Exception {
+		tag.setElementID("x");
+		tag.setUiType("FIELD");
+		tag.doStartTag();
+		
+	}
+	public void testDoStartTagValidationError() throws Exception {
+		tag.setUiType("junk");
+		tag.setElementID("x");
+		try{
+			tag.doStartTag();
+		}catch(Exception e){
+			assertEquals ("Unknown value for uiType attribute allowed entries are - DIVISION, FIELD" , e.getMessage());
+		}
+	}
+
+}
