cb945bdc0aa916874ff7b722a9be2884792439ed
Biju Joseph <biju.joseph@semanticbits.com>
2008-09-08 20:16:31 +0000

    updated the tabs for ParticipantDetailsTab
    SVN-Revision: 6127

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/domain/TimeValueTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/domain/TimeValueTest.java
@@ -7,11 +7,14 @@ public class TimeValueTest extends TestCase {
 
 	public void testToString() {
 		TimeValue tv = new TimeValue();
-		assertEquals("00:00", tv.toString());
+		tv.setHour(1);
+		tv.setMinute(2);
+		assertEquals("01:02 AM", tv.toString());
 		
 		tv.setHour(12);
 		tv.setMinute(12);
-		assertEquals("12:12", tv.toString());
+		tv.setType(1);
+		assertEquals("12:12 PM", tv.toString());
 	}
 
 }
