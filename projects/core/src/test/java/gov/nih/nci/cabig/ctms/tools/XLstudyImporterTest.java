f4e2ac0f940c6193ae3b60aae89fe58d9aee145e
Monish Dombla <monish.dombla@semanticbits.com>
2008-09-08 20:34:46 +0000

    SVN-Revision: 6129

--- projects/core/src/test/java/gov/nih/nci/cabig/ctms/tools/XLstudyImporterTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/ctms/tools/XLstudyImporterTest.java
@@ -1,24 +1,35 @@
 package gov.nih.nci.cabig.ctms.tools;
 
+import gov.nih.nci.cabig.caaers.dao.StudyDao;
+import gov.nih.nci.cabig.caaers.domain.Identifier;
+import gov.nih.nci.cabig.caaers.domain.Study;
 import gov.nih.nci.cabig.caaers.security.SecurityTestUtils;
 import gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo;
 import gov.nih.nci.security.acegi.csm.authorization.AuthorizationSwitch;
-import junit.framework.Test;
-import junit.framework.TestSuite;
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Required;
-import org.springframework.mock.jndi.SimpleNamingContextBuilder;
-import org.springframework.test.AbstractTransactionalSpringContextTests;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
 import javax.naming.NamingException;
-import java.io.File;
-import java.util.Date;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.springframework.test.AbstractTransactionalSpringContextTests;
 
 public class XLstudyImporterTest extends AbstractTransactionalSpringContextTests {
 
 
     private XLstudyImporter XLstudyImporter;
     private static String fileName;
+    StudyDao studydao;
 
     public XLstudyImporterTest(String testName) {
         super(testName);
@@ -27,7 +38,7 @@ public class XLstudyImporterTest extends AbstractTransactionalSpringContextTests
 
     public static void main(final String[] args) {
         if (StringUtils.isBlank(args[0])) {
-            System.out.println("no file name specified");
+            System.out.println("No file name specified");
             return;
         }
         fileName = args[0];
@@ -48,23 +59,43 @@ public class XLstudyImporterTest extends AbstractTransactionalSpringContextTests
 
     public void testImport() {
         try {
-            File inputFile = new File(fileName);
-
-            XLstudyImporter.importXLstudy(inputFile);
-            setComplete();
-            endTransaction();
+        	File inputFile = null;
+        	boolean runAsTestCase = true;
+        	if(StringUtils.isBlank(fileName)){
+        		inputFile = getResources("classpath*:/gov/nih/nci/cabig/ctms/tools/validStudy.xls")[0].getFile();
+        	}else{
+        		runAsTestCase = false;
+        		inputFile = new File(fileName);
+        	}
+            
+        	XLstudyImporter.importXLstudy(inputFile);
+            
+            if(runAsTestCase){
+            	Study study = studydao.getByIdentifier(Identifier.createTemplate("8xts1c14425"));
+        		study = studydao.getStudyDesignById(study.getId());
+        		assertNotNull(study);
+        		assertEquals("N027D", study.getShortTitle());
+        		assertEquals("A Phase I Study of CCI-779 and Temozolomide in Combination with Radiation Therapy in Glioblastoma Multiforme", study.getLongTitle());
+        		assertEquals("I", study.getPhaseCode());
+        		assertEquals(2, study.getStudyAgents().size());
+        		assertEquals(1, study.getCtepStudyDiseases().size());
+        		assertEquals(12, study.getTreatmentAssignments().size());
+        		assertEquals(3, study.getStudyOrganizations().size());
+        		assertEquals(2, study.getStudyTherapies().size());
+            }else{
+            	setComplete();
+                endTransaction();
+            }
         }
         catch (Exception ex) {
             System.out.println("\n Error occured: ");
             ex.printStackTrace();
         }
-
-
     }
 
     @Override
     protected void onSetUpInTransaction() throws Exception {
-        super.onSetUpInTransaction();    //To change body of overridden methods use File | Settings | File Templates.
+        super.onSetUpInTransaction();    
         try {
             SimpleNamingContextBuilder.emptyActivatedContextBuilder();
         } catch (NamingException e) {
@@ -81,11 +112,17 @@ public class XLstudyImporterTest extends AbstractTransactionalSpringContextTests
         gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo.setLocal(new DataAuditInfo(identity,
                 "localhost", new Date(), info));
 
-
     }
-
+    
+    private static Resource[] getResources(String pattern) throws IOException {
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources(pattern);
+        return resources;
+    }
+    
     @Override
     public String[] getConfigLocations() {
+
         return new String[]{
                 "classpath*:gov/nih/nci/cabig/caaers/applicationContext-configProperties.xml",
                 "classpath*:gov/nih/nci/cabig/caaers/applicationContext-core-dao.xml",
@@ -102,4 +139,11 @@ public class XLstudyImporterTest extends AbstractTransactionalSpringContextTests
     public void setXLstudyImporter(XLstudyImporter XLstudyImporter) {
         this.XLstudyImporter = XLstudyImporter;
     }
+    
+    @Required
+	public void setStudydao(StudyDao studydao) {
+		this.studydao = studydao;
+	}
+    
+    
 }
