7fb94e844470674c3bb942b1eb8c9449568f1e6d
Srini Akkala <akkalas@gmail.com>
2008-09-10 15:03:04 +0000

    added logic to check old values in mandatory field
    SVN-Revision: 6178

--- projects/core/src/main/db/migrate/011_Phase2_construction_1/036_modify_mandatory_field_defs.groovy
+++ projects/core/src/main/db/migrate/011_Phase2_construction_1/036_modify_mandatory_field_defs.groovy
@@ -1,7 +1,9 @@
 class ModifyMandatoryFieldDefs extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
-		dropColumn("mandatory_field_defs", "mandatory");
+		renameColumn("mandatory_field_defs", "mandatory", "mandatory_temp");
 		addColumn("mandatory_field_defs", "mandatory", "integer", nullable: false, defaultValue: 0);
+		execute("update mandatory_field_defs set mandatory = (case when (mandatory_temp = true) then 1 else 0 end)");
+		dropColumn("mandatory_field_defs", "mandatory_temp");
 	}
 	
 	void down() {
