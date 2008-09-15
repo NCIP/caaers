b151ca71f2ba5cd591ef6cc3c151dacc12b521b9
Karthik Iyer <iyerk0@gmail.com>
2008-09-15 19:14:29 +0000

    added unit tests to AdverseEventQueryServiceTest
    SVN-Revision: 6265

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/api/AdverseEventQueryServiceTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/api/AdverseEventQueryServiceTest.java
@@ -6,109 +6,135 @@ import gov.nih.nci.cabig.caaers.domain.Grade;
 import gov.nih.nci.cabig.caaers.domain.Hospitalization;
 import gov.nih.nci.cabig.caaers.domain.Participant;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class AdverseEventQueryServiceTest extends CaaersDbTestCase {
 
 	private AdverseEventQueryService svc = null;
-	
+
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		svc = (AdverseEventQueryService)getDeployedApplicationContext().getBean("adverseEventQueryService");
+		svc = (AdverseEventQueryService) getDeployedApplicationContext()
+				.getBean("adverseEventQueryService");
 	}
-	
-	public void testGetByParticipant() throws Exception {
+
+	public void testGetByParticipantWithFirstName() throws Exception {
 		Participant participant = new Participant();
 		participant.setFirstName("Dilbert");
-		List<AdverseEvent> aes = svc.getByParticipant(participant);		
+		List<AdverseEvent> aes = svc.getByParticipant(participant);
 		assertEquals(2, aes.size());
 		System.out.println(svc.getText(svc.getXML(aes)));
-		
+
 		participant = new Participant();
 		participant.setFirstName("John");
 		aes = svc.getByParticipant(participant);
-		assertEquals(0, aes.size());		
+		assertEquals(0, aes.size());
 	}
 	
-	public void testGetByParticipantAdverseEvent() {
+	public void testGetByParticipantWithLastName() throws Exception {
+		Participant participant = new Participant();
+		participant.setLastName("Scott");
+		List<AdverseEvent> aes = svc.getByParticipant(participant);
+		assertEquals(2, aes.size());
+		//System.out.println(svc.getText(svc.getXML(aes)));
+	}
+	
+	public void testGetByParticipantWithGender() throws Exception {
+		Participant participant = new Participant();
+		participant.setGender("Male");
+		List<AdverseEvent> aes = svc.getByParticipant(participant);
+		assertEquals(1, aes.size());
+		AdverseEvent resultAe = aes.get(0);
+		assertEquals(-4, resultAe.getId().intValue());
+		//System.out.println(svc.getText(svc.getXML(aes)));
+	}
+	public void testAEsForParticipantWithBlankAE() {
 		Participant participant = new Participant();
 		participant.setFirstName("Dilbert");
 		AdverseEvent ae = new AdverseEvent();
-		ae.setGrade(Grade.DEATH);	
-		List<AdverseEvent> aes = svc.getByParticipant(participant,ae);		
+		List<AdverseEvent> aes = svc.getByParticipant(participant, ae);
+		assertEquals(0, aes.size());
+	}
+	public void testAEsForParticipantWithGrade() {
+		Participant participant = new Participant();
+		participant.setFirstName("Dilbert");
+		AdverseEvent ae = new AdverseEvent();
+		ae.setGrade(Grade.DEATH);
+		List<AdverseEvent> aes = svc.getByParticipant(participant, ae);
 		assertEquals(1, aes.size());
+	}
 		
+	public void testAEsForParticipantWithHosp() {
+		Participant participant = new Participant();
+		participant.setFirstName("Dilbert");
+		AdverseEvent ae = new AdverseEvent();
+		ae.setHospitalization(Hospitalization.NO);
+		List<AdverseEvent> aes = svc.getByParticipant(participant, ae);
+		assertEquals(1, aes.size());
 		AdverseEvent resultAe = aes.get(0);
-		assertEquals(-3, resultAe.getId().intValue());
-		
-		ae = new AdverseEvent();
-		ae.setGrade(Grade.SEVERE);
-		ae.setHospitalization(Hospitalization.YES);
-		aes = svc.getByParticipant(participant,ae);	
-		assertEquals(1, aes.size());
-		
-		resultAe = aes.get(0);
-		assertEquals(-2, resultAe.getId().intValue());
-		
-		participant = new Participant();
+		assertEquals(-4, resultAe.getId().intValue());
+	}
+	
+	public void testAEsForParticipantWithStartDate() {
+		Participant participant = new Participant();
+		participant.setFirstName("Dilbert");
+		AdverseEvent ae = new AdverseEvent();
+		try{
+			Calendar c = Calendar.getInstance();
+			c.set(2007, 9, 12);
+			ae.setStartDate(c.getTime());
+			//ae.setStartDate(DateFormat.getInstance().parse("09/12/2007"));
+		}
+		catch(Exception pe){pe.printStackTrace();}
+		List<AdverseEvent> aes = svc.getByParticipant(participant, ae);
+		assertEquals(2, aes.size());
+	}
+	public void testAEsForNonExistingParticipant() {
+		Participant participant = new Participant();
 		participant.setFirstName("Dilbert");
 		participant.setLastName("Akkala");
-		aes = svc.getByParticipant(participant,ae);
+		AdverseEvent ae = new AdverseEvent();
+		List<AdverseEvent> aes = svc.getByParticipant(participant, ae);
 		assertEquals(0, aes.size());
-	}	
-	
-	
-/*
-	public void testGetByStudy() {
-		Study study = new Study();
-		study.setShortTitle("Short Title");
-		List<AdverseEvent> aes = svc.getByStudy(study);		
-		assertEquals(2, aes.size());
-		
-		study = new Study();
-		study.setShortTitle("Not Short Title");
-		aes = svc.getByStudy(study);	
-		assertEquals(0, aes.size());		
 	}
-	public void atestGet() {
-		try {
-			
-			Identifier participantIdentifier = new Identifier();
-			participantIdentifier.setValue("NCB438");
-			participantIdentifier.setPrimaryIndicator(true);
-			
-			Participant participant = new Participant();
-			participant.setFirstName("Chris");
-			participant.setLastName("Wilson");
-			DateValue dv = new DateValue();
-			dv.setMonth(12);
-			dv.setYear(1956);
-			participant.setDateOfBirth(dv);
-			//participant.addIdentifier(participantIdentifier);
-			
-			AdverseEvent ae = new AdverseEvent();
-			ae.setGrade(Grade.SEVERE);
-			ae.setSolicited(true);
-			//ae.setHospitalization(Hospitalization.YES);
-			
-			//List<AdverseEvent> l = svc.getByParticipant(participant);
-			
-			Study study = new Study();
-			study.setShortTitle("% study oct July%");
-			
 
-			
-			List<AdverseEvent> l = svc.getByStudy(study);
-			
-			//for (AdverseEvent ae1:l) {
-				System.out.println(svc.getXML(l));
-			//}
-			System.out.println(l.size());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-*/
+	/*
+	 * public void testGetByStudy() { Study study = new Study();
+	 * study.setShortTitle("Short Title"); List<AdverseEvent> aes =
+	 * svc.getByStudy(study); assertEquals(2, aes.size());
+	 * 
+	 * study = new Study(); study.setShortTitle("Not Short Title"); aes =
+	 * svc.getByStudy(study); assertEquals(0, aes.size()); } public void
+	 * atestGet() { try {
+	 * 
+	 * Identifier participantIdentifier = new Identifier();
+	 * participantIdentifier.setValue("NCB438");
+	 * participantIdentifier.setPrimaryIndicator(true);
+	 * 
+	 * Participant participant = new Participant();
+	 * participant.setFirstName("Chris"); participant.setLastName("Wilson");
+	 * DateValue dv = new DateValue(); dv.setMonth(12); dv.setYear(1956);
+	 * participant.setDateOfBirth(dv);
+	 * //participant.addIdentifier(participantIdentifier);
+	 * 
+	 * AdverseEvent ae = new AdverseEvent(); ae.setGrade(Grade.SEVERE);
+	 * ae.setSolicited(true); //ae.setHospitalization(Hospitalization.YES);
+	 * 
+	 * //List<AdverseEvent> l = svc.getByParticipant(participant);
+	 * 
+	 * Study study = new Study(); study.setShortTitle("% study oct July%");
+	 * 
+	 * 
+	 * 
+	 * List<AdverseEvent> l = svc.getByStudy(study);
+	 * 
+	 * //for (AdverseEvent ae1:l) { System.out.println(svc.getXML(l)); //}
+	 * System.out.println(l.size()); } catch (Exception e) { // TODO
+	 * Auto-generated catch block e.printStackTrace(); } }
+	 */
 }
