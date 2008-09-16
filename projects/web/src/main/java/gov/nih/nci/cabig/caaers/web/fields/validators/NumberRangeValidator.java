b0f25fe0b361588bd208612651585b2d8f98224e
Biju Joseph <biju.joseph@semanticbits.com>
2008-09-16 20:45:03 +0000

    hour / minute validation
    SVN-Revision: 6301

---
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/fields/validators/NumberRangeValidator.java
@@ -0,0 +1,30 @@
+package gov.nih.nci.cabig.caaers.web.fields.validators;
+
+import org.apache.commons.lang.math.NumberUtils;
+
+/**
+ * Will validate wheter a number is in a give range or not. 
+ * @author Biju Joseph
+ *
+ */
+public class NumberRangeValidator extends FieldValidator {
+	int begin;
+	int end;
+	public NumberRangeValidator(int begin, int end){
+		this.begin = begin;
+		this.end = end;
+	}
+	@Override
+	public String getMessagePrefix() {
+		return "Incorrect";
+	}
+
+	@Override
+	public boolean isValid(Object fieldValue) {
+		if(fieldValue == null)return true;
+		if(!NumberUtils.isNumber(fieldValue.toString())) return false;
+		int n = Integer.parseInt(fieldValue.toString());
+		return (n < begin && n > end); 
+	}
+
+}
