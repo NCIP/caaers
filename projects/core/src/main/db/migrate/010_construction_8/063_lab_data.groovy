45d6106ef2f0036bcba27e6a7ce80d055a3a5015
Monish Dombla <monish.dombla@semanticbits.com>
2008-09-22 16:18:15 +0000

    SVN-Revision: 6453

---
+++ projects/core/src/main/db/migrate/010_construction_8/063_lab_data.groovy
@@ -0,0 +1,25 @@
+class CreateLabViewerLabs extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+        execute('drop sequence seq_labviewer_labs_id');
+        execute('drop TABLE labviewer_labs');
+        
+        createTable("labs") { t ->
+            t.addVersionColumn()
+            t.addColumn("name", "string", nullable:false)
+            t.addColumn("lab_date", "date")
+            t.addColumn("result", "string")
+            t.addColumn("units", "string")
+            t.addColumn("assignment_id", "integer", nullable:false)
+            t.addColumn("dismissed", "boolean", nullable:false)
+            t.addColumn("grid_id" , "string" , nullable:true)
+            
+        }  
+        execute('create sequence seq_labs_id increment by 1');
+        execute('ALTER TABLE labs ADD CONSTRAINT fk_sp_assignment_id FOREIGN KEY (assignment_id) REFERENCES participant_assignments');
+      
+    }
+    
+    void down() {
+        dropTable("labs")
+    }
+}
