47a16e5448af4f3369b5b814b2d190eb16c1e290
Srini Akkala <akkalas@gmail.com>
2008-09-23 04:09:31 +0000

    used schema from c3d team
    SVN-Revision: 6471

--- projects/grid/introduce/LabConsumerService/src/gov/nih/nci/ccts/grid/common/LabConsumerServiceI.java
+++ projects/grid/introduce/LabConsumerService/src/gov/nih/nci/ccts/grid/common/LabConsumerServiceI.java
@@ -12,13 +12,13 @@ import java.rmi.RemoteException;
  */
 public interface LabConsumerServiceI {
 
-  public gov.nih.nci.cabig.ccts.loadlabs.domain.Acknowledgement loadLabs(gov.nih.nci.cabig.ccts.loadlabs.domain.LoadLabsRequest loadLabsRequest) throws RemoteException ;
-
   public org.oasis.wsrf.properties.GetMultipleResourcePropertiesResponse getMultipleResourceProperties(org.oasis.wsrf.properties.GetMultipleResourceProperties_Element params) throws RemoteException ;
 
   public org.oasis.wsrf.properties.GetResourcePropertyResponse getResourceProperty(javax.xml.namespace.QName params) throws RemoteException ;
 
   public org.oasis.wsrf.properties.QueryResourcePropertiesResponse queryResourceProperties(org.oasis.wsrf.properties.QueryResourceProperties_Element params) throws RemoteException ;
+
+  public gov.nih.nci.cabig.ccts.domain.loadlabs.Acknowledgement loadLabs(gov.nih.nci.cabig.ccts.domain.loadlabs.LoadLabsRequest loadLabsRequest) throws RemoteException ;
 
 }
 
