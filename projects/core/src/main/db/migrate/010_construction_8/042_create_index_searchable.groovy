d1b89e6b16a591885901cc162987db01c3d2eab4
Biju Joseph <biju.joseph@semanticbits.com>
2008-09-29 19:50:30 +0000

    index creation script issues and fixes (related to long index width)
    SVN-Revision: 6613

--- projects/core/src/main/db/migrate/010_construction_8/042_create_index_searchable.groovy
+++ projects/core/src/main/db/migrate/010_construction_8/042_create_index_searchable.groovy
@@ -2,7 +2,8 @@ class CreateIndicies extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
  		execute('CREATE INDEX studies_short_title_idx ON studies (short_title)')
  		execute('CREATE INDEX identifiers_valtyp_idx ON identifiers (value)')
- 		execute('CREATE INDEX participants_flname_idx ON participants (first_name, last_name)')
+ 		execute('CREATE INDEX participants_flname_idx ON participants (first_name)')
+ 		execute('CREATE INDEX participants_flname_idx ON participants (last_name)')
  		execute('CREATE INDEX participants_eg_idx ON participants (ethnicity, gender)')
  		execute('CREATE INDEX participants_dob_idx ON participants (birth_year, birth_month)') 
  		execute('CREATE INDEX organizations_namecode_idx ON organizations (name, nci_institute_code)')
