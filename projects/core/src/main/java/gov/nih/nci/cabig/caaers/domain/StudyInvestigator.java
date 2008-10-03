85ebf9e0608afecc20564c795fd9e1447610b1bc
Biju Joseph <biju.joseph@semanticbits.com>
2008-10-03 21:29:27 +0000

    SVN-Revision: 6706

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/StudyInvestigator.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/StudyInvestigator.java
@@ -71,29 +71,28 @@ public class StudyInvestigator extends AbstractMutableDomainObject implements
     // /OBJECT METHODS
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((roleCode == null) ? 0 : roleCode.hashCode());
-        result = prime * result + ((siteInvestigator == null) ? 0 : siteInvestigator.hashCode());
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-        return result;
-    }
+        StudyInvestigator that = (StudyInvestigator) o;
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (getClass() != obj.getClass()) return false;
-        final StudyInvestigator other = (StudyInvestigator) obj;
-        if (roleCode == null) {
-            if (other.roleCode != null) return false;
-        } else if (!roleCode.equals(other.roleCode)) return false;
-        if (siteInvestigator == null) {
-            if (other.siteInvestigator != null) return false;
-        } else if (!siteInvestigator.equals(other.siteInvestigator)) return false;
+        if (roleCode != null ? !roleCode.equals(that.roleCode) : that.roleCode != null) return false;
+        if (siteInvestigator != null ? !siteInvestigator.equals(that.siteInvestigator) : that.siteInvestigator != null)
+            return false;
+        if (statusCode != null ? !statusCode.equals(that.statusCode) : that.statusCode != null) return false;
+        if (studyOrganization != null ? !studyOrganization.equals(that.studyOrganization) : that.studyOrganization != null)
+            return false;
 
         return true;
     }
 
+    @Override
+    public int hashCode() {
+        int result = roleCode != null ? roleCode.hashCode() : 0;
+        result = 31 * result + (statusCode != null ? statusCode.hashCode() : 0);
+        result = 31 * result + (siteInvestigator != null ? siteInvestigator.hashCode() : 0);
+        result = 31 * result + (studyOrganization != null ? studyOrganization.hashCode() : 0);
+        return result;
+    }
 }
