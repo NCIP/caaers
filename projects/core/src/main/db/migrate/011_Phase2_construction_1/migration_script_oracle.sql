178ff21e289c735b086a184988107edbb882742b
Monish Dombla <monish.dombla@semanticbits.com>
2008-10-03 16:47:19 +0000

    SVN-Revision: 6690

--- projects/core/src/main/db/migrate/011_Phase2_construction_1/migration_script_oracle.sql
+++ projects/core/src/main/db/migrate/011_Phase2_construction_1/migration_script_oracle.sql
@@ -2,9 +2,6 @@ DECLARE
     study_rec studies%ROWTYPE;
     CURSOR c_studies IS SELECT * FROM studies;
 	
-	staff_rec research_staffs%ROWTYPE;
-    CURSOR c_research_staffs IS SELECT * FROM research_staffs;
-
 BEGIN
 	--Studies migration
 	FOR study_rec IN c_studies LOOP
@@ -18,11 +15,6 @@ BEGIN
 	    INSERT INTO epochs (id,version,name,description,study_id,order_no,grid_id) values(SEQ_EPOCHS_ID.NEXTVAL,0,'Post-treatment',null,study_rec.id,2,null);
 	    INSERT INTO arms (id,version,name,description,epoch_id,grid_id) values(SEQ_ARMS_ID.NEXTVAL,0,'Post-treatment',null,SEQ_EPOCHS_ID.CURRVAL,null);
   	END LOOP;
-
-	--Researchstaff migration
-	FOR staff_rec IN c_research_staffs LOOP
-		UPDATE research_staffs set login_id=email_address where id=staff_rec.id;
-	END LOOP;
 
 END;
 /
