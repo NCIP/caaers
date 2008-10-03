7a11a64e2f2894a41c2c0808fde19dc109ec43fd
Srini Akkala <akkalas@gmail.com>
2008-10-03 20:51:17 +0000

    SVN-Revision: 6704

--- projects/core/src/main/db/migrate/011_Phase2_construction_1/044_migrate_participant_invalid_race.groovy
+++ projects/core/src/main/db/migrate/011_Phase2_construction_1/044_migrate_participant_invalid_race.groovy
@@ -1,6 +1,7 @@
 class MigrateParticpantRace extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
         execute("update participants set race='Unknown' where race in ('Hispanic or Latino','More than one race','Other')");
+        execute("update participants set race='Native Hawaiian or Other Pacific Islander' where race in ('Native Hawaiian or other Pacific Islander')");
 	}
 	
 	void down(){
