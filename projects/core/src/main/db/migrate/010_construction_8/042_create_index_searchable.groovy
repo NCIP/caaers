8f9cbc90f126d3b65c0ea23ed100c0e653279b87
Biju Joseph <biju.joseph@semanticbits.com>
2008-10-03 18:35:09 +0000

    fix for index issue.
    SVN-Revision: 6692

--- projects/core/src/main/db/migrate/010_construction_8/042_create_index_searchable.groovy
+++ projects/core/src/main/db/migrate/010_construction_8/042_create_index_searchable.groovy
@@ -4,9 +4,12 @@ class CreateIndicies extends edu.northwestern.bioinformatics.bering.Migration {
  		execute('CREATE INDEX identifiers_valtyp_idx ON identifiers (value)')
  		execute('CREATE INDEX participants_flname_idx ON participants (first_name)')
  		execute('CREATE INDEX participants_flname_idx2 ON participants (last_name)')
- 		execute('CREATE INDEX participants_eg_idx ON participants (ethnicity, gender)')
- 		execute('CREATE INDEX participants_dob_idx ON participants (birth_year, birth_month)') 
- 		execute('CREATE INDEX organizations_namecode_idx ON organizations (name, nci_institute_code)')
+ 		execute('CREATE INDEX participants_eg_idx1 ON participants (ethnicity)')
+ 		execute('CREATE INDEX participants_eg_idx2 ON participants (gender)')
+ 		execute('CREATE INDEX participants_dob_idx1 ON participants (birth_year)')
+ 		execute('CREATE INDEX participants_dob_idx2 ON participants (birth_month)') 
+ 		execute('CREATE INDEX organizations_namecode_idx1 ON organizations (name)')
+ 		execute('CREATE INDEX organizations_namecode_idx2 ON organizations (nci_institute_code)')
  		
     }
 
@@ -15,8 +18,12 @@ class CreateIndicies extends edu.northwestern.bioinformatics.bering.Migration {
         dropTable('DROP INDEX identifiers_valtyp_idx')
         dropTable('DROP INDEX participants_flname_idx')
         dropTable('DROP INDEX participants_flname_idx2')
-        dropTable('DROP INDEX participants_eg_idx')
-        dropTable('DROP INDEX participants_dob_idx')
-        dropTable('DROP INDEX organizations_namecode_idx')
+        dropTable('DROP INDEX participants_eg_idx1')
+        dropTable('DROP INDEX participants_eg_idx2')
+        dropTable('DROP INDEX participants_eg_idx1')
+        dropTable('DROP INDEX participants_eg_idx2')
+        dropTable('DROP INDEX organizations_namecode_idx1')
+        dropTable('DROP INDEX organizations_namecode_idx2')
+        
     }
 }
