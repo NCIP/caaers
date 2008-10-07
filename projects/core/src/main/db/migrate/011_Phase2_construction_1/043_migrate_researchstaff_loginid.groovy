2c435e13e06b615e79c0297cd1ba0189c16f2a28
Monish Dombla <monish.dombla@semanticbits.com>
2008-10-07 17:05:40 +0000

    SVN-Revision: 6755

--- projects/core/src/main/db/migrate/011_Phase2_construction_1/043_migrate_researchstaff_loginid.groovy
+++ projects/core/src/main/db/migrate/011_Phase2_construction_1/043_migrate_researchstaff_loginid.groovy
@@ -1,6 +1,11 @@
 class MigrateResearchStaff extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
-        execute("UPDATE research_staffs set login_id=email_address");
+	
+	    if (databaseMatches('oracle')) {
+            execute("update research_staffs set login_id=(select login_name from csm_user cu where login_id=cu.user_id)");
+        } else if (databaseMatches('postgresql')){
+            execute("update research_staffs set login_id=(select login_name from csm_user cu where login_id=text(cu.user_id))");
+        } 
 	}
 	
 	void down(){
