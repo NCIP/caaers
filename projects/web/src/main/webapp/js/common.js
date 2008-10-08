d78b1c3937d65712017b98b97d6ca4600c61fef9
Biju Joseph <biju.joseph@semanticbits.com>
2008-10-08 19:08:14 +0000

    Bug   	 CAAERS-1145
    SVN-Revision: 6773

--- projects/web/src/main/webapp/js/common.js
+++ projects/web/src/main/webapp/js/common.js
@@ -678,3 +678,23 @@ function suppressEnter(e) {
 
     if (keynum == 13) Event.stop(e);
 }
+
+
+/*
+ * http://blog.stevenlevithan.com/archives/faster-than-innerhtml
+  This is much faster than using (el.innerHTML = str) when there are many existing descendants, because in some browsers, innerHTML spends much longer
+   removing existing elements than it does creating new ones. 
+ */
+function replaceHtml(el, html) {
+	var oldEl = typeof el === "string" ? document.getElementById(el) : el;
+	/*@cc_on // Pure innerHTML is slightly faster in IE
+		oldEl.innerHTML = html;
+		return oldEl;
+	@*/
+	var newEl = oldEl.cloneNode(false);
+	newEl.innerHTML = html;
+	oldEl.parentNode.replaceChild(newEl, oldEl);
+	/* Since we just removed the old element from the DOM, return a reference
+	to the new element, which can be used to restore variable references. */
+	return newEl;
+};
