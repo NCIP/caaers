04023b6fc11d399a7c477c33dd4842e34576a01d
Biju Joseph <biju.joseph@semanticbits.com>
2008-10-16 14:15:18 +0000

    Testcase fixes : AbstractNoSecurityTest
    SVN-Revision: 6855

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/AbstractNoSecurityTestCase.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/AbstractNoSecurityTestCase.java
@@ -3,6 +3,7 @@ package gov.nih.nci.cabig.caaers;
 import gov.nih.nci.cabig.caaers.security.StudyParticipantAssignmentAspect;
 import gov.nih.nci.cabig.caaers.security.stub.AspectJSecurityInterceptorStub;
 
+import org.acegisecurity.intercept.method.aspectj.AspectJSecurityInterceptor;
 import org.aspectj.lang.Aspects;
 
 /**
@@ -11,17 +12,26 @@ import org.aspectj.lang.Aspects;
  *
  */
 public class AbstractNoSecurityTestCase extends AbstractTestCase {
-	
+	private StudyParticipantAssignmentAspect aspect;
+	private AspectJSecurityInterceptor interceptor ;
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		/*StudyParticipantAssignmentAspect aspect = null;
 		try{
 			aspect = Aspects.aspectOf(StudyParticipantAssignmentAspect.class);
+			interceptor = aspect.getSecurityInterceptor();
+			AspectJSecurityInterceptorStub interceptorStub = new AspectJSecurityInterceptorStub();
+	        aspect.setSecurityInterceptor(interceptorStub);
+			
 		}catch(Exception e){
 			aspect = new StudyParticipantAssignmentAspect();
 		}
-		AspectJSecurityInterceptorStub interceptorStub = new AspectJSecurityInterceptorStub();
-        aspect.setSecurityInterceptor(interceptorStub);*/
+		
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		aspect.setSecurityInterceptor(interceptor);
+		super.tearDown();
 	}
 }
