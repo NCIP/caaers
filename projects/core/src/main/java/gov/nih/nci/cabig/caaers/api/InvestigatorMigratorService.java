9868ac19418d2c978af1302ac41fc442deb6d9ea
Monish Dombla <monish.dombla@semanticbits.com>
2008-10-23 20:18:36 +0000

    Removed unused methods
    SVN-Revision: 6953

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/api/InvestigatorMigratorService.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/api/InvestigatorMigratorService.java
@@ -1,11 +1,6 @@
 package gov.nih.nci.cabig.caaers.api;
 
-import gov.nih.nci.cabig.caaers.domain.Investigator;
 import gov.nih.nci.cabig.caaers.integration.schema.common.CaaersServiceResponse;
-import gov.nih.nci.cabig.caaers.integration.schema.investigator.InvestigatorType;
-import gov.nih.nci.cabig.caaers.service.DomainObjectImportOutcome;
-
-import java.util.List;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -16,15 +11,6 @@ import javax.jws.WebService;
 public interface InvestigatorMigratorService {
 	
 	@WebMethod
-	public CaaersServiceResponse saveInvestigator(@WebParam(name="Staff") gov.nih.nci.cabig.caaers.integration.schema.investigator.Staff staff) ;//throws RemoteException;
+	public CaaersServiceResponse saveInvestigator(@WebParam(name="Staff") gov.nih.nci.cabig.caaers.integration.schema.investigator.Staff staff) ;
 	
-	public List<DomainObjectImportOutcome<Investigator>> getImportableInvestigators();
-	
-	public List<DomainObjectImportOutcome<Investigator>> getNonImportableInvestigators();
-	
-	public DomainObjectImportOutcome<Investigator> processInvestigator(InvestigatorType xmlInvestigator);
-	
-	//public List<InvestigatorType> getNonImportableInvestigators();
-	
-	//public void deleteInvestigator(Staff staff) throws RemoteException;
 }
--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/api/ResearchStaffMigratorService.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/api/ResearchStaffMigratorService.java
@@ -1,11 +1,6 @@
 package gov.nih.nci.cabig.caaers.api;
 
-import gov.nih.nci.cabig.caaers.domain.ResearchStaff;
 import gov.nih.nci.cabig.caaers.integration.schema.common.CaaersServiceResponse;
-import gov.nih.nci.cabig.caaers.integration.schema.researchstaff.ResearchStaffType;
-import gov.nih.nci.cabig.caaers.service.DomainObjectImportOutcome;
-
-import java.util.List;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -13,16 +8,8 @@ import javax.jws.WebService;
 
 @WebService(name="ResearchStaffMigratorServiceIntf",targetNamespace="http://researchstaff.schema.integration.caaers.cabig.nci.nih.gov")
 public interface ResearchStaffMigratorService {
-	//public void createStudy(@WebParam(name="Study") gov.nih.nci.cabig.caaers.webservice.Study xmlStudy);
-	
+
 	@WebMethod
-	public CaaersServiceResponse saveResearchStaff(@WebParam(name="Staff") gov.nih.nci.cabig.caaers.integration.schema.researchstaff.Staff staff) ;//throws RemoteException;
+	public CaaersServiceResponse saveResearchStaff(@WebParam(name="Staff") gov.nih.nci.cabig.caaers.integration.schema.researchstaff.Staff staff) ;
 	
-	public List<DomainObjectImportOutcome<ResearchStaff>> getImportableResearchStaff();
-	
-	public List<DomainObjectImportOutcome<ResearchStaff>> getNonImportableResearchStaff();
-	
-	public DomainObjectImportOutcome<ResearchStaff> processResearchStaff(ResearchStaffType xmlResearchStaff);
-	
-	//public void deleteResearchStaff(Staff staff) throws RemoteException;
 }
