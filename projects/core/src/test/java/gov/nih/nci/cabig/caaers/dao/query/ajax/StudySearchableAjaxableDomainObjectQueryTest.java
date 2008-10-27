808ba9798292cbead7f11bb1571722578f5a7f04
Sameer Sawant <sameer.sawant@semanticbits.com>
2008-10-27 17:07:28 +0000

    CAAERS-1215, 1216, 1217, 1218
    Fixed the test case. The query had changed.
    
    SVN-Revision: 6962

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/ajax/StudySearchableAjaxableDomainObjectQueryTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/ajax/StudySearchableAjaxableDomainObjectQueryTest.java
@@ -9,85 +9,79 @@ public class StudySearchableAjaxableDomainObjectQueryTest extends TestCase {
 
     public void testQueryConstructor() throws Exception {
         AbstractAjaxableDomainObjectQuery query = new StudySearchableAjaxableDomainObjectQuery();
-//
-//        assertEquals("wrong parsing for constructor",
-//                "Select study.id,study.shortTitle,identifier.value,identifier.primaryIndicator,study.phaseCode,study.status," +
-//                        "(select sfs.organization.nciInstituteCode from StudyFundingSponsor sfs  where sfs.study.id =study.id) as fundingSponsor from Study study " +
-//                        "left join study.identifiers as identifier order by study.shortTitle", query
-//                        .getQueryString());
-        assertTrue(true);
+
+        assertEquals("wrong parsing for constructor",
+        		"Select study.id,study.shortTitle,identifier.value,identifier.primaryIndicator,study.phaseCode,study.status," +
+        		"(select sfs.organization.nciInstituteCode from StudyFundingSponsor sfs  where sfs.study.id =study.id) as " +
+        		"fundingSponsor, ss.organization.name,ss.id,ss.class from Study study left join study.identifiers as identifier " +
+        		"left join study.studyOrganizations as ss order by study.shortTitle", query.getQueryString());
 
     }
 
     public void testFilterByParticipantFirstName() throws Exception {
-//        StudySearchableAjaxableDomainObjectQuery query = new StudySearchableAjaxableDomainObjectQuery();
-//        query.filterByParticipant("a", null, null, null, null, null);
-//
-//        assertEquals("Select study.id,study.shortTitle,identifier.value,identifier.primaryIndicator,study.phaseCode,study.status," +
-//                "(select sfs.organization.nciInstituteCode from StudyFundingSponsor sfs  where sfs.study.id =study.id) as fundingSponsor from Study study " +
-//                "left join study.identifiers as identifier " +
-//                "left join study.studyOrganizations as ss " +
-//                "left join ss.studyParticipantAssignments as spa " +
-//                "left join spa.participant as p WHERE lower(p.firstName) LIKE :firstName order by study.shortTitle",
-//                query.getQueryString());
-//        assertEquals("wrong number of parameters", query.getParameterMap().size(), 1);
-//
-//        assertTrue("missing paramenter name", query.getParameterMap().containsKey(
-//                "firstName"));
-//        assertEquals("wrong parameter value", query.getParameterMap().get("firstName"),
-//                "%a%");
-    	assertTrue(true);
+        StudySearchableAjaxableDomainObjectQuery query = new StudySearchableAjaxableDomainObjectQuery();
+        query.filterByParticipant("a", null, null, null, null, null);
+
+        assertEquals("Select study.id,study.shortTitle,identifier.value,identifier.primaryIndicator,study.phaseCode,study.status," +
+        		"(select sfs.organization.nciInstituteCode from StudyFundingSponsor sfs  where sfs.study.id =study.id) as fundingSponsor, " +
+        		"ss.organization.name,ss.id,ss.class from Study study left join study.identifiers as identifier left join " +
+        		"study.studyOrganizations as ss left join ss.studyParticipantAssignments as spa left join spa.participant as p " +
+        		"WHERE lower(p.firstName) LIKE :firstName order by study.shortTitle", query.getQueryString());
+        assertEquals("wrong number of parameters", query.getParameterMap().size(), 1);
+
+        assertTrue("missing paramenter name", query.getParameterMap().containsKey(
+                "firstName"));
+        assertEquals("wrong parameter value", query.getParameterMap().get("firstName"),
+                "%a%");
 
     }
 
     public void testFilterByParticipantFirstNameAndLastName() throws Exception {
-//        StudySearchableAjaxableDomainObjectQuery query = new StudySearchableAjaxableDomainObjectQuery();
-//        query.filterByParticipant("a", "b", null, null, null, null);
-//
-//        assertEquals("Select study.id,study.shortTitle,identifier.value,identifier.primaryIndicator,study.phaseCode,study.status," +
-//                "(select sfs.organization.nciInstituteCode from StudyFundingSponsor sfs  where sfs.study.id =study.id) as fundingSponsor from Study study " +
-//                "left join study.identifiers as identifier " +
-//                "left join study.studyOrganizations as ss " +
-//                "left join ss.studyParticipantAssignments as spa " +
-//                "left join spa.participant as p WHERE lower(p.firstName) LIKE :firstName AND lower(p.lastName) LIKE :lastName order by study.shortTitle",
-//                query.getQueryString());
-//        assertEquals("wrong number of parameters", query.getParameterMap().size(), 2);
-//
-//        assertTrue("missing paramenter name", query.getParameterMap().containsKey(
-//                "firstName"));
-//        assertTrue("missing paramenter name", query.getParameterMap().containsKey(
-//                "lastName"));
-//        assertEquals("wrong parameter value", query.getParameterMap().get("firstName"),
-//                "%a%");
-//        assertEquals("wrong parameter value", query.getParameterMap().get("lastName"),
-//                "%b%");
-    	assertTrue(true);
+        StudySearchableAjaxableDomainObjectQuery query = new StudySearchableAjaxableDomainObjectQuery();
+        query.filterByParticipant("a", "b", null, null, null, null);
 
+
+        assertEquals("Select study.id,study.shortTitle,identifier.value,identifier.primaryIndicator,study.phaseCode,study.status," +
+        		"(select sfs.organization.nciInstituteCode from StudyFundingSponsor sfs  where sfs.study.id =study.id) as fundingSponsor, " +
+        		"ss.organization.name,ss.id,ss.class from Study study left join study.identifiers as identifier left join " +
+        		"study.studyOrganizations as ss left join ss.studyParticipantAssignments as spa left join spa.participant as p WHERE " +
+        		"lower(p.firstName) LIKE :firstName AND lower(p.lastName) LIKE :lastName order by study.shortTitle", query.getQueryString());
+        assertEquals("wrong number of parameters", query.getParameterMap().size(), 2);
+
+        assertTrue("missing paramenter name", query.getParameterMap().containsKey(
+                "firstName"));
+        assertTrue("missing paramenter name", query.getParameterMap().containsKey(
+                "lastName"));
+        assertEquals("wrong parameter value", query.getParameterMap().get("firstName"),
+                "%a%");
+        assertEquals("wrong parameter value", query.getParameterMap().get("lastName"),
+                "%b%");
+    
     }
 
 
     public void testFilterByBothParticipantAndText() throws Exception {
-//        StudySearchableAjaxableDomainObjectQuery query = new StudySearchableAjaxableDomainObjectQuery();
-//        query.filterStudiesWithMatchingShortTitleOnly("a");
-//        query.filterByParticipant("a", null, null, null, null, null);
-//
-//        assertEquals("Select study.id,study.shortTitle,identifier.value,identifier.primaryIndicator,study.phaseCode,study.status," +
-//                "(select sfs.organization.nciInstituteCode from StudyFundingSponsor sfs  where sfs.study.id =study.id) as fundingSponsor " +
-//                "from Study study left join study.identifiers as identifier left join study.studyOrganizations as ss left join ss.studyParticipantAssignments as spa " +
-//                "left join spa.participant as p WHERE (lower(study.shortTitle) LIKE :shortTitle ) AND lower(p.firstName) LIKE :firstName order by study.shortTitle",
-//                query.getQueryString());
-//        assertEquals("wrong number of parameters", 2, query.getParameterMap().size());
-//
-//        assertTrue("missing paramenter name", query.getParameterMap().containsKey(
-//                "shortTitle"));
-//        assertEquals("wrong parameter value", query.getParameterMap().get("shortTitle"),
-//                "%a%");
-//
-//        assertTrue("missing paramenter name", query.getParameterMap().containsKey(
-//                "firstName"));
-//        assertEquals("wrong parameter value", query.getParameterMap().get("firstName"),
-//                "%a%");
-    	assertTrue(true);
+        StudySearchableAjaxableDomainObjectQuery query = new StudySearchableAjaxableDomainObjectQuery();
+        query.filterStudiesWithMatchingShortTitleOnly("a");
+        query.filterByParticipant("a", null, null, null, null, null);
+
+        assertEquals("Select study.id,study.shortTitle,identifier.value,identifier.primaryIndicator,study.phaseCode,study.status," +
+        		"(select sfs.organization.nciInstituteCode from StudyFundingSponsor sfs  where sfs.study.id =study.id) as fundingSponsor, " +
+        		"ss.organization.name,ss.id,ss.class from Study study left join study.identifiers as identifier left join " +
+        		"study.studyOrganizations as ss left join ss.studyParticipantAssignments as spa left join spa.participant as p " +
+        		"WHERE (lower(study.shortTitle) LIKE :shortTitle ) AND lower(p.firstName) LIKE :firstName order by study.shortTitle", query.getQueryString());
+        
+        assertEquals("wrong number of parameters", 2, query.getParameterMap().size());
+
+        assertTrue("missing paramenter name", query.getParameterMap().containsKey(
+                "shortTitle"));
+        assertEquals("wrong parameter value", query.getParameterMap().get("shortTitle"),
+                "%a%");
+
+        assertTrue("missing paramenter name", query.getParameterMap().containsKey(
+                "firstName"));
+        assertEquals("wrong parameter value", query.getParameterMap().get("firstName"),
+                "%a%");
        
 
     }
