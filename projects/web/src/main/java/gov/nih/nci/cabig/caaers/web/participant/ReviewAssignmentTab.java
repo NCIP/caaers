4a20d1abedf5b221617f28a6d5911f787987838a
Biju Joseph <biju.joseph@semanticbits.com>
2008-10-28 17:04:39 +0000

    CAAERS-1241
    SVN-Revision: 6992

--- projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/participant/ReviewAssignmentTab.java
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/participant/ReviewAssignmentTab.java
@@ -1,11 +1,13 @@
 package gov.nih.nci.cabig.caaers.web.participant;
 
-import gov.nih.nci.cabig.ctms.web.tabs.Tab;
-import gov.nih.nci.cabig.caaers.web.fields.TabWithFields;
+import gov.nih.nci.cabig.caaers.dao.StudySiteDao;
 import gov.nih.nci.cabig.caaers.web.fields.InputFieldGroup;
 import gov.nih.nci.cabig.caaers.web.fields.InputFieldGroupMap;
+import gov.nih.nci.cabig.caaers.web.fields.TabWithFields;
 
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 
@@ -13,7 +15,7 @@ import java.util.Map;
  * 
  */
 public class ReviewAssignmentTab extends TabWithFields<AssignParticipantStudyCommand> {
-
+	private StudySiteDao studySiteDao;
     public ReviewAssignmentTab() {
         super("Review", "Review", "par/reg_review_submit");
 //        super("Review and Submit", "Review and Submit", "par/par_confirmation");
@@ -23,4 +25,17 @@ public class ReviewAssignmentTab extends TabWithFields<AssignParticipantStudyCom
         InputFieldGroupMap map = new InputFieldGroupMap();
         return map;
     }
+    
+    @Override
+    public void onDisplay(HttpServletRequest request,AssignParticipantStudyCommand command) {
+    	super.onDisplay(request, command);
+    	studySiteDao.lock(command.getStudySite());
+    }
+    
+    public StudySiteDao getStudySiteDao() {
+		return studySiteDao;
+	}
+    public void setStudySiteDao(StudySiteDao studySiteDao) {
+		this.studySiteDao = studySiteDao;
+	}
 }
