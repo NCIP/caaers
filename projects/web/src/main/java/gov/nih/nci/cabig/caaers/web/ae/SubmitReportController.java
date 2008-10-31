828a1b1d6fe2810742d2bfa941e41f5b6989fe26
Biju Joseph <biju.joseph@semanticbits.com>
2008-10-31 18:37:00 +0000

    CAAERS-1280 - https://wiki.nci.nih.gov/display/CTMS/Reqt+-+Save+copies+of+submitted+reports Implementation of the usecase Refactored the submission flow.
    SVN-Revision: 7032

--- projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/ae/SubmitReportController.java
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/ae/SubmitReportController.java
@@ -1,19 +1,11 @@
 package gov.nih.nci.cabig.caaers.web.ae;
 
-import gov.nih.nci.cabig.caaers.api.AdeersReportGenerator;
-import gov.nih.nci.cabig.caaers.api.AdverseEventReportSerializer;
-import gov.nih.nci.cabig.caaers.dao.ExpeditedAdverseEventReportDao;
-import gov.nih.nci.cabig.caaers.dao.report.ReportDao;
 import gov.nih.nci.cabig.caaers.domain.ExpeditedAdverseEventReport;
-import gov.nih.nci.cabig.caaers.domain.ReportStatus;
 import gov.nih.nci.cabig.caaers.domain.report.Report;
-import gov.nih.nci.cabig.caaers.domain.report.ReportDelivery;
-import gov.nih.nci.cabig.caaers.domain.report.ReportDeliveryDefinition;
-import gov.nih.nci.cabig.caaers.service.SchedulerService;
+import gov.nih.nci.cabig.caaers.service.ReportSubmissionService;
 import gov.nih.nci.cabig.caaers.web.RenderDecisionManager;
 import gov.nih.nci.cabig.ctms.web.tabs.FlowFactory;
 
-import java.util.Date;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
@@ -31,7 +24,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 public class SubmitReportController extends AbstractAdverseEventInputController {
 
-	private SchedulerService schedulerService;
+	protected ReportSubmissionService reportSubmissionService;
 	
 	protected final Log log = LogFactory.getLog(getClass());
 	
@@ -60,18 +53,15 @@ public class SubmitReportController extends AbstractAdverseEventInputController 
     }
     
     @Override
-    protected ExpeditedAdverseEventInputCommand save(
-    		ExpeditedAdverseEventInputCommand command, Errors errors) {
+    protected ExpeditedAdverseEventInputCommand save(ExpeditedAdverseEventInputCommand command, Errors errors) {
     	log.debug("In overriden save");
     	command.save();
     	return null;
     }
 
     @Override
-    // @Transactional
     @SuppressWarnings("unchecked")
-    protected ModelAndView processFinish(HttpServletRequest request, HttpServletResponse response,
-                    Object oCommand, BindException errors) throws Exception {
+    protected ModelAndView processFinish(HttpServletRequest request, HttpServletResponse response, Object oCommand, BindException errors) throws Exception {
     	log.debug("In processFinish");
         SubmitExpeditedAdverseEventCommand command = (SubmitExpeditedAdverseEventCommand) oCommand;
         Integer reportIndex = Integer.valueOf(command.getReportIndex());
@@ -80,67 +70,8 @@ public class SubmitReportController extends AbstractAdverseEventInputController 
         ExpeditedAdverseEventReport aeReport = command.getAeReport();
         Report report = aeReport.getReports().get(((int) reportIndex));
 
-        ReportStatus status = null;
-        Date date = new Date();
-        String message = "";
-        String url = "";
-
-        boolean endPointUrl = false;
-        for (ReportDelivery delivery : report.getReportDeliveries()) {
-            ReportDeliveryDefinition rdd = delivery.getReportDeliveryDefinition();
-
-            if (rdd.getEndPointType().equals(ReportDeliveryDefinition.ENDPOINT_TYPE_URL)) {
-                endPointUrl = true;
-                break;
-            }
-        }
-
-
-        if (!endPointUrl) {
-            // TODO: take out
-            status = ReportStatus.COMPLETED;
-            schedulerService.unScheduleNotification(report);
-        } else {
-            status = ReportStatus.INPROCESS;
-        }
-
-        // generate report and send ...
-        AdeersReportGenerator aegen = (AdeersReportGenerator) getApplicationContext().getBean(
-                        "adeersReportGenerator");
+        reportSubmissionService.submitReport(report);
         
-       
-        
-        AdverseEventReportSerializer aeser = new AdverseEventReportSerializer();
-        String xml = aeser.serialize(aeReport);
-        
-        report.setStatus(status);
-        report.setSubmissionUrl(url);
-        report.setSubmissionMessage(message);
-        report.setSubmittedOn(date);
-        report.getLastVersion().setReportStatus(status);
-        report.getLastVersion().setSubmissionUrl(url);
-        report.getLastVersion().setSubmissionMessage(message);
-        report.getLastVersion().setSubmittedOn(date);
-        log.debug("About to call save");
-        command.save();
-        command.flush();
-        log.debug("Saved and flushed");
-        try {
-            aegen.generateAndNotify(aeReport.getId() + "", report, xml);
-            log.debug("sucessfully notified");
-        } catch (Exception e) {
-            log.error("Error broadcasting message to ESB " ,e);
-            status = ReportStatus.FAILED;
-            message = "Problem communicating with ESB <br> Please try to resubmit the report <br>"
-                            + e.getMessage();
-            
-            report.getLastVersion().setReportStatus(status);
-            report.getLastVersion().setSubmissionMessage(message);
-            command.save();
-            log.debug("Saved and updated the status as failed");
-        }
-       
-
         ModelAndView modelAndView;
         if (command.getFrom() != null && command.getFrom().equals("list")) {
             Map<String, Object> model = new ModelMap("participant", command.getParticipant()
@@ -157,8 +88,13 @@ public class SubmitReportController extends AbstractAdverseEventInputController 
         return modelAndView;
     }
     
-    public void setSchedulerService(SchedulerService schedulerService) {
-		this.schedulerService = schedulerService;
-	}
-
+   public ReportSubmissionService getReportSubmissionService() {
+	   return reportSubmissionService;
+   }
+  
+   @Required
+   public void setReportSubmissionService(ReportSubmissionService reportSubmissionService) {
+	   this.reportSubmissionService = reportSubmissionService;
+   }
+   
 }
