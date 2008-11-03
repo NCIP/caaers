7297db6d1941c57439aea35ca3d0c11796538eb1
Srini Akkala <akkalas@gmail.com>
2008-11-03 16:27:38 +0000

    addd getCommonFileContext
    SVN-Revision: 7036

--- projects/rules/src/test/java/gov/nih/nci/cabig/caaers/rules/common/XMLUtilTest.java
+++ projects/rules/src/test/java/gov/nih/nci/cabig/caaers/rules/common/XMLUtilTest.java
@@ -1,5 +1,11 @@
 package gov.nih.nci.cabig.caaers.rules.common;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import org.springframework.core.io.ClassPathResource;
+
 import gov.nih.nci.cabig.caaers.rules.RulesTestCase;
 import junit.framework.TestCase;
 
@@ -18,7 +24,8 @@ public class XMLUtilTest extends RulesTestCase {
     }
 
     public void testUnmarshal() throws Exception {
-        String xml = getFileContext("test_rule2.xml");
+        String xml = getCommonFileContext("test_rule2.xml");
+        System.out.println(xml);
         Object p = XMLUtil.unmarshal(xml);
         assertNotNull("Object cannot be null", p);
     	assertTrue(true);
@@ -26,7 +33,7 @@ public class XMLUtilTest extends RulesTestCase {
     }
 
     public void testMarshal() throws Exception {
-        String xml = getFileContext("test_rule2.xml");
+        String xml = getCommonFileContext("test_rule2.xml");
         Object p = XMLUtil.unmarshal(xml);
         assertNotNull("Object cannot be null", p);
         String xml2 = XMLUtil.marshal(p);
@@ -35,9 +42,22 @@ public class XMLUtilTest extends RulesTestCase {
     }
 
     public void testUnmarshalToPackage() throws Exception {
-        String xml = getFileContext("test_rule.xml");
+        String xml = getCommonFileContext("test_rule.xml");
         org.drools.rule.Package p = XMLUtil.unmarshalToPackage(xml);
         assertNotNull("Package cannot be null", p);
+    }
+    
+    private String getCommonFileContext(String fileName) throws Exception {
+    	
+        File testFile = new ClassPathResource("/gov/nih/nci/cabig/caaers/rules/common/" + fileName).getFile();
+        BufferedReader ds = new BufferedReader(new FileReader(testFile));
+        String line = null;
+        StringBuffer xml = new StringBuffer();
+        while ((line = ds.readLine()) != null) {
+            xml.append(line);
+        }
+        assertTrue("Content of the xml should not be null", xml.toString().length() > 0);
+        return xml.toString();
     }
 
 }
