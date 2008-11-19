a78841abcba37812a52031619f402b658035883f
Srini Akkala <akkalas@gmail.com>
2008-11-19 21:46:46 +0000

    CaaersDbNoSecurityTestCase
    SVN-Revision: 7188

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/domain/repository/ajax/ParticipantAjaxableDomainObjectRepositoryIntegrationTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/domain/repository/ajax/ParticipantAjaxableDomainObjectRepositoryIntegrationTest.java
@@ -1,6 +1,6 @@
 package gov.nih.nci.cabig.caaers.domain.repository.ajax;
 
-import gov.nih.nci.cabig.caaers.CaaersDbTestCase;
+import gov.nih.nci.cabig.caaers.CaaersDbNoSecurityTestCase;
 import gov.nih.nci.cabig.caaers.dao.query.ajax.ParticipantAjaxableDomainObjectQuery;
 import gov.nih.nci.cabig.caaers.domain.ajax.ParticipantAjaxableDomainObject;
 
@@ -9,7 +9,7 @@ import java.util.List;
 /**
  * @author Biju Joseph
  */
-public class ParticipantAjaxableDomainObjectRepositoryIntegrationTest extends CaaersDbTestCase {
+public class ParticipantAjaxableDomainObjectRepositoryIntegrationTest extends CaaersDbNoSecurityTestCase {
     private ParticipantAjaxableDomainObjectQuery participantAjaxableDomainObjectQuery;
 
     private ParticipantAjaxableDomainObjectRepository participantAjaxableDomainObjectRepository = (ParticipantAjaxableDomainObjectRepository)
