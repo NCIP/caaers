c0fb5e52bce26df7498529befa648e3aa37cfef3
Monish Dombla <monish.dombla@semanticbits.com>
2008-12-02 14:10:30 +0000

    New Test Case Added
    SVN-Revision: 7320

---
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/validator/InvestigatorDomainObjectValidatorTest.java
@@ -0,0 +1,36 @@
+package gov.nih.nci.cabig.caaers.validator;
+
+import gov.nih.nci.cabig.caaers.CaaersTestCase;
+import gov.nih.nci.cabig.caaers.domain.Investigator;
+import gov.nih.nci.cabig.caaers.validation.validator.DomainObjectValidator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InvestigatorDomainObjectValidatorTest  extends CaaersTestCase{
+	
+	Investigator investigator;
+	List<String> errors;
+	DomainObjectValidator domainObjectValidator;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		investigator = new Investigator();
+		errors = new ArrayList<String>();
+		domainObjectValidator = (DomainObjectValidator)getDeployedApplicationContext().getBean("domainObjectValidator");
+	}
+
+	public void testInvestigatorValid(){
+		investigator.setFirstName("John");
+		investigator.setLastName("Doe");
+		investigator.setEmailAddress("john.doe@semanticbits.com");
+		investigator.setNciIdentifier("JON-D1");
+		investigator.setPhoneNumber("121-111-1211");
+		investigator.setFaxNumber("121-111-1212");
+		
+		errors = domainObjectValidator.validate(investigator);
+		
+		assertEquals(0, errors.size());
+	}
+}
