63e2eea7f4412c9f17cdd407c5b25cdcf048a713
Monish Dombla <monish.dombla@semanticbits.com>
2008-12-02 14:10:59 +0000

    New Test Case Added
    SVN-Revision: 7321

---
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/validator/ResearchStaffDomainObjectValidatorTest.java
@@ -0,0 +1,42 @@
+package gov.nih.nci.cabig.caaers.validator;
+
+import gov.nih.nci.cabig.caaers.CaaersTestCase;
+import gov.nih.nci.cabig.caaers.domain.Fixtures;
+import gov.nih.nci.cabig.caaers.domain.Organization;
+import gov.nih.nci.cabig.caaers.domain.ResearchStaff;
+import gov.nih.nci.cabig.caaers.validation.validator.DomainObjectValidator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResearchStaffDomainObjectValidatorTest extends CaaersTestCase{
+	ResearchStaff rStaff;
+	Organization organization;
+	List<String> errors;
+	DomainObjectValidator domainObjectValidator;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		rStaff = new ResearchStaff();
+		organization = Fixtures.createOrganization("NCI");
+		errors = new ArrayList<String>();
+		domainObjectValidator = (DomainObjectValidator)getDeployedApplicationContext().getBean("domainObjectValidator");
+	}
+	
+	public void testResearchStaffValid(){
+		
+		rStaff.setFirstName("John");
+		rStaff.setLastName("Doe");
+		rStaff.setEmailAddress("john.doe@semanticbits.com");
+		rStaff.setPhoneNumber("111-111-1111");
+		rStaff.setFaxNumber("111-111-1112");
+		rStaff.setNciIdentifier("JOHN-D2");
+		rStaff.setOrganization(organization);
+		
+		errors = domainObjectValidator.validate(rStaff);
+		
+		assertEquals(0,errors.size());
+	}
+
+}
