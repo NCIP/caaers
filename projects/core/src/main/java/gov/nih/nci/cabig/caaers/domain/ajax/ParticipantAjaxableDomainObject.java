88aa8bf6f2fd14aefe1662d0619c9cf3ebd8daea
Srini Akkala <akkalas@gmail.com>
2008-12-04 01:23:54 +0000

    SVN-Revision: 7356

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/ajax/ParticipantAjaxableDomainObject.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/ajax/ParticipantAjaxableDomainObject.java
@@ -1,5 +1,8 @@
 package gov.nih.nci.cabig.caaers.domain.ajax;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  *
@@ -8,9 +11,15 @@ public class ParticipantAjaxableDomainObject extends AbstractAjaxableDomainObjec
 
 
     private String lastName;
-
     private String firstName;
+    private String gender;
+    private String race;
+    private String ethnicity;
+    
     private String primaryIdentifierValue;
+    
+    List<StudySiteAjaxableDomainObject> studySites = new ArrayList<StudySiteAjaxableDomainObject>();
+    List<StudySearchableAjaxableDomainObject> studies = new ArrayList<StudySearchableAjaxableDomainObject>();
 
     //FIXME : this logic belongs to participant
     public String getDisplayName() {
@@ -56,4 +65,58 @@ public class ParticipantAjaxableDomainObject extends AbstractAjaxableDomainObjec
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
+    public List<StudySearchableAjaxableDomainObject> getStudies() {
+        return studies;
+    }
+
+	public void addStudy(StudySearchableAjaxableDomainObject studySearchableAjaxableDomainObject) {
+        if (getObjectById(this.getStudies(), studySearchableAjaxableDomainObject.getId()) == null) {
+        	getStudies().add(studySearchableAjaxableDomainObject);
+        }
+
+    }
+    public List<StudySiteAjaxableDomainObject> getStudySites() {
+        return studySites;
+    }
+
+	public void addStudySite(StudySiteAjaxableDomainObject studySiteAjaxableDomainObject) {
+        if (getObjectById(this.getStudySites(), studySiteAjaxableDomainObject.getId()) == null) {
+            getStudySites().add(studySiteAjaxableDomainObject);
+        }
+
+    }    
+    protected AbstractAjaxableDomainObject getObjectById(List<? extends AbstractAjaxableDomainObject> ajaxableDomainObjects, Integer id) {
+        for (AbstractAjaxableDomainObject object : ajaxableDomainObjects) {
+            if (object.getId().equals(id)) {
+                return object;
+            }
+        }
+        return null;
+    }
+
+	public String getEthnicity() {
+		return ethnicity;
+	}
+
+	public void setEthnicity(String ethnicity) {
+		this.ethnicity = ethnicity;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getRace() {
+		return race;
+	}
+
+	public void setRace(String race) {
+		this.race = race;
+	}
+    
 }
