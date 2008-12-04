830d29981f7be9b29623736e49969ee89d2e2ef6
Srini Akkala <akkalas@gmail.com>
2008-12-04 01:21:52 +0000

    SVN-Revision: 7353

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/query/ajax/StudySearchableAjaxableDomainObjectQuery.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/dao/query/ajax/StudySearchableAjaxableDomainObjectQuery.java
@@ -13,10 +13,11 @@ public class StudySearchableAjaxableDomainObjectQuery extends AbstractAjaxableDo
     private static String queryString = "Select study.id,study.shortTitle," +
             "identifier.value,identifier.primaryIndicator,study.phaseCode,study.status," +
             "(select sfs.organization.nciInstituteCode from StudyFundingSponsor sfs  where sfs.study.id =study.id) as fundingSponsor, " +
-            "ss.organization.name,ss.id,ss.class,ss.organization.nciInstituteCode " +
+            "ss.organization.name,ss.id,ss.class,ss.organization.nciInstituteCode, stper.researchStaff.id " +
             "from Study study " +
             "left join study.identifiers as identifier " +
-            "left join study.studyOrganizations as ss " +
+           // "left join study.studyOrganizations as ss  " +
+            "join study.studyOrganizations as ss left join ss.studyPersonnelsInternal as stper " +
             "order by study.shortTitle ";
 
 
