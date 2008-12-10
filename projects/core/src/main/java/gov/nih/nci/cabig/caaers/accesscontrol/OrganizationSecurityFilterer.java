e5fa5c567a1fb1d5dafbee4fe0a43ef3c720dd47
Srini Akkala <akkalas@gmail.com>
2008-12-10 03:03:51 +0000

    SVN-Revision: 7411

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/OrganizationSecurityFilterer.java
@@ -0,0 +1,76 @@
+package gov.nih.nci.cabig.caaers.accesscontrol;
+
+import gov.nih.nci.cabig.caaers.dao.ResearchStaffDao;
+import gov.nih.nci.cabig.caaers.dao.StudyDao;
+import gov.nih.nci.cabig.caaers.dao.query.ResearchStaffQuery;
+import gov.nih.nci.cabig.caaers.domain.ExpeditedAdverseEventReport;
+import gov.nih.nci.cabig.caaers.domain.Organization;
+import gov.nih.nci.cabig.caaers.domain.ResearchStaff;
+import gov.nih.nci.cabig.caaers.domain.Study;
+import gov.nih.nci.cabig.caaers.domain.StudyOrganization;
+import gov.nih.nci.cabig.caaers.domain.StudyPersonnel;
+import gov.nih.nci.cabig.caaers.domain.StudySite;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.acegisecurity.Authentication;
+import org.acegisecurity.GrantedAuthority;
+import org.acegisecurity.userdetails.User;
+
+public class OrganizationSecurityFilterer implements DomainObjectSecurityFilterer {
+	
+	private ResearchStaffDao researchStaffDao;
+
+
+	public Object filter(Authentication authentication, String permission, Object returnObject) {
+		
+		return ((Filterer)returnObject).getFilteredObject();
+		/*
+		//get user
+		User user = (User)authentication.getPrincipal();
+		
+		//no filtering if super user
+        GrantedAuthority[] grantedAuthorities = user.getAuthorities();
+        for (int i=0; i<grantedAuthorities.length; i++) {
+        	GrantedAuthority grantedAuthority = (GrantedAuthority)grantedAuthorities[i];
+        	if ( grantedAuthority.getAuthority().equals("ROLE_caaers_super_user")) {
+        		if (returnObject instanceof Filterer) {
+        			return ((Filterer)returnObject).getFilteredObject();
+        		} else {
+        			return returnObject;
+        		}
+        		
+        	}
+        }
+             
+        
+        // get research staff and associated organization.
+		ResearchStaffQuery rsQuery = new ResearchStaffQuery();
+    	rsQuery.filterByLoginId(user.getUsername());
+        List<ResearchStaff> rsList = researchStaffDao.searchResearchStaff(rsQuery);
+        
+        ResearchStaff researchStaff = rsList.get(0);
+        Organization userOrganization = researchStaff.getOrganization();
+        
+		Filterer filterer = (Filterer)returnObject;
+		Iterator collectionIter = filterer.iterator();
+		
+		while (collectionIter.hasNext()) {
+        	Object domainObject = collectionIter.next();
+        	Organization organization = (Organization)domainObject;
+        	if (!organization.getNciInstituteCode().equals(userOrganization.getNciInstituteCode())) {
+        		filterer.remove(organization);
+        	}
+        }
+		
+		return filterer.getFilteredObject();
+		*/
+	}
+
+	public void setResearchStaffDao(ResearchStaffDao researchStaffDao) {
+		this.researchStaffDao = researchStaffDao;
+	}
+
+
+}
