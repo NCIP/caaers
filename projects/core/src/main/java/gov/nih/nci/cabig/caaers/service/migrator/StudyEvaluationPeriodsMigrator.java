0676ca5f3e90bce36826260d6084c7ed8e18c7f0
Monish Dombla <monish.dombla@semanticbits.com>
2008-12-18 16:39:53 +0000

    New Migrator for Epochs
    SVN-Revision: 7458

---
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/service/migrator/StudyEvaluationPeriodsMigrator.java
@@ -0,0 +1,86 @@
+package gov.nih.nci.cabig.caaers.service.migrator;
+
+import gov.nih.nci.cabig.caaers.dao.CtcTermDao;
+import gov.nih.nci.cabig.caaers.dao.meddra.LowLevelTermDao;
+import gov.nih.nci.cabig.caaers.domain.Arm;
+import gov.nih.nci.cabig.caaers.domain.CtcTerm;
+import gov.nih.nci.cabig.caaers.domain.Epoch;
+import gov.nih.nci.cabig.caaers.domain.SolicitedAdverseEvent;
+import gov.nih.nci.cabig.caaers.domain.Study;
+import gov.nih.nci.cabig.caaers.domain.meddra.LowLevelTerm;
+import gov.nih.nci.cabig.caaers.service.DomainObjectImportOutcome;
+
+import java.util.List;
+
+public class StudyEvaluationPeriodsMigrator implements Migrator<Study> {
+	
+	private CtcTermDao ctcTermDao;
+	private LowLevelTermDao lowLevelTermDao;
+	
+	public void migrate(Study src, Study dest,
+			DomainObjectImportOutcome<Study> outcome) {
+		
+		List<Epoch> xmlEpochs = src.getEpochs();
+		if(xmlEpochs != null && !xmlEpochs.isEmpty()){
+			Epoch domainEpoch = null;
+			for(Epoch xmlEpoch : xmlEpochs){
+				domainEpoch = new Epoch();
+				domainEpoch.setName(xmlEpoch.getName());
+				domainEpoch.setDescriptionText(xmlEpoch.getDescriptionText());
+				domainEpoch.setEpochOrder(xmlEpoch.getEpochOrder());
+				if(xmlEpoch.getArms() != null && !xmlEpoch.getArms().isEmpty()){
+					Arm domainArm = null;
+					for(Arm xmlArm : xmlEpoch.getArms()){
+						domainArm = new Arm();
+						domainArm.setName(xmlArm.getName());
+						domainArm.setDescriptionText(xmlArm.getDescriptionText());
+						if(xmlArm.getSolicitedAdverseEvents() != null && !xmlArm.getSolicitedAdverseEvents().isEmpty()){
+							SolicitedAdverseEvent domainSolicitedAdverseEvent = null;
+							for(SolicitedAdverseEvent xmlSolicitedAdverseEvent : xmlArm.getSolicitedAdverseEvents()){
+								domainSolicitedAdverseEvent = new SolicitedAdverseEvent();
+								if(xmlSolicitedAdverseEvent.getCtcterm() != null && !"".equals(xmlSolicitedAdverseEvent.getCtcterm().getCtepCode())){
+									if(dest.getAeTerminology().getCtcVersion() != null){
+										List<CtcTerm> ctcTerms = ctcTermDao.getByCtepCodeandVersion(xmlSolicitedAdverseEvent.getCtcterm().getCtepCode(), dest.getAeTerminology().getCtcVersion().getId());
+										if(ctcTerms != null && !ctcTerms.isEmpty()){
+											domainSolicitedAdverseEvent.setCtcterm(ctcTerms.get(0));
+										}
+									}
+								}
+								if(xmlSolicitedAdverseEvent.getLowLevelTerm() != null && !"".equals(xmlSolicitedAdverseEvent.getLowLevelTerm().getMeddraCode())){
+									if(dest.getAeTerminology().getMeddraVersion() != null){
+										List<LowLevelTerm> lowLevelTerms = lowLevelTermDao.getByMeddraCodeandVersion(xmlSolicitedAdverseEvent.getLowLevelTerm().getMeddraCode(), dest.getAeTerminology().getMeddraVersion().getId());
+										if(lowLevelTerms != null && !lowLevelTerms.isEmpty()){
+											domainSolicitedAdverseEvent.setLowLevelTerm(lowLevelTerms.get(0));
+										}
+									}
+								}
+								if(xmlSolicitedAdverseEvent.getOtherTerm() != null && !"".equals(xmlSolicitedAdverseEvent.getOtherTerm().getMeddraCode())){
+									if(dest.getOtherMeddra() != null){
+										List<LowLevelTerm> lowLevelTerms = lowLevelTermDao.getByMeddraCodeandVersion(xmlSolicitedAdverseEvent.getOtherTerm().getMeddraCode(), dest.getOtherMeddra().getId());
+										if(lowLevelTerms != null && !lowLevelTerms.isEmpty()){
+											domainSolicitedAdverseEvent.setOtherTerm(lowLevelTerms.get(0));
+										}
+									}
+								}
+								domainArm.getSolicitedAdverseEvents().add(domainSolicitedAdverseEvent);
+							}
+						}
+						domainEpoch.getArms().add(domainArm);
+					}
+				}
+				dest.getEpochs().add(domainEpoch);
+			}
+		}
+	}
+
+	public void setCtcTermDao(CtcTermDao ctcTermDao) {
+		this.ctcTermDao = ctcTermDao;
+	}
+
+	public void setLowLevelTermDao(LowLevelTermDao lowLevelTermDao) {
+		this.lowLevelTermDao = lowLevelTermDao;
+	}
+	
+}
+
+
