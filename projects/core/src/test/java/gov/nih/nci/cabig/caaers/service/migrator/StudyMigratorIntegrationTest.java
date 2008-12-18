bd934ffb9262e5420af25eb32c18efb01637b90f
Monish Dombla <monish.dombla@semanticbits.com>
2008-12-18 17:05:18 +0000

    added another migrator.
    SVN-Revision: 7460

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/service/migrator/StudyMigratorIntegrationTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/service/migrator/StudyMigratorIntegrationTest.java
@@ -23,7 +23,7 @@ public class StudyMigratorIntegrationTest extends CaaersTestCase {
 	public void testLoad(){
 		StudyMigrator migrator = (StudyMigrator)getDeployedApplicationContext().getBean("studyMigrator");
 		List<Migrator<Study>> migrators = migrator.getChildren();
-		assertEquals(8, migrators.size());
+		assertEquals(9, migrators.size());
 		assertTrue(migrator.getChildren().get(4) instanceof IdentifierMigrator);
 	}
 	
