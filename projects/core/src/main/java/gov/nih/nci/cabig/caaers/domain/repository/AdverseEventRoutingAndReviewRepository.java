efbb4324932f553adc8e582618009094b575ce20
Biju Joseph <biju.joseph@semanticbits.com>
2008-12-29 20:17:25 +0000

    CAAERS-1305 - Implementation of routing and review
    SVN-Revision: 7496

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/repository/AdverseEventRoutingAndReviewRepository.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/repository/AdverseEventRoutingAndReviewRepository.java
@@ -8,27 +8,71 @@ import gov.nih.nci.cabig.caaers.domain.ReviewStatus;
 import gov.nih.nci.cabig.caaers.domain.Study;
 import gov.nih.nci.cabig.caaers.domain.StudySite;
 import gov.nih.nci.cabig.caaers.domain.dto.AdverseEventReportingPeriodDTO;
+import gov.nih.nci.cabig.caaers.domain.report.Report;
 import gov.nih.nci.cabig.caaers.domain.workflow.ReviewComment;
 /**
+ * This interface has methods to save and retrieve the {@link ReviewComment}s, and to change the review state of {@link Report} and {@link AdverseEventReportingPeriod} 
  * 
- * Will query the {@link AdverseEventReportingPeriod} objects, matching the criteria. 
  * 
  * @author Biju Joseph
  *
  */
 public interface AdverseEventRoutingAndReviewRepository {
-	
+	/**
+	 * Will query the {@link AdverseEventReportingPeriodDTO} objects, matching the criteria. 
+	 * @param participant
+	 * @param study
+	 * @param studySite
+	 * @param reviewStatus
+	 * @return
+	 */
 	public List<AdverseEventReportingPeriodDTO> findAdverseEventReportingPeriods(Participant participant, Study study, StudySite studySite, ReviewStatus reviewStatus);
 	
+	/**
+	 * Will list the review comments associated to a report
+	 * @param reportId
+	 * @return
+	 */
 	public List<? extends ReviewComment> fetchReviewCommentsForReport(Integer reportId);
 	
+	/**
+	 * Will list the review comments associated to reporting period
+	 * @param rpId
+	 * @return
+	 */
 	public List<? extends ReviewComment> fetchReviewCommentsForReportingPeriod(Integer rpId);
 	
+	/**
+	 * Will add a review comment against the {@link Report}
+	 * @param reportId
+	 * @param comment
+	 * @param userId
+	 */
 	public void addReportReviewComment(Integer reportId, String comment, String userId);
 	
+	/**
+	 * Will add a review comment against the reporting period
+	 * @param reportingPeriodId
+	 * @param comment
+	 * @param userId
+	 */
 	public void addReportingPeriodReviewComment(Integer reportingPeriodId, String comment, String userId);
 	
+	/**
+	 * Will advance the workflow to its next step, for the Report
+	 * @param workflowId
+	 * @param reviewStatus
+	 * @param id
+	 * @return
+	 */
 	public List<ReviewStatus> advanceReportWorkflow(Integer workflowId, ReviewStatus reviewStatus, Integer id);
 	
+	/**
+	 * Will advance the workflow to its next step, for the reporting period
+	 * @param workflowId
+	 * @param reviewStatus
+	 * @param id
+	 * @return
+	 */
 	public List<ReviewStatus> advanceReportingPeriodWorkflow(Integer workflowId, ReviewStatus reviewStatus, Integer id);
 }
