a4c3cd1377e4ca242f41326eaab90a1182d73ff4
Biju Joseph <biju.joseph@semanticbits.com>
2009-01-02 21:49:43 +0000

    caaers-1523 correcting the testcase failure at hudson
    SVN-Revision: 7510

---
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/domain/InvestigatorTest.java
@@ -0,0 +1,38 @@
+package gov.nih.nci.cabig.caaers.domain;
+
+import junit.framework.TestCase;
+/**
+ * 
+ * @author Biju Joseph
+ *
+ */
+public class InvestigatorTest extends TestCase {
+	
+	private Investigator inv;
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+		inv = new  Investigator();
+		inv.setEmailAddress("joel.biju@kk.com");
+		inv.setFirstName("Joel");
+		inv.setLastName("Joseph");
+		
+	}
+	public void testHashCode() {
+		int hashcode = inv.hashCode();
+		assertNotNull(hashcode);
+	}
+
+	public void testEqualsObject() {
+		Investigator inv2 = new  Investigator();
+		inv2.setEmailAddress("joel.biju@kk.com");
+		inv2.setFirstName("Joel");
+		inv2.setLastName("Joseph");
+		
+		assertTrue(inv.equals(inv2));
+		inv2.setEmailAddress("jank@rediffmail.com");
+		assertFalse(inv.equals(inv2));
+	}
+
+}
