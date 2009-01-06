add1a1d185f052ca838e317708ce64311b298b57
Monish Dombla <monish.dombla@semanticbits.com>
2009-01-06 16:51:41 +0000

    More Tests added for Code Coverage
    SVN-Revision: 7559

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/LabLoadDaoTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/LabLoadDaoTest.java
@@ -2,8 +2,19 @@ package gov.nih.nci.cabig.caaers.dao;
 
 import gov.nih.nci.cabig.caaers.DaoTestCase;
 import gov.nih.nci.cabig.caaers.domain.LabLoad;
+import gov.nih.nci.cabig.caaers.domain.StudyParticipantAssignment;
+
+import java.util.Calendar;
+import java.util.List;
 
 public class LabLoadDaoTest extends DaoTestCase<LabLoadDao> {
+	
+	private StudyParticipantAssignmentDao assignmentDao = (StudyParticipantAssignmentDao) getApplicationContext().getBean("studyParticipantAssignmentDao");
+	
+	public void testGetDomainClass(){
+		Object obj = getDao().domainClass();
+		assertNotNull(obj);
+	}
 	
 	public void testGet() throws Exception {
 		LabLoad loaded = getDao().getById(-1001);
@@ -11,4 +22,39 @@ public class LabLoadDaoTest extends DaoTestCase<LabLoadDao> {
         assertEquals("New Lab", loaded.getName());
     }
 	
+	public void testSave(){
+		LabLoad labLoad = new LabLoad();
+		StudyParticipantAssignment assignment = assignmentDao.getById(-14);
+		assignment.setId(-14);
+		labLoad.setName("Name");
+		labLoad.setLabDate(Calendar.getInstance().getTime());
+		labLoad.setResult("abc");	
+		labLoad.setDismissed(false);
+		labLoad.setAssignment(assignment);
+		getDao().save(labLoad);
+		List<LabLoad> labs = getDao().getByAssignment(assignment);
+		assertEquals(3, labs.size());
+	}
+	
+	public void testGetByAssignment(){
+		StudyParticipantAssignment assignment = assignmentDao.getById(-14);
+		List<LabLoad> labs = getDao().getByAssignment(assignment);
+		assertEquals(2, labs.size());
+	}
+	
+	public void testDeleteById(){
+		getDao().deleteById(-1002);
+		StudyParticipantAssignment assignment = assignmentDao.getById(-14);
+		List<LabLoad> labs = getDao().getByAssignment(assignment);
+		assertEquals(1, labs.size());
+	}
+	
+	public void testDelete(){
+		StudyParticipantAssignment assignment = assignmentDao.getById(-14);
+		List<LabLoad> labs = getDao().getByAssignment(assignment);
+		assertEquals(2, labs.size());
+		getDao().delete(labs.get(0));
+		labs = getDao().getByAssignment(assignment);
+		assertEquals(1, labs.size());
+	}
 }
