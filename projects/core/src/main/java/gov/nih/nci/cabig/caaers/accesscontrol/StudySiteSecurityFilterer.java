810a668a866e1b496f7bf93eb7d3225625aa5006
Srini Akkala <akkalas@gmail.com>
2009-01-14 17:43:04 +0000

    commented ROLE_caaers_physician
    SVN-Revision: 7653

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/StudySiteSecurityFilterer.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/StudySiteSecurityFilterer.java
@@ -64,7 +64,9 @@ public class StudySiteSecurityFilterer implements DomainObjectSecurityFilterer {
         for (int i=0; i<grantedAuthorities.length; i++) {
         	GrantedAuthority grantedAuthority = (GrantedAuthority)grantedAuthorities[i];
         	if ( grantedAuthority.getAuthority().equals("ROLE_caaers_participant_cd") 
-        			|| grantedAuthority.getAuthority().equals("ROLE_caaers_ae_cd") || grantedAuthority.getAuthority().equals("ROLE_caaers_physician")) {
+        			|| grantedAuthority.getAuthority().equals("ROLE_caaers_ae_cd") 
+        			//|| grantedAuthority.getAuthority().equals("ROLE_caaers_physician")
+        			) {
         		studyFilteringRequired = true;
         		break;
         	}
