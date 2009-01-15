8c4cab22ba8f1372407086fc3287d8931b840339
Srini Akkala <akkalas@gmail.com>
2009-01-15 06:08:39 +0000

    SVN-Revision: 7674

--- projects/jbi/caaers-adeers-service-assembly/caaers-adeers-webservice-su/src/test/java/webservice/adeers/SubmitToAdeersTest.java
+++ projects/jbi/caaers-adeers-service-assembly/caaers-adeers-webservice-su/src/test/java/webservice/adeers/SubmitToAdeersTest.java
@@ -12,6 +12,15 @@ public class SubmitToAdeersTest extends BaseCaller {
 		String caaersXMLFile = "Study-5876-agent-adjust.xml"; 		
 		transformAndSubmitSuccessPath(caaersXMLFile);
 	}
+	public void test5876CtepAndNonCtepAgent() throws Exception {
+		String caaersXMLFile = "Study-5876-ctep-and-nonCtep-agent.xml"; 		
+		transformAndSubmitSuccessPath(caaersXMLFile);
+	}
+	public void test5876DuplicatePriorTherapies() throws Exception {
+		String caaersXMLFile = "Study-5876-duplicate-prior-therapies.xml"; 		
+		AEReportJobInfo aeReportJobInfo = transformAndSubmitSuccessPath(caaersXMLFile);
+		assertEquals("SUCCESS", aeReportJobInfo.getReportStatus().toString());
+	}
 	public void testN027DRadiation() throws Exception {
 		String caaersXMLFile = "Study-N027D-radiation.xml"; 		
 		transformAndSubmitSuccessPath(caaersXMLFile);
@@ -34,5 +43,22 @@ public class SubmitToAdeersTest extends BaseCaller {
     	} catch (Exception e) {
     		throw new Exception ("caAERS to AdEERS submission Failed ", e);
     	}		
+	}
+	private AEReportJobInfo transformAndSubmit(String caaersXMLFile) throws Exception {
+    	String detachedXML = "";
+    	String tempPath = "caaers-adeers-webservice-su/src/test/resources/webservice/adeers/";
+		try {
+			detachedXML = transform(tempPath+caaersXMLFile);
+    	} catch (Exception e) {
+    		throw new Exception ("caAERS to AdEERS transformation Failed ", e);
+    	}
+    	AEReportJobInfo aeReportJobInfo = null;
+    	try {
+    		aeReportJobInfo = submit(detachedXML);
+           
+    	} catch (Exception e) {
+    		throw new Exception ("caAERS to AdEERS submission Failed ", e);
+    	}	
+    	return aeReportJobInfo;
 	}
 }
