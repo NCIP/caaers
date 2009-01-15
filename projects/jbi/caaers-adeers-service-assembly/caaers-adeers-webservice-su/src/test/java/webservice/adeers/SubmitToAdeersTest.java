59adb85504067de6f4871cd79f7fe3d0c5b7c3a7
Srini Akkala <akkalas@gmail.com>
2009-01-15 05:56:11 +0000

    SVN-Revision: 7673

--- projects/jbi/caaers-adeers-service-assembly/caaers-adeers-webservice-su/src/test/java/webservice/adeers/SubmitToAdeersTest.java
+++ projects/jbi/caaers-adeers-service-assembly/caaers-adeers-webservice-su/src/test/java/webservice/adeers/SubmitToAdeersTest.java
@@ -3,12 +3,25 @@ package webservice.adeers;
 import caaers.client.AEReportJobInfo;
 
 public class SubmitToAdeersTest extends BaseCaller {
-	private String tempPath = "caaers-adeers-webservice-su/src/test/resources/webservice/adeers/";
+	
 	public void test5876AgentOnly() throws Exception {
-		String caaersXMLFile = tempPath+"Study-5876-agent-only.xml"; 		
+		String caaersXMLFile = "Study-5876-agent-only.xml"; 		
+		transformAndSubmitSuccessPath(caaersXMLFile);
+	}
+	public void test5876AgentAdjust() throws Exception {
+		String caaersXMLFile = "Study-5876-agent-adjust.xml"; 		
+		transformAndSubmitSuccessPath(caaersXMLFile);
+	}
+	public void testN027DRadiation() throws Exception {
+		String caaersXMLFile = "Study-N027D-radiation.xml"; 		
+		transformAndSubmitSuccessPath(caaersXMLFile);
+	}
+	
+	private void transformAndSubmitSuccessPath(String caaersXMLFile) throws Exception {
     	String detachedXML = "";
+    	String tempPath = "caaers-adeers-webservice-su/src/test/resources/webservice/adeers/";
 		try {
-			detachedXML = transform(caaersXMLFile);
+			detachedXML = transform(tempPath+caaersXMLFile);
     	} catch (Exception e) {
     		throw new Exception ("caAERS to AdEERS transformation Failed ", e);
     	}
@@ -20,6 +33,6 @@ public class SubmitToAdeersTest extends BaseCaller {
             
     	} catch (Exception e) {
     		throw new Exception ("caAERS to AdEERS submission Failed ", e);
-    	}
+    	}		
 	}
 }
