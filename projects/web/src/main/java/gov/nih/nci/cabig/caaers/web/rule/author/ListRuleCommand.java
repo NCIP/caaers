2fb1c3aa6347cea4c81a79791df1dba3ada4ba6b
Srini Akkala <akkalas@gmail.com>
2009-01-21 20:02:31 +0000

    CAAERS-1570
    SVN-Revision: 7740

--- projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/rule/author/ListRuleCommand.java
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/rule/author/ListRuleCommand.java
@@ -2,9 +2,12 @@ package gov.nih.nci.cabig.caaers.web.rule.author;
 
 import gov.nih.nci.cabig.caaers.rules.author.RuleAuthoringService;
 import gov.nih.nci.cabig.caaers.rules.brxml.RuleSet;
+import gov.nih.nci.cabig.caaers.utils.CollectionFilterer;
+import gov.nih.nci.cabig.caaers.utils.Filterer;
 import gov.nih.nci.cabig.caaers.web.rule.RuleInputCommand;
 
 import java.rmi.RemoteException;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -17,6 +20,15 @@ public class ListRuleCommand implements RuleInputCommand {
 
     public ListRuleCommand(RuleAuthoringService ruleAuthoringService) throws RemoteException {
         ruleSets = ruleAuthoringService.getAllRuleSets();
+        Filterer filterer = new CollectionFilterer(ruleSets); 
+        Iterator collectionIter = filterer.iterator();
+        while (collectionIter.hasNext()) {
+        	RuleSet ruleSet = (RuleSet)collectionIter.next();
+        	if (ruleSet.getDescription().equals("The default rule package")) {
+        		filterer.remove(ruleSet);
+        	}
+        }
+        ruleSets = (List<RuleSet>) filterer.getFilteredObject();
         // ruleSets.remove(0);//removing the default
     }
 
