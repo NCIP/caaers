2516db88eda6c1d82956ce17442f07383e81b30c
Biju Joseph <biju.joseph@semanticbits.com>
2009-02-11 00:16:29 +0000

    fixing the test failures
    SVN-Revision: 8059

--- projects/web/src/test/java/gov/nih/nci/cabig/caaers/web/ae/CaptureAdverseEventInputCommandTest.java
+++ projects/web/src/test/java/gov/nih/nci/cabig/caaers/web/ae/CaptureAdverseEventInputCommandTest.java
@@ -1,6 +1,6 @@
 package gov.nih.nci.cabig.caaers.web.ae;
 
-import gov.nih.nci.cabig.caaers.AbstractTestCase;
+import gov.nih.nci.cabig.caaers.AbstractNoSecurityTestCase;
 import gov.nih.nci.cabig.caaers.dao.AdverseEventReportingPeriodDao;
 import gov.nih.nci.cabig.caaers.dao.StudyDao;
 import gov.nih.nci.cabig.caaers.dao.StudyParticipantAssignmentDao;
@@ -19,7 +19,7 @@ import java.util.List;
  * @author biju
  *
  */
-public class CaptureAdverseEventInputCommandTest extends AbstractTestCase {
+public class CaptureAdverseEventInputCommandTest extends AbstractNoSecurityTestCase {
 	
 	CaptureAdverseEventInputCommand command;
 	
