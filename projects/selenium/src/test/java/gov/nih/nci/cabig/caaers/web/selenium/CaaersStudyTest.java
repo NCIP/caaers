425f363fbe6b4f8728b6cc7c18fdd8ff7116ef96
Karthik Iyer <iyerk0@gmail.com>
2009-02-12 19:55:33 +0000

    latest selenium updates
    SVN-Revision: 8097

--- projects/selenium/src/test/java/gov/nih/nci/cabig/caaers/web/selenium/CaaersStudyTest.java
+++ projects/selenium/src/test/java/gov/nih/nci/cabig/caaers/web/selenium/CaaersStudyTest.java
@@ -9,41 +9,64 @@ public class CaaersStudyTest extends CaaersSeleniumTestCase {
 	
 
 	public void testCreateCTCStudy() throws Exception {
-		checkLogin();
-		createCTCStudy(studyId+"-ctc");
-		assertTrue("Create Study failure", selenium
-				.isTextPresent("The following study is saved successfully"));
+		try {
+			checkLogin();
+			createCTCStudy(studyId+"-ctc");
+			assertTrue("Create Study failure", selenium
+					.isTextPresent("The following study is saved successfully"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("test failed");
+		}
 	}
 	
 	public void testCreateMeddraStudy() throws Exception {
-		checkLogin();
-		createMeddraStudy(studyId+"-meddra");
-		assertTrue("Create Study failure", selenium
-				.isTextPresent("The following study is saved successfully"));
+		try {
+			checkLogin();
+			createMeddraStudy(studyId+"-meddra");
+			assertTrue("Create Study failure", selenium
+					.isTextPresent("The following study is saved successfully"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("test failed");
+		}
 	}
 	public void testSearchStudy() throws Exception {
-		checkLogin();
-		studyId="N027D";
-		searchStudy(studyId);
-	
-		assertTrue(
-				"Study with id:" + studyId + "was not found",
-				selenium
-						.isTextPresent("Phase I Study of CCI-779 and Temozolomide in Combination with Radiation Therapy in Glioblastoma Multiforme"));
+		try {
+			checkLogin();
+			studyId = "N027D";
+			searchStudy(studyId);
+			assertTrue(
+					"Study with id:" + studyId + "was not found",
+					selenium
+							.isTextPresent("Phase I Study of CCI-779 and Temozolomide in Combination with Radiation Therapy in Glioblastoma Multiforme"));
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			fail("test failed");
+		}
 	}
 
 	public void testEditStudy() throws Exception {
-		checkLogin();
-		//studyId=studyId+"-ctc";
-		studyId="N027D";
-		searchStudy(studyId);
-		String studyLink = selenium
-				.getAttribute("//a[text()='"+studyId+"']@href");
-		selenium.click("link="+studyId);
-		selenium.waitForPageToLoad("30000");
-		editStudy();
-		assertTrue("Create Study failure", true);
-		assertTrue("Create Study failure", selenium.isTextPresent("Information saved successfully"));
+		try {
+			checkLogin();
+			//studyId=studyId+"-ctc";
+			studyId="N027D";
+			searchStudy(studyId);
+			String studyLink = selenium
+					.getAttribute("//a[text()='"+studyId+"']@href");
+			selenium.click("link="+studyId);
+			selenium.waitForPageToLoad("30000");
+			editStudy();
+			assertTrue("Create Study failure", true);
+			assertTrue("Create Study failure", selenium.isTextPresent("Information saved successfully"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("test failed");
+		}
 	}
 
 }
