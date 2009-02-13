1c9b98b563b688d3f6d92de54b1d7b97f1596bfa
Ion Olaru <ion.olaru@gmail.com>
2009-02-13 20:26:19 +0000

    CAAERS-1495
    SVN-Revision: 8107

---
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/NotificationDaoTest.java
@@ -0,0 +1,44 @@
+package gov.nih.nci.cabig.caaers.dao;
+
+import static gov.nih.nci.cabig.caaers.CaaersUseCase.MAPPING_VOCAB;
+import gov.nih.nci.cabig.caaers.CaaersUseCases;
+import gov.nih.nci.cabig.caaers.DaoTestCase;
+import gov.nih.nci.cabig.caaers.domain.*;
+
+import java.util.List;
+
+/**
+ * @author Ion C. Olaru
+ */
+
+public class NotificationDaoTest extends DaoTestCase<NotificationDao> {
+
+    /*
+   * Test the loading by ID functionality
+   *
+   * */
+    public void testDomainClass() throws Exception {
+        Class<Notification> n = getDao().domainClass();
+        assertNotNull(n);
+    }
+
+    public void testGetById() throws Exception {
+        Notification notification = getDao().getById(-1);
+        assertNotNull(notification);
+        assertEquals("Wrong Object", -18, (int) notification.getId());
+    }
+
+    public void testSave() throws Exception {
+        Notification notification = new Notification();
+        notification.setContent("C");
+        notification.setGridId("");
+        getDao().save(notification);
+        assertEquals(true, notification.getId() > 0);
+    }
+
+    public void testGetAll() throws Exception {
+        List<Notification> all = getDao().getAll();
+        assertEquals(3, all.size());
+    }
+
+}
