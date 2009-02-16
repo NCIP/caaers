45e8b39386dab9217e209345392f2f27a543b452
Sameer Sawant <sameer.sawant@semanticbits.com>
2009-02-16 18:28:49 +0000

    CAAERS-1598
    SVN-Revision: 8149

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/ajax/AdverseEventReportingPeriodAjaxableDomainObject.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/domain/ajax/AdverseEventReportingPeriodAjaxableDomainObject.java
@@ -1,5 +1,8 @@
 package gov.nih.nci.cabig.caaers.domain.ajax;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 /**
  *
@@ -7,6 +10,12 @@ package gov.nih.nci.cabig.caaers.domain.ajax;
 public class AdverseEventReportingPeriodAjaxableDomainObject extends AbstractAjaxableDomainObject {
 
 	private String name;
+	private String startDate;
+	private String endDate;
+	private String epochName;
+	private String tacCode;
+	private String tacDescription;
+	private Integer cycleNumber;
 	
 	public void setName(String name){
 		this.name = name;
@@ -14,5 +23,61 @@ public class AdverseEventReportingPeriodAjaxableDomainObject extends AbstractAja
 	
 	public String getName(){
 		return name;
+	}
+	
+	public void setStartDate(Date startDate){
+		if(startDate != null){
+			SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+		    this.startDate = formatter.format(startDate);
+		}else
+			this.startDate = "";
+	}
+	
+	public String getStartDate(){
+		return startDate;
+	}
+	
+	public void setEndDate(Date endDate){
+		if(endDate != null){
+			SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+		    this.endDate = formatter.format(endDate);
+		}else
+			this.endDate = "";
+	}
+	
+	public String getEndDate(){
+		return endDate;
+	}
+	
+	public void setEpochName(String epochName){
+		this.epochName = epochName;
+	}
+	
+	public String getEpochName(){
+		return epochName;
+	}
+	
+	public void setTacCode(String tacCode){
+		this.tacCode = tacCode;
+	}
+	
+	public String getTacCode(){
+		return tacCode;
+	}
+	
+	public void setTacDescription(String tacDescription){
+		this.tacDescription = tacDescription;
+	}
+	
+	public String getTacDescription(){
+		return tacDescription;
+	}
+	
+	public void setCycleNumber(Integer cycleNumber){
+		this.cycleNumber = cycleNumber;
+	}
+	
+	public Integer getCycleNumber(){
+		return cycleNumber;
 	}
 }
