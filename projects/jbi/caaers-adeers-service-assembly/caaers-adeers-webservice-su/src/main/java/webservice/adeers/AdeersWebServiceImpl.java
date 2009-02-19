0811f3b8f0af722e5e8462cb434d1d302a6ce2e7
Monish Dombla <monish.dombla@semanticbits.com>
2009-02-19 21:19:09 +0000

    Log statements added.
    SVN-Revision: 8250

--- projects/jbi/caaers-adeers-service-assembly/caaers-adeers-webservice-su/src/main/java/webservice/adeers/AdeersWebServiceImpl.java
+++ projects/jbi/caaers-adeers-service-assembly/caaers-adeers-webservice-su/src/main/java/webservice/adeers/AdeersWebServiceImpl.java
@@ -32,27 +32,19 @@ public class AdeersWebServiceImpl implements AdeersWebService {
 	
 	private String submitAEDataXMLAsAttachment(String aeReportWithCaaersId) throws Exception {
 		 
-		//log.info(" REPORT ID " + aeReportWithCaaersId);
+		log.info("caAERS-adEERS-Service-Assembly processing report submitted by caAERS bering Report Id " + aeReportWithCaaersId);
 		String aeReport = detach(aeReportWithCaaersId);	
-		//System.out.println("EXTERNAL EPRS " + this.externalEPRs);
 		String adeersEPR = externalEPRs.split(",")[0];
-		
 		String url=adeersEPR.split("::")[0];
 		String uid=adeersEPR.split("::")[1];
 		String pwd=adeersEPR.split("::")[2];
-		
 		String clientTrustStore = "caAERs-AdEERS";
 		String userDir = System.getProperty("user.home");
 		String fileSeparator = System.getProperty("file.separator");
-		//String clientAbsoluteTrustStore = System.getenv("SERVICEMIX_HOME") + fileSeparator + clientTrustStore;
 		String clientAbsoluteTrustStore = System.getProperty("user.home") + fileSeparator + clientTrustStore;
 
-		log.info("PATH " + clientAbsoluteTrustStore);
-		
-    	//System.setProperty("javax.net.ssl.trustStore", "/home/caaers_dev/apache-servicemix-3.1.2/conf/caAERs-AdEERS");
-		//System.setProperty("javax.net.ssl.trustStore", "/Users/sakkala/temp/certs/caAERs-AdEERS");
+		//log.info("PATH " + clientAbsoluteTrustStore);
 		System.setProperty("javax.net.ssl.trustStore", clientAbsoluteTrustStore);
-		
 		AEReportXMLServiceSoapBindingStub binding;
         try {
             binding = (AEReportXMLServiceSoapBindingStub)   new AEReportXMLService_ServiceLocator(url).getAEReportXMLService();
@@ -60,27 +52,23 @@ public class AdeersWebServiceImpl implements AdeersWebService {
         catch (javax.xml.rpc.ServiceException jre) {
             if(jre.getLinkedCause()!=null)
                 jre.getLinkedCause().printStackTrace();
+            log.error("caAERS-adEERS-Service-Assembly caught JAX-RPC ServiceException :", jre);
             throw new junit.framework.AssertionFailedError("JAX-RPC ServiceException caught: " + jre);
         }
         // Time out after a minute
         binding.setTimeout(60000);
         binding.setUsername(uid);
         binding.setPassword(pwd);
-  
         	
         StringReader reader = new StringReader(aeReport);
-        
-        System.out.println("SUBMITTING TO WEB SERVICE ..." + clientAbsoluteTrustStore);
+        log.info("Submitting to adEERS...");
         Source attachment = new StreamSource(reader,"");
         //call the web service                
         binding.submitAEDataXMLAsAttachment(ReportingMode.SYNCHRONOUS, attachment);
         String s = binding._getCall().getMessageContext().getResponseMessage().getSOAPPartAsString();
-
-        
         //attach the id to the returned message
         s=s.replaceAll("</ns1:AEReportJobInfo>","<CAEERS_AEREPORT_ID>"+caaersAeReportId+"</CAEERS_AEREPORT_ID><REPORT_ID>"+reportId+"</REPORT_ID><SUBMITTER_EMAIL>"+submitterEmail+"</SUBMITTER_EMAIL></ns1:AEReportJobInfo>");
-        
-        //System.out.println(s);
+        log.info("Response Received from adEERS");
         
         return s;
 		
@@ -115,64 +103,36 @@ public class AdeersWebServiceImpl implements AdeersWebService {
 		return aeReport;
 	}
 	
-	public static void main (String[] args){
-		AdeersWebServiceImpl a = new AdeersWebServiceImpl();
-		String str1 = "";
-		try {
-			//FileInputStream fs = new FileInputStream("/Users/sakkala/tech/adeers/instance0.xml");
-			
-			
-			//fs.close();
-			
-			FileReader input = new FileReader("/Users/sakkala/tech/adeers/instance2.xml");
-			BufferedReader bufRead = new BufferedReader(input);
-			String line = bufRead.readLine();
-			
-			while (line != null) {
-				str1 = str1 + line; 
-				line = bufRead.readLine();
-			}
-			System.out.println(str1);
-			
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		
-		try {
-			a.submitAEDataXMLAsAttachment(str1);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-	}
-	
+//	public static void main (String[] args){
+//		AdeersWebServiceImpl a = new AdeersWebServiceImpl();
+//		String str1 = "";
+//		try {
+//			//FileInputStream fs = new FileInputStream("/Users/sakkala/tech/adeers/instance0.xml");
+//			
+//			
+//			//fs.close();
+//			
+//			FileReader input = new FileReader("/Users/sakkala/tech/adeers/instance2.xml");
+//			BufferedReader bufRead = new BufferedReader(input);
+//			String line = bufRead.readLine();
+//			
+//			while (line != null) {
+//				str1 = str1 + line; 
+//				line = bufRead.readLine();
+//			}
+//			System.out.println(str1);
+//			
+//		} catch (Exception e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//		
+//		
+//		try {
+//			a.submitAEDataXMLAsAttachment(str1);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 }
-
-
-/*
-System.out.print(aeReportJobInfo.getJobID());
-String reportStatus = aeReportJobInfo.getReportStatus().getValue();
-if (reportStatus.equals(ReportStatus._SUCCESS)) {
-	System.out.println("Response is: SUCCESS");
-}
-if (reportStatus.equals(ReportStatus._REJECTED)) {
-	System.out.println("Response is: REJECTED");
-}       
-if (reportStatus.equals(ReportStatus._ERROR)) {
-	System.out.println("Response is: ERROR");
-}        
-*/
-/*
-StringWriter         objWriter  = new StringWriter();
-org.apache.axis.encoding.Serializer ser = aeReportJobInfo.getSerializer("", AEReportJobInfo.class, AEReportJobInfo.getTypeDesc().getXmlType());
-SerializationContext context    = new SerializationContext(objWriter, binding._getCall().getMessageContext());
-ser.serialize(AEReportJobInfo.getTypeDesc().getXmlType(), null, aeReportJobInfo, context);
-objWriter.flush();
-objWriter.close();
-System.out.println(objWriter.toString());
-return objWriter.toString();
-*/
