6e753dd454acb2445d9361a452164314a7815deb
Monish Dombla <monish.dombla@semanticbits.com>
2009-03-05 20:15:25 +0000

    Add more testcases for code coverage.
    SVN-Revision: 8415

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/ParticipantQueryTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/ParticipantQueryTest.java
@@ -42,7 +42,7 @@ public class ParticipantQueryTest extends TestCase {
 
     }
 
-    public void testFilterByIdentifier() throws Exception {
+    public void testFilterByIdentifierValue() throws Exception {
         ParticipantQuery participantQuery = new ParticipantQuery();
         participantQuery.leftJoinFetch("p.identifiers");
         participantQuery.filterByIdentifierValue("a");
@@ -54,6 +54,36 @@ public class ParticipantQueryTest extends TestCase {
                         "identifier"));
         assertEquals("wrong parameter value", participantQuery.getParameterMap().get("identifier"),
                         "%a%");
+
+    }
+    
+    public void testFilterByIdentifierValueExactMatch() throws Exception {
+        ParticipantQuery participantQuery = new ParticipantQuery();
+        participantQuery.leftJoinFetch("p.identifiers");
+        participantQuery.filterByIdentifierValueExactMatch("a");
+        assertEquals(
+                        "SELECT distinct p from Participant p left join fetch p.identifiers WHERE p.identifiers.value = :identifier order by p.id",
+                        participantQuery.getQueryString());
+        assertEquals("wrong number of parameters", participantQuery.getParameterMap().size(), 1);
+        assertTrue("missing paramenter name", participantQuery.getParameterMap().containsKey(
+                        "identifier"));
+        assertEquals("wrong parameter value", participantQuery.getParameterMap().get("identifier"),
+                        "a");
+
+    }
+    
+    public void testFilterByIdentifierTypeExactMatch() throws Exception {
+        ParticipantQuery participantQuery = new ParticipantQuery();
+        participantQuery.leftJoinFetch("p.identifiers");
+        participantQuery.filterByIdentifierTypeExactMatch("a");
+        assertEquals(
+                        "SELECT distinct p from Participant p left join fetch p.identifiers WHERE p.identifiers.type = :type order by p.id",
+                        participantQuery.getQueryString());
+        assertEquals("wrong number of parameters", participantQuery.getParameterMap().size(), 1);
+        assertTrue("missing paramenter name", participantQuery.getParameterMap().containsKey(
+                        "type"));
+        assertEquals("wrong parameter value", participantQuery.getParameterMap().get("type"),
+                        "a");
 
     }
 
@@ -75,6 +105,58 @@ public class ParticipantQueryTest extends TestCase {
         assertEquals("wrong parameter value",
                         participantQuery.getParameterMap().get("studySiteId"), Integer.valueOf(1));
 
+    }
+    
+    public void testExcludeHavingGender() throws Exception {
+        ParticipantQuery participantQuery = new ParticipantQuery();
+        participantQuery.leftJoinFetch("p.identifiers");
+        participantQuery.filterByFirstName("a");
+        participantQuery.excludeHavingGender("Male");
+        assertEquals(
+                        "SELECT distinct p from Participant p left join fetch p.identifiers WHERE lower(p.firstName) LIKE :firstName AND p.gender != :gender order by p.id",
+                        participantQuery.getQueryString());
+        assertEquals("wrong number of parameters", participantQuery.getParameterMap().size(), 2);
+        assertTrue("missing paramenter name", participantQuery.getParameterMap().containsKey(
+                        "firstName"));
+        assertEquals("wrong parameter value", participantQuery.getParameterMap().get("firstName"),
+                        "%a%");
+        assertEquals("wrong parameter value", participantQuery.getParameterMap().get("gender"),
+        "Male");
+    }
+    
+    public void testExcludeHavingRace() throws Exception {
+        ParticipantQuery participantQuery = new ParticipantQuery();
+        participantQuery.leftJoinFetch("p.identifiers");
+        participantQuery.filterByFirstName("a");
+        participantQuery.excludeHavingRace("Asian");
+        assertEquals(
+                        "SELECT distinct p from Participant p left join fetch p.identifiers WHERE lower(p.firstName) LIKE :firstName AND p.race != :race order by p.id",
+                        participantQuery.getQueryString());
+        assertEquals("wrong number of parameters", participantQuery.getParameterMap().size(), 2);
+        assertTrue("missing paramenter name", participantQuery.getParameterMap().containsKey(
+                        "firstName"));
+        assertEquals("wrong parameter value", participantQuery.getParameterMap().get("firstName"),
+                        "%a%");
+        assertEquals("wrong parameter value", participantQuery.getParameterMap().get("race"),
+        "Asian");
+    }
+    
+    
+    public void testExcludeHavingEthnicity() throws Exception {
+        ParticipantQuery participantQuery = new ParticipantQuery();
+        participantQuery.leftJoinFetch("p.identifiers");
+        participantQuery.filterByFirstName("a");
+        participantQuery.excludeHavingEthnicity("Ethnicity");
+        assertEquals(
+                        "SELECT distinct p from Participant p left join fetch p.identifiers WHERE lower(p.firstName) LIKE :firstName AND p.ethnicity != :ethnicity order by p.id",
+                        participantQuery.getQueryString());
+        assertEquals("wrong number of parameters", participantQuery.getParameterMap().size(), 2);
+        assertTrue("missing paramenter name", participantQuery.getParameterMap().containsKey(
+                        "firstName"));
+        assertEquals("wrong parameter value", participantQuery.getParameterMap().get("firstName"),
+                        "%a%");
+        assertEquals("wrong parameter value", participantQuery.getParameterMap().get("ethnicity"),
+        "Ethnicity");
     }
 
 }
