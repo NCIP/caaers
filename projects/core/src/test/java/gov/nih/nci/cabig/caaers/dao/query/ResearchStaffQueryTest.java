6cec4b137a7bdb4c4ed28edd607e71901a0a0c67
Sameer Sawant <sameer.sawant@semanticbits.com>
2009-03-05 18:56:51 +0000

    CAAERS - 1651
    Unit tests - code coverage.
    
    SVN-Revision: 8412

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/ResearchStaffQueryTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/ResearchStaffQueryTest.java
@@ -22,33 +22,79 @@ public class ResearchStaffQueryTest extends TestCase {
         assertEquals("wrong number of parameters", reserachStaffQuery.getParameterMap().size(), 1);
         assertTrue("missing paramenter name", reserachStaffQuery.getParameterMap().containsKey(
                         "name"));
-        assertEquals("wrong parameter value", reserachStaffQuery.getParameterMap().get("name"),
-                        "%a%");
-
-        // reserachStaffQuery.filterByNciInstituteCode("b");
-        // assertEquals("wrong number of parameters", reserachStaffQuery.getParameterMap().size(),
-        // 2);
-        // assertTrue("missing paramenter name",
-        // reserachStaffQuery.getParameterMap().containsKey("nciInstituteCode"));
-        // assertEquals("wrong parameter value",
-        // reserachStaffQuery.getParameterMap().get("nciInstituteCode"), "%b%");
+        assertEquals("wrong parameter value", "%a%", reserachStaffQuery.getParameterMap().get("name"));
 
     }
-
-    public void testFilterByNCICode() throws Exception {
-        // ResearchStaffQuery reserachStaffQuery = new ResearchStaffQuery();
-        // reserachStaffQuery.filterByNciInstituteCode("a");
-        // assertEquals(
-        // "SELECT distinct o from ReserchStaff o WHERE o.nciInstituteCode LIKE :nciInstituteCode
-        // order by o.id",
-        // reserachStaffQuery.getQueryString());
-        // assertEquals("wrong number of parameters", reserachStaffQuery.getParameterMap().size(),
-        // 1);
-        // assertTrue("missing paramenter name",
-        // reserachStaffQuery.getParameterMap().containsKey("nciInstituteCode"));
-        // assertEquals("wrong parameter value",
-        // reserachStaffQuery.getParameterMap().get("nciInstituteCode"), "%a%");
-
+    
+    public void testFilterByFirstName() throws Exception{
+    	ResearchStaffQuery researchStaffQuery = new ResearchStaffQuery();
+    	researchStaffQuery.filterByFirstName("a");
+    	assertEquals("wrong number of parameters", researchStaffQuery.getParameterMap().size(), 1);
+    	assertTrue("missing paramenter name", researchStaffQuery.getParameterMap().containsKey(
+        "firstName"));
+        assertEquals("wrong parameter value", "%a%", researchStaffQuery.getParameterMap().get("firstName"));
+        assertEquals("Incorrect query created",
+                "SELECT distinct rs from ResearchStaff rs left join fetch rs.organization WHERE lower(rs.firstName) LIKE :firstName order by rs.id",
+                researchStaffQuery.getQueryString());
     }
-
+    
+    public void testFilterByLastName() throws Exception{
+    	ResearchStaffQuery researchStaffQuery = new ResearchStaffQuery();
+    	researchStaffQuery.filterByLastName("a");
+    	assertEquals("wrong number of parameters", researchStaffQuery.getParameterMap().size(), 1);
+    	assertTrue("missing paramenter name", researchStaffQuery.getParameterMap().containsKey(
+        "lastName"));
+    	assertEquals("wrong parameter value", "%a%",researchStaffQuery.getParameterMap().get("lastName"));
+    	assertEquals("Incorrect query created",
+                "SELECT distinct rs from ResearchStaff rs left join fetch rs.organization WHERE lower(rs.lastName) LIKE :lastName order by rs.id",
+                researchStaffQuery.getQueryString());
+    }
+    
+    public void testFilterByEmailAddress() throws Exception{
+    	ResearchStaffQuery researchStaffQuery = new ResearchStaffQuery();
+    	researchStaffQuery.filterByEmailAddress("a");
+    	assertEquals("wrong number of parameters", researchStaffQuery.getParameterMap().size(), 1);
+    	assertTrue("missing paramenter name", researchStaffQuery.getParameterMap().containsKey(
+        "emailAddress"));
+    	assertEquals("wrong parameter value", "%a%", researchStaffQuery.getParameterMap().get("emailAddress"));
+    	assertEquals("Incorrect query created",
+                "SELECT distinct rs from ResearchStaff rs left join fetch rs.organization WHERE lower(rs.emailAddress) LIKE :emailAddress order by rs.id",
+                researchStaffQuery.getQueryString());
+    }
+    
+    public void testFilterByLoginId() throws Exception{
+    	ResearchStaffQuery researchStaffQuery = new ResearchStaffQuery();
+    	researchStaffQuery.filterByLoginId("a");
+    	assertEquals("wrong number of parameters", researchStaffQuery.getParameterMap().size(), 1);
+    	assertTrue("missing paramenter name", researchStaffQuery.getParameterMap().containsKey(
+        "loginId"));
+    	assertEquals("wrong parameter value", "%a%", researchStaffQuery.getParameterMap().get("loginId"));
+    	assertEquals("Incorrect query created",
+                "SELECT distinct rs from ResearchStaff rs left join fetch rs.organization WHERE lower(rs.loginId) LIKE :loginId order by rs.id",
+                researchStaffQuery.getQueryString());
+    }
+    
+    public void testFilterByNciIdentifier() throws Exception{
+    	ResearchStaffQuery researchStaffQuery = new ResearchStaffQuery();
+    	researchStaffQuery.filterByNciIdentifier("a");
+    	assertEquals("wrong number of parameters", researchStaffQuery.getParameterMap().size(), 1);
+    	assertTrue("missing paramenter name", researchStaffQuery.getParameterMap().containsKey(
+        "nciIdentifier"));
+    	assertEquals("wrong parameter value", "%a%", researchStaffQuery.getParameterMap().get("nciIdentifier"));
+    	assertEquals("Incorrect query created",
+                "SELECT distinct rs from ResearchStaff rs left join fetch rs.organization WHERE lower(rs.nciIdentifier) LIKE :nciIdentifier order by rs.id",
+                researchStaffQuery.getQueryString());
+    }
+    
+    public void testExcudeHavingId() throws Exception{
+    	ResearchStaffQuery researchStaffQuery = new ResearchStaffQuery();
+    	researchStaffQuery.excludeHavingId(1);
+    	assertEquals("wrong number of parameters", researchStaffQuery.getParameterMap().size(), 1);
+    	assertTrue("missing paramenter name", researchStaffQuery.getParameterMap().containsKey(
+        "rsId"));
+    	assertEquals("wrong parameter value", 1, researchStaffQuery.getParameterMap().get("rsId"));
+    	assertEquals("Incorrect query created",
+                "SELECT distinct rs from ResearchStaff rs left join fetch rs.organization WHERE rs.id != :rsId order by rs.id",
+                researchStaffQuery.getQueryString());
+    }
 }
