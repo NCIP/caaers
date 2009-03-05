a00519b49e610a65cd0511c500e779aeb7bc8752
Sameer Sawant <sameer.sawant@semanticbits.com>
2009-03-05 19:02:37 +0000

    CAAERS - 1516 Unit tests code coverage.
    SVN-Revision: 8413

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/CtcCategoryDaoTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/CtcCategoryDaoTest.java
@@ -7,12 +7,18 @@ import java.util.List;
 import gov.nih.nci.cabig.caaers.CaaersUseCases;
 import gov.nih.nci.cabig.caaers.DaoTestCase;
 import gov.nih.nci.cabig.caaers.domain.CtcCategory;
+import gov.nih.nci.cabig.caaers.domain.DiseaseCategory;
 
 /**
  * @author Sameer Sawant
  */
 @CaaersUseCases( { MAPPING_VOCAB })
 public class CtcCategoryDaoTest extends DaoTestCase<CtcCategoryDao> {
+	
+	public void testDomainClass() throws Exception {
+        Class<CtcCategory> dcc = getDao().domainClass();
+        assertNotNull(dcc);
+    }
 	
 	public void testGetAll() throws Exception{
 		List<CtcCategory> ctcCategoryList = getDao().getAll();
