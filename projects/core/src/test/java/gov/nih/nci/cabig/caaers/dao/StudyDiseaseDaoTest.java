d87d8f11310d9d0c89183a999992baafb3e5d7ed
Ion Olaru <ion.olaru@gmail.com>
2009-03-06 14:27:39 +0000

    CAAERS-1823 -  StudyDiseaseDao
    SVN-Revision: 8432

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/StudyDiseaseDaoTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/dao/StudyDiseaseDaoTest.java
@@ -2,7 +2,7 @@ package gov.nih.nci.cabig.caaers.dao;
 
 import gov.nih.nci.cabig.caaers.DaoTestCase;
 
-public class StudyDiseaseDaoTest extends DaoTestCase<AbstractStudyDiseaseDao> {
+public class StudyDiseaseDaoTest extends DaoTestCase<StudyDiseaseDao> {
     public void testGetDomainClass(){
         Object obj = getDao().domainClass();
         assertNotNull(obj);
