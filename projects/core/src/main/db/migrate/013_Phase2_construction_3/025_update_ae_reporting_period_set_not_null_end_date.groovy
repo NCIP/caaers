e5da6d049c26bc6a611b9efdebc6afa2fe94c79d
Ion Olaru <ion.olaru@gmail.com>
2009-03-16 19:59:31 +0000

    SVN-Revision: 8551

--- projects/core/src/main/db/migrate/013_Phase2_construction_3/025_update_ae_reporting_period_set_not_null_end_date.groovy
+++ projects/core/src/main/db/migrate/013_Phase2_construction_3/025_update_ae_reporting_period_set_not_null_end_date.groovy
@@ -1,9 +1,21 @@
 class updateAETreatmentAssignment extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-        execute("ALTER TABLE ae_reporting_periods ALTER COLUMN end_date DROP NOT NULL");
+		if (databaseMatches('oracle')) {
+			execute(" execute("ALTER TABLE ae_reporting_periods MODIFY end_date NOT NULL");");
+		}
+
+		if (databaseMatches('postgres')) {
+			execute("ALTER TABLE ae_reporting_periods ALTER COLUMN end_date SET NOT NULL");
+	 	}
     }
 
     void down(){
-        execute("ALTER TABLE ae_reporting_periods ALTER COLUMN end_date SET NOT NULL");
+		if (databaseMatches('oracle')) {
+			execute(" execute("ALTER TABLE ae_reporting_periods MODIFY end_date NULL");");
+		}
+
+		if (databaseMatches('postgres')) {
+			execute("ALTER TABLE ae_reporting_periods ALTER COLUMN end_date SET NULL");
+	 	}
     }
 }
