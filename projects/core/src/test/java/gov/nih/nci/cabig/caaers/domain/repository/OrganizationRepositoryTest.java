c9a5642caf1905ea96cfd1125e3c434d1c7e4a4a
Srini Akkala <akkalas@gmail.com>
2009-04-24 05:23:41 +0000

    SVN-Revision: 9097

--- projects/core/src/test/java/gov/nih/nci/cabig/caaers/domain/repository/OrganizationRepositoryTest.java
+++ projects/core/src/test/java/gov/nih/nci/cabig/caaers/domain/repository/OrganizationRepositoryTest.java
@@ -56,5 +56,32 @@ public class OrganizationRepositoryTest extends DaoNoSecurityTestCase<Organizati
 		assertNotNull(list);
 		assertEquals(5, list.size());
 	}
+	
+	public void testSearch(){
+		OrganizationRepository organizationRepository = (OrganizationRepository)this.getDeployedApplicationContext().getBean("organizationRepository");
+		OrganizationQuery query = new OrganizationQuery();
+		query.filterByOrganizationName("National Cancer Institute B");
+		//query.filterByNciInstituteCode("NCI");
+		//query.getParameterMap();
+		List<Organization> organizations = organizationRepository.searchOrganization(query);
+		
+		for(Object org : organizations){
+            try {
+            	Organization obj = (Organization)org;
+            	System.out.println(obj.getName());
+            	System.out.println(obj.getCity());
+            	System.out.println(obj.getCountry());
+            	System.out.println(obj.getNciInstituteCode());
+            	System.out.println(obj.getExternalId());
+            	System.out.println(org.getClass().getName());
+                //deserialize coppa payload from caXchange response xml and get coppa object
+                //objList.add(remoteHealthcareSiteResolver.deSerializeFromCoppa(me.getAsString()));
+            } catch (Exception e) {
+         
+                e.printStackTrace();
+            }
+        }
+		
+	}
 
 }
