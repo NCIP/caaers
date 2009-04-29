6431140b391140241e54220818856be78e97fe6f
Biju Joseph <biju.joseph@semanticbits.com>
2009-04-29 16:12:02 +0000

    fixing hudson failure
    SVN-Revision: 9161

--- projects/core/src/main/db/migrate/013_Phase2_construction_3/026_alter_organizations.groovy
+++ projects/core/src/main/db/migrate/013_Phase2_construction_3/026_alter_organizations.groovy
@@ -2,13 +2,22 @@ class AddDTypeForOrganization extends edu.northwestern.bioinformatics.bering.Mig
 	void up() {
 		addColumn("organizations","type", "string", defaultValue:'LOCAL')
 		execute("UPDATE organizations SET TYPE='LOCAL'")
-		execute("ALTER TABLE organizations ALTER COLUMN name DROP NOT NULL")
+		 if (databaseMatches('postgresql')){
+			execute("ALTER TABLE organizations ALTER COLUMN name DROP NOT NULL")
+		}else if(databaseMatches('oracle')){
+			execute("ALTER TABLE organizations modify name NULL")
+		}
 		addColumn("organizations","external_id", "string")
 	}
 	
 	void down() {
 		dropColumn("organizations", "type")
-		execute("ALTER TABLE organizations ALTER COLUMN name SET NOT NULL")
+		if (databaseMatches('postgresql')){
+			execute("ALTER TABLE organizations ALTER COLUMN name SET NOT NULL")
+		}else if(databaseMatches('oracle')){
+			//will not work in oracle.
+		}
+		
 		dropColumn("organizations", "external_id")
 	}
 }
