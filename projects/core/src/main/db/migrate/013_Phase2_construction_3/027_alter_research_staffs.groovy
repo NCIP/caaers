7673373745b8084d32ee07c5d6aaa35f490ef7b8
Biju Joseph <biju.joseph@semanticbits.com>
2009-04-29 16:14:37 +0000

    fixing hudson failure
    SVN-Revision: 9162

--- projects/core/src/main/db/migrate/013_Phase2_construction_3/027_alter_research_staffs.groovy
+++ projects/core/src/main/db/migrate/013_Phase2_construction_3/027_alter_research_staffs.groovy
@@ -2,13 +2,23 @@ class AddDTypeForResearcStaff extends edu.northwestern.bioinformatics.bering.Mig
 	void up() {
 		addColumn("research_staffs","type", "string", defaultValue:'LOCAL')
 		execute("UPDATE research_staffs SET TYPE='LOCAL'")
-		execute("ALTER TABLE research_staffs ALTER COLUMN phone_number DROP NOT NULL")
+		if (databaseMatches('postgresql')){
+         	execute("ALTER TABLE research_staffs ALTER COLUMN phone_number DROP NOT NULL")
+     	}else if(databaseMatches('oracle')){
+     		execute("ALTER TABLE research_staffs modify phone_number NULL")
+     	}
+		
 		addColumn("research_staffs","external_id", "string")
 	}
 	
 	void down() {
 		dropColumn("research_staffs", "type")
-		execute("ALTER TABLE research_staffs ALTER COLUMN phone_number SET NOT NULL")
+		if (databaseMatches('postgresql')){
+         	execute("ALTER TABLE research_staffs ALTER COLUMN phone_number SET NOT NULL")
+     	}else if(databaseMatches('oracle')){
+     		//will not work in oracle
+     	}
+		
 		dropColumn("research_staffs", "external_id")
 	}
 }
--- projects/core/src/main/db/migrate/013_Phase2_construction_3/028_alter_investigators.groovy
+++ projects/core/src/main/db/migrate/013_Phase2_construction_3/028_alter_investigators.groovy
@@ -2,13 +2,23 @@ class AddDTypeForInvestigator extends edu.northwestern.bioinformatics.bering.Mig
 	void up() {
 		addColumn("investigators","type", "string", defaultValue:'LOCAL')
 		execute("UPDATE investigators SET TYPE='LOCAL'")
-		execute("ALTER TABLE investigators ALTER COLUMN phone_number DROP NOT NULL")
+		if (databaseMatches('postgresql')){
+         	execute("ALTER TABLE investigators ALTER COLUMN phone_number DROP NOT NULL")
+     	}else if(databaseMatches('oracle')){
+     		execute("ALTER TABLE investigators modify phone_number NULL")
+     	}
+		
 		addColumn("investigators","external_id", "string")
 	}
 	
 	void down() {
 		dropColumn("investigators", "type")
-		execute("ALTER TABLE investigators ALTER COLUMN phone_number SET NOT NULL")
+		if (databaseMatches('postgresql')){
+         	execute("ALTER TABLE investigators ALTER COLUMN phone_number SET NOT NULL")
+     	}else if(databaseMatches('oracle')){
+     		//not work in oracle
+     	}
+		
 		dropColumn("investigators", "external_id")
 	}
 }
