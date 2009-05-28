6130e0fef375feaa762ad03c2b411b92ac130dac
Monish Dombla <monish.dombla@semanticbits.com>
2009-05-28 13:09:21 +0000

    UI Changes for INV and RS
    SVN-Revision: 9391

---
+++ projects/web/src/main/java/gov/nih/nci/cabig/caaers/web/study/SearchResearchStaffCommand.java
@@ -0,0 +1,40 @@
+package gov.nih.nci.cabig.caaers.web.study;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Kulasekaran
+ */
+
+public class SearchResearchStaffCommand {
+
+    private List<SearchCommand> searchCriteria = new ArrayList<SearchCommand>();
+
+    private Integer organization;
+    
+    public List<SearchCommand> getSearchCriteria() {
+        return searchCriteria;
+    }
+
+    public void addSearchCriterion(SearchCommand criterion) {
+        searchCriteria.add(criterion);
+    }
+
+    public void removeSearchCriterion(int index) {
+        searchCriteria.remove(index);
+    }
+
+    public void setSearchCriteria(List<SearchCommand> searchCriteria) {
+        this.searchCriteria = searchCriteria;
+    }
+
+	public Integer getOrganization() {
+		return organization;
+	}
+
+	public void setOrganization(Integer organization) {
+		this.organization = organization;
+	}
+
+}
