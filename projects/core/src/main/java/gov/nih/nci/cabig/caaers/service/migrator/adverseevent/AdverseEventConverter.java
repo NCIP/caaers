c1d0f64e9e45f33fab1650c10aaa0749090ceea5
Srini Akkala <akkalas@gmail.com>
2009-05-28 19:58:05 +0000

    SVN-Revision: 9412

--- projects/core/src/main/java/gov/nih/nci/cabig/caaers/service/migrator/adverseevent/AdverseEventConverter.java
+++ projects/core/src/main/java/gov/nih/nci/cabig/caaers/service/migrator/adverseevent/AdverseEventConverter.java
@@ -3,18 +3,22 @@ package gov.nih.nci.cabig.caaers.service.migrator.adverseevent;
 import gov.nih.nci.cabig.caaers.CaaersSystemException;
 import gov.nih.nci.cabig.caaers.api.AdverseEventManagementService;
 import gov.nih.nci.cabig.caaers.dao.CtcTermDao;
+import gov.nih.nci.cabig.caaers.dao.meddra.LowLevelTermDao;
 import gov.nih.nci.cabig.caaers.domain.AdverseEvent;
 import gov.nih.nci.cabig.caaers.domain.AdverseEventCtcTerm;
+import gov.nih.nci.cabig.caaers.domain.AdverseEventMeddraLowLevelTerm;
 import gov.nih.nci.cabig.caaers.domain.Attribution;
 import gov.nih.nci.cabig.caaers.domain.CtcTerm;
 import gov.nih.nci.cabig.caaers.domain.Grade;
 import gov.nih.nci.cabig.caaers.domain.Hospitalization;
+import gov.nih.nci.cabig.caaers.domain.TimeValue;
 import gov.nih.nci.cabig.caaers.domain.meddra.LowLevelTerm;
+import gov.nih.nci.cabig.caaers.webservice.adverseevent.AdverseEventMeddraLowLevelTermType;
 import gov.nih.nci.cabig.caaers.webservice.adverseevent.AdverseEventType;
-import gov.nih.nci.cabig.caaers.webservice.adverseevent.LowLevelTermType;
 
 public class AdverseEventConverter {
 	private CtcTermDao ctcTermDao = null;
+	private LowLevelTermDao lowLevelTermDao = null;
 
 	public void convertAdverseEventDtoToAdverseEventDomain(gov.nih.nci.cabig.caaers.webservice.adverseevent.AdverseEventType adverseEventDto, 
 			AdverseEvent adverseEvent, String operation) throws CaaersSystemException{
@@ -23,6 +27,9 @@ public class AdverseEventConverter {
 		}
 		
 		try{
+			if (adverseEventDto.getVerbatim() != null) { 
+				adverseEvent.setDetailsForOther(adverseEventDto.getVerbatim());
+			}
 			populateGrade(adverseEventDto,adverseEvent);
 			populateHospitalization(adverseEventDto,adverseEvent);
 			adverseEvent.setExpected(adverseEventDto.isExpected());
@@ -35,28 +42,36 @@ public class AdverseEventConverter {
 			if(adverseEventDto.getEndDate() != null){
 				adverseEvent.setEndDate(adverseEventDto.getEndDate().toGregorianCalendar().getTime());
 			}	
-		//	populateLowLevelTerm(adverseEventDto,adverseEvent); 
+
 			if (operation.equals(AdverseEventManagementService.CREATE)) {
 				populateCtcTerm(adverseEventDto,adverseEvent);
+				populateLowLevelTerm(adverseEventDto,adverseEvent); 
 			}
+			if (adverseEventDto.getEventApproximateTime() != null) { 
+				TimeValue tv = new TimeValue();
+				tv.setHour(adverseEventDto.getEventApproximateTime().getHour());
+				tv.setMinute(adverseEventDto.getEventApproximateTime().getMinute());
+				adverseEvent.setEventApproximateTime(tv);
+			}
+			/*
 			if (adverseEventDto.isSolicited() != null){
 				adverseEvent.setSolicited(adverseEventDto.isSolicited());
 			}			
-			//serious
+			
 			if (adverseEventDto.isRequiresReporting() != null) {
 				adverseEvent.setRequiresReporting(adverseEventDto.isRequiresReporting());
-			}
+			}*/
 			if (adverseEventDto.getEventLocation() != null) { 
 				adverseEvent.setEventLocation(adverseEventDto.getEventLocation());
 			}
-			
+			/*
 			if(adverseEventDto.getGradedDate() != null){
 				adverseEvent.setGradedDate(adverseEventDto.getGradedDate().toGregorianCalendar().getTime());
 			}
 						
 			if (adverseEventDto.isReported() != null) {
 				adverseEvent.setReported(adverseEventDto.isReported());
-			}
+			}*/
 			
 		}catch(Exception e){
 			throw new CaaersSystemException(e);
@@ -82,11 +97,19 @@ public class AdverseEventConverter {
 		}
 	}
 	private void populateLowLevelTerm(AdverseEventType adverseEventDto, AdverseEvent adverseEvent) {
-		if (adverseEventDto.getLowLevelTerm() != null) {
-			LowLevelTermType xmlLowLevelTerm = adverseEventDto.getLowLevelTerm();
-			LowLevelTerm lowLevelTerm = new LowLevelTerm();
-			lowLevelTerm.setMeddraCode(xmlLowLevelTerm.getMeddraCode());
-			lowLevelTerm.setMeddraTerm(xmlLowLevelTerm.getMeddraTerm());
+		if (adverseEventDto.getAdverseEventMeddraLowLevelTerm() != null) {
+			AdverseEventMeddraLowLevelTermType xmlLowLevelTerm = adverseEventDto.getAdverseEventMeddraLowLevelTerm();
+			
+			LowLevelTerm lowLevelTerm = lowLevelTermDao.getByCodeAndTerm(xmlLowLevelTerm.getMeddraCode(),xmlLowLevelTerm.getMeddraTerm());
+			if (lowLevelTerm == null) {
+				throw new CaaersSystemException ("Meddra term not found " + xmlLowLevelTerm.getMeddraCode().toString());
+				
+			} else {			
+				AdverseEventMeddraLowLevelTerm adverseEventMeddraLowLevelTerm = new AdverseEventMeddraLowLevelTerm();
+				adverseEventMeddraLowLevelTerm.setLowLevelTerm(lowLevelTerm);
+				adverseEventMeddraLowLevelTerm.setAdverseEvent(adverseEvent);
+				adverseEvent.setAdverseEventMeddraLowLevelTerm(adverseEventMeddraLowLevelTerm);
+			}
 		}
 	}
 	private void populateCtcTerm(AdverseEventType adverseEventDto, AdverseEvent adverseEvent) throws CaaersSystemException{
@@ -113,6 +136,11 @@ public class AdverseEventConverter {
 
 	public void setCtcTermDao(CtcTermDao ctcTermDao) {
 		this.ctcTermDao = ctcTermDao;
+	}
+
+
+	public void setLowLevelTermDao(LowLevelTermDao lowLevelTermDao) {
+		this.lowLevelTermDao = lowLevelTermDao;
 	}
 
 
