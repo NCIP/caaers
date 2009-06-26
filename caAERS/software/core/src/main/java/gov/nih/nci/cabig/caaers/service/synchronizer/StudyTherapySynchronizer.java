1605f1adad8d3b3c3c92f96c91b76db6e02c347b
Monish Dombla <monish.dombla@semanticbits.com>
2009-06-26 16:01:03 +0000

    Study Therapy Update was not happening. Fixed issue raised by CALGB. CAAERS-2361.
    SVN-Revision: 9669

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/service/synchronizer/StudyTherapySynchronizer.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/service/synchronizer/StudyTherapySynchronizer.java
@@ -2,6 +2,7 @@ package gov.nih.nci.cabig.caaers.service.synchronizer;
 
 import gov.nih.nci.cabig.caaers.domain.Study;
 import gov.nih.nci.cabig.caaers.domain.StudyTherapy;
+import gov.nih.nci.cabig.caaers.domain.StudyTherapyType;
 import gov.nih.nci.cabig.caaers.service.DomainObjectImportOutcome;
 import gov.nih.nci.cabig.caaers.service.migrator.Migrator;
 
@@ -13,33 +14,25 @@ public class StudyTherapySynchronizer implements Migrator<gov.nih.nci.cabig.caae
 	public void migrate(Study dbStudy, Study xmlStudy,
 			DomainObjectImportOutcome<Study> outcome) {
 		
-		List<StudyTherapy> newStudyTherapyList = new ArrayList<StudyTherapy>();
-		
-		//Identify newly added StudyTherapy.
-		for(StudyTherapy xmlStudyTherapy : xmlStudy.getStudyTherapies()){
-			for(StudyTherapy dbStudyTherapy : dbStudy.getStudyTherapies()){
-				xmlStudyTherapy.setId(dbStudyTherapy.getId());
-				if(xmlStudyTherapy.getStudyTherapyType().equals(dbStudyTherapy.getStudyTherapyType())){
-					break;
-				}else{
-					xmlStudyTherapy.setId(null);
-				}
-			}
-			if(xmlStudyTherapy.getId() == null){
-				newStudyTherapyList.add(xmlStudyTherapy);
-			}
-		}
-		
-		//Adding the new StudyTherapies to the existing Study.
-		for(StudyTherapy newStudyTherapy : newStudyTherapyList){
-			dbStudy.getStudyTherapies().add(newStudyTherapy);
-		}
-		
-		//Need to set the Study for the update to function
-		for(StudyTherapy studyTherapy : dbStudy.getStudyTherapies()){
-			studyTherapy.setStudy(dbStudy);
-		}
-		
-	}
+		dbStudy.getStudyTherapies().clear();
 
+		if (xmlStudy.getDrugAdministrationTherapyType()) {
+	    	dbStudy.addStudyTherapy(StudyTherapyType.DRUG_ADMINISTRATION);
+		}
+
+		if (xmlStudy.getDeviceTherapyType()) {
+			dbStudy.addStudyTherapy(StudyTherapyType.DEVICE);
+		}
+
+		if (xmlStudy.getRadiationTherapyType()) {
+			dbStudy.addStudyTherapy(StudyTherapyType.RADIATION);
+		}
+		if (xmlStudy.getSurgeryTherapyType()) {
+			dbStudy.addStudyTherapy(StudyTherapyType.SURGERY);
+		}
+
+		if (xmlStudy.getBehavioralTherapyType()) {
+			dbStudy.addStudyTherapy(StudyTherapyType.BEHAVIORAL);
+		}
+	}
 }
