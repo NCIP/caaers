1f81896afba29bec11d4fc8e6aeaacd9ba6e2674
Monish Dombla <monish.dombla@semanticbits.com>
2009-06-29 15:44:09 +0000

    Added 2 new testcases to test equals method of Identifier.
    SVN-Revision: 9676

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/domain/IdentifierTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/domain/IdentifierTest.java
@@ -29,4 +29,45 @@ public class IdentifierTest extends CaaersTestCase {
         identifier.setPrimaryIndicator(true);
         assertTrue(identifier.isPrimary());
     }
+    
+    
+    public void testEquals(){
+    	Identifier id1 = new OrganizationAssignedIdentifier();
+    	id1.setType("SFC");
+    	id1.setValue("SFC-001");
+    	id1.setPrimaryIndicator(true);
+    	Identifier id2 = new OrganizationAssignedIdentifier();
+    	id2.setType("SFC");
+    	id2.setValue("SFC-001");
+    	id2.setPrimaryIndicator(false);
+    	
+    	assertTrue(id1.equals(id2));
+    	
+    	Identifier id3 = new OrganizationAssignedIdentifier();
+    	id3.setType("SFC");
+    	id3.setValue("SFC-001");
+    	id3.setPrimaryIndicator(true);
+    	Identifier id4 = new OrganizationAssignedIdentifier();
+    	id4.setType("SFC");
+    	id4.setValue("SFC-001");
+    	id4.setPrimaryIndicator(true);
+    	
+    	assertTrue(id3.equals(id4));
+    	
+    }
+    
+    public void testNotEquals(){
+    	Identifier id1 = new OrganizationAssignedIdentifier();
+    	id1.setType("SFC");
+    	id1.setValue("SFC-001");
+    	id1.setPrimaryIndicator(true);
+    	Identifier id2 = new OrganizationAssignedIdentifier();
+    	id2.setType("SCC");
+    	id2.setValue("SCC-001");
+    	id2.setPrimaryIndicator(false);
+    	
+    	assertFalse(id1.equals(id2));
+    }
+    
+    
 }
