56a611b0115083f81869fecbe7e78a4dbc907a1f
Monish Dombla <monish.dombla@semanticbits.com>
2009-07-17 16:22:05 +0000

    Commented sql.
    SVN-Revision: 9788

--- caAERS/software/core/src/main/db/migrate/015_Phase3_construction_1/migrate_research_staffs_oracle.sql
+++ caAERS/software/core/src/main/db/migrate/015_Phase3_construction_1/migrate_research_staffs_oracle.sql
@@ -1,8 +1,8 @@
-insert into 
-	site_research_staffs (id,researchstaff_id,site_id) 
-		select seq_srstaffs_id.nextval,id,site_id from research_staffs;
+--insert into 
+--	site_research_staffs (id,researchstaff_id,site_id) 
+--		select seq_srstaffs_id.nextval,id,site_id from research_staffs;
 
-update study_personnel stper 
-set site_research_staffs_id = 
-		(select srs.id from 
-			site_research_staffs srs where srs.researchstaff_id = stper.research_staffs_id);
+--update study_personnel stper 
+--set site_research_staffs_id = 
+--		(select srs.id from 
+--			site_research_staffs srs where srs.researchstaff_id = stper.research_staffs_id);
