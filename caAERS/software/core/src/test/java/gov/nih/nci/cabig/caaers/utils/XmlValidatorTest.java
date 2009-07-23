a5ff6d01b8bc22c87e41faf00ffa16bff9759a56
Monish Dombla <monish.dombla@semanticbits.com>
2009-07-23 18:45:50 +0000

    SVN-Revision: 9813

---
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/utils/XmlValidatorTest.java
@@ -0,0 +1,31 @@
+package gov.nih.nci.cabig.caaers.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+public class XmlValidatorTest extends TestCase {
+
+	public void testValidateAgainstSchema() throws Exception {
+		
+		File xmlFile = null;
+		xmlFile = getResources("classpath*:gov/nih/nci/cabig/caaers/api/testdata/CreateResearchStaffTest.xml")[0].getFile();
+		String xmlContent = FileUtils.readFileToString(xmlFile);
+		StringBuffer sb = new StringBuffer();
+		XmlValidator.validateAgainstSchema(xmlContent, "/schema/integration/ResearchStaff.xsd", sb);
+		System.out.println(sb.toString());
+	}
+	
+	private static Resource[] getResources(String pattern) throws IOException {
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources(pattern);
+        return resources;
+    }
+
+}
