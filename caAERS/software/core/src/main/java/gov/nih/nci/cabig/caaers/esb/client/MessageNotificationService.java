7f8c9626d407e60c8f545c8f8d7492f3ea22b366
Srini Akkala <akkalas@gmail.com>
2009-07-28 19:08:58 +0000

    SVN-Revision: 9862

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/esb/client/MessageNotificationService.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/esb/client/MessageNotificationService.java
@@ -2,17 +2,20 @@ package gov.nih.nci.cabig.caaers.esb.client;
 
 import gov.nih.nci.cabig.caaers.dao.ExpeditedAdverseEventReportDao;
 import gov.nih.nci.cabig.caaers.dao.report.ReportDao;
-import gov.nih.nci.cabig.caaers.dao.report.ReportTrackingDao;
 import gov.nih.nci.cabig.caaers.domain.ReportStatus;
 import gov.nih.nci.cabig.caaers.domain.report.Report;
 import gov.nih.nci.cabig.caaers.domain.report.ReportDelivery;
 import gov.nih.nci.cabig.caaers.domain.report.ReportDeliveryDefinition;
 import gov.nih.nci.cabig.caaers.domain.report.ReportTracking;
 import gov.nih.nci.cabig.caaers.domain.report.ReportVersion;
+import gov.nih.nci.cabig.caaers.domain.repository.ReportRepository;
 import gov.nih.nci.cabig.caaers.service.SchedulerService;
+import gov.nih.nci.cabig.caaers.service.ReportSubmissionService.ReportSubmissionContext;
 import gov.nih.nci.cabig.caaers.tools.configuration.Configuration;
 import gov.nih.nci.cabig.caaers.tools.mail.CaaersJavaMailSender;
 import gov.nih.nci.cabig.caaers.utils.Tracker;
+import gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo;
+import gov.nih.nci.security.acegi.csm.authorization.AuthorizationSwitch;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -25,29 +28,39 @@ import java.util.Map;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.acegisecurity.Authentication;
+import org.acegisecurity.GrantedAuthority;
+import org.acegisecurity.GrantedAuthorityImpl;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.acegisecurity.providers.TestingAuthenticationToken;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.orm.hibernate3.support.OpenSessionInViewInterceptor;
 import org.springframework.web.context.request.WebRequest;
 
-public class MessageNotificationService {
+public class MessageNotificationService implements ApplicationContextAware{
     protected Configuration configuration;
 
     protected ExpeditedAdverseEventReportDao expeditedAdverseEventReportDao;
-    
-    private ReportTrackingDao reportTrackingDao;
 
     protected OpenSessionInViewInterceptor openSessionInViewInterceptor;
     
     private SchedulerService schedulerService;
+    
+    private ReportRepository reportRepository;
 
     protected final Log log = LogFactory.getLog(getClass());
 
     private ReportDao reportDao;
     
     protected CaaersJavaMailSender caaersJavaMailSender;
+    
+    private ApplicationContext applicationContext;
     
 	public void setReportDao(ReportDao reportDao) {
         this.reportDao = reportDao;
@@ -57,11 +70,28 @@ public class MessageNotificationService {
 
         WebRequest stubWebRequest = new StubWebRequest();
         openSessionInViewInterceptor.preHandle(stubWebRequest);
+        
         return stubWebRequest;
     }
 
     private void postProcess(WebRequest stubWebRequest) {
         openSessionInViewInterceptor.afterCompletion(stubWebRequest, null);
+    }
+    
+    private void doPostSubmitReport(ReportSubmissionContext context){
+    	Report report = context.report;
+    	
+    	//un-schedule all the notifications
+    	schedulerService.unScheduleNotification(report);
+    	
+    	//now update the post submission updated date on submitted adverse events. 
+    	report.getAeReport().clearPostSubmissionUpdatedDate();
+    	
+    	//update the reported flag on the adverse events.
+    	report.getAeReport().updateReportedFlagOnAdverseEvents();
+    	
+    	//create child reports
+    	reportRepository.createChildReports(report);
     }
 
     public void sendNotificationToReporter(String submitterEmail, String messages,
@@ -70,6 +100,14 @@ public class MessageNotificationService {
         // get AEreport by using this id
 
         WebRequest stubWebRequest = preProcess();
+        
+		boolean authorizationOnByDefault = enableAuthorization(false);
+		switchUser("SYSTEM_ADMIN", "ROLE_caaers_super_user");
+        
+        String info = configuration.get(Configuration.ESB_URL);
+        gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo.setLocal(new DataAuditInfo("SYSTEM_ADMIN",
+                info, new Date(), info));
+        
       //  ExpeditedAdverseEventReport aeReport = expeditedAdverseEventReportDao.getById(Integer
         //                .parseInt(aeReportId));
 
@@ -101,7 +139,7 @@ public class MessageNotificationService {
         ReportTracking rtToUpdate = rv.getLastReportTracking();
         System.out.println("ATTEMPT NUMBER" + rtToUpdate.getAttemptNumber());
         
-        postProcess(stubWebRequest);
+        
         
         /*
         for (ReportTracking rt:rts) {
@@ -135,11 +173,9 @@ public class MessageNotificationService {
             rv.setSubmissionUrl(url);
             rv.setSubmittedOn(new Date());
             rv.setReportStatus(ReportStatus.COMPLETED);
+            ReportSubmissionContext context = ReportSubmissionContext.getSubmissionContext(r);
+            doPostSubmitReport(context);
             Tracker.logSubmissionToExternalSystem(rtToUpdate, true, messages, new Date());
-            //reportTrackingDao.save(rtToUpdate);
-            // Unschedule all the notifications, once the report is submitted successfully.
-            schedulerService.unScheduleNotification(r);
-
         } else {
             r.setSubmittedOn(new Date());
             r.setStatus(ReportStatus.FAILED);
@@ -153,7 +189,10 @@ public class MessageNotificationService {
         }
         rv.setSubmissionMessage(messages);
         r.setSubmissionMessage(messages);
+
         reportDao.save(r);
+        
+        postProcess(stubWebRequest);
         
         String subject = "";
         String attachment = null;
@@ -185,6 +224,9 @@ public class MessageNotificationService {
         	reportDao.save(r);
         	throw new Exception(" Error in sending email , please check the confiuration " , e);
         }
+        
+        enableAuthorization(authorizationOnByDefault);
+		switchUser(null);
     }
 
 	public void sendMail(String[] to, String subject, String content, String attachment) throws Exception {
@@ -311,8 +353,34 @@ public class MessageNotificationService {
         }
     }
 
-	public void setReportTrackingDao(ReportTrackingDao reportTrackingDao) {
-		this.reportTrackingDao = reportTrackingDao;
+
+	public void setReportRepository(ReportRepository reportRepository) {
+		this.reportRepository = reportRepository;
 	}
+	
+	private boolean enableAuthorization(boolean on) {
+        AuthorizationSwitch sw = (AuthorizationSwitch) this.applicationContext.getBean("authorizationSwitch");
+        if (sw == null) throw new RuntimeException("Authorization switch not found");
+        boolean current = sw.isOn();
+        sw.setOn(on);
+        return current;
+    }
+	
+	private void switchUser(String userName, String... roles) {
+        GrantedAuthority[] authorities = new GrantedAuthority[roles.length];
+        for (int i = 0; i < roles.length; i++) {
+            authorities[i] = new GrantedAuthorityImpl(roles[i]);
+        }
+        Authentication auth = new TestingAuthenticationToken(userName, "ignored", authorities);
+        auth.setAuthenticated(true);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext= applicationContext;
+		
+	}
+
+
 
 }
