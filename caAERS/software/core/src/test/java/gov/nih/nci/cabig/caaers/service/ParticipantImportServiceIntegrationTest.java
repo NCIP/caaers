a99596f3c4f6358933a314651aa8c2620dd76b38
Monish Dombla <monish.dombla@semanticbits.com>
2009-07-30 18:11:18 +0000

    SVN-Revision: 9937

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/service/ParticipantImportServiceIntegrationTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/service/ParticipantImportServiceIntegrationTest.java
@@ -200,7 +200,7 @@ public class ParticipantImportServiceIntegrationTest extends AbstractNoSecurityT
         //EasyMock.expect(participantRepository.checkIfParticipantExistsForGivenIdentifiers(xstreamParticipant.getIdentifiers())).andReturn(false);
         EasyMock.expect(organizationDao.getByName(organization.getName())).andReturn(organization);
         EasyMock.expect(studyDao.getByIdentifier(organizationAssignedIdentifier)).andReturn(study);
-        studyDao.updateStudyForServiceUseOnly(study);
+        //studyDao.updateStudyForServiceUseOnly(study);
         studyRepository.save(study);
         replayMocks();
         DomainObjectImportOutcome<Participant> participantDomainObjectImportOutcome = participantImportService.importParticipant(xstreamParticipant);
