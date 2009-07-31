d4b8cfca82038029be531bd0d4729bf6f9f7baf4
Srini Akkala <akkalas@gmail.com>
2009-07-31 15:03:40 +0000

    SVN-Revision: 9974

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/dao/report/ReportVersionDao.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/dao/report/ReportVersionDao.java
@@ -119,33 +119,7 @@ public class ReportVersionDao extends GridIdentifiableDao<ReportVersion> {
     	return getHibernateTemplate().find(hsql);
     }
     
-    @Transactional(readOnly = false)
-    public void updateInProcessReports() {
-    	List<ReportVersion> rvs = getAllInProcessReports();
-    	NowFactory nowFactory = new NowFactory();
-    	//System.out.println(rvs.size());
-    	for (ReportVersion rv:rvs) {
-    		Date submittedOrAmendedDate = null;
-    		if (rv.getAmendedOn() != null) {
-    			submittedOrAmendedDate = rv.getAmendedOn();
-    		} else if (rv.getSubmittedOn() != null){
-    			submittedOrAmendedDate = rv.getSubmittedOn();
-    		}
-    		if (submittedOrAmendedDate != null) {
-    			long timeDiff = (nowFactory.getNowTimestamp().getTime() - rv.getSubmittedOn().getTime())/60000;
-    			System.out.println(timeDiff);
-    			if (timeDiff > 5) {
-    				rv.setReportStatus(ReportStatus.FAILED);
-    				rv.setSubmissionMessage("Submission failed for unknown reason , Please resubmit");
-    				save(rv);
-    				
-    				System.out.println(rv.getId() +" : " +rv.getReportStatus());
-    				System.out.println(rv.getSubmissionMessage());
-    			}
 
-    		}
-    	}    	
-    }
     
 
 }
