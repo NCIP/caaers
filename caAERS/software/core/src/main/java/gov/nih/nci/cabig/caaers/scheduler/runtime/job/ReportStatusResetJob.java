0bcb9bdceaae1b0ffdf3aec27fa2dbb5349a7f96
Srini Akkala <akkalas@gmail.com>
2009-07-31 06:23:38 +0000

    SVN-Revision: 9961

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/scheduler/runtime/job/ReportStatusResetJob.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/scheduler/runtime/job/ReportStatusResetJob.java
@@ -2,19 +2,22 @@ package gov.nih.nci.cabig.caaers.scheduler.runtime.job;
 
 import gov.nih.nci.cabig.caaers.dao.report.ReportVersionDao;
 
-import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
 
-public class ReportStatusResetJob implements Job {
+public class ReportStatusResetJob extends  QuartzJobBean {
 	private ReportVersionDao reportVersionDao;
 	
-	public void execute(JobExecutionContext context) throws JobExecutionException {
-		reportVersionDao.updateInProcessReports();
-	}
-
 	public void setReportVersionDao(ReportVersionDao reportVersionDao) {
 		this.reportVersionDao = reportVersionDao;
+	}
+
+	@Override
+	protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
+		System.out.println("Running Job");
+		reportVersionDao.updateInProcessReports();
+		
 	}
 
 }
