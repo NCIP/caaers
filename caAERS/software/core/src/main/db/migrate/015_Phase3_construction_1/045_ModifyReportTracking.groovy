e0a3544f44cc53a6fb9d9c2c8b1f0a19e215f4c9
Monish Dombla <monish.dombla@semanticbits.com>
2009-08-04 19:37:31 +0000

    Fixed oracle alter error
    SVN-Revision: 10097

--- caAERS/software/core/src/main/db/migrate/015_Phase3_construction_1/045_ModifyReportTracking.groovy
+++ caAERS/software/core/src/main/db/migrate/015_Phase3_construction_1/045_ModifyReportTracking.groovy
@@ -1,7 +1,7 @@
 class ModifyReportTracking extends edu.northwestern.bioinformatics.bering.Migration {
         void up() {
       	if (databaseMatches('oracle')) {
-        	execute('alter table report_tracking_status modify column recorded_time  DEFAULT SYSDATE')
+        	execute('alter table report_tracking_status modify recorded_time DEFAULT SYSDATE')
         }else{
         	execute('alter table report_tracking_status ALTER column recorded_time SET DEFAULT CURRENT_DATE')
         }
