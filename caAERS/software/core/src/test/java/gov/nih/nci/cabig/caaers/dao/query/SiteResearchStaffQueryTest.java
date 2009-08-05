7a45dad26f6b6d0a2980394507eb027d85040ba9
Monish Dombla <monish.dombla@semanticbits.com>
2009-08-05 01:35:59 +0000

    SVN-Revision: 10116

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/SiteResearchStaffQueryTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/SiteResearchStaffQueryTest.java
@@ -6,7 +6,7 @@ public class SiteResearchStaffQueryTest extends TestCase {
 
     public void testQueryConstructor() throws Exception {
         SiteResearchStaffQuery siteReserachStaffQuery = new SiteResearchStaffQuery();
-        assertEquals("wrong parsing for constructor", "SELECT...", siteReserachStaffQuery.getQueryString());
+        assertEquals("wrong parsing for constructor", "SELECT distinct srs from SiteResearchStaff srs left join fetch srs.researchStaff rs left join fetch srs.organization org order by srs.id", siteReserachStaffQuery.getQueryString());
     }
 
     public void testFilterByFirstName() throws Exception{
@@ -14,8 +14,8 @@ public class SiteResearchStaffQueryTest extends TestCase {
     	researchStaffQuery.filterByFirstName("THIS_IS_A_FIRST_NAME");
     	assertEquals("wrong number of parameters", researchStaffQuery.getParameterMap().size(), 1);
     	assertTrue("missing paramenter name", researchStaffQuery.getParameterMap().containsKey("firstName"));
-        assertEquals("wrong parameter value", "%THIS_IS_A_FIRST_NAME%", researchStaffQuery.getParameterMap().get("firstName"));
-        assertEquals("Incorrect query created","SELECT ...",researchStaffQuery.getQueryString());
+        assertEquals("wrong parameter value", "%this_is_a_first_name%", researchStaffQuery.getParameterMap().get("firstName"));
+        assertEquals("Incorrect query created","SELECT distinct rs from ResearchStaff rs left join fetch rs.siteResearchStaffsInternal srs WHERE lower(rs.firstName) LIKE :firstName order by rs.id",researchStaffQuery.getQueryString());
     }
 
 }
