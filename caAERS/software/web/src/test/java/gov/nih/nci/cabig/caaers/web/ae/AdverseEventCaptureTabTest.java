e0be44189b6a2a92926de564d54561637be24d93
Ion Olaru <ion.olaru@gmail.com>
2009-08-10 19:09:18 +0000

    SVN-Revision: 10208

--- caAERS/software/web/src/test/java/gov/nih/nci/cabig/caaers/web/ae/AdverseEventCaptureTabTest.java
+++ caAERS/software/web/src/test/java/gov/nih/nci/cabig/caaers/web/ae/AdverseEventCaptureTabTest.java
@@ -37,6 +37,7 @@ import org.springframework.validation.Errors;
  *
  */
 public class AdverseEventCaptureTabTest extends WebTestCase {
+    
 	AdverseEventCaptureTab tab;
 	CaptureAdverseEventInputCommand command;
 	Errors errors;
