950bd45bd79621cc81e7c318bae935c9a95c160c
Biju Joseph <biju.joseph@semanticbits.com>
2009-08-13 19:11:01 +0000

    CAAERS-2657
    SVN-Revision: 10239

---
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/service/ReportWithdrawalService.java
@@ -0,0 +1,25 @@
+package gov.nih.nci.cabig.caaers.service;
+
+import gov.nih.nci.cabig.caaers.domain.ReportStatus;
+import gov.nih.nci.cabig.caaers.domain.report.Report;
+
+import org.springframework.transaction.annotation.Transactional;
+/**
+ * This service is used to withdraw report from external agency, via ServiceMix
+ * @author Biju Joseph
+ *
+ */
+@Transactional
+public class ReportWithdrawalService {
+	/**
+	 * 
+	 * @param report - Report to withdraw from external agency
+	 */
+	public void withdrawExternalReport(Report report){
+		
+		assert !report.getStatus().equals(ReportStatus.WITHDRAWN) : "Cannot withdraw a report that is already withdrawn";
+		
+		//send message to service mix.
+		
+	}
+}
