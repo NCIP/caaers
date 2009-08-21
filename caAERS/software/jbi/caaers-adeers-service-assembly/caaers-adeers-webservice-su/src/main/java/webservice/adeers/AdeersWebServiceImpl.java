aed1f7cef9faf46dc209a37dce8336b4ad7050bc
Srini Akkala <akkalas@gmail.com>
2009-08-21 02:09:39 +0000

    SVN-Revision: 10299

--- caAERS/software/jbi/caaers-adeers-service-assembly/caaers-adeers-webservice-su/src/main/java/webservice/adeers/AdeersWebServiceImpl.java
+++ caAERS/software/jbi/caaers-adeers-service-assembly/caaers-adeers-webservice-su/src/main/java/webservice/adeers/AdeersWebServiceImpl.java
@@ -1,10 +1,10 @@
 package webservice.adeers;
 
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import gov.nih.nci.ctep.adeers.client.AEReportXMLServiceSoapBindingStub;
+import gov.nih.nci.ctep.adeers.client.AEReportXMLService_ServiceLocator;
+import gov.nih.nci.ctep.adeers.client.ReportingMode;
+
 import java.io.StringReader;
 
 import javax.xml.transform.Source;
@@ -13,28 +13,22 @@ import javax.xml.transform.stream.StreamSource;
 import org.apache.log4j.Logger;
 
 import webservice.AdeersWebService;
-import caaers.client.AEReportXMLServiceSoapBindingStub;
-import caaers.client.AEReportXMLService_ServiceLocator;
-import caaers.client.ReportingMode;
 
 
 public class AdeersWebServiceImpl implements AdeersWebService {
-	private String caaersAeReportId = "";
-	private String externalEPRs = "";
-	private String reportId = "";
-	private String submitterEmail = "";
+
 	Logger log = Logger.getLogger(getClass());
 	
 	public String callWebService(String aeReport) throws Exception {
 		
-		return submitAEDataXMLAsAttachment(aeReport);
+		return submitOrWithdraw(aeReport);
 	}
 	
-	private String submitAEDataXMLAsAttachment(String aeReportWithCaaersId) throws Exception {
-		 
+	private String submitOrWithdraw(String aeReportWithCaaersId) throws Exception {
+		ServiceContext serviceContext = ServiceContext.getServiceContext();
 		log.info("caAERS-adEERS-Service-Assembly processing report submitted by caAERS"); 
-		String aeReport = detach(aeReportWithCaaersId);	
-		String adeersEPR = externalEPRs.split(",")[0];
+		String aeReport = detach(aeReportWithCaaersId,serviceContext);	
+		String adeersEPR = serviceContext.externalEPRs.split(",")[0];
 		String url=adeersEPR.split("::")[0];
 		String uid=adeersEPR.split("::")[1];
 		String pwd=adeersEPR.split("::")[2];
@@ -61,77 +55,77 @@ public class AdeersWebServiceImpl implements AdeersWebService {
         binding.setPassword(pwd);
         	
         StringReader reader = new StringReader(aeReport);
-        log.info("Submitting to adEERS...");
-        Source attachment = new StreamSource(reader,"");
-        //call the web service                
-        binding.submitAEDataXMLAsAttachment(ReportingMode.SYNCHRONOUS, attachment);
-        String s = binding._getCall().getMessageContext().getResponseMessage().getSOAPPartAsString();
-        //attach the id to the returned message
-        s=s.replaceAll("</ns1:AEReportJobInfo>","<CAEERS_AEREPORT_ID>"+caaersAeReportId+"</CAEERS_AEREPORT_ID><REPORT_ID>"+reportId+"</REPORT_ID><SUBMITTER_EMAIL>"+submitterEmail+"</SUBMITTER_EMAIL></ns1:AEReportJobInfo>");
-        log.info("Response Received from adEERS");
-        return s;
+        String reponseStr = "";
+        if (serviceContext.withdraw) {
+        	log.info("Withdraw to adEERS...");
+        	Source attachment = new StreamSource(reader,"");
+        	log.info("MESSAGE TO ADEERS : " + aeReport);
+	        //call the web service  - withdraw method..              
+	        binding.withdrawAEReport( attachment);
+	        reponseStr = binding._getCall().getMessageContext().getResponseMessage().getSOAPPartAsString();
+	        //attach the id to the returned message
+	        reponseStr=reponseStr.replaceAll("</ns1:AEReportCancelInfo>","<CAEERS_AEREPORT_ID>"+serviceContext.caaersAeReportId+"</CAEERS_AEREPORT_ID><REPORT_ID>"+serviceContext.reportId+"</REPORT_ID><SUBMITTER_EMAIL>"+serviceContext.submitterEmail+"</SUBMITTER_EMAIL></ns1:AEReportCancelInfo>");
+	        log.info("Response Received from adEERS");
+        } else {
+	        log.info("Submitting to adEERS...");
+	        Source attachment = new StreamSource(reader,"");
+	        //call the web service    - submit method ..   
+	        log.info("MESSAGE TO ADEERS : " + aeReport);
+	        binding.submitAEDataXMLAsAttachment(ReportingMode.SYNCHRONOUS, attachment);
+	        reponseStr = binding._getCall().getMessageContext().getResponseMessage().getSOAPPartAsString();
+	        //attach the id to the returned message
+	        reponseStr=reponseStr.replaceAll("</ns1:AEReportJobInfo>","<CAEERS_AEREPORT_ID>"+serviceContext.caaersAeReportId+"</CAEERS_AEREPORT_ID><REPORT_ID>"+serviceContext.reportId+"</REPORT_ID><SUBMITTER_EMAIL>"+serviceContext.submitterEmail+"</SUBMITTER_EMAIL></ns1:AEReportJobInfo>");
+	        log.info("Response Received from adEERS");
+        }
+        return reponseStr;
 		
 	}
 
-	private String detach(String aeReportWithCaaersId) throws Exception {
+	private String detach(String aeReportWithCaaersId,ServiceContext serviceContext) throws Exception {
 		//detach the id and store it to attach later
 		
 		int si = aeReportWithCaaersId.indexOf("<CAEERS_AEREPORT_ID>");
 		int ei = aeReportWithCaaersId.indexOf("</CAEERS_AEREPORT_ID>");
-		caaersAeReportId = aeReportWithCaaersId.substring(si+20, ei);
+		serviceContext.caaersAeReportId = aeReportWithCaaersId.substring(si+20, ei);
 
 		si = aeReportWithCaaersId.indexOf("<EXTERNAL_SYSTEMS>");
 		ei = aeReportWithCaaersId.indexOf("</EXTERNAL_SYSTEMS>");
-		externalEPRs = aeReportWithCaaersId.substring(si+18, ei);
+		serviceContext.externalEPRs = aeReportWithCaaersId.substring(si+18, ei);
 		
 		si = aeReportWithCaaersId.indexOf("<REPORT_ID>");
 		ei = aeReportWithCaaersId.indexOf("</REPORT_ID>");
-		reportId = aeReportWithCaaersId.substring(si+11, ei);
+		serviceContext.reportId = aeReportWithCaaersId.substring(si+11, ei);
 		
 		si = aeReportWithCaaersId.indexOf("<SUBMITTER_EMAIL>");
 		ei = aeReportWithCaaersId.indexOf("</SUBMITTER_EMAIL>");
-		submitterEmail = aeReportWithCaaersId.substring(si+17, ei);
+		serviceContext.submitterEmail = aeReportWithCaaersId.substring(si+17, ei);
 		
-
-
-		String aeReport = aeReportWithCaaersId.replaceAll("<CAEERS_AEREPORT_ID>"+caaersAeReportId+"</CAEERS_AEREPORT_ID>", "");
-		aeReport = aeReport.replaceAll("<EXTERNAL_SYSTEMS>"+externalEPRs+"</EXTERNAL_SYSTEMS>", "");
-		aeReport = aeReport.replaceAll("<REPORT_ID>"+reportId+"</REPORT_ID>", "");
-		aeReport = aeReport.replaceAll("<SUBMITTER_EMAIL>"+submitterEmail+"</SUBMITTER_EMAIL>", "");
+		int withdrawIndex = aeReportWithCaaersId.indexOf("<WITHDRAW>true</WITHDRAW>");
+		if (withdrawIndex > 0 ) {
+			serviceContext.withdraw = true;
+		} 
+		
+		String aeReport = aeReportWithCaaersId.replaceAll("<CAEERS_AEREPORT_ID>"+serviceContext.caaersAeReportId+"</CAEERS_AEREPORT_ID>", "");
+		aeReport = aeReport.replaceAll("<EXTERNAL_SYSTEMS>"+serviceContext.externalEPRs+"</EXTERNAL_SYSTEMS>", "");
+		aeReport = aeReport.replaceAll("<REPORT_ID>"+serviceContext.reportId+"</REPORT_ID>", "");
+		aeReport = aeReport.replaceAll("<SUBMITTER_EMAIL>"+serviceContext.submitterEmail+"</SUBMITTER_EMAIL>", "");
 		aeReport = aeReport.replaceAll("<ADDITIONAL_INFORMATION/>", "");
+		aeReport = aeReport.replaceAll("<WITHDRAW>true</WITHDRAW>", "");
 		return aeReport;
 	}
 	
-//	public static void main (String[] args){
-//		AdeersWebServiceImpl a = new AdeersWebServiceImpl();
-//		String str1 = "";
-//		try {
-//			//FileInputStream fs = new FileInputStream("/Users/sakkala/tech/adeers/instance0.xml");
-//			
-//			
-//			//fs.close();
-//			
-//			FileReader input = new FileReader("/Users/sakkala/tech/adeers/instance2.xml");
-//			BufferedReader bufRead = new BufferedReader(input);
-//			String line = bufRead.readLine();
-//			
-//			while (line != null) {
-//				str1 = str1 + line; 
-//				line = bufRead.readLine();
-//			}
-//			System.out.println(str1);
-//			
-//		} catch (Exception e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-//		
-//		
-//		try {
-//			a.submitAEDataXMLAsAttachment(str1);
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
+
+	public static class ServiceContext {
+		public String caaersAeReportId = "";
+		public String externalEPRs = "";
+		public String reportId = "";
+		public String submitterEmail = "";
+		public boolean withdraw = false;
+		
+
+		
+		public static ServiceContext getServiceContext(){
+			return new ServiceContext();
+		}
+	}
 }
