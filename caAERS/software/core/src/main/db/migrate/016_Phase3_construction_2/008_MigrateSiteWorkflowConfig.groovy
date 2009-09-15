5d4197c813e51f8d9884565a893ba0df6c6ad682
Karthik Iyer <iyerk0@gmail.com>
2009-09-15 14:40:10 +0000

    fix for oracle drop table issue by biju
    SVN-Revision: 10551

--- caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/008_MigrateSiteWorkflowConfig.groovy
+++ caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/008_MigrateSiteWorkflowConfig.groovy
@@ -6,7 +6,7 @@ class MigrateWorkflowConfigs extends edu.northwestern.bioinformatics.bering.Migr
 	    }else if (databaseMatches('oracle')){
 	    	execute("insert into study_site_wf_cfgs (id, name, study_organization_id, workflow_cfg_id) select seq_study_site_wf_cfgs_id.nextval, wf_entity, site_id, wf_config_id from site_workflow_configs");
 	    }
-		  dropTable("site_workflow_configs")
+		  execute("drop table site_workflow_configs")
 	}
 	void down(){
 		//not required
