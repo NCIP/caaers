4c94deb68cf3d540a7448ad6e78685d5c0b3f4ea
Karthik Iyer <iyerk0@gmail.com>
2009-10-14 15:43:05 +0000

    CAAERS-2366, CAAERS-2765, CAAERS-2773, CAAERS-2774
    SVN-Revision: 10970

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/security/CaaersCSMAuthenticationProvider.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/security/CaaersCSMAuthenticationProvider.java
@@ -5,9 +5,6 @@ import gov.nih.nci.cabig.caaers.domain.User;
 import gov.nih.nci.cabig.caaers.domain.security.passwordpolicy.PasswordPolicy;
 import gov.nih.nci.cabig.caaers.service.security.passwordpolicy.PasswordPolicyService;
 import gov.nih.nci.cabig.caaers.service.security.passwordpolicy.validators.LoginPolicyValidator;
-import gov.nih.nci.cabig.caaers.service.security.passwordpolicy.validators.PasswordTooOldException;
-import gov.nih.nci.cabig.caaers.service.security.passwordpolicy.validators.TooManyAllowedFailedLoginAttemptsException;
-import gov.nih.nci.cabig.caaers.service.security.passwordpolicy.validators.UserLockedOutException;
 import gov.nih.nci.cabig.caaers.service.security.user.Credential;
 import gov.nih.nci.security.acegi.csm.authentication.CSMAuthenticationProvider;
 
@@ -15,6 +12,10 @@ import java.util.Date;
 
 import org.acegisecurity.AccountExpiredException;
 import org.acegisecurity.AuthenticationException;
+import org.acegisecurity.BadCredentialsException;
+import org.acegisecurity.CredentialsExpiredException;
+import org.acegisecurity.DisabledException;
+import org.acegisecurity.LockedException;
 import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
 import org.acegisecurity.userdetails.UserDetails;
 import org.apache.commons.logging.Log;
@@ -37,6 +38,9 @@ public class CaaersCSMAuthenticationProvider extends CSMAuthenticationProvider{
 		this.passwordPolicyService = passwordPolicyService;
 	}
 
+	/**
+	 * This method will do Login policy validations and authentication checks.
+	 */
 	@Override
 	protected void additionalAuthenticationChecks(UserDetails user, UsernamePasswordAuthenticationToken token) 
 	throws AccountExpiredException{
@@ -54,34 +58,42 @@ public class CaaersCSMAuthenticationProvider extends CSMAuthenticationProvider{
 		caaersUser = userDao.getByLoginId(user.getUsername());
 		
 		try {
+			// If the user is a caAERS user, then apply Login Policy Validations
 			if(caaersUser!=null) {				
 				credential.setUser(caaersUser);
-				loginPolicyValidator.validate(passwordPolicy, credential);
+				loginPolicyValidator.validate(passwordPolicy, credential, null);
 			}
 			
+			// Irrespective of the type of user, apply authentication checks
 			super.additionalAuthenticationChecks(user, token);
 			
 			if(caaersUser != null){
+				// If the caAERS user passes the checks and validations, then do the following
 				caaersUser.setFailedLoginAttempts(0);
 				caaersUser.setLastFailedLoginAttemptTime(null);
 			}
 			
-		} catch (AuthenticationException authEx) {
-			if(caaersUser!=null) {
-				caaersUser.setFailedLoginAttempts(caaersUser.getFailedLoginAttempts()+1);
-			}
-			throw authEx;
-		} catch (TooManyAllowedFailedLoginAttemptsException attemptsEx) {
+		} catch (DisabledException attemptsEx) {
+			// This exception is thrown when too many failed login attempts occur.
 			caaersUser.setLastFailedLoginAttemptTime(new Date());
 			caaersUser.setFailedLoginAttempts(0);
 			throw attemptsEx;
-		} catch (UserLockedOutException lockEx) {
+		} catch (LockedException lockEx) {
+			// This exception is thrown when user tries to login while the account is locked.
 			throw lockEx;
-		}catch (PasswordTooOldException oldEx) {
+		}catch (CredentialsExpiredException oldEx) {
+			// This exception is thrown when the password is too old.
 			caaersUser.setFailedLoginAttempts(0);
 			caaersUser.setLastFailedLoginAttemptTime(null);
 			throw oldEx;
+		} catch (AuthenticationException authEx) {
+			// This exception is thrown when invalid credentials are used to login.
+			if(caaersUser!=null) {
+				caaersUser.setFailedLoginAttempts(caaersUser.getFailedLoginAttempts()+1);
+			}
+			throw new BadCredentialsException("Invalid login credentials");
 		} finally {
+			// save the caAERS user properties.
 			if(caaersUser!=null) {
 			userDao.save(caaersUser);
 			}
