1ee441317929842e726603c23d36e45935dd37b1
Srini Akkala <akkalas@gmail.com>
2009-10-15 17:53:37 +0000

    CAAERS-3123
    SVN-Revision: 10996

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/utils/XMLUtil.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/utils/XMLUtil.java
@@ -3,6 +3,7 @@ package gov.nih.nci.cabig.caaers.utils;
 import gov.nih.nci.cagrid.caxchange.client.CaXchangeRequestProcessorClient;
 import gov.nih.nci.cagrid.common.Utils;
 import gov.nih.nci.caxchange.ResponseMessage;
+import gov.nih.nci.caxchange.Statuses;
 
 import java.io.InputStream;
 import java.io.StringReader;
@@ -14,11 +15,11 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 
 import org.apache.axis.message.MessageElement;
+import org.apache.log4j.Logger;
 
 public class XMLUtil {
-	public static void main (String[] args ) {
-		XMLUtil.getObjectsFromCoppaResponse("");
-	}
+	private static Logger log = Logger.getLogger(XMLUtil.class);
+	
     public static String getXML(Object rootElement) throws Exception {
 
     	
@@ -32,11 +33,11 @@ public class XMLUtil {
     }
     
     public static List<String>getObjectsFromCoppaResponse(String response ){
-    	if (response == null) {
-    		return new ArrayList<String>();
-    	}
-    	List<String> objList = new ArrayList<String>();
 
+    	List<String> objList = new ArrayList<String>();
+    	if (response == null) {
+    		return objList;
+    	}
             StringReader reader = new StringReader(response);
             InputStream wsddIs = CaXchangeRequestProcessorClient.class.getResourceAsStream("/gov/nih/nci/cagrid/caxchange/client/client-config.wsdd");
             Object deserializedObject = null;
@@ -47,6 +48,14 @@ public class XMLUtil {
             }
             if (deserializedObject != null) {
 	            ResponseMessage rm = (ResponseMessage)deserializedObject;
+	            // error processing message ..
+	            if (rm.getResponse().getResponseStatus().equals(Statuses.FAILURE)) {
+	            	if (rm.getResponse().getCaXchangeError() != null) {
+	            		log.error("caXchange ERROR  : " + rm.getResponse().getCaXchangeError().getErrorDescription().toString());
+	            	}
+	            	return objList;
+	            }
+	            
 	            MessageElement mElement = rm.getResponse().getTargetResponse()[0].getTargetBusinessMessage().get_any()[0];
 	            if( mElement.getName() .equals("Array")) {
 	            	List<MessageElement> meList = mElement.getChildren();
@@ -74,5 +83,27 @@ public class XMLUtil {
 
         return objList;
     }
+    
+    /*
+     	public static void main (String[] args ) throws Exception {
+		
+		XMLUtil.getObjectsFromCoppaResponse(XMLUtil.readFileAsString("/Users/sakkala/tech/caxresponse.txt"));
+	}
+	private static String readFileAsString(String filePath)
+    throws java.io.IOException{
+        StringBuffer fileData = new StringBuffer(1000);
+        BufferedReader reader = new BufferedReader(
+                new FileReader(filePath));
+        char[] buf = new char[1024];
+        int numRead=0;
+        while((numRead=reader.read(buf)) != -1){
+            String readData = String.valueOf(buf, 0, numRead);
+            fileData.append(readData);
+            buf = new char[1024];
+        }
+        reader.close();
+        return fileData.toString();
+    }
+     */
 
 }
