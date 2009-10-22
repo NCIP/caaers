adb06883313c57c0974a9e7b9225b0437b0a0c16
Monish Dombla <monish.dombla@semanticbits.com>
2009-10-22 20:29:05 +0000

    caaers-3160
    SVN-Revision: 11086

--- caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/017_AlterResearchStaff.groovy
+++ caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/017_AlterResearchStaff.groovy
@@ -1,17 +1,29 @@
 class AlterResearchStaff extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
             if (databaseMatches('oracle')) {
+            addColumn("research_staffs", "zip_new", "string")
+            execute('UPDATE research_staffs set zip_new = zip')
+            execute('UPDATE research_staffs set zip = NULL')
             execute('ALTER TABLE research_staffs MODIFY zip VARCHAR2(20)')
+            execute('UPDATE research_staffs set zip = zip_new')
+            execute('UPDATE research_staffs set zip_new = NULL')
+            dropColumn("research_staffs", "zip_new")
+
+            dropColumn("site_research_staffs", "zip_new")
+            addColumn("site_research_staffs", "zip_new", "string")
+            execute('UPDATE site_research_staffs set zip_new = zip')
+            execute('UPDATE site_research_staffs set zip = NULL')
             execute('ALTER TABLE site_research_staffs MODIFY zip VARCHAR2(20)')
+            execute('UPDATE site_research_staffs set zip = zip_new')
+            execute('UPDATE site_research_staffs set zip_new = NULL')
+            dropColumn("site_research_staffs", "zip_new")
             } else if (databaseMatches('postgresql')){
-        execute('ALTER TABLE research_staffs ALTER zip TYPE text')
-        execute('ALTER TABLE site_research_staffs ALTER zip TYPE text')
+                execute('ALTER TABLE research_staffs ALTER zip TYPE text')
+                execute('ALTER TABLE site_research_staffs ALTER zip TYPE text')
             } else {
-        execute('ALTER TABLE research_staffs ALTER zip TYPE text')
-        execute('ALTER TABLE site_research_staffs ALTER zip TYPE text')
+                execute('ALTER TABLE research_staffs ALTER zip TYPE text')
+                execute('ALTER TABLE site_research_staffs ALTER zip TYPE text')
             }
-
-
     }
 
     void down() {
--- caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/018_AlterAeReportPeople.groovy
+++ caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/018_AlterAeReportPeople.groovy
@@ -1,7 +1,13 @@
 class AlterAeReportPeople extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
             if (databaseMatches('oracle')) {
+            addColumn("ae_report_people", "zip_new", "string")
+            execute('UPDATE ae_report_people set zip_new = zip')
+            execute('UPDATE ae_report_people set zip = NULL')
             execute('ALTER TABLE ae_report_people MODIFY zip VARCHAR2(20)')
+            execute('UPDATE ae_report_people set zip = zip_new')
+            execute('UPDATE ae_report_people set zip_new = NULL')
+            dropColumn("ae_report_people", "zip_new")
             } else if (databaseMatches('postgresql')){
         execute('ALTER TABLE ae_report_people ALTER zip TYPE text')
             } else {
