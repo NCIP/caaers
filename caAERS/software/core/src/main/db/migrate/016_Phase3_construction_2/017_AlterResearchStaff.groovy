0c6926ac66c30cfd985b3ac71c92fdd9d5504756
Monish Dombla <monish.dombla@semanticbits.com>
2009-10-22 21:00:34 +0000

    caaers-3160
    SVN-Revision: 11089

--- caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/017_AlterResearchStaff.groovy
+++ caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/017_AlterResearchStaff.groovy
@@ -9,7 +9,6 @@ class AlterResearchStaff extends edu.northwestern.bioinformatics.bering.Migratio
             execute('UPDATE research_staffs set zip_new = NULL')
             dropColumn("research_staffs", "zip_new")
 
-            dropColumn("site_research_staffs", "zip_new")
             addColumn("site_research_staffs", "zip_new", "string")
             execute('UPDATE site_research_staffs set zip_new = zip')
             execute('UPDATE site_research_staffs set zip = NULL')
