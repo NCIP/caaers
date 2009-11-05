9dca8ed0495370057f1a1181aed3271a2a04bb3e
Sameer Sawant <sameer.sawant@semanticbits.com>
2009-11-05 21:39:38 +0000

    CAAERS-2490 The groovy was failing for oracle.
    SVN-Revision: 11196

--- caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/026_AssociateReviewCommentsToReport.groovy
+++ caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/026_AssociateReviewCommentsToReport.groovy
@@ -1,8 +1,9 @@
 class AlterWFComments extends edu.northwestern.bioinformatics.bering.Migration {
-void up() {  
-		execute("ALTER TABLE wf_review_comments DROP CONSTRAINT fk_wf_comments_ae_rp_id")
-		execute("ALTER TABLE wf_review_comments ADD CONSTRAINT fk_wf_comments_ae_rp_id FOREIGN KEY (report_id) REFERENCES report_schedules (id)")
-		
+	void up() {  
+		if (databaseMatches('postgresql')){
+			execute("ALTER TABLE wf_review_comments DROP CONSTRAINT fk_wf_comments_ae_rp_id")
+			execute("ALTER TABLE wf_review_comments ADD CONSTRAINT fk_wf_comments_ae_rp_id FOREIGN KEY (report_id) REFERENCES report_schedules (id)")
+		}	
 	}      
   	void down() {        
   	} 
