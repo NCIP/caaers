0d009928abc64d5d7b54a50d710e33d0e92a21a4
Biju Joseph <biju.joseph@semanticbits.com>
2009-11-24 16:02:55 +0000

    fixed import statements
    SVN-Revision: 11444

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/domain/LocalStudy.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/domain/LocalStudy.java
@@ -1,6 +1,5 @@
 package gov.nih.nci.cabig.caaers.domain;
 
-import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Transient;
