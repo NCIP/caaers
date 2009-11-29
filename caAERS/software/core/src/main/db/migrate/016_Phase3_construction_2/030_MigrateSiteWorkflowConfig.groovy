f868c332ced49f504b3d9a0ccc04e7cf431549f5
Biju Joseph <biju.joseph@semanticbits.com>
2009-11-29 19:29:17 +0000

    COPPA - Investigator check for loginId, email address
    SVN-Revision: 11492

--- caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/030_MigrateSiteWorkflowConfig.groovy
+++ caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/030_MigrateSiteWorkflowConfig.groovy
@@ -15,10 +15,10 @@ class MigrateWorkflowConfigs extends edu.northwestern.bioinformatics.bering.Migr
 			execute("insert into temp_n3 values ('reportingPeriod')");
 			execute("insert into temp_n3 values ('report')");
 			execute("insert into study_site_wf_cfgs (id, study_organization_id, workflow_cfg_id, name) " + 
-                    "select seq_study_site_wf_cfgs_id.nextval,so.id ,(select id from workflow_configuration where workflow_definition_name = 'reportingperiod_coordinating_center'), " +
+                    "select (select seq_study_site_wf_cfgs_id.nextval from dual),so.id ,(select id from workflow_configuration where workflow_definition_name = 'reportingperiod_coordinating_center'), " +
                     "(select n1 from temp_n3 where n1 ='reportingPeriod')  from study_organizations so where so.type = 'SST' and so.id not in (select study_organization_id from study_site_wf_cfgs) " +
                     "UNION " + 
-                    "select seq_study_site_wf_cfgs_id.nextval,so.id ,(select id from workflow_configuration where workflow_definition_name = 'expedited_domestic'), " +
+                    "select (select seq_study_site_wf_cfgs_id.nextval from dual),so.id ,(select id from workflow_configuration where workflow_definition_name = 'expedited_domestic'), " +
                     "(select n1 from temp_n3 where n1 = 'report') from study_organizations so where so.type = 'SST' and so.id not in (select study_organization_id from study_site_wf_cfgs)");
 			execute("drop table temp_n3")
 	    }
