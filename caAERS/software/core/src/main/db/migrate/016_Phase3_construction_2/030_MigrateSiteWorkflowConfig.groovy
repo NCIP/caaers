23d22cf0e094cfa38996745125b2e9ce17c68c49
Biju Joseph <biju.joseph@semanticbits.com>
2009-12-01 00:06:23 +0000

    fixing testcases
    SVN-Revision: 11528

--- caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/030_MigrateSiteWorkflowConfig.groovy
+++ caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/030_MigrateSiteWorkflowConfig.groovy
@@ -11,16 +11,16 @@ class MigrateWorkflowConfigs extends edu.northwestern.bioinformatics.bering.Migr
                     "text('report') from study_organizations so where so.type = 'SST' and so.id not in (select study_organization_id from study_site_wf_cfgs)");
 		
 		}else if (databaseMatches('oracle')){
-			execute("create table temp_n3 (n1 varchar2(2000))");
-			execute("insert into temp_n3 values ('reportingPeriod')");
-			execute("insert into temp_n3 values ('report')");
-			execute("insert into study_site_wf_cfgs (id, study_organization_id, workflow_cfg_id, name) " + 
-                    "select (select seq_study_site_wf_cfgs_id.nextval from dual),so.id ,(select id from workflow_configuration where workflow_definition_name = 'reportingperiod_coordinating_center'), " +
-                    "(select n1 from temp_n3 where n1 ='reportingPeriod')  from study_organizations so where so.type = 'SST' and so.id not in (select study_organization_id from study_site_wf_cfgs) " +
-                    "UNION " + 
-                    "select (select seq_study_site_wf_cfgs_id.nextval from dual),so.id ,(select id from workflow_configuration where workflow_definition_name = 'expedited_domestic'), " +
-                    "(select n1 from temp_n3 where n1 = 'report') from study_organizations so where so.type = 'SST' and so.id not in (select study_organization_id from study_site_wf_cfgs)");
-			execute("drop table temp_n3")
+//			execute("create table temp_n3 (n1 varchar2(2000))");
+//			execute("insert into temp_n3 values ('reportingPeriod')");
+//			execute("insert into temp_n3 values ('report')");
+//			execute("insert into study_site_wf_cfgs (id, study_organization_id, workflow_cfg_id, name) " + 
+//                    "select (select seq_study_site_wf_cfgs_id.nextval from dual),so.id ,(select id from workflow_configuration where workflow_definition_name = 'reportingperiod_coordinating_center'), " +
+//                    "(select n1 from temp_n3 where n1 ='reportingPeriod')  from study_organizations so where so.type = 'SST' and so.id not in (select study_organization_id from study_site_wf_cfgs) " +
+//                    "UNION " + 
+//                    "select (select seq_study_site_wf_cfgs_id.nextval from dual),so.id ,(select id from workflow_configuration where workflow_definition_name = 'expedited_domestic'), " +
+//                    "(select n1 from temp_n3 where n1 = 'report') from study_organizations so where so.type = 'SST' and so.id not in (select study_organization_id from study_site_wf_cfgs)");
+//			execute("drop table temp_n3")
 	    }
 		  
 	}
