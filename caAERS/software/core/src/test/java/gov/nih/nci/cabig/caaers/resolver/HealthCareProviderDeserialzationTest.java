8e7cbe892d519e29f15e74fc2fb60a1f36384419
Biju Joseph <biju.joseph@semanticbits.com>
2009-12-03 19:07:30 +0000

    Deserialization Issue of COPPA HealthcareProvider testing
    SVN-Revision: 11590

---
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/resolver/HealthCareProviderDeserialzationTest.java
@@ -0,0 +1,70 @@
+package gov.nih.nci.cabig.caaers.resolver;
+
+import gov.nih.nci.cabig.caaers.AbstractTestCase;
+import gov.nih.nci.coppa.iso.DSet;
+import gov.nih.nci.coppa.iso.Ii;
+import gov.nih.nci.coppa.po.HealthCareProvider;
+import gov.nih.nci.coppa.po.Id;
+import gov.nih.nci.coppa.po.grid.dto.transform.po.IdTransformer;
+import gov.nih.nci.coppa.services.grid.dto.transform.iso.DSETIITransformer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+
+import org.iso._21090.DSETII;
+import org.springframework.core.io.ClassPathResource;
+
+import com.semanticbits.coppasimulator.util.XMLUtilities;
+/**
+ * 
+ * @author Biju Joseph
+ *
+ */
+public class HealthCareProviderDeserialzationTest extends AbstractTestCase {
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+	}
+	//tests a valid HealthcareProvider xml island serialization. 
+	public void testSerializationOf_HealthcareProviderXML() throws Exception{
+		String filename = "EachHealthCareProvider.xml";
+		Reader reader = getTestXMLFile(filename);
+		assertNotNull(reader);
+		HealthCareProvider hp = (HealthCareProvider)new XMLUtilities().deserializeObject(HealthCareProvider.class, reader, com.semanticbits.coppasimulator.util.CoppaObjectFactory.COPPA_WSDD_FILE);
+		assertEquals("NCI person entity identifier", hp.getPlayerIdentifier().getIdentifierName());
+		assertEquals("NCI organization entity identifier", hp.getScoperIdentifier().getIdentifierName());
+	}
+	
+	//Tests an xml, without having scoperIdentifier element.
+	public void testSerializationOf_HealthcareProviderXML_WithNoScoperIdentifier() throws Exception{
+		String filename = "EachHealthCareProvider_NoScoperIdentifier.xml";
+		Reader reader = getTestXMLFile(filename);
+		assertNotNull(reader);
+		HealthCareProvider hp = (HealthCareProvider)new XMLUtilities().deserializeObject(HealthCareProvider.class, reader, com.semanticbits.coppasimulator.util.CoppaObjectFactory.COPPA_WSDD_FILE);
+		assertEquals("NCI person entity identifier", hp.getPlayerIdentifier().getIdentifierName());
+		assertNull(hp.getScoperIdentifier());
+	}
+	
+	//check to see that Idtransformer returns null, if input is null
+	public void testScoperIdentifierBeingNull(){
+		DSETIITransformer dsetIITransformer = DSETIITransformer.INSTANCE;
+		DSETII dsetii = new DSETII();
+		dsetii.getItem().add(null);
+		DSet<Ii> dsetIi= dsetIITransformer.toDto(dsetii);
+		Id id = IdTransformer.INSTANCE.toXml(dsetIi.getItem().iterator().next());
+		assertNull(id);
+	}
+	
+	//load the xml file from classpath. 
+	 private Reader getTestXMLFile(String fileName) throws Exception {
+	    	
+	        File testFile = new ClassPathResource("/gov/nih/nci/cabig/caaers/resolver/xml/" + fileName).getFile();
+	        BufferedReader ds = new BufferedReader(new FileReader(testFile));
+	        return ds;
+	      
+	    }
+
+}
