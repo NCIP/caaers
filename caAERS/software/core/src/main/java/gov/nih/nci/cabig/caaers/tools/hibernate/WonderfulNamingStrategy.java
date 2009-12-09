683ea41eabec0f117f1dd93f1b8371a1ac943a7a
Biju Joseph <biju.joseph@semanticbits.com>
2009-12-09 15:32:48 +0000

    CAAERS-3378
    SVN-Revision: 11680

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/tools/hibernate/WonderfulNamingStrategy.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/tools/hibernate/WonderfulNamingStrategy.java
@@ -1,6 +1,9 @@
 package gov.nih.nci.cabig.caaers.tools.hibernate;
 
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 import org.hibernate.cfg.ImprovedNamingStrategy;
+import org.springframework.beans.factory.annotation.Required;
 
 /**
  * @author Rhett Sutphin
@@ -10,6 +13,10 @@ import org.hibernate.cfg.ImprovedNamingStrategy;
  * into a shared library.
  */
 public class WonderfulNamingStrategy extends ImprovedNamingStrategy {
+	
+	private String[] uppercaseColumns;
+	private String uppercaseColumnNames;
+	
     @Override
     public String foreignKeyColumnName(String propertyName, String propertyEntityName, String propertyTableName, String referencedColumnName) {
         return columnName(propertyName) + "_id";
@@ -19,7 +26,18 @@ public class WonderfulNamingStrategy extends ImprovedNamingStrategy {
     public String classToTableName(String className) {
         return pluralize(super.classToTableName(className));
     }
+    
+   
+    
+    @Override
+    public String logicalColumnName(String columnName, String propertyName) {
+    	
+    	if(ArrayUtils.contains(uppercaseColumns, columnName)) 
+        	return super.logicalColumnName(StringUtils.upperCase(columnName), propertyName);
 
+    	return super.logicalColumnName(columnName, propertyName);
+    }
+    
     private String pluralize(String name) {
         StringBuilder p = new StringBuilder(name);
         if (name.endsWith("y")) {
@@ -30,4 +48,15 @@ public class WonderfulNamingStrategy extends ImprovedNamingStrategy {
         }
         return p.toString();
     }
+    
+    @Required
+    public String getUppercaseColumnNames() {
+		return uppercaseColumnNames;
+	}
+    
+    @Required
+    public void setUppercaseColumnNames(String uppercaseColumnNames) {
+    	this.uppercaseColumnNames = uppercaseColumnNames;
+		this.uppercaseColumns = StringUtils.split(uppercaseColumnNames, ',');
+	}
 }
