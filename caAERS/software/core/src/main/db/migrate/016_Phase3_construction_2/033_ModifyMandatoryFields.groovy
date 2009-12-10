14f021cf0e09b9122a2dbf94950cceaed4c21bbb
Biju Joseph <biju.joseph@semanticbits.com>
2009-12-10 15:16:40 +0000

    groovy files in trunk
    SVN-Revision: 11696

--- caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/033_ModifyMandatoryFields.groovy
+++ caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/033_ModifyMandatoryFields.groovy
@@ -1,9 +1,10 @@
-class AlterCourseAgents extends edu.northwestern.bioinformatics.bering.Migration {
+class ModifyMandatoryFields extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
     	execute("update mandatory_field_defs set mandatory=0 where field_path ='diseaseHistory.diagnosisDate'")
     }
 
     void down() {
+    	//not possible execute the same.
     	execute("update mandatory_field_defs set mandatory=0 where field_path ='diseaseHistory.diagnosisDate'")
     }
 }
