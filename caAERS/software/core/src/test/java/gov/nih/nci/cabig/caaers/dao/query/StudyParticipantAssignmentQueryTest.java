fa09e167e08ff34f1ff7ec66795f6867058ee95b
Ion Olaru <ion.olaru@gmail.com>
2009-12-17 15:23:32 +0000

    CAAERS-3406 - testCase
    SVN-Revision: 11770

---
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/StudyParticipantAssignmentQueryTest.java
@@ -0,0 +1,32 @@
+package gov.nih.nci.cabig.caaers.dao.query;
+
+import junit.framework.TestCase;
+
+public class StudyParticipantAssignmentQueryTest extends TestCase {
+
+    public void testQueryConstructor() throws Exception {
+        StudyParticipantAssignmentQuery q = new StudyParticipantAssignmentQuery();
+        assertEquals("Wrong Constructor Parameter", "SELECT spa FROM StudyParticipantAssignment spa", q.getQueryString());
+
+    }
+
+    public void testFilterByStudySiteId() throws Exception {
+        StudyParticipantAssignmentQuery q = new StudyParticipantAssignmentQuery();
+        q.filterByStudySiteId(10);
+        assertEquals("Wrong Query String", "SELECT spa FROM StudyParticipantAssignment spa WHERE spa.studySite.id = :SS_ID", q.getQueryString());
+
+    }
+
+    public void testFilterByStudySubjectIdentifier() throws Exception {
+        StudyParticipantAssignmentQuery q = new StudyParticipantAssignmentQuery();
+        q.filterByStudySubjectIdentifier("S-90");
+        assertEquals("Wrong Query String", "SELECT spa FROM StudyParticipantAssignment spa WHERE spa.studySubjectIdentifier = :SSI", q.getQueryString());
+    }
+
+    public void testFilterByParticipantExcluded() throws Exception {
+        StudyParticipantAssignmentQuery q = new StudyParticipantAssignmentQuery();
+        q.filterByParticipantExcluded(4);
+        assertEquals("Wrong Query String", "SELECT spa FROM StudyParticipantAssignment spa WHERE spa.participant.id != :P_ID", q.getQueryString());
+    }
+
+}
