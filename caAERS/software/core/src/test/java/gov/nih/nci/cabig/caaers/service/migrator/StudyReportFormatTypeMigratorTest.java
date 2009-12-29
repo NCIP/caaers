d984989b43157be96d81f783ae3b1d94470b602c
Ion Olaru <ion.olaru@gmail.com>
2009-12-29 16:34:23 +0000

    CAAERS-3152
    Jira Id: 3152
    
    
    SVN-Revision: 11817

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/service/migrator/StudyReportFormatTypeMigratorTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/service/migrator/StudyReportFormatTypeMigratorTest.java
@@ -1,44 +1,41 @@
 package gov.nih.nci.cabig.caaers.service.migrator;
 
 import gov.nih.nci.cabig.caaers.AbstractTestCase;
-import gov.nih.nci.cabig.caaers.domain.Fixtures;
-import gov.nih.nci.cabig.caaers.domain.LocalStudy;
-import gov.nih.nci.cabig.caaers.domain.Study;
-import gov.nih.nci.cabig.caaers.domain.StudyTherapyType;
+import gov.nih.nci.cabig.caaers.domain.*;
 import gov.nih.nci.cabig.caaers.service.DomainObjectImportOutcome;
 
-public class StudyTherapyMigratorTest extends AbstractTestCase {
-	
-	StudyTherapyMigrator migrator;
+public class StudyReportFormatTypeMigratorTest extends AbstractTestCase {
+
+	StudyReportFormatTypeMigrator migrator;
 	Study xstreamStudy;
 	DomainObjectImportOutcome<Study> outcome;
 	Study dest;
+
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		dest = new LocalStudy();
-		xstreamStudy = Fixtures.createStudy("short title");
+		xstreamStudy = Fixtures.createStudy("Short Title");
 		outcome = new DomainObjectImportOutcome<Study>();
-		migrator = new StudyTherapyMigrator();
+		migrator = new StudyReportFormatTypeMigrator();
 	}
-	
-	
+
+
 	public void testMigrate() {
-		xstreamStudy.addStudyTherapy(StudyTherapyType.DRUG_ADMINISTRATION);
-		xstreamStudy.addStudyTherapy(StudyTherapyType.RADIATION);
-		
+		xstreamStudy.addReportFormatType(ReportFormatType.ADEERSPDF);
+		xstreamStudy.addReportFormatType(ReportFormatType.MEDWATCHPDF);
+		xstreamStudy.addReportFormatType(ReportFormatType.CAAERSXML);
 		migrator.migrate(xstreamStudy, dest, outcome);
-		assertEquals(2, dest.getStudyTherapies().size());
+		assertEquals(3, xstreamStudy.getReportFormats().size());
+		assertEquals(3, dest.getReportFormats().size());
 		assertTrue("No errors should be there", outcome.getMessages().isEmpty());
 
 	}
-	
-	
-	public void testMigrateWithNoTherapy() {
-		migrator.migrate(xstreamStudy, dest, outcome);
-		assertEquals(0, dest.getStudyTherapies().size());
-		assertTrue("No errors should be there", outcome.getMessages().isEmpty());
 
+	public void testMigrateWithNoReportFormat() {
+		migrator.migrate(xstreamStudy, dest, outcome);
+		assertEquals(0, dest.getReportFormats().size());
+		assertTrue("No errors should be there", outcome.getMessages().isEmpty());
 	}
 
 }
