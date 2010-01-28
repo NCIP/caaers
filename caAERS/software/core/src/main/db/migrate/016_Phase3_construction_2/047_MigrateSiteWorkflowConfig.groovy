f82aac854c78c0dd41321382d447fcd6cc22bd17
Biju Joseph <biju.joseph@semanticbits.com>
2010-01-28 15:34:36 +0000

    CAAERS-3657 - error creting new course
    SVN-Revision: 12053

--- caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/047_MigrateSiteWorkflowConfig.groovy
+++ caAERS/software/core/src/main/db/migrate/016_Phase3_construction_2/047_MigrateSiteWorkflowConfig.groovy
@@ -2,18 +2,19 @@ class MigrateWorkflowConfigs extends edu.northwestern.bioinformatics.bering.Migr
 	void up(){
 
 		if (databaseMatches('oracle')){
+          
+            execute("delete from study_site_wf_cfgs")
+
 			execute("create table temp_n3 (n1 varchar2(2000))");
 			execute("insert into temp_n3 values ('reportingPeriod')");
 			execute("insert into temp_n3 values ('report')");
 
-            execute("alter table study_site_wf_cfgs add (ABC number)");
-            execute("insert into study_site_wf_cfgs (id, name, study_organization_id, workflow_cfg_id, ABC)" +
-                    "select seq_study_site_wf_cfgs_id.nextval, (select n1 from temp_n3 where n1 ='reportingPeriod') as C4, so.id, (select id from workflow_configuration where workflow_definition_name = 'reportingperiod_coordinating_center') as C3, 1 " +
-                    "from study_organizations so where so.type = 'SST' and so.id not in (select study_organization_id from study_site_wf_cfgs where ABC = 1)");
-            execute("insert into study_site_wf_cfgs (id, name, study_organization_id, workflow_cfg_id, ABC)" +
-                    "select seq_study_site_wf_cfgs_id.nextval, (select n1 from temp_n3 where n1 = 'report') as C4, so.id, (select id from workflow_configuration where workflow_definition_name = 'expedited_domestic') as C3, 2 " +
-                    "from study_organizations so where so.type = 'SST' and so.id not in (select study_organization_id from study_site_wf_cfgs where ABC = 2)");
-            execute("alter table study_site_wf_cfgs drop column ABC");
+            execute("insert into study_site_wf_cfgs (id, name, study_organization_id, workflow_cfg_id)" +
+                    "select seq_study_site_wf_cfgs_id.nextval, (select n1 from temp_n3 where n1 ='reportingPeriod') as C4, so.id, (select id from workflow_configuration where workflow_definition_name = 'reportingperiod_coordinating_center') as C3 " +
+                    "from study_organizations so where so.type = 'SST' ");
+            execute("insert into study_site_wf_cfgs (id, name, study_organization_id, workflow_cfg_id)" +
+                    "select seq_study_site_wf_cfgs_id.nextval, (select n1 from temp_n3 where n1 = 'report') as C4, so.id, (select id from workflow_configuration where workflow_definition_name = 'expedited_domestic') as C3 " +
+                    "from study_organizations so where so.type = 'SST' ");
 
 			execute("drop table temp_n3")
 	    }
