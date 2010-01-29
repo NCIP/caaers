3f259e40a6490582a500eab7a4d9dac9494bc31c
Ion Olaru <ion.olaru@gmail.com>
2010-01-29 17:04:59 +0000

    CAAERS-3676 - Make Report Definition Header and Footer fields only show if Custom PDF is select
    SVN-Revision: 12067

--- caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/rule/notification/BasicsTab.java
+++ caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/rule/notification/BasicsTab.java
@@ -66,14 +66,16 @@ public class BasicsTab extends TabWithFields<ReportDefinitionCommand> {
         InputFieldAttributes.setColumns(descField, 50);
         fields.add(descField);
 
-        InputField headerField = InputFieldFactory.createTextArea("reportDefinition.header", "Header", false);
-        InputFieldAttributes.setColumns(headerField, 50);
-        fields.add(headerField);
+        if (command.getReportDefinition() == null || command.getReportDefinition().getReportFormatType() == ReportFormatType.CUSTOM_REPORT) {
+            InputField headerField = InputFieldFactory.createTextArea("reportDefinition.header", "Header", false);
+            InputFieldAttributes.setColumns(headerField, 50);
+            fields.add(headerField);
 
-        InputField footerField = InputFieldFactory.createTextArea("reportDefinition.footer", "Footer", false);
-        InputFieldAttributes.setColumns(footerField, 50);
-        fields.add(footerField);
-
+            InputField footerField = InputFieldFactory.createTextArea("reportDefinition.footer", "Footer", false);
+            InputFieldAttributes.setColumns(footerField, 50);
+            fields.add(footerField);
+        }
+        
         InputField amendableField = InputFieldFactory.createBooleanSelectField("reportDefinition.amendable", "Amendable?", true);
         fields.add(amendableField);
 
