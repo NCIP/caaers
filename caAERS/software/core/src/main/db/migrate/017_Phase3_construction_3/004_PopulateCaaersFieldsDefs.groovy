e09fc76526473791f30661852554c3d9ad5534a0
Ion Olaru <ion.olaru@gmail.com>
2010-02-19 19:09:10 +0000

    CAAERS-3755 - Allow configuration of Mandatory, Optional, and N/A fields on course pop-up
    SVN-Revision: 12178

--- caAERS/software/core/src/main/db/migrate/017_Phase3_construction_3/004_PopulateCaaersFieldsDefs.groovy
+++ caAERS/software/core/src/main/db/migrate/017_Phase3_construction_3/004_PopulateCaaersFieldsDefs.groovy
@@ -5,14 +5,14 @@ class PopulateCaaersFieldsDefs extends edu.northwestern.bioinformatics.bering.Mi
         execute("INSERT INTO caaers_field_defs(version, field_path, mandatory, tab_name, grid_id) values (0, 'assignment.startDateOfFirstCourse', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(version, field_path, mandatory, tab_name, grid_id) values (0, 'reportingPeriod.startDate', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(version, field_path, mandatory, tab_name, grid_id) values (0, 'reportingPeriod.endDate', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
-        execute("INSERT INTO caaers_field_defs(version, field_path, mandatory, tab_name, grid_id) values (0, 'eportingPeriod.epoch', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
+        execute("INSERT INTO caaers_field_defs(version, field_path, mandatory, tab_name, grid_id) values (0, 'reportingPeriod.epoch', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(version, field_path, mandatory, tab_name, grid_id) values (0, 'reportingPeriod.cycleNumber', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(version, field_path, mandatory, tab_name, grid_id) values (0, 'reportingPeriod.treatmentAssignment', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
       } else if (databaseMatches('oracle')) {
         execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'assignment.startDateOfFirstCourse', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
-        execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'eportingPeriod.startDate', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
-        execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'eportingPeriod.endDate', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
-        execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'eportingPeriod.epoch', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
+        execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.startDate', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
+        execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.endDate', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
+        execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.epoch', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.cycleNumber', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.treatmentAssignmentt', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
       }
