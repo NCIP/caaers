2dbd7daf3d8897f18b36dee8cf059b8fabff7ff5
Ion Olaru <ion.olaru@gmail.com>
2010-02-19 19:09:30 +0000

    CAAERS-3755 - Allow configuration of Mandatory, Optional, and N/A fields on course pop-up
    SVN-Revision: 12179

--- caAERS/software/core/src/main/db/migrate/017_Phase3_construction_3/004_PopulateCaaersFieldsDefs.groovy
+++ caAERS/software/core/src/main/db/migrate/017_Phase3_construction_3/004_PopulateCaaersFieldsDefs.groovy
@@ -14,7 +14,7 @@ class PopulateCaaersFieldsDefs extends edu.northwestern.bioinformatics.bering.Mi
         execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.endDate', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.epoch', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.cycleNumber', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
-        execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.treatmentAssignmentt', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
+        execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.treatmentAssignment', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
       }
     }
 
