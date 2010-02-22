b84e394a522cda809fcd9e098ea39e059b4e2bdd
Ion Olaru <ion.olaru@gmail.com>
2010-02-22 19:42:58 +0000

    CAAERS-3755 - Allow configuration of Mandatory, Optional, and N/A fields on course pop-up
    SVN-Revision: 12196

--- caAERS/software/core/src/main/db/migrate/017_Phase3_construction_3/004_PopulateCaaersFieldsDefs.groovy
+++ caAERS/software/core/src/main/db/migrate/017_Phase3_construction_3/004_PopulateCaaersFieldsDefs.groovy
@@ -8,6 +8,8 @@ class PopulateCaaersFieldsDefs extends edu.northwestern.bioinformatics.bering.Mi
         execute("INSERT INTO caaers_field_defs(version, field_path, mandatory, tab_name, grid_id) values (0, 'reportingPeriod.epoch', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(version, field_path, mandatory, tab_name, grid_id) values (0, 'reportingPeriod.cycleNumber', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(version, field_path, mandatory, tab_name, grid_id) values (0, 'reportingPeriod.treatmentAssignment', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
+        execute("ALTER TABLE ae_reporting_periods ALTER COLUMN start_date DROP NOT NULL");
+
       } else if (databaseMatches('oracle')) {
         execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'assignment.startDateOfFirstCourse', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.startDate', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
@@ -15,6 +17,7 @@ class PopulateCaaersFieldsDefs extends edu.northwestern.bioinformatics.bering.Mi
         execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.epoch', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.cycleNumber', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
         execute("INSERT INTO caaers_field_defs(id, version, field_path, mandatory, tab_name, grid_id) values (SEQ_CAAERS_FIELD_DEFS_ID.nextval, 0, 'reportingPeriod.treatmentAssignment', 0, 'gov.nih.nci.cabig.caaers.web.ae.CourseCycleTab', null )");
+        execute("ALTER TABLE ae_reporting_periods MODIFY start_date NULL");
       }
     }
 
