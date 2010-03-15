9e100fca0b3f22d5c1833fad844fb55772225cef
Ion Olaru <ion.olaru@gmail.com>
2010-03-15 15:20:49 +0000

    CAAERS-3130 - Support entry of verbatim first
    SVN-Revision: 12304

--- caAERS/software/core/src/main/db/migrate/018_Phase3_construction_4/002_UpdateStudyVerbatimFirst.groovy
+++ caAERS/software/core/src/main/db/migrate/018_Phase3_construction_4/002_UpdateStudyVerbatimFirst.groovy
@@ -1,9 +1,9 @@
 class UpdateStudyVerbatimFirst extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
       if (databaseMatches('oracle')) {
-              addColumn("studies", "verbatim_first", "boolean", nullable: false, defaultValue: 0)
+              addColumn("studies", "verbatim_first", "boolean", nullable: true, defaultValue: 0)
       } else {
-              addColumn("studies", "verbatim_first", "boolean", nullable: false, defaultValue: false)
+              addColumn("studies", "verbatim_first", "boolean", nullable: true, defaultValue: false)
       }
 	}
 
