7b7f7f454b4438276b7ecd114ec6d6ea413330df
Sameer Sawant <sameer.sawant@semanticbits.com>
2010-04-07 18:59:28 +0000

    CAAERS-3882
    Fixed the groovy to use sequences for IDs instead of "positive numbers". Upgrade was failing on demo instance.
    
    SVN-Revision: 12437

--- caAERS/software/core/src/main/db/migrate/018_Phase3_construction_4/008_AddOrganizationProtectionElement.groovy
+++ caAERS/software/core/src/main/db/migrate/018_Phase3_construction_4/008_AddOrganizationProtectionElement.groovy
@@ -1,16 +1,16 @@
 class AddOrganizationProtectionElement extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
 
-	  insert('csm_protection_element',
-	  		 [protection_element_id: -7942, protection_element_name: 'gov.nih.nci.cabig.caaers.domain.Organization', object_id: 'gov.nih.nci.cabig.caaers.domain.Organization', application_id: -1],
-	  		 primaryKey: false);
-	  insert('csm_pg_pe',
-	  		 [pg_pe_id: 9012, protection_group_id: -5, protection_element_id: -7942],
-	  		 primaryKey: false);
+	  if (databaseMatches('postgresql')){
+			execute("insert into csm_protection_element (protection_element_id,protection_element_name,object_id,application_id) values (-7942,'gov.nih.nci.cabig.caaers.domain.Organization','gov.nih.nci.cabig.caaers.domain.Organization',-1)")
+			execute("insert into csm_pg_pe (pg_pe_id,protection_group_id,protection_element_id) values ((select nextval('csm_pg_pe_id_seq')),-5,-7942)")
+		}else if(databaseMatches('oracle')){
+			execute("insert into csm_protection_element (protection_element_id,protection_element_name,object_id,application_id) values (-7942,'gov.nih.nci.cabig.caaers.domain.Organization','gov.nih.nci.cabig.caaers.domain.Organization',-1)")
+			execute("insert into csm_pg_pe (pg_pe_id,protection_group_id,protection_element_id) values (CSM_PG_PE_PG_PE_ID_SEQ.nextval,-5,-7942)")
+		}		 
     }
 
     void down(){
         execute("delete from csm_protection_element where protection_element_id = -7942");
-        execute("delete from csm_pg_pe where pg_pe_id = 9012");
     }
 }
