e4accb26e0dc7b32f10f85377eb7c95328935b39
Srini Akkala <akkalas@gmail.com>
2010-04-22 15:28:53 +0000

    CAAERS-3879
    SVN-Revision: 12542

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/accesscontrol/query/impl/ParticipantdFetcherTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/accesscontrol/query/impl/ParticipantdFetcherTest.java
@@ -2,7 +2,6 @@ package gov.nih.nci.cabig.caaers.accesscontrol.query.impl;
 
 import gov.nih.nci.cabig.caaers.CaaersDaoTestCase;
 import gov.nih.nci.cabig.caaers.dao.ParticipantDao;
-import gov.nih.nci.cabig.caaers.dao.query.ajax.ParticipantAjaxableDomainObjectQuery;
 import gov.nih.nci.cabig.caaers.domain.UserGroupType;
 import gov.nih.nci.cabig.caaers.security.SecurityTestUtils;
 import gov.nih.nci.cabig.caaers.utils.FetcherUtils;
@@ -15,7 +14,7 @@ public class ParticipantdFetcherTest extends CaaersDaoTestCase {
 
 	private CaaersParticipantIdFetcherImpl participantIdFetcher = null;
 	private FetcherUtils fetcherUtils = null;
-	private ParticipantDao participantDao = null;
+
 	
 	private void enableAuthorization(){
 		SecurityTestUtils.enableAuthorization(true, applicationContext);
@@ -25,11 +24,7 @@ public class ParticipantdFetcherTest extends CaaersDaoTestCase {
 		super.setUp();
 		participantIdFetcher = 
 			(CaaersParticipantIdFetcherImpl) getApplicationContext().getBean("participantIdFetcher");
-		participantDao = 
-			(ParticipantDao) getApplicationContext().getBean("participantDao");
-		
-		participantDao = 
-			(ParticipantDao) getApplicationContext().getBean("participantDao");
+
 		
 		fetcherUtils = registerMockFor(FetcherUtils.class);
 		
@@ -46,7 +41,7 @@ public class ParticipantdFetcherTest extends CaaersDaoTestCase {
 		boolean studyFilteringRequired = true;
 		EasyMock.expect(fetcherUtils.studyFilteringRequired((List<UserGroupType>)EasyMock.anyObject(),(List<String>)EasyMock.anyObject())).andReturn(studyFilteringRequired);
 		replayMocks();
-		ParticipantAjaxableDomainObjectQuery query = new ParticipantAjaxableDomainObjectQuery();
+
 		List<String> filteredList = participantIdFetcher.fetch("1000@def.com");
 		assertEquals(1,filteredList.size());	
 		/*
