dd4744049cc0338688444637d375a6322b555eb7
Srini Akkala <akkalas@gmail.com>
2010-04-22 20:38:35 +0000

    SVN-Revision: 12553

---
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/aspects/StudyQueryFilterByLoginIdAspect.java
@@ -0,0 +1,11 @@
+package gov.nih.nci.cabig.caaers.accesscontrol.aspects;
+
+public class StudyQueryFilterByLoginIdAspect extends AbstractFilterByLoginIdAspect{
+
+	@Override
+	public String getInQuery() {
+		// TODO Auto-generated method stub
+		return "study.id IN (:ids)";
+	}
+
+}
