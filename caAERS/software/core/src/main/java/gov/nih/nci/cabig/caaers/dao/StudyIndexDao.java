470c56396b572978bfab6940d4589b33ad166c51
Srini Akkala <akkalas@gmail.com>
2010-04-23 20:17:39 +0000

    CAAERS-3879
    SVN-Revision: 12571

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/dao/StudyIndexDao.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/dao/StudyIndexDao.java
@@ -1,10 +1,27 @@
 package gov.nih.nci.cabig.caaers.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Properties;
+
 import gov.nih.nci.cabig.caaers.domain.StudyIndex;
 
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 public class StudyIndexDao extends CaaersDao<StudyIndex> {
+
+    private JdbcTemplate jdbcTemplate;
+    
+    private Properties properties;
+    
+    private static final String DB_NAME  = "databaseName";
+    
+    private static final String ORACLE_DB = "oracle";
+    
+
 
 	@Override
 	public Class<StudyIndex> domainClass() {
@@ -16,5 +33,55 @@ public class StudyIndexDao extends CaaersDao<StudyIndex> {
     public void save(final StudyIndex studyIndex) {
         getHibernateTemplate().saveOrUpdate(studyIndex);
     }
+    
+    @Override
+    @Transactional(readOnly = false)
+    public int[] updateIndex(final List pIds , final String userName){
+    	String sql = "insert into study_index (login_id,study_id) "
+            + "values (?,?)";
+    	
+        String dataBase = "";
+    	if(properties.getProperty(DB_NAME) != null){
+    		dataBase = properties.getProperty(DB_NAME);
+    	}
+    	if(dataBase.equals(ORACLE_DB))
+    		sql = "insert into study_index (id,login_id,study_id) "
+                + "values (seq_study_index_id.NEXTVAL,?,?,?,?)";
+    	
+    	
+		BatchPreparedStatementSetter setter = null;
+        setter = new BatchPreparedStatementSetter() {
+
+            public int getBatchSize() {
+                return pIds.size();
+            }
+
+            public void setValues(PreparedStatement ps, int index) throws SQLException {
+            	Integer pId = (Integer) pIds.get(index);
+            	ps.setString(1, userName);
+                ps.setInt(2, pId);
+            }
+
+
+        };
+        return jdbcTemplate.batchUpdate(sql, setter);
+    	
+    }
+    
+    @Override
+    @Transactional(readOnly = false)
+    public void clearIndex(String userName) {
+    	String sql = "delete from study_index where login_id = '"+userName+"'";
+    	jdbcTemplate.update(sql);
+
+    }
+    
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
 
 }
