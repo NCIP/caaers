8a6d6970bf80a4b3964b3f075248f071c3ab0803
Srini Akkala <akkalas@gmail.com>
2010-04-23 15:16:51 +0000

    CAAERS-3879
    SVN-Revision: 12566

---
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/ParticipantIndexDaoTest.java
@@ -0,0 +1,34 @@
+package gov.nih.nci.cabig.caaers.dao;
+
+import gov.nih.nci.cabig.caaers.DaoNoSecurityTestCase;
+import gov.nih.nci.cabig.caaers.domain.Participant;
+import gov.nih.nci.cabig.caaers.domain.ParticipantIndex;
+
+public class ParticipantIndexDaoTest extends DaoNoSecurityTestCase<ParticipantIndexDao> {
+	
+	ParticipantDao participantDao ; 
+	@Override
+	protected void setUp() throws Exception {
+		// change the security interceptor with stub.
+		super.setUp();
+		participantDao = (ParticipantDao)getApplicationContext().getBean("participantDao");
+	}
+	
+	
+	public void testSave() throws Exception {
+        Participant participant = participantDao.getById(-100);
+        String userName = "srakkala";
+        ParticipantIndex participantIndex = new ParticipantIndex();
+        participantIndex.setLoginId(userName);
+        participantIndex.setParticipant(participant);
+        getDao().save(participantIndex);
+        
+        participant = participantDao.getById(-101);
+        participantIndex = new ParticipantIndex();
+        participantIndex.setLoginId(userName);
+        participantIndex.setParticipant(participant);
+        getDao().save(participantIndex);
+        
+        System.out.print("sss");
+    }
+}
---
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/StudyIndexDaoTest.java
@@ -0,0 +1,25 @@
+package gov.nih.nci.cabig.caaers.dao;
+
+import gov.nih.nci.cabig.caaers.DaoNoSecurityTestCase;
+import gov.nih.nci.cabig.caaers.domain.Study;
+import gov.nih.nci.cabig.caaers.domain.StudyIndex;
+
+public class StudyIndexDaoTest extends DaoNoSecurityTestCase<StudyIndexDao> {
+	StudyDao studyDao ; 
+	@Override
+	protected void setUp() throws Exception {
+		// change the security interceptor with stub.
+		super.setUp();
+		studyDao = (StudyDao)getApplicationContext().getBean("studyDao");
+	}
+	
+	
+	public void testSave() throws Exception {
+        Study study = studyDao.getById(-2);
+        String userName = "srakkala";
+        StudyIndex studyIndex = new StudyIndex();
+        studyIndex.setLoginId(userName);
+        studyIndex.setStudy(study);
+        getDao().save(studyIndex);
+    }
+}
