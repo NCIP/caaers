879e699159506e0b52a8191191f6833c25881b9c
Srini Akkala <akkalas@gmail.com>
2010-04-26 16:26:41 +0000

    deprecated .. CAAERS-3879
    SVN-Revision: 12595

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/dataproviders/FilteredDataProvider.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/dataproviders/FilteredDataProvider.java
@@ -14,6 +14,7 @@ import org.apache.log4j.Logger;
 import com.semanticbits.security.contentfilter.IdFetcher;
 import com.semanticbits.security.contentfilter.cache.QueryCacheManager;
 
+@Deprecated
 public class FilteredDataProvider extends BaseSecurityFilterer {
 	
 	private Authentication authentication;
