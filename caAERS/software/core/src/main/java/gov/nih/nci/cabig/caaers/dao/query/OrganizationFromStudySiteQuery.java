c31ec0ec2ad997a34b47525b0c79fe4e6333e293
Srini Akkala <akkalas@gmail.com>
2010-04-27 18:31:34 +0000

    CAAERS-3879
    SVN-Revision: 12634

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/dao/query/OrganizationFromStudySiteQuery.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/dao/query/OrganizationFromStudySiteQuery.java
@@ -2,7 +2,7 @@ package gov.nih.nci.cabig.caaers.dao.query;
 
 public class OrganizationFromStudySiteQuery extends AbstractQuery {
 
-    private static String queryString = "SELECT distinct ss.organization from StudySite ss order by ss.organization.name";
+    private static String queryString = "SELECT distinct ss.organization from StudySite ss";//order by ss.organization.name";
 
     private static String ORGANIZATION_NAME = "name";
 
@@ -11,6 +11,7 @@ public class OrganizationFromStudySiteQuery extends AbstractQuery {
     public OrganizationFromStudySiteQuery() {
 
         super(queryString);
+        orderBy("ss.organization.name");
     }
 
     public void filterByOrganizationName(String name) {
