cdaacb84ab6f95b01cfe1e1251b423f7928d20b9
Srini Akkala <akkalas@gmail.com>
2010-04-28 15:12:41 +0000

    CAAERS-3879
    SVN-Revision: 12658

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/aspects/QuerySecurityFilterAspect.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/aspects/QuerySecurityFilterAspect.java
@@ -1,32 +1,28 @@
 package gov.nih.nci.cabig.caaers.accesscontrol.aspects;
 
+import gov.nih.nci.cabig.caaers.accesscontrol.filter.QuerySecurityFiltererDispatcher;
+import gov.nih.nci.cabig.caaers.dao.query.AbstractQuery;
+import gov.nih.nci.cabig.caaers.domain.UserGroupType;
+import gov.nih.nci.cabig.caaers.security.SecurityUtils;
+
 import org.acegisecurity.Authentication;
 import org.acegisecurity.context.SecurityContextHolder;
 
-import gov.nih.nci.cabig.caaers.accesscontrol.BaseSecurityFilterer;
-import gov.nih.nci.cabig.caaers.accesscontrol.filter.QuerySecurityFiltererDispatcher;
-import gov.nih.nci.cabig.caaers.dao.query.AbstractQuery;
-import gov.nih.nci.cabig.caaers.security.SecurityUtils;
-
-public class QuerySecurityFilterAspect extends BaseSecurityFilterer {
+public class QuerySecurityFilterAspect  {
 
 	private QuerySecurityFiltererDispatcher querySecurityFilterDispatcher;
 
 	public Object applyFilter(AbstractQuery qry) throws Throwable {
-    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
 		if(authentication != null) {
-			
-			if (filteringNotRequired(authentication)) {
+			if (SecurityUtils.checkAuthorization(UserGroupType.caaers_super_user)) {
 				return new Object[] {qry};	
 			}
 			String userName = SecurityUtils.getUserLoginName(authentication);
 			querySecurityFilterDispatcher.filter(qry, userName);
 		}
-		
-		//
-		
-		return new Object[] {qry};	
-		
+		return new Object[] {qry};			
 	}
 	public void setQuerySecurityFilterDispatcher(
 			QuerySecurityFiltererDispatcher querySecurityFilterDispatcher) {
