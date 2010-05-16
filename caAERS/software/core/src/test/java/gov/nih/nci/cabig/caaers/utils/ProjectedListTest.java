f52327cf2156328d543c34863a14ab673aa1e177
Karthik Iyer <iyerk0@gmail.com>
2010-05-16 03:17:06 +0000

    cleaned up test logic
    SVN-Revision: 12781

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/utils/ProjectedListTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/utils/ProjectedListTest.java
@@ -8,21 +8,23 @@ import junit.framework.TestCase;
 
 public class ProjectedListTest extends TestCase {
 	List l = new ArrayList();
+	List bl; //backing list used for validating list state
 	ProjectedList<Integer> pl;
 	Integer i;
-	int[] j;
+	int[] j = new int[3];
 
 	protected void setUp() throws Exception {
 		super.setUp();
 		l.add("one");
 		l.add("two");
 		l.add("three");
-		j[1] = 1;
-		j[2] = 2;
-		j[3] = 3;
+		j[0] = 1;
+		j[1] = 2;
+		j[2] = 3;
 		l.add(new Integer(j[0]));
 		l.add(new Integer(j[1]));
 		l.add(new Integer(j[2]));
+		bl=(List) ((ArrayList)l).clone();
 		pl = new ProjectedList<Integer>(l, Integer.class);
 		i = new Integer(22);
 
@@ -31,42 +33,42 @@ public class ProjectedListTest extends TestCase {
 	protected void tearDown() throws Exception {
 		super.tearDown();
 		pl = null;
+		bl=null;
 		l = null;
 	}
 
 	public void testAddElement() {
 		int size = pl.size();
 		pl.add(i);
-		assertEquals("Added integer " + i + " to Projected List but did not"
-				+ "get the same value back", i, pl.get(size));
+		bl.add(i);
+		assertEquals("Incorrect addition of element to list",pl.getInternalList(),bl );
 	}
 
 	public void testAddElementAtIndex() {
 		pl.add(1, i);
-		assertEquals("Added integer " + i
-				+ " to Projected List at position 1 but did not"
-				+ "get the same value back", i, pl.get(1));
+		bl.add(4,i);
+		assertEquals("Incorrect addition of element to list at specified index",bl, pl.getInternalList());
 	}
 
 	public void testRemoveElementAtIndex() {
-		pl.add(1, i);
 		pl.remove(1);
-		assertSame("Wrong object was removed from list", i, pl.remove(1));
-		assertFalse("Deleted Object is still available in list", pl.contains(i));
-		assertEquals("List is of the wrong size", 3, pl.size());
+		bl.remove(4);
+		assertEquals("Incorrect removal of element to list at specified index", bl, pl.getInternalList());
+//		assertSame("Wrong object was removed from list", i, pl.remove(1));
+//		assertFalse("Deleted Object is still available in list", pl.contains(i));
+//		assertEquals("List is of the wrong size", 3, pl.size());
 	}
 
 	public void testListIterator() {
-		l.add(3, new String("test string"));
-		pl.add(3, i);
+		
 		ListIterator<Integer> li = pl.listIterator();
 		int size = 0;
 		
 		while (li.hasNext()) {
-			assertEquals("Wrong elements returned",j[li.nextIndex()-1] ,li.next().intValue());
+			assertEquals("Wrong elements returned",j[li.nextIndex()] ,li.next().intValue());
 			size++;
 		}
-		assertEquals("Returned ListIterator is not of correct size", 4, size);
+		assertEquals("Returned ListIterator is not of correct size", 3, size);
 	}
 
 	public void testRemoveElementAtIndex1() {
