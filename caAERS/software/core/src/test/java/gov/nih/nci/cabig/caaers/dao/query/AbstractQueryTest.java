afe4697185b7eb8c5a026a8c039009c860d2bea8
Biju Joseph <biju.joseph@semanticbits.com>
2010-05-18 14:31:32 +0000

    CAAERS-3959
    SVN-Revision: 12798

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/AbstractQueryTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/AbstractQueryTest.java
@@ -26,7 +26,7 @@ public class AbstractQueryTest extends TestCase {
 	public void testGetQueryString_WithoutAnd(){
 		query.orWhere("y = 2");
 		query.orWhere("z = 3");
-		assertEquals("select * from emp OR y = 2 OR z = 3",query.getQueryString());
+		assertEquals("select * from emp WHERE y = 2 OR z = 3",query.getQueryString());
 	}
 	
 	public void test2(){
@@ -34,4 +34,13 @@ public class AbstractQueryTest extends TestCase {
         assertEquals("select * from emp WHERE y = 3", query.getQueryString());
 	}
 
+
+    public void testWithMultipleAndAndOr(){
+        query.join("dept on dept.id = emp.id");
+        query.andWhere("j = 1");
+        query.andWhere("m = 5");
+		query.orWhere("y = 3");
+        query.orWhere("y = 4");
+        assertEquals("select * from emp join dept on dept.id = emp.id WHERE j = 1 AND m = 5 AND ( y = 3 OR y = 4 )", query.getQueryString());
+	}
 }
