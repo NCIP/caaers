7c50a741ef9b314a12eaef84848fe729c68ee3bc
Srini Akkala <akkalas@gmail.com>
2010-05-19 03:13:30 +0000

    CAAERS-3968
    SVN-Revision: 12805

---
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/tools/spring/EventMulticasterFactoryBean.java
@@ -0,0 +1,44 @@
+package gov.nih.nci.cabig.caaers.tools.spring;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.TaskExecutor;
+
+public class EventMulticasterFactoryBean implements FactoryBean, InitializingBean {
+	
+	private SimpleApplicationEventMulticaster simpleApplicationEventMulticaster;
+	private TaskExecutor  taskExecutor;
+	
+	public Object getObject() throws Exception {
+		simpleApplicationEventMulticaster.setTaskExecutor(taskExecutor);
+		return this.simpleApplicationEventMulticaster;
+	}
+
+	public Class getObjectType() {
+		// TODO Auto-generated method stub
+		return SimpleApplicationEventMulticaster.class;
+	}
+
+	public boolean isSingleton() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setSimpleApplicationEventMulticaster(
+			SimpleApplicationEventMulticaster simpleApplicationEventMulticaster) {
+		this.simpleApplicationEventMulticaster = simpleApplicationEventMulticaster;
+	}
+
+	public void setTaskExecutor(TaskExecutor taskExecutor) {
+		this.taskExecutor = taskExecutor;
+	}
+
+
+
+}
