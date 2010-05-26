a3ef8b3b9fd318c8c5c744eb9785e9bec44d39a3
Biju Joseph <biju.joseph@semanticbits.com>
2010-05-26 18:45:28 +0000

    CAAERS-3959
    SVN-Revision: 12861

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/ajax/StudySiteAjaxableDomainObjectQueryTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/ajax/StudySiteAjaxableDomainObjectQueryTest.java
@@ -9,7 +9,7 @@ public class StudySiteAjaxableDomainObjectQueryTest extends TestCase {
 		ssQuery.filterByStudy(5);
 		String queryString = ssQuery.getQueryString();
 		assertEquals("select site.id, site.organization.name from " +
-				"StudySite site WHERE study.id = :STUDY_ID order by " +
+				"StudySite site WHERE study.id = :STUDY_ID  order by " +
 				"site.organization.name", queryString);
 	}
 
