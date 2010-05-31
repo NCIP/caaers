113d2b1abdd11e6d15284e7cb76e5b7e44a1ea8f
Ion Olaru <ion.olaru@gmail.com>
2010-05-31 13:30:40 +0000

    CAAERS-3959 - The system should support a dashboard for an AE Coordinator user
    SVN-Revision: 12891

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/ExpeditedAdverseEventReportQueryTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/ExpeditedAdverseEventReportQueryTest.java
@@ -11,7 +11,7 @@ public class ExpeditedAdverseEventReportQueryTest extends TestCase{
 
 	public void testQueryConstructor() throws Exception {
 		ReportVersionDTOQuery q = new ReportVersionDTOQuery();
-        assertEquals("Wrong parsing for constructor", "select rv from ExpeditedAdverseEventReport aer join aer.reports as report join report.reportVersions as rv", q.getQueryString());
+        assertEquals("Wrong parsing for constructor", "select new gov.nih.nci.cabig.caaers.domain.report.ReportVersionDTO(rv, rd.label, subject.firstName, subject.lastName, study.shortTitle, identifier.value) from ExpeditedAdverseEventReport aer join aer.reports as report join report.reportVersions as rv join report.reportDefinition as rd join report.aeReport as aeReport join aeReport.reportingPeriod as period join period.assignment as assignment join assignment.participant as subject left join subject.identifiers as identifier with identifier.primaryIndicator = :primaryIndicator join assignment.studySite as ss join ss.study as study", q.getQueryString());
     }
 
 }
