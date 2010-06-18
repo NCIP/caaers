2db5c34995053d25109396aef63edb10ccad27c0
Denis Krylov <denis.krylov@semanticbits.com>
2010-06-18 19:44:58 +0000

    CAAERS-4070: Investigator (UI) modification: partial fix. Jira Id: 4070
    SVN-Revision: 13062

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/datamigrator/CaaersDataMigratorDelegate.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/datamigrator/CaaersDataMigratorDelegate.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class CaaersDataMigratorDelegate {
 
 	private List<CaaersDataMigrator> migrators;
-	private String authenticationMode;
+	private String authenticationMode="local";
 	
 	/**
 	 * 
