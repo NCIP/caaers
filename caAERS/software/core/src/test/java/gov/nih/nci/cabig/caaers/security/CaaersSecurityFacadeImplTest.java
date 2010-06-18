1e0f0692736073d19402fda0e6c38134c04ae643
Biju Joseph <biju.joseph@semanticbits.com>
2010-06-18 16:41:23 +0000

    CAAERS-4056 - checkAuthorization
    SVN-Revision: 13058

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/security/CaaersSecurityFacadeImplTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/security/CaaersSecurityFacadeImplTest.java
@@ -2,6 +2,7 @@ package gov.nih.nci.cabig.caaers.security;
 
 import gov.nih.nci.cabig.caaers.AbstractTestCase;
 import gov.nih.nci.cabig.caaers.CaaersTestCase;
+import gov.nih.nci.cabig.caaers.domain.UserGroupType;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import junit.framework.TestCase;
@@ -39,7 +40,18 @@ public class CaaersSecurityFacadeImplTest extends CaaersTestCase {
     }
 
     public void testCheckAuthorization() throws Exception{
-        fail("Not implemented");
+        SecurityTestUtils.switchUser("tester",
+                UserGroupType.business_administrator.getSecurityRoleName(),
+                UserGroupType.registrar.getSecurityRoleName());
+        
+        boolean b = facade.checkAuthorization(SecurityUtils.getAuthentication(),  "gov.nih.nci.cabig.caaers.domain.CaaersFieldDefinition", "READ");
+        assertTrue(b);
+
+        SecurityTestUtils.switchUser("tester",
+                UserGroupType.system_administrator.getSecurityRoleName(),
+                UserGroupType.registrar.getSecurityRoleName());
+        b = facade.checkAuthorization(SecurityUtils.getAuthentication(),  "gov.nih.nci.cabig.caaers.domain.CaaersFieldDefinition", "READ");
+        assertFalse(b);
     }
 
     public void testGetAccessibleProtectionElements() throws Exception {
