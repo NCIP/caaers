f14073d8ffb49034442ab5f64f8d220f63366a57
Biju Joseph <biju.joseph@semanticbits.com>
2010-06-24 03:50:36 +0000

    CAAERS-4086 - Advance Search > UI modification
    SVN-Revision: 13146

--- caAERS/software/core/src/main/db/migrate/020_Phase3_construction_6/021_RolePrivilege_AdvanceSearch.groovy
+++ caAERS/software/core/src/main/db/migrate/020_Phase3_construction_6/021_RolePrivilege_AdvanceSearch.groovy
@@ -1,4 +1,4 @@
-class RolePrivilegeSeedDataOrganization extends edu.northwestern.bioinformatics.bering.Migration {
+class RolePrivilegeAdvanceSearch extends edu.northwestern.bioinformatics.bering.Migration {
 
     void up() {
       insert('role_privilege', [ version: '0',role_name : 'data_reader',object_id : 'gov.nih.nci.cabig.caaers.domain.AdvanceSearch',privilege :'ACCESS']);
