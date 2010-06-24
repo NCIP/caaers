4745cd0da91b1f1dff3811472b4d8137ff870062
Biju Joseph <biju.joseph@semanticbits.com>
2010-06-24 16:07:55 +0000

    CAAERS-4091 - Administration > Import UI modification
    SVN-Revision: 13154

--- caAERS/software/core/src/main/db/migrate/020_Phase3_construction_6/022_RolePrivilege_Import.groovy
+++ caAERS/software/core/src/main/db/migrate/020_Phase3_construction_6/022_RolePrivilege_Import.groovy
@@ -2,6 +2,15 @@ class RolePrivilegeImport extends edu.northwestern.bioinformatics.bering.Migrati
 
     void up() {
       insert('role_privilege', [ version: '0',role_name : 'data_importer',object_id : 'gov.nih.nci.cabig.caaers.domain.Import',privilege :'ACCESS']);
+      insert('role_privilege', [ version: '0',role_name : 'data_importer',object_id : 'gov.nih.nci.cabig.caaers.domain.Investigator',privilege :'UPDATE']);
+      insert('role_privilege', [ version: '0',role_name : 'data_importer',object_id : 'gov.nih.nci.cabig.caaers.domain.ResearchStaff',privilege :'UPDATE']);
+      insert('role_privilege', [ version: '0',role_name : 'data_importer',object_id : 'gov.nih.nci.cabig.caaers.domain.Organization',privilege :'UPDATE']);
+      insert('role_privilege', [ version: '0',role_name : 'data_importer',object_id : 'gov.nih.nci.cabig.caaers.domain.Agent',privilege :'UPDATE']);
+      insert('role_privilege', [ version: '0',role_name : 'data_importer',object_id : 'gov.nih.nci.cabig.caaers.domain.Study',privilege :'UPDATE']);
+      insert('role_privilege', [ version: '0',role_name : 'data_importer',object_id : 'gov.nih.nci.cabig.caaers.domain.Participant',privilege :'UPDATE']);
+      insert('role_privilege', [ version: '0',role_name : 'data_importer',object_id : 'gov.nih.nci.cabig.caaers.domain.AgentSpecificTerm',privilege :'UPDATE']);
+      insert('role_privilege', [ version: '0',role_name : 'data_importer',object_id : 'gov.nih.nci.cabig.caaers.domain.MeddraVersion',privilege :'UPDATE']);
+      
       
     }
     void down(){
