d09aec99cfe99d53d4c0178b079612dacfe4fa02
Biju Joseph <biju.joseph@semanticbits.com>
2010-06-24 03:27:54 +0000

    CAAERS-4087 - Groovy for "Advance Search" 1st and 2nd Level Tab
    SVN-Revision: 13145

---
+++ caAERS/software/core/src/main/db/migrate/020_Phase3_construction_6/021_RolePrivilege_AdvanceSearch.groovy
@@ -0,0 +1,12 @@
+class RolePrivilegeSeedDataOrganization extends edu.northwestern.bioinformatics.bering.Migration {
+
+    void up() {
+      insert('role_privilege', [ version: '0',role_name : 'data_reader',object_id : 'gov.nih.nci.cabig.caaers.domain.AdvanceSearch',privilege :'ACCESS']);
+      insert('role_privilege', [ version: '0',role_name : 'data_analyst',object_id : 'gov.nih.nci.cabig.caaers.domain.AdvanceSearch',privilege :'ACCESS']);
+      insert('role_privilege', [ version: '0',role_name : 'data_analyst',object_id : 'gov.nih.nci.cabig.caaers.domain.AdvanceSearch',privilege :'CREATE']);
+      
+    }
+    void down(){
+      //not required. 
+    }
+}
