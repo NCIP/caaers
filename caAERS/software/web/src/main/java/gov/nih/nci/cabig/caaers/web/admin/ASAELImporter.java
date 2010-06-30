72aa402a698caabe463c2da24ac551e9c297c058
Biju Joseph <biju.joseph@semanticbits.com>
2010-06-30 20:48:52 +0000

    CAAERS-4067 - Unified Security Roles : UI Changes
    SVN-Revision: 13239

--- caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/admin/ASAELImporter.java
+++ caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/admin/ASAELImporter.java
@@ -1,5 +1,7 @@
 package gov.nih.nci.cabig.caaers.web.admin;
 
+import gov.nih.nci.cabig.caaers.dataimport.AgentSpecificTermsImporter;
+
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.util.Map;
