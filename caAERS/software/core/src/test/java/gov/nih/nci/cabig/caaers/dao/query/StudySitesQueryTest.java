ef05011a54b09718feae8d4b39f915f9b66fcb11
Biju Joseph <biju.joseph@semanticbits.com>
2010-07-19 21:50:51 +0000

    CAAERS-4164
    SVN-Revision: 13396

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/StudySitesQueryTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/StudySitesQueryTest.java
@@ -7,9 +7,9 @@ import java.util.Arrays;
 
 public class StudySitesQueryTest extends TestCase {
 
-    public void testOne() {
+    public void testFilterByOrgId() {
         StudySitesQuery q = new StudySitesQuery();
         q.filterByOrganizationId(5);
-        assertEquals("SELECT ss FROM StudySite ss left join ss.study.identifiers as identifier join ss.organization AS o WHERE o.id = :orgId", q.getQueryString());
+        assertEquals("SELECT distinct ss FROM StudySite ss left join ss.study.identifiers as identifier join ss.organization AS o WHERE o.id = :orgId", q.getQueryString());
     }
 }
