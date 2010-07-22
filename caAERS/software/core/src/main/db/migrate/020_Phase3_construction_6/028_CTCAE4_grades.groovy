9c88dd84634f720379c49a5c07720607dda432a9
Ion Olaru <ion.olaru@gmail.com>
2010-07-22 21:25:06 +0000

    CAAERS-4187
    SVN-Revision: 13446

--- caAERS/software/core/src/main/db/migrate/020_Phase3_construction_6/028_CTCAE4_grades.groovy
+++ caAERS/software/core/src/main/db/migrate/020_Phase3_construction_6/028_CTCAE4_grades.groovy
@@ -2,9 +2,9 @@ class CTCAE4_Grades extends edu.northwestern.bioinformatics.bering.Migration {
 
     void up() {
       if (databaseMatches('oracle')) {
-          external("CTCAE4_Grades_Oracle.sql")
+          // external("CTCAE4_Grades_Oracle.sql")
       } else if (databaseMatches('postgresql')){
-          external("CTCAE4_Grades_Postgres.sql")  
+          //singleStatement("UPDATE ctc_grades SET grade_text = 'Mild; asymptomatic or mild symptoms; clinical or diagnostic observations only; intervention not indicated.' WHERE grade_code = 1 AND term_id IN (SELECT t.id FROM ctc_categories c JOIN ctc_terms t ON t.category_id = c.id JOIN ctc_grades g ON t.id = g.term_id WHERE c.version_id = 4 AND t.other_required = true AND grade_code = 1);");
       }
     }
 
