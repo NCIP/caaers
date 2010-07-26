6a84e78b404fc196ce70c4a2e881d5ac5ee9109e
Srini Akkala <akkalas@gmail.com>
2010-07-26 14:11:44 +0000

    CAAERS-4213
    SVN-Revision: 13460

--- caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/listener/ResearchStaffModificationEventListener.java
+++ caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/listener/ResearchStaffModificationEventListener.java
@@ -1,14 +1,32 @@
 package gov.nih.nci.cabig.caaers.web.listener;
 
+import gov.nih.nci.cabig.caaers.domain.ResearchStaff;
 import gov.nih.nci.cabig.caaers.event.ResearchStaffModificationEvent;
+import gov.nih.nci.cabig.caaers.security.CaaersSecurityFacade;
+
 import org.springframework.context.ApplicationEvent;
 
 /**
  * @author: Biju Joseph
  */
 public class ResearchStaffModificationEventListener extends AbstractEventListener {
-    @Override
+    
+	private CaaersSecurityFacade caaersSecurityFacade;
+
+	@Override
     public boolean isSupported(ApplicationEvent event) {
         return event instanceof ResearchStaffModificationEvent;
     }
+    
+    @Override
+    public void preProcess(ApplicationEvent event){
+    	ResearchStaff rs = (ResearchStaff)event.getSource();
+    	String userName = rs.getLoginId();
+    	caaersSecurityFacade.clearUserCache(userName);
+    }
+    
+	
+	public void setCaaersSecurityFacade(CaaersSecurityFacade caaersSecurityFacade) {
+		this.caaersSecurityFacade = caaersSecurityFacade;
+	}
 }
