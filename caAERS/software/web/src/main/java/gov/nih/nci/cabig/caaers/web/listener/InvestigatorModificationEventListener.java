9b810c38d7029d0187f21fef8f7bb1ecb3d90420
Srini Akkala <akkalas@gmail.com>
2010-07-26 14:12:00 +0000

    CAAERS-4213
    SVN-Revision: 13461

--- caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/listener/InvestigatorModificationEventListener.java
+++ caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/listener/InvestigatorModificationEventListener.java
@@ -1,15 +1,33 @@
 package gov.nih.nci.cabig.caaers.web.listener;
 
+import gov.nih.nci.cabig.caaers.domain.Investigator;
 import gov.nih.nci.cabig.caaers.event.InvestigatorModificationEvent;
-import gov.nih.nci.cabig.caaers.event.StudyModificationEvent;
+import gov.nih.nci.cabig.caaers.security.CaaersSecurityFacade;
+
 import org.springframework.context.ApplicationEvent;
 
 /**
  * @author: Biju Joseph
  */
 public class InvestigatorModificationEventListener extends AbstractEventListener {
-    @Override
+   
+	private CaaersSecurityFacade caaersSecurityFacade;
+	
+	@Override
     public boolean isSupported(ApplicationEvent event) {
         return event instanceof InvestigatorModificationEvent;
     }
+    
+    @Override
+    public void preProcess(ApplicationEvent event){
+    	Investigator inv = (Investigator)event.getSource();
+    	String userName = inv.getLoginId();
+    	caaersSecurityFacade.clearUserCache(userName);
+    }
+    
+	
+	public void setCaaersSecurityFacade(CaaersSecurityFacade caaersSecurityFacade) {
+		this.caaersSecurityFacade = caaersSecurityFacade;
+	}
+	
 }
