ce97c7548fa9a77c281628cf857c6b4ccb01e0a5
Srini Akkala <akkalas@gmail.com>
2010-08-03 04:02:12 +0000

    added debug to get fetching and loading times
    SVN-Revision: 13549

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/dataproviders/FilteredDataLoader.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/dataproviders/FilteredDataLoader.java
@@ -1,15 +1,20 @@
 package gov.nih.nci.cabig.caaers.accesscontrol.dataproviders;
 
-import com.semanticbits.security.contentfilter.IdFetcher;
 import gov.nih.nci.cabig.caaers.dao.index.AbstractIndexDao;
 import gov.nih.nci.cabig.caaers.domain.index.IndexEntry;
 import gov.nih.nci.cabig.caaers.security.SecurityUtils;
-import org.acegisecurity.Authentication;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
+
+import org.acegisecurity.Authentication;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.semanticbits.security.contentfilter.IdFetcher;
 
 /**
  * Will take care of updating an index
@@ -20,13 +25,25 @@ public class FilteredDataLoader {
 	
 	private List<IdFetcher> idFetchers = new ArrayList<IdFetcher>();
 	private LinkedHashMap idFetcherIndexDaoMap;
+	protected final Log log = LogFactory.getLog(getClass());
 
 	public void updateIndexByUserName(Authentication authentication){
 		for (IdFetcher idFetcher : idFetchers) {
 			String userName = SecurityUtils.getUserLoginName(authentication);
+			Date da = new Date();
 			List<IndexEntry> indexEntries = idFetcher.fetch(userName);
+			Date db = new Date();
+			Long diff0 = db.getTime()-da.getTime();
+			log.debug("TIME TOOK TO FETCH IDS " +idFetcher.getClass().getName() +" ..."+ diff0/1000 + " seconds");
+			System.out.println("TIME TOOK TO FETCH IDS " +idFetcher.getClass().getName() +" ..."+ diff0/1000 + " seconds");
 			AbstractIndexDao indexDao = (AbstractIndexDao)idFetcherIndexDaoMap.get(idFetcher);
+			Date d1 = new Date();
 			updateAnIndex(indexEntries, userName, indexDao);
+			Date d2 = new Date();
+			Long diff = d2.getTime()-d1.getTime();
+			log.debug("TIME TOOK TO UPDATE INDEX ..."+ diff/1000 + " seconds");
+			System.out.println("TIME TOOK TO UPDATE INDEX ..."+ diff/1000 + " seconds");
+
 		}
 	}
 
