051071f6990a78ad132b56cf567cff65da80ef4a
Srini Akkala <akkalas@gmail.com>
2010-08-11 04:32:52 +0000

    CAAERS-4259
    SVN-Revision: 13618

--- caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/participant/SelectStudyForParticipantTab.java
+++ caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/participant/SelectStudyForParticipantTab.java
@@ -60,6 +60,7 @@ public class SelectStudyForParticipantTab <T extends ParticipantInputCommand> ex
         if (searchtext != null && type != null && !searchtext.equals("")) {
             participantCommand.setStudies(new ArrayList<Study>());
             StudyHavingStudySiteQuery query = new StudyHavingStudySiteQuery();
+            query.joinStudyOrganization();
             query.filterByDataEntryStatus(true);
             //query.filterByStudySiteName(participantCommand.getOrganization().getName());
             query.filterByStudySiteNciInstituteCode(participantCommand.getOrganization().getNciInstituteCode());
@@ -68,7 +69,7 @@ public class SelectStudyForParticipantTab <T extends ParticipantInputCommand> ex
             } else if ("idtf".equals(type)) {
                 query.filterByIdentifierValue(searchtext);
             }
-
+            query.filterBySST();
             List<Study> studies = studyRepository.find(query);
             
         //    if (command.getLoggedinResearchStaff() != null) {
