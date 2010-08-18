df20526f8042787b3eb83be6b1c07a889ed27d4b
Srini Akkala <akkalas@gmail.com>
2010-08-18 14:53:42 +0000

    CAAERS-4307
    SVN-Revision: 13655

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/query/impl/CaaersStudyIdFetcherImpl.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/query/impl/CaaersStudyIdFetcherImpl.java
@@ -1,8 +1,9 @@
 package gov.nih.nci.cabig.caaers.accesscontrol.query.impl;
 
+import gov.nih.nci.cabig.caaers.domain.UserGroupType;
 import gov.nih.nci.cabig.caaers.domain.index.IndexEntry;
+import gov.nih.nci.cabig.caaers.security.SecurityUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.semanticbits.security.contentfilter.IdFetcher;
@@ -29,7 +30,25 @@ import com.semanticbits.security.contentfilter.IdFetcher;
  *
  */
 public class CaaersStudyIdFetcherImpl extends AbstractIdFetcher implements IdFetcher {
+    /**
+     * All the Site scoped roles that require subject indexing
+     *
+     * @return
+     */
+    @Override
+    public UserGroupType[] getApplicableSiteScopedRoles() {
+        return new UserGroupType[0];  //To change body of implemented methods use File | Settings | File Templates.
+    }
 
+    /**
+     * All the Study scoped roles that require subject indexing
+     *
+     * @return
+     */
+    @Override
+    public UserGroupType[] getApplicableStudyScopedRoles() {
+        return new UserGroupType[0];  //To change body of implemented methods use File | Settings | File Templates.
+    }
     
 	@Override
 	public List fetch(String loginId) {
