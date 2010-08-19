297fc28ec90f60c2321e1c33665556da18fa20c5
Monish Dombla <monish.dombla@semanticbits.com>
2010-08-19 18:05:28 +0000

    CAAERS-3880
    SVN-Revision: 13667

---
+++ caAERS/software/core/src/main/db/migrate/020_Phase3_construction_6/034_Update_caAERSBaseHelpURL.groovy
@@ -0,0 +1,12 @@
+class UpdatecaAERSBaseHelpURL extends edu.northwestern.bioinformatics.bering.Migration {
+	
+    void up() {
+
+        execute("update configuration set value = 'https://cabig-kc.nci.nih.gov/CTMS/KC/index.php' where key = 'caaersBaseHelpUrl'");
+
+    }
+
+    void down() {
+      // not required
+    }
+}
