79d091014e6669232978270bdcb4211d306668ef
Biju Joseph <biju.joseph@semanticbits.com>
2010-08-20 14:39:51 +0000

    CAAERS-4164
    SVN-Revision: 13698

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/domain/repository/CSMUserRepositoryIntegrationTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/domain/repository/CSMUserRepositoryIntegrationTest.java
@@ -1,10 +1,11 @@
 package gov.nih.nci.cabig.caaers.domain.repository;
 
+import gov.nih.nci.cabig.caaers.CaaersDbNoSecurityTestCase;
 import gov.nih.nci.cabig.caaers.CaaersTestCase;
 
 import java.util.Date;
 
-public class CSMUserRepositoryIntegrationTest extends CaaersTestCase{
+public class CSMUserRepositoryIntegrationTest extends CaaersDbNoSecurityTestCase{
 	
 	private CSMUserRepositoryImpl csmRepo;
 
@@ -14,12 +15,12 @@ public class CSMUserRepositoryIntegrationTest extends CaaersTestCase{
 	}
 	
 	public void testGetUser(){
-		gov.nih.nci.security.authorization.domainobjects.User user = csmRepo.getCSMUserByName("SYSTEM");
+		gov.nih.nci.security.authorization.domainobjects.User user = csmRepo.getCSMUserByName("j_reporter");
 		assertNotNull(user);
 	}
 	
 	public void testModiyUser(){
-		gov.nih.nci.security.authorization.domainobjects.User user = csmRepo.getCSMUserByName("SYSTEM");
+		gov.nih.nci.security.authorization.domainobjects.User user = csmRepo.getCSMUserByName("j_reporter");
 		user.setUpdateDate(new Date());
 		user.setStartDate(new Date());
 		csmRepo.saveCSMUser(user);
