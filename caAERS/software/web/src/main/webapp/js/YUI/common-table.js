5dc709b6e0de61b9071c34c72d342d007d7e2150
Ion Olaru <ion.olaru@gmail.com>
2010-09-08 21:32:57 +0000

    CAAERS-3351 - Research staff and Investigator search results sorting not working
    SVN-Revision: 13824

--- caAERS/software/web/src/main/webapp/js/YUI/common-table.js
+++ caAERS/software/web/src/main/webapp/js/YUI/common-table.js
@@ -1,13 +1,15 @@
-//@author : Ion C. Olaru
-/**
- * Initialize YUI data-table.
- *
- * http://developer.yahoo.com/yui/datatable/#start
- * @param tableId       - DOM element to build the YUI table for
- * @param responseData  - JSON data to populate the YUI table with
- * @param columnDefs    - Column definition for the YUI table
- * @param fields        - fields to be displayed for the YUI table
- * 
+/*
+
+  @author : Ion C. Olaru
+
+  Initialize YUI data-table.
+ 
+  http://developer.yahoo.com/yui/datatable/#start
+  @param tableId       - DOM element to build the YUI table for
+  @param responseData  - JSON data to populate the YUI table with
+  @param columnDefs    - Column definition for the YUI table
+  @param fields        - fields to be displayed for the YUI table
+
  */
 function initializeYUITable(tableId, responseData, columnDefs, fields) {
 
