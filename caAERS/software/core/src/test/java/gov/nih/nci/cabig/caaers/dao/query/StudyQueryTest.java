33855eec5213452e462bf5d3a8bfb075e243aed3
Monish Dombla <monish.dombla@semanticbits.com>
2010-09-08 20:41:05 +0000

    CAAERS-4164 - fixing hudson testcases
    SVN-Revision: 13823

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/StudyQueryTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/StudyQueryTest.java
@@ -8,7 +8,7 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 
         assertEquals("wrong parsing for constructor",
-                        "select distinct s from Study s", studyQuery
+                        "select  distinct s from Study s", studyQuery
                                         .getQueryString());
 
     }
@@ -17,32 +17,32 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.join("short");
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s join short", studyQuery
+                "select  distinct s from Study s join short", studyQuery
                                 .getQueryString());
 		
 		//select s from Study s join short join s.identifiers as identifier join s.studyOrganizations as ss join ss.studyParticipantAssignments as spa join spa.participant as p join p.identifiers as pIdentifier
 		studyQuery = new StudyQuery();
 		studyQuery.joinIdentifier();
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s join s.identifiers as identifier", studyQuery
+                "select  distinct s from Study s join s.identifiers as identifier", studyQuery
                                 .getQueryString());
 		
 		studyQuery = new StudyQuery();
 		studyQuery.joinStudyOrganization();
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s join s.studyOrganizations as ss", studyQuery
+                "select  distinct s from Study s join s.studyOrganizations as ss", studyQuery
                                 .getQueryString());
 		
 		studyQuery = new StudyQuery();
 		studyQuery.joinStudyParticipantAssignment();
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s join s.studyOrganizations as ss join ss.studyParticipantAssignments as spa", studyQuery
+                "select  distinct s from Study s join s.studyOrganizations as ss join ss.studyParticipantAssignments as spa", studyQuery
                                 .getQueryString());
 		
 		studyQuery = new StudyQuery();
 		studyQuery.joinParticipantIdentifier();
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s join s.studyOrganizations as ss join ss.studyParticipantAssignments as spa join spa.participant as p join p.identifiers as pIdentifier", studyQuery
+                "select  distinct s from Study s join s.studyOrganizations as ss join ss.studyParticipantAssignments as spa join spa.participant as p join p.identifiers as pIdentifier", studyQuery
                                 .getQueryString());
         
 		
@@ -58,7 +58,7 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.filterByNonAdministrativelyComplete();
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s WHERE s.status <> 'Administratively Complete'", studyQuery
+                "select  distinct s from Study s WHERE s.status <> 'Administratively Complete'", studyQuery
                                 .getQueryString());
 		
 		assertEquals("wrong number of parameters", studyQuery.getParameterMap().size(), 0);
@@ -70,7 +70,7 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.filterByIdentifierValue("idvalue");
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s join s.identifiers as identifier WHERE lower(identifier.value) LIKE :identifier", studyQuery
+                "select  distinct s from Study s join s.identifiers as identifier WHERE lower(identifier.value) LIKE :identifier", studyQuery
                                 .getQueryString());
         
 		assertEquals("wrong number of parameters", studyQuery.getParameterMap().size(), 1);
@@ -85,7 +85,7 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.filterByIdentifierValueExactMatch("idvalue");
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s join s.identifiers as identifier WHERE lower(identifier.value) LIKE :identifier", studyQuery
+                "select  distinct s from Study s join s.identifiers as identifier WHERE lower(identifier.value) LIKE :identifier", studyQuery
                                 .getQueryString());
         
 		assertEquals("wrong number of parameters", studyQuery.getParameterMap().size(), 1);
@@ -101,7 +101,7 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.filterByIdentifierType("type");
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s join s.identifiers as identifier WHERE identifier.type LIKE :idType", studyQuery
+                "select  distinct s from Study s join s.identifiers as identifier WHERE identifier.type LIKE :idType", studyQuery
                                 .getQueryString());
         
 		assertEquals("wrong number of parameters", studyQuery.getParameterMap().size(), 1);
@@ -115,7 +115,7 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.filterByShortTitle("short");
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s WHERE lower(s.shortTitle) LIKE :shortTitle", studyQuery
+                "select  distinct s from Study s WHERE lower(s.shortTitle) LIKE :shortTitle", studyQuery
                                 .getQueryString());
         
 		assertEquals("wrong number of parameters", studyQuery.getParameterMap().size(), 1);
@@ -130,7 +130,7 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.filterByParticipantFirstName("fName","like");
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s WHERE lower(p.firstName) like :pfName", studyQuery
+                "select  distinct s from Study s WHERE lower(p.firstName) like :pfName", studyQuery
                                 .getQueryString());
         
 		assertEquals("wrong number of parameters", studyQuery.getParameterMap().size(), 1);
@@ -144,7 +144,7 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.filterByParticipantLastName("lName","like");
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s WHERE lower(p.lastName) like :plName", studyQuery
+                "select  distinct s from Study s WHERE lower(p.lastName) like :plName", studyQuery
                                 .getQueryString());
         
 		assertEquals("wrong number of parameters", studyQuery.getParameterMap().size(), 1);
@@ -159,21 +159,21 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.filterByParticipantEthnicity("Ethnicity","like");
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s WHERE lower(p.ethnicity) like :pEthenicity", studyQuery
+                "select  distinct s from Study s WHERE lower(p.ethnicity) like :pEthenicity", studyQuery
                                 .getQueryString());
         
 		assertEquals("wrong number of parameters", studyQuery.getParameterMap().size(), 1);
 		assertTrue("missing paramenter name", studyQuery.getParameterMap().containsKey(
         "pEthenicity"));
 		assertEquals("wrong parameter value", studyQuery.getParameterMap().get("pEthenicity"),
-        "%ethnicity%");
+        "ethnicity");
 	}
 	
 	public void testFilterByParticipantGender(){
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.filterByParticipantGender("Gender","like");
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s WHERE lower(p.gender) like :pGender", studyQuery
+                "select  distinct s from Study s WHERE lower(p.gender) like :pGender", studyQuery
                                 .getQueryString());
         
 		assertEquals("wrong number of parameters", studyQuery.getParameterMap().size(), 1);
@@ -187,7 +187,7 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.filterByParticipantDateOfBirth("01/01/1970");
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s WHERE p.dateOfBirth = :pDOB", studyQuery
+                "select  distinct s from Study s WHERE p.dateOfBirth = :pDOB", studyQuery
                                 .getQueryString());
         
 		assertEquals("wrong number of parameters", studyQuery.getParameterMap().size(), 1);
@@ -199,7 +199,7 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.filterByParticipantIdentifierValue("value");
 		assertEquals("wrong parsing for constructor",
-                "select distinct s from Study s WHERE lower(pIdentifier.value) LIKE :pIdVal", studyQuery
+                "select  distinct s from Study s WHERE lower(pIdentifier.value) LIKE :pIdVal", studyQuery
                                 .getQueryString());
         
 		assertEquals("wrong number of parameters", studyQuery.getParameterMap().size(), 1);
@@ -214,6 +214,6 @@ public class StudyQueryTest extends TestCase{
 		StudyQuery studyQuery = new StudyQuery();
 		studyQuery.joinStudyOrganization();
 		studyQuery.filterBySponsorOrganizationId(-1001);
-		assertEquals("select distinct s from Study s join s.studyOrganizations as ss WHERE ss.class = 'SFS' AND ss.organization.id = :sponsorId", studyQuery.getQueryString());
+		assertEquals("select  distinct s from Study s join s.studyOrganizations as ss WHERE ss.class = 'SFS' AND ss.organization.id = :sponsorId", studyQuery.getQueryString());
 	}
 }
