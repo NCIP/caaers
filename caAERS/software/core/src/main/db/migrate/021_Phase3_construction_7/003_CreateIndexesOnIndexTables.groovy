7e7ce7ac9bd4a00bc60d979a4e9098b333c405a7
Srini Akkala <akkalas@gmail.com>
2010-09-30 18:03:26 +0000

    SVN-Revision: 13938

--- caAERS/software/core/src/main/db/migrate/021_Phase3_construction_7/003_CreateIndexesOnIndexTables.groovy
+++ caAERS/software/core/src/main/db/migrate/021_Phase3_construction_7/003_CreateIndexesOnIndexTables.groovy
@@ -1,14 +1,14 @@
 class CreateIndexes extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
         if (databaseMatches('oracle')) {
-            execute('CREATE OR REPLACE INDEX "org-index-login-id-idx1" ON organization_index(login_id)')
-            execute('CREATE OR REPLACE INDEX "ae-index-login-id-idx1" ON adverseevent_index(login_id)')
-            execute('CREATE OR REPLACE INDEX "eae-index-login-id-idx1" ON expedited_ae_index(login_id)')
-            execute('CREATE OR REPLACE INDEX "inv-index-login-id-idx1" ON investigator_index(login_id)')
-            execute('CREATE OR REPLACE INDEX "par-index-login-id-idx1" ON participant_index(login_id)')
-            execute('CREATE OR REPLACE INDEX "rs-index-login-id-idx1" ON researchstaff_index(login_id)')
-            execute('CREATE OR REPLACE INDEX "rp-index-login-id-idx1" ON reportingperiod_index(login_id)')
-            execute('CREATE OR REPLACE INDEX "study-index-login-id-idx1" ON study_index(login_id)')
+            execute('CREATE INDEX "org-index-login-id-idx1" ON organization_index(login_id)')
+            execute('CREATE INDEX "ae-index-login-id-idx1" ON adverseevent_index(login_id)')
+            execute('CREATE INDEX "eae-index-login-id-idx1" ON expedited_ae_index(login_id)')
+            execute('CREATE INDEX "inv-index-login-id-idx1" ON investigator_index(login_id)')
+            execute('CREATE INDEX "par-index-login-id-idx1" ON participant_index(login_id)')
+            execute('CREATE INDEX "rs-index-login-id-idx1" ON researchstaff_index(login_id)')
+            execute('CREATE INDEX "rp-index-login-id-idx1" ON reportingperiod_index(login_id)')
+            execute('CREATE INDEX "study-index-login-id-idx1" ON study_index(login_id)')
         } else if (databaseMatches('postgresql')){
             execute('CREATE INDEX "org-index-org-id-idx1" ON organization_index(organization_id)')
             execute('CREATE INDEX "ae-index-ae-id-idx1" ON adverseevent_index(adverseevent_id)')
