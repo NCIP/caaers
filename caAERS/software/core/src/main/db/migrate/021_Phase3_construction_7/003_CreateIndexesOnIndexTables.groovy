f8c3bcd846ffad19a1a7ed271d3950f65d7dd2cf
Biju Joseph <biju.joseph@semanticbits.com>
2010-10-06 15:44:43 +0000

    CAAERS-4376 Index droping and recreating (if exists) logic incorporation.
    SVN-Revision: 13963

--- caAERS/software/core/src/main/db/migrate/021_Phase3_construction_7/003_CreateIndexesOnIndexTables.groovy
+++ caAERS/software/core/src/main/db/migrate/021_Phase3_construction_7/003_CreateIndexesOnIndexTables.groovy
@@ -1,34 +1,25 @@
 class CreateIndexes extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
         if (databaseMatches('oracle')) {
-            execute('CREATE INDEX "org-index-login-id-idx1" ON organization_index(login_id)')
-            execute('CREATE INDEX "ae-index-login-id-idx1" ON adverseevent_index(login_id)')
-            execute('CREATE INDEX "eae-index-login-id-idx1" ON expedited_ae_index(login_id)')
-            execute('CREATE INDEX "inv-index-login-id-idx1" ON investigator_index(login_id)')
-            execute('CREATE INDEX "par-index-login-id-idx1" ON participant_index(login_id)')
-            execute('CREATE INDEX "rs-index-login-id-idx1" ON researchstaff_index(login_id)')
-            execute('CREATE INDEX "rp-index-login-id-idx1" ON reportingperiod_index(login_id)')
-            execute('CREATE INDEX "study-index-login-id-idx1" ON study_index(login_id)')
+
+            try { execute('CREATE INDEX "org-index-login-id-idx1" ON organization_index(login_id)') }catch(Exception e){ }
+            try { execute('CREATE INDEX "ae-index-login-id-idx1" ON adverseevent_index(login_id)')  }catch(Exception e){ }
+            try { execute('CREATE INDEX "eae-index-login-id-idx1" ON expedited_ae_index(login_id)')  }catch(Exception e){ }
+            try { execute('CREATE INDEX "inv-index-login-id-idx1" ON investigator_index(login_id)') }catch(Exception e){ }
+            try { execute('CREATE INDEX "par-index-login-id-idx1" ON participant_index(login_id)')  }catch(Exception e){ }
+            try { execute('CREATE INDEX "rs-index-login-id-idx1" ON researchstaff_index(login_id)')   }catch(Exception e){ }
+            try { execute('CREATE INDEX "rp-index-login-id-idx1" ON reportingperiod_index(login_id)')  }catch(Exception e){ }
+            try { execute('CREATE INDEX "study-index-login-id-idx1" ON study_index(login_id)')    }catch(Exception e){ }
+          
         } else if (databaseMatches('postgresql')){
-
-            //droping indexes if they exist   (only supported in 8.2 and up)
-            execute('DROP INDEX IF EXISTS "org-index-org-id-idx1"')
-            execute('DROP INDEX  IF EXISTS "ae-index-ae-id-idx1"')
-            execute('DROP INDEX  IF EXISTS "eae-index-eae-id-idx1"')
-            execute('DROP INDEX  IF EXISTS "inv-index-inv-id-idx1"')
-            execute('DROP INDEX  IF EXISTS "par-index-par-id-idx1"')
-            execute('DROP INDEX  IF EXISTS "rs-index-rs-id-idx1"')
-            execute('DROP INDEX IF EXISTS  "rp-index-rp-id-idx1"')
-            execute('DROP INDEX IF EXISTS  "study-index-study-id-idx1"')
-
-            execute('CREATE INDEX "org-index-org-id-idx1" ON organization_index(organization_id)')
-            execute('CREATE INDEX "ae-index-ae-id-idx1" ON adverseevent_index(adverseevent_id)')
-            execute('CREATE INDEX "eae-index-eae-id-idx1" ON expedited_ae_index(expedited_ae_id)')
-            execute('CREATE INDEX "inv-index-inv-id-idx1" ON investigator_index(investigator_id)')
-            execute('CREATE INDEX "par-index-par-id-idx1" ON participant_index(participant_id)')
-            execute('CREATE INDEX "rs-index-rs-id-idx1" ON researchstaff_index(researchstaff_id)')
-            execute('CREATE INDEX "rp-index-rp-id-idx1" ON reportingperiod_index(reportingperiod_id)')
-            execute('CREATE INDEX "study-index-study-id-idx1" ON study_index(study_id)')
+           try { execute('CREATE INDEX "org-index-org-id-idx1" ON organization_index(organization_id)') }catch(Exception e){ }
+           try { execute('CREATE INDEX "ae-index-ae-id-idx1" ON adverseevent_index(adverseevent_id)') }catch(Exception e){ }
+           try { execute('CREATE INDEX "eae-index-eae-id-idx1" ON expedited_ae_index(expedited_ae_id)')  }catch(Exception e){ }
+           try { execute('CREATE INDEX "inv-index-inv-id-idx1" ON investigator_index(investigator_id)') }catch(Exception e){ }
+           try { execute('CREATE INDEX "par-index-par-id-idx1" ON participant_index(participant_id)')  }catch(Exception e){ }
+           try { execute('CREATE INDEX "rs-index-rs-id-idx1" ON researchstaff_index(researchstaff_id)') }catch(Exception e){ }
+           try { execute('CREATE INDEX "rp-index-rp-id-idx1" ON reportingperiod_index(reportingperiod_id)')  }catch(Exception e){ }
+           try { execute('CREATE INDEX "study-index-study-id-idx1" ON study_index(study_id)')   }catch(Exception e){ }
         } 
     }
     
