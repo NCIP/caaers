3bd9e6df605d519a23433b9673ad8c6ea176b7f2
Ion Olaru <ion.olaru@gmail.com>
2010-10-08 19:11:13 +0000

    CAAERS-4425 - Devices
    SVN-Revision: 13977

---
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/domain/DeviceTest.java
@@ -0,0 +1,58 @@
+package gov.nih.nci.cabig.caaers.domain;
+
+import junit.framework.TestCase;
+/**
+ *
+ * @author Ion C. Olaru
+ *
+ */
+public class DeviceTest extends TestCase {
+	Device device;
+
+    protected void setUp() throws Exception {
+		super.setUp();
+		device = new Device();
+	}
+
+    public void testEqualsAll() {
+        device.setBrandName("Init brand name");
+        device.setCommonName("Init common name");
+        device.setType("Init type");
+
+        Device other = new Device();
+        other.setBrandName("Init brand name");
+        other.setCommonName("Init common name");
+        other.setType("Init type");
+
+        assertTrue(device.equals(other));
+        assertTrue(device.hashCode() == other.hashCode());
+    }
+
+    public void testBrandNameDiff() {
+        device.setBrandName("Init brand name");
+        device.setCommonName("Init common name");
+        device.setType("Init type");
+
+        Device other = new Device();
+        other.setBrandName("Other brand name");
+        other.setCommonName("Init common name");
+        other.setType("Init type");
+
+        assertFalse(device.equals(other));
+        assertFalse(device.hashCode() == other.hashCode());
+    }
+
+    public void testCommonNameDiff() {
+        device.setBrandName("Init brand name");
+        device.setCommonName("Init common name");
+        device.setType("Init type");
+
+        Device other = new Device();
+        other.setBrandName("Init brand name");
+        other.setCommonName("Other common name");
+        other.setType("Init type");
+
+        assertFalse(device.equals(other));
+        assertFalse(device.hashCode() == other.hashCode());
+    }
+}
