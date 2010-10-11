c438cbbdb27dc775f5dc273bad63915978812ce0
Ion Olaru <ion.olaru@gmail.com>
2010-10-11 16:19:56 +0000

    CAAERS-4425 - Devices
    SVN-Revision: 13985

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/domain/repository/DeviceRepository.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/domain/repository/DeviceRepository.java
@@ -1,9 +1,13 @@
 package gov.nih.nci.cabig.caaers.domain.repository;
 
 import gov.nih.nci.cabig.caaers.dao.DeviceDao;
+import gov.nih.nci.cabig.caaers.dao.query.DeviceQuery;
+import gov.nih.nci.cabig.caaers.domain.Device;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author Ion C. Olaru
@@ -21,4 +25,17 @@ public class DeviceRepository {
     public void setDeviceDao(DeviceDao deviceDao) {
         this.deviceDao = deviceDao;
     }
+
+    public Device getById(int id) {
+        return deviceDao.getById(id);
+    }
+
+    public List<? extends Object> search(final DeviceQuery query) {
+        return deviceDao.search(query);
+    }
+
+    public List<Device> getAllDevices() {
+        return deviceDao.getAllDevices();
+    }
+    
 }
--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/domain/repository/DeviceRepositoryTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/domain/repository/DeviceRepositoryTest.java
@@ -2,6 +2,10 @@ package gov.nih.nci.cabig.caaers.domain.repository;
 
 import gov.nih.nci.cabig.caaers.DaoNoSecurityTestCase;
 import gov.nih.nci.cabig.caaers.dao.OrganizationDao;
+import gov.nih.nci.cabig.caaers.dao.query.DeviceQuery;
+import gov.nih.nci.cabig.caaers.domain.Device;
+
+import java.util.List;
 
 /**
  * @author Ion C. Olaru
@@ -21,5 +25,26 @@ public class DeviceRepositoryTest extends DaoNoSecurityTestCase<OrganizationDao>
         assertNotNull(deviceRepository);
         assertNotNull(deviceRepository.getDeviceDao());
     }
+    public void testGetAll() {
+        List all = deviceRepository.getAllDevices();
+        assertEquals(3, all.size());
+    }
+
+    public void testGetById() {
+        Device d = deviceRepository.getById(-1);
+        assertNotNull(d);
+        assertEquals("Device-01 common name", d.getCommonName());
+        assertEquals("Device-01 brand name", d.getBrandName());
+        assertEquals("Type A", d.getType());
+    }
+
+    public void testGetALLFilterByType() {
+        DeviceQuery dq = new DeviceQuery();
+        dq.filterByType("Type A");
+        List l = deviceRepository.search(dq);
+        assertNotNull(l);
+        assertEquals(2, l.size());
+    }
+
     
 }
