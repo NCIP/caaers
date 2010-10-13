3a5bfecdd1fe31702bea3ef1d3c1a91c70a11dd2
Ion Olaru <ion.olaru@gmail.com>
2010-10-13 19:34:40 +0000

    CAAERS-4425 - Devices
    SVN-Revision: 13995

--- caAERS/software/core/src/main/db/migrate/021_Phase3_construction_7/004_CreateDevice.groovy
+++ caAERS/software/core/src/main/db/migrate/021_Phase3_construction_7/004_CreateDevice.groovy
@@ -2,7 +2,7 @@ class CreateDevices extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
       createTable("devices") { t ->
           t.addVersionColumn()
-          t.addColumn("brand_name", "string", nullable:false)
+          t.addColumn("brand_name", "string", nullable:true)
           t.addColumn("common_name", "string", nullable:true)
           t.addColumn("device_type", "string", nullable:true)
           t.addColumn('grid_id' , 'string' , nullable:true);
