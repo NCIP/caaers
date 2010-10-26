973288e27ea814ec06c1dfdecd4ae770a1903938
Ion Olaru <ion.olaru@gmail.com>
2010-10-26 16:12:15 +0000

    Testcases
    SVN-Revision: 14047

--- caAERS/software/web/src/test/java/gov/nih/nci/cabig/caaers/web/search/cell/ParticipantAjaxFacadeTest.java
+++ caAERS/software/web/src/test/java/gov/nih/nci/cabig/caaers/web/search/cell/ParticipantAjaxFacadeTest.java
@@ -2,9 +2,13 @@ package gov.nih.nci.cabig.caaers.web.search.cell;
 
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.*;
+
+import gov.nih.nci.cabig.caaers.dao.query.StudySitesQuery;
 import gov.nih.nci.cabig.caaers.dao.query.ajax.StudySearchableAjaxableDomainObjectQuery;
+import gov.nih.nci.cabig.caaers.domain.*;
 import gov.nih.nci.cabig.caaers.domain.ajax.StudySearchableAjaxableDomainObject;
 import gov.nih.nci.cabig.caaers.domain.ajax.StudySiteAjaxableDomainObject;
+import gov.nih.nci.cabig.caaers.domain.repository.StudyRepository;
 import gov.nih.nci.cabig.caaers.domain.repository.ajax.StudySearchableAjaxableDomainObjectRepository;
 import gov.nih.nci.cabig.caaers.web.DwrFacadeTestCase;
 import gov.nih.nci.cabig.caaers.web.WebTestCase;
@@ -22,39 +26,53 @@ import java.util.List;
 public class ParticipantAjaxFacadeTest extends DwrFacadeTestCase {
 
     private SearchStudyAjaxFacade searchStudyAjaxFacade;
-    private StudySearchableAjaxableDomainObject study;
-    private StudySiteAjaxableDomainObject nciStudySite, ctepStudySite;
+    private Study study;
+    private StudySite nciStudySite, ctepStudySite;
     private StudySearchableAjaxableDomainObjectRepository studySearchableAjaxableDomainObjectRepository;
-
+    private StudyRepository studyRepository;
+    private StudySite studySite;
+    private StudyFundingSponsor ctepStudySponsor;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         searchStudyAjaxFacade = new SearchStudyAjaxFacade();
         studySearchableAjaxableDomainObjectRepository = registerMockFor(StudySearchableAjaxableDomainObjectRepository.class);
-        study = new StudySearchableAjaxableDomainObject();
+        studyRepository = registerMockFor(StudyRepository.class);
+        study = new LocalStudy();
         study.setShortTitle("short title");
         study.setId(1);
+        studySite = new StudySite();
+        studySite.setStudy(study);
 
-        nciStudySite = new StudySiteAjaxableDomainObject();
+        nciStudySite = new StudySite();
         nciStudySite.setId(1);
-        nciStudySite.setName("National Cancer Inst");
-        ctepStudySite = new StudySiteAjaxableDomainObject();
+        nciStudySite.setOrganization(new LocalOrganization()); nciStudySite.getOrganization().setName("National Cancer Inst");
+
+        ctepStudySite = new StudySite();
         ctepStudySite.setId(2);
-        ctepStudySite.setName("Ctep Org");
+        ctepStudySite.setOrganization(new LocalOrganization()); nciStudySite.getOrganization().setName("Ctep Org");
+
+        ctepStudySponsor = new StudyFundingSponsor();
+        ctepStudySponsor.setId(3);
+        ctepStudySponsor.setPrimary(true);
+        ctepStudySponsor.setOrganization(new LocalOrganization()); ctepStudySponsor.getOrganization().setName("Ctep Org");
+
+        study.addStudyFundingSponsor(ctepStudySponsor);
+        study.addStudySite(nciStudySite);
+        study.addStudySite(ctepStudySite);
 
         study.addStudySite(nciStudySite);
         study.addStudySite(ctepStudySite);
         searchStudyAjaxFacade.setStudySearchableAjaxableDomainObjectRepository(studySearchableAjaxableDomainObjectRepository);
-
-
+        searchStudyAjaxFacade.setStudyRepository(studyRepository);
     }
 
     public void testGetTable() {
     	AssignParticipantStudyCommand command = new AssignParticipantStudyCommand();
     	session.setAttribute( AssignParticipantController.class.getName() + ".FORM.command", command);
     	expect(webContext.getSession()).andReturn(session).anyTimes();
-        expect(studySearchableAjaxableDomainObjectRepository.findStudies(isA(StudySearchableAjaxableDomainObjectQuery.class), eq(""), eq(""))).andReturn(Arrays.asList(study));
+        expect(studyRepository.search(isA(StudySitesQuery.class), eq(""), eq(""), eq(false))).andReturn(Arrays.asList(studySite));
         replayMocks();
         List table = searchStudyAjaxFacade.getTableForAssignParticipant(null, "", "", request);
         verifyMocks();
