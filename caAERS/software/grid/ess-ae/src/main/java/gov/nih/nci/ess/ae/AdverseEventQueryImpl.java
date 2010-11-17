5be8ae2b70f9976f65648c480495bc67ef8fd760
Srini Akkala <akkalas@gmail.com>
2010-11-17 15:11:45 +0000

    SVN-Revision: 14171

--- caAERS/software/grid/ess-ae/src/main/java/gov/nih/nci/ess/ae/AdverseEventQueryImpl.java
+++ caAERS/software/grid/ess-ae/src/main/java/gov/nih/nci/ess/ae/AdverseEventQueryImpl.java
@@ -1,15 +1,34 @@
 package gov.nih.nci.ess.ae;
 
+import java.util.List;
+
 import ess.caaers.nci.nih.gov.AdverseEvent;
 import ess.caaers.nci.nih.gov.Id;
+import gov.nih.nci.cabig.caaers.dao.AdverseEventDao;
 
 public class AdverseEventQueryImpl {
+
+	private AdverseEventDao adverseEventDao ;
+	private GridToDomainObjectConverter gridToDomainObjectConverter;
 	
 	public AdverseEvent[] findAdverseEvents(AdverseEvent adverseEvent) {
+		gov.nih.nci.cabig.caaers.domain.AdverseEvent caaersAe = gridToDomainObjectConverter.convertAdverseEvent(adverseEvent);
+		List<gov.nih.nci.cabig.caaers.domain.AdverseEvent> aes = adverseEventDao.findByExample(caaersAe);
+		// convert back to grid AES and return . 
 		return null;
 	}
 	
 	public AdverseEvent getAdverseEventData(Id adverseEventIdentifier) {
 		return null;
+	}
+
+
+	public void setAdverseEventDao(AdverseEventDao adverseEventDao) {
+		this.adverseEventDao = adverseEventDao;
+	}
+
+	public void setGridToDomainObjectConverter(
+			GridToDomainObjectConverter gridToDomainObjectConverter) {
+		this.gridToDomainObjectConverter = gridToDomainObjectConverter;
 	}
 }
