70adebccb9e5451434c2f5c60347e9858f1e6d36
Srini Akkala <akkalas@gmail.com>
2010-11-17 04:50:55 +0000

    SVN-Revision: 14159

---
+++ caAERS/software/grid/introduce/AdverseEventEnterpriseService/src/gov/nih/nci/ess/ae/service/AdverseEventEnterpriseServiceImpl.java
@@ -0,0 +1,19 @@
+package gov.nih.nci.ess.ae.service;
+
+import java.rmi.RemoteException;
+
+/** 
+ * TODO:I am the service side implementation class.  IMPLEMENT AND DOCUMENT ME
+ * 
+ * @created by Introduce Toolkit version 1.3
+ * 
+ */
+public class AdverseEventEnterpriseServiceImpl extends AdverseEventEnterpriseServiceImplBase {
+
+	
+	public AdverseEventEnterpriseServiceImpl() throws RemoteException {
+		super();
+	}
+	
+}
+
