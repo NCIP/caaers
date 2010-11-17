cd5568ad29f8b1df6950dba1985b4f0582ad9861
Srini Akkala <akkalas@gmail.com>
2010-11-17 14:54:48 +0000

    SVN-Revision: 14168

--- caAERS/software/grid/ess-ae/src/main/java/gov/nih/nci/ess/ae/AdverseEventQueryImpl.java
+++ caAERS/software/grid/ess-ae/src/main/java/gov/nih/nci/ess/ae/AdverseEventQueryImpl.java
@@ -1,5 +1,15 @@
 package gov.nih.nci.ess.ae;
 
-public class AdverseEventQueryImpl {
+import ess.caaers.nci.nih.gov.AdverseEvent;
+import ess.caaers.nci.nih.gov.Id;
 
+public class AdverseEventQueryImpl {
+	
+	public AdverseEvent[] findAdverseEvents(AdverseEvent adverseEvent) {
+		return null;
+	}
+	
+	public AdverseEvent getAdverseEventData(Id adverseEventIdentifier) {
+		return null;
+	}
 }
