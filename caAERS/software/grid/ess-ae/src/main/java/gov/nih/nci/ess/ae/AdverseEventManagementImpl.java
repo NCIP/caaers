d9f17e21090127881a3cf0d4001cbbe5fd7295eb
Srini Akkala <akkalas@gmail.com>
2010-11-17 14:53:14 +0000

    SVN-Revision: 14167

--- caAERS/software/grid/ess-ae/src/main/java/gov/nih/nci/ess/ae/AdverseEventManagementImpl.java
+++ caAERS/software/grid/ess-ae/src/main/java/gov/nih/nci/ess/ae/AdverseEventManagementImpl.java
@@ -1,5 +1,20 @@
 package gov.nih.nci.ess.ae;
 
+import ess.caaers.nci.nih.gov.AdverseEvent;
+import ess.caaers.nci.nih.gov.Id;
+
 public class AdverseEventManagementImpl {
+
+	public AdverseEvent initiateAdverseEvent(AdverseEvent adverseEvent  , Id studySubjectIdentifier) {
+		return null;
+	}
+	
+	public AdverseEvent updateAdverseEvent(AdverseEvent adverseEvent ) {
+		return null;
+	}
+	
+	public AdverseEvent deactivateAdverseEvent(Id adverseEventIdentifier) {
+		return null;
+	}
 
 }
