a9f9d05a29c13d653552696975e2bbdc5d33172c
Srini Akkala <akkalas@gmail.com>
2010-11-19 15:58:45 +0000

    SVN-Revision: 14191

---
+++ caAERS/software/grid/introduce/AdverseEventEnterpriseService/src/gov/nih/nci/ess/ae/service/common/AdverseEventEnterpriseServiceConstants.java
@@ -0,0 +1,13 @@
+package gov.nih.nci.ess.ae.service.common;
+
+import javax.xml.namespace.QName;
+
+
+/**
+ * Constants class that extends the introduce managed constants.  Developers can add constants to this file.
+ *
+ * @created by Introduce Toolkit version 1.3
+ */
+public interface AdverseEventEnterpriseServiceConstants extends AdverseEventEnterpriseServiceConstantsBase {
+	
+}
---
+++ caAERS/software/grid/introduce/AdverseEventEnterpriseService/src/gov/nih/nci/ess/ae/service/management/service/globus/resource/ManagementResource.java
@@ -0,0 +1,17 @@
+package gov.nih.nci.ess.ae.service.management.service.globus.resource;
+
+import org.globus.wsrf.InvalidResourceKeyException;
+import org.globus.wsrf.NoSuchResourceException;
+import org.globus.wsrf.ResourceException;
+import org.globus.wsrf.ResourceKey;
+
+
+/** 
+ * The implementation of this ManagementResource type.
+ * 
+ * @created by Introduce Toolkit version 1.3
+ * 
+ */
+public class ManagementResource extends ManagementResourceBase {
+
+}
