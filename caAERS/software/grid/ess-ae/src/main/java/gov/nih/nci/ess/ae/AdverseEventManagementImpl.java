e3e900d1a7846e8dc3e9efabed6cde85762632c6
Denis Krylov <denis.krylov@semanticbits.com>
2010-11-30 21:43:11 +0000

    CAAERS-4517: implementation in progress. Implemented updateAdverseEvent operation.
    SVN-Revision: 14241

--- caAERS/software/grid/ess-ae/src/main/java/gov/nih/nci/ess/ae/AdverseEventManagementImpl.java
+++ caAERS/software/grid/ess-ae/src/main/java/gov/nih/nci/ess/ae/AdverseEventManagementImpl.java
@@ -3,6 +3,7 @@ package gov.nih.nci.ess.ae;
 import ess.caaers.nci.nih.gov.AdverseEvent;
 import ess.caaers.nci.nih.gov.Id;
 import ess.caaers.nci.nih.gov.TsDateTime;
+import gov.nih.nci.cabig.caaers.dao.AdverseEventDao;
 import gov.nih.nci.cabig.caaers.dao.AdverseEventReportingPeriodDao;
 import gov.nih.nci.cabig.caaers.dao.StudyParticipantAssignmentDao;
 import gov.nih.nci.cabig.caaers.domain.AdverseEventReportingPeriod;
@@ -18,15 +19,19 @@ import gov.nih.nci.logging.api.util.StringUtils;
 import java.util.Date;
 import java.util.Locale;
 
+import org.apache.commons.lang.math.NumberUtils;
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceAware;
 import org.springframework.context.NoSuchMessageException;
+
+import _21090.org.iso.II;
 
 /**
  * @author Denis G. Krylov
  * 
  */
-public class AdverseEventManagementImpl implements ManagementI, MessageSourceAware {
+public class AdverseEventManagementImpl implements ManagementI,
+		MessageSourceAware {
 
 	private static final String INVALID_PARTICIPANT_ID_ERR = "WS_AEMS_032";
 	private static final String PARTICIPANT_NOT_FOUND_ERR = "WS_AEMS_033";
@@ -35,6 +40,8 @@ public class AdverseEventManagementImpl implements ManagementI, MessageSourceAwa
 	private static final String INVALID_COURSE_START_DATE_ERR = "WS_AEMS_036";
 	private static final String PARTICIPANT_NOT_ASSIGNED_ERR = "WS_AEMS_037";
 	private static final String NO_REPORTING_PERIOD_ERR = "WS_AEMS_038";
+	private static final String INVALID_AE_ID = "WS_AEMS_039";
+	private static final String ADVERSE_EVENT_NOT_FOUND = "WS_AEMS_040";
 	private GridToDomainObjectConverter gridToDomainConverter;
 	private DomainToGridObjectConverter domainToGridConverter;
 	private MessageSource messageSource;
@@ -42,14 +49,15 @@ public class AdverseEventManagementImpl implements ManagementI, MessageSourceAwa
 	private StudyRepository studyRepository;
 	private StudyParticipantAssignmentDao studyParticipantAssignmentDao;
 	private AdverseEventReportingPeriodDao adverseEventReportingPeriodDao;
+	private AdverseEventDao adverseEventDao;
 
 	/**
 	 * @param adverseEvent
 	 * @param participantId
 	 * @return
 	 */
-	public AdverseEvent initiateAdverseEvent(
-			Id participantId, Id studyId, AdverseEvent adverseEvent, TsDateTime courseStartISODate) {
+	public AdverseEvent initiateAdverseEvent(Id participantId, Id studyId,
+			AdverseEvent adverseEvent, TsDateTime courseStartISODate) {
 		Participant participant = getParticipantByPrimaryId(participantId);
 		Study study = getStudyByPrimaryId(studyId);
 		Date courseStartDate = gridToDomainConverter
@@ -60,13 +68,15 @@ public class AdverseEventManagementImpl implements ManagementI, MessageSourceAwa
 							.getMessage(INVALID_COURSE_START_DATE_ERR,
 									new Object[] {}, Locale.getDefault()));
 		}
-		StudyParticipantAssignment assignment = getStudyParticipantAssignment(participant, study);
-		AdverseEventReportingPeriod period = getReportingPeriod(courseStartDate, assignment);
+		StudyParticipantAssignment assignment = getStudyParticipantAssignment(
+				participant, study);
+		AdverseEventReportingPeriod period = getReportingPeriod(
+				courseStartDate, assignment);
 		gov.nih.nci.cabig.caaers.domain.AdverseEvent ae = gridToDomainConverter
 				.convertAdverseEvent(adverseEvent);
 		ae.setReportingPeriod(period);
 		period.addAdverseEvent(ae);
-		adverseEventReportingPeriodDao.save(period);		
+		adverseEventReportingPeriodDao.save(period);
 		return domainToGridConverter.convertAdverseEvent(ae);
 	}
 
@@ -76,15 +86,15 @@ public class AdverseEventManagementImpl implements ManagementI, MessageSourceAwa
 	 * @throws AdverseEventServiceException
 	 * @throws NoSuchMessageException
 	 */
-	private AdverseEventReportingPeriod getReportingPeriod(Date courseStartDate,
-			StudyParticipantAssignment assignment)
+	private AdverseEventReportingPeriod getReportingPeriod(
+			Date courseStartDate, StudyParticipantAssignment assignment)
 			throws AdverseEventServiceException, NoSuchMessageException {
-		AdverseEventReportingPeriod period = assignment.getReportingPeriod(courseStartDate);
-		if (period==null) {
-			throw new AdverseEventServiceException(
-					NO_REPORTING_PERIOD_ERR, getMessageSource()
-							.getMessage(NO_REPORTING_PERIOD_ERR,
-									new Object[] {}, Locale.getDefault()));
+		AdverseEventReportingPeriod period = assignment
+				.getReportingPeriod(courseStartDate);
+		if (period == null) {
+			throw new AdverseEventServiceException(NO_REPORTING_PERIOD_ERR,
+					getMessageSource().getMessage(NO_REPORTING_PERIOD_ERR,
+							new Object[] {}, Locale.getDefault()));
 		}
 		return period;
 	}
@@ -95,9 +105,9 @@ public class AdverseEventManagementImpl implements ManagementI, MessageSourceAwa
 	 * @throws AdverseEventServiceException
 	 * @throws NoSuchMessageException
 	 */
-	private StudyParticipantAssignment getStudyParticipantAssignment(Participant participant,
-			Study study) throws AdverseEventServiceException,
-			NoSuchMessageException {
+	private StudyParticipantAssignment getStudyParticipantAssignment(
+			Participant participant, Study study)
+			throws AdverseEventServiceException, NoSuchMessageException {
 		StudyParticipantAssignment assignment = studyParticipantAssignmentDao
 				.getAssignment(participant, study);
 		if (assignment == null) {
@@ -158,8 +168,36 @@ public class AdverseEventManagementImpl implements ManagementI, MessageSourceAwa
 		return participant;
 	}
 
-	public AdverseEvent updateAdverseEvent(AdverseEvent adverseEvent) {
-		return null;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * gov.nih.nci.ess.ae.service.management.common.ManagementI#updateAdverseEvent
+	 * (ess.caaers.nci.nih.gov.AdverseEvent)
+	 */
+	public AdverseEvent updateAdverseEvent(AdverseEvent gridAe) {
+		gov.nih.nci.cabig.caaers.domain.AdverseEvent ae = findAdverseEvent(gridAe
+				.getIdentifier());
+		gridToDomainConverter.convertAdverseEvent(gridAe, ae);
+		adverseEventDao.save(ae);
+		return domainToGridConverter.convertAdverseEvent(ae);
+	}
+
+	private gov.nih.nci.cabig.caaers.domain.AdverseEvent findAdverseEvent(II id) {
+		if (id == null || !NumberUtils.isNumber(id.getExtension())) {
+			throw new AdverseEventServiceException(INVALID_AE_ID,
+					getMessageSource().getMessage(INVALID_AE_ID,
+							new Object[] {}, Locale.getDefault()));
+		}
+		int aeId = Integer.parseInt(id.getExtension());
+		gov.nih.nci.cabig.caaers.domain.AdverseEvent ae = adverseEventDao
+				.getById(aeId);
+		if (ae == null) {
+			throw new AdverseEventServiceException(ADVERSE_EVENT_NOT_FOUND,
+					getMessageSource().getMessage(ADVERSE_EVENT_NOT_FOUND,
+							new Object[] {}, Locale.getDefault()));
+		}
+		return ae;
 	}
 
 	public AdverseEvent deactivateAdverseEvent(Id adverseEventIdentifier) {
@@ -268,11 +306,27 @@ public class AdverseEventManagementImpl implements ManagementI, MessageSourceAwa
 	}
 
 	/**
-	 * @param adverseEventReportingPeriodDao the adverseEventReportingPeriodDao to set
+	 * @param adverseEventReportingPeriodDao
+	 *            the adverseEventReportingPeriodDao to set
 	 */
 	public final void setAdverseEventReportingPeriodDao(
 			AdverseEventReportingPeriodDao adverseEventReportingPeriodDao) {
 		this.adverseEventReportingPeriodDao = adverseEventReportingPeriodDao;
+	}
+
+	/**
+	 * @return the adverseEventDao
+	 */
+	public final AdverseEventDao getAdverseEventDao() {
+		return adverseEventDao;
+	}
+
+	/**
+	 * @param adverseEventDao
+	 *            the adverseEventDao to set
+	 */
+	public final void setAdverseEventDao(AdverseEventDao adverseEventDao) {
+		this.adverseEventDao = adverseEventDao;
 	}
 
 }
