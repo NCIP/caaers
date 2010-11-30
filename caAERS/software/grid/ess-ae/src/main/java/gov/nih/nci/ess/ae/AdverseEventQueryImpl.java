377b29efb5e8b002380b77abb2ddecdc7c3d9f1e
Srini Akkala <akkalas@gmail.com>
2010-11-30 16:05:58 +0000

    SVN-Revision: 14236

--- caAERS/software/grid/ess-ae/src/main/java/gov/nih/nci/ess/ae/AdverseEventQueryImpl.java
+++ caAERS/software/grid/ess-ae/src/main/java/gov/nih/nci/ess/ae/AdverseEventQueryImpl.java
@@ -1,24 +1,37 @@
 package gov.nih.nci.ess.ae;
 
-import java.util.List;
-
 import ess.caaers.nci.nih.gov.AdverseEvent;
 import ess.caaers.nci.nih.gov.Id;
 import gov.nih.nci.cabig.caaers.dao.AdverseEventDao;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdverseEventQueryImpl {
 
 	private AdverseEventDao adverseEventDao ;
 	private GridToDomainObjectConverter gridToDomainObjectConverter;
+	private DomainToGridObjectConverter domainToGridObjectConverter;
 	
 	public AdverseEvent[] findAdverseEvents(AdverseEvent adverseEvent) {
 		gov.nih.nci.cabig.caaers.domain.AdverseEvent caaersAe = gridToDomainObjectConverter.convertAdverseEvent(adverseEvent);
 		List<gov.nih.nci.cabig.caaers.domain.AdverseEvent> aes = adverseEventDao.findByExample(caaersAe);
 		// convert back to grid AES and return . 
-		return null;
+		List<AdverseEvent> aesToReturnList = new ArrayList<AdverseEvent>();
+		for (gov.nih.nci.cabig.caaers.domain.AdverseEvent ae:aes){
+			AdverseEvent gridAe = domainToGridObjectConverter.convertAdverseEvent(ae);
+			aesToReturnList.add(gridAe);
+		}
+		return (AdverseEvent[]) aesToReturnList.toArray();
 	}
 	
 	public AdverseEvent getAdverseEventData(Id adverseEventIdentifier) {
+		if (adverseEventIdentifier != null) {
+			Integer id = Integer.parseInt(adverseEventIdentifier.getExtension());
+			gov.nih.nci.cabig.caaers.domain.AdverseEvent ae = adverseEventDao.getById(id);
+			AdverseEvent gridAe = domainToGridObjectConverter.convertAdverseEvent(ae);
+			return gridAe;
+		}
 		return null;
 	}
 
@@ -30,5 +43,10 @@ public class AdverseEventQueryImpl {
 	public void setGridToDomainObjectConverter(
 			GridToDomainObjectConverter gridToDomainObjectConverter) {
 		this.gridToDomainObjectConverter = gridToDomainObjectConverter;
+	}
+
+	public void setDomainToGridObjectConverter(
+			DomainToGridObjectConverter domainToGridObjectConverter) {
+		this.domainToGridObjectConverter = domainToGridObjectConverter;
 	}
 }
