94a286a3c558255f0783617f9d2996eb631fe240
Denis Krylov <denis.krylov@semanticbits.com>
2010-12-07 19:47:10 +0000

    CAAERS-4517: implementation in progress. Fixed wsrf-caaers build and start-up issues.
    SVN-Revision: 14274

--- caAERS/software/grid/introduce/AdverseEventEnterpriseService/src/gov/nih/nci/ess/ae/service/management/service/ManagementImpl.java
+++ caAERS/software/grid/introduce/AdverseEventEnterpriseService/src/gov/nih/nci/ess/ae/service/management/service/ManagementImpl.java
@@ -2,43 +2,76 @@ package gov.nih.nci.ess.ae.service.management.service;
 
 import gov.nih.nci.ess.ae.service.common.AdverseEventEnterpriseServiceI;
 import gov.nih.nci.ess.ae.service.management.common.ManagementI;
+import gov.nih.nci.ess.ae.service.misc.SpringContextHolder;
 
 import java.rmi.RemoteException;
 
 import org.globus.wsrf.config.ContainerConfig;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/** 
- * TODO:I am the service side implementation class.  IMPLEMENT AND DOCUMENT ME
+/**
+ * TODO:I am the service side implementation class. IMPLEMENT AND DOCUMENT ME
  * 
  * @created by Introduce Toolkit version 1.3
  * 
  */
 public class ManagementImpl extends ManagementImplBase {
-	
+
 	private static final String BEAN_NAME = "adverseEventManagementImpl";
 
-    private ManagementI aeManagement;
-	
+	private ManagementI aeManagement;
+
 	public ManagementImpl() throws RemoteException {
 		super();
-		String exp = ContainerConfig.getConfig().getOption(AdverseEventEnterpriseServiceI.SPRING_CLASSPATH_EXPRESSION, AdverseEventEnterpriseServiceI.DEFAULT_SPRING_CLASSPATH_EXPRESSION);
-	    ApplicationContext ctx = new ClassPathXmlApplicationContext(exp);
-	    aeManagement = (ManagementI) ctx.getBean(BEAN_NAME);
-   }
-	
-  public ess.caaers.nci.nih.gov.AdverseEvent initiateAdverseEvent(ess.caaers.nci.nih.gov.Id subjectIdentifier,ess.caaers.nci.nih.gov.Id studyIdentifier,ess.caaers.nci.nih.gov.AdverseEvent adverseEvent,ess.caaers.nci.nih.gov.TsDateTime courseStartDate) throws RemoteException, gov.nih.nci.ess.ae.service.management.stubs.types.AdverseEventServiceException {
-	  return aeManagement.initiateAdverseEvent(subjectIdentifier, studyIdentifier, adverseEvent, courseStartDate);
-  }
+		ApplicationContext ctx = getApplicationContext();
+		aeManagement = (ManagementI) ctx.getBean(BEAN_NAME);
+	}
 
-  public ess.caaers.nci.nih.gov.AdverseEvent updateAdverseEvent(ess.caaers.nci.nih.gov.AdverseEvent adverseEvent) throws RemoteException, gov.nih.nci.ess.ae.service.management.stubs.types.AdverseEventServiceException {
-    return aeManagement.updateAdverseEvent(adverseEvent);
-  }
+	/**
+	 * @return
+	 * @throws BeansException
+	 */
+	private ApplicationContext getApplicationContext() throws BeansException {
+		ApplicationContext ctx = null;
+		String exp = ContainerConfig
+				.getConfig()
+				.getOption(
+						AdverseEventEnterpriseServiceI.SPRING_CLASSPATH_EXPRESSION,
+						AdverseEventEnterpriseServiceI.DEFAULT_SPRING_CLASSPATH_EXPRESSION);
+		if (SpringContextHolder.getApplicationContext() == null) {
+			ctx = new ClassPathXmlApplicationContext(exp);
+			SpringContextHolder.setApplicationContext(ctx);
+		} else {
+			ctx = SpringContextHolder.getApplicationContext();
+		}
+		return ctx;
+	}
 
-  public ess.caaers.nci.nih.gov.AdverseEvent deactivateAdverseEvent(ess.caaers.nci.nih.gov.Id adverseEventIdentifier) throws RemoteException, gov.nih.nci.ess.ae.service.management.stubs.types.AdverseEventServiceException {
-    return aeManagement.deactivateAdverseEvent(adverseEventIdentifier);
-  }
+	public ess.caaers.nci.nih.gov.AdverseEvent initiateAdverseEvent(
+			ess.caaers.nci.nih.gov.Id subjectIdentifier,
+			ess.caaers.nci.nih.gov.Id studyIdentifier,
+			ess.caaers.nci.nih.gov.AdverseEvent adverseEvent,
+			ess.caaers.nci.nih.gov.TsDateTime courseStartDate)
+			throws RemoteException,
+			gov.nih.nci.ess.ae.service.management.stubs.types.AdverseEventServiceException {
+		return aeManagement.initiateAdverseEvent(subjectIdentifier,
+				studyIdentifier, adverseEvent, courseStartDate);
+	}
+
+	public ess.caaers.nci.nih.gov.AdverseEvent updateAdverseEvent(
+			ess.caaers.nci.nih.gov.AdverseEvent adverseEvent)
+			throws RemoteException,
+			gov.nih.nci.ess.ae.service.management.stubs.types.AdverseEventServiceException {
+		return aeManagement.updateAdverseEvent(adverseEvent);
+	}
+
+	public ess.caaers.nci.nih.gov.AdverseEvent deactivateAdverseEvent(
+			ess.caaers.nci.nih.gov.Id adverseEventIdentifier)
+			throws RemoteException,
+			gov.nih.nci.ess.ae.service.management.stubs.types.AdverseEventServiceException {
+		return aeManagement.deactivateAdverseEvent(adverseEventIdentifier);
+	}
 
 }
-
--- caAERS/software/grid/introduce/AdverseEventEnterpriseService/src/gov/nih/nci/ess/ae/service/query/service/QueryImpl.java
+++ caAERS/software/grid/introduce/AdverseEventEnterpriseService/src/gov/nih/nci/ess/ae/service/query/service/QueryImpl.java
@@ -1,16 +1,18 @@
 package gov.nih.nci.ess.ae.service.query.service;
 
 import gov.nih.nci.ess.ae.service.common.AdverseEventEnterpriseServiceI;
+import gov.nih.nci.ess.ae.service.misc.SpringContextHolder;
 import gov.nih.nci.ess.ae.service.query.common.QueryI;
 
 import java.rmi.RemoteException;
 
 import org.globus.wsrf.config.ContainerConfig;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/** 
- * TODO:I am the service side implementation class.  IMPLEMENT AND DOCUMENT ME
+/**
+ * TODO:I am the service side implementation class. IMPLEMENT AND DOCUMENT ME
  * 
  * @created by Introduce Toolkit version 1.3
  * 
@@ -19,22 +21,46 @@ public class QueryImpl extends QueryImplBase {
 
 	private static final String BEAN_NAME = "adverseEventQueryImpl";
 
-    private QueryI aeQuery;
-    
+	private QueryI aeQuery;
+
 	public QueryImpl() throws RemoteException {
 		super();
-		String exp = ContainerConfig.getConfig().getOption(AdverseEventEnterpriseServiceI.SPRING_CLASSPATH_EXPRESSION, AdverseEventEnterpriseServiceI.DEFAULT_SPRING_CLASSPATH_EXPRESSION);
-	    ApplicationContext ctx = new ClassPathXmlApplicationContext(exp);
-	    aeQuery = (QueryI) ctx.getBean(BEAN_NAME);
+		ApplicationContext ctx = getApplicationContext();
+		aeQuery = (QueryI) ctx.getBean(BEAN_NAME);
 	}
-	
-  public ess.caaers.nci.nih.gov.AdverseEvent[] findAdverseEvents(ess.caaers.nci.nih.gov.AdverseEvent adverseEvent) throws RemoteException, gov.nih.nci.ess.ae.service.management.stubs.types.AdverseEventServiceException {
-    return aeQuery.findAdverseEvents(adverseEvent);
-  }
 
-  public ess.caaers.nci.nih.gov.AdverseEvent getAdverseEventData(ess.caaers.nci.nih.gov.Id adverseEventIdentifier) throws RemoteException, gov.nih.nci.ess.ae.service.management.stubs.types.AdverseEventServiceException {
-	  return aeQuery.getAdverseEventData(adverseEventIdentifier);
-  }
+	/**
+	 * @return
+	 * @throws BeansException
+	 */
+	private ApplicationContext getApplicationContext() throws BeansException {
+		ApplicationContext ctx = null;
+		String exp = ContainerConfig
+				.getConfig()
+				.getOption(
+						AdverseEventEnterpriseServiceI.SPRING_CLASSPATH_EXPRESSION,
+						AdverseEventEnterpriseServiceI.DEFAULT_SPRING_CLASSPATH_EXPRESSION);
+		if (SpringContextHolder.getApplicationContext() == null) {
+			ctx = new ClassPathXmlApplicationContext(exp);
+			SpringContextHolder.setApplicationContext(ctx);
+		} else {
+			ctx = SpringContextHolder.getApplicationContext();
+		}
+		return ctx;
+	}
+
+	public ess.caaers.nci.nih.gov.AdverseEvent[] findAdverseEvents(
+			ess.caaers.nci.nih.gov.AdverseEvent adverseEvent)
+			throws RemoteException,
+			gov.nih.nci.ess.ae.service.management.stubs.types.AdverseEventServiceException {
+		return aeQuery.findAdverseEvents(adverseEvent);
+	}
+
+	public ess.caaers.nci.nih.gov.AdverseEvent getAdverseEventData(
+			ess.caaers.nci.nih.gov.Id adverseEventIdentifier)
+			throws RemoteException,
+			gov.nih.nci.ess.ae.service.management.stubs.types.AdverseEventServiceException {
+		return aeQuery.getAdverseEventData(adverseEventIdentifier);
+	}
 
 }
-
