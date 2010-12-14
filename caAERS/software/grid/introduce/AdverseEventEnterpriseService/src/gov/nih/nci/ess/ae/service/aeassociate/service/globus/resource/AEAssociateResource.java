606b4534a81fe42fcbb5ec81e4202a9113f6250c
Srini Akkala <akkalas@gmail.com>
2010-12-14 02:20:49 +0000

    SVN-Revision: 14300

---
+++ caAERS/software/grid/introduce/AdverseEventEnterpriseService/src/gov/nih/nci/ess/ae/service/aeassociate/service/globus/resource/AEAssociateResource.java
@@ -0,0 +1,17 @@
+package gov.nih.nci.ess.ae.service.aeassociate.service.globus.resource;
+
+import org.globus.wsrf.InvalidResourceKeyException;
+import org.globus.wsrf.NoSuchResourceException;
+import org.globus.wsrf.ResourceException;
+import org.globus.wsrf.ResourceKey;
+
+
+/** 
+ * The implementation of this AEAssociateResource type.
+ * 
+ * @created by Introduce Toolkit version 1.3
+ * 
+ */
+public class AEAssociateResource extends AEAssociateResourceBase {
+
+}
