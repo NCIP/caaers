2cc3e169510ea62363de8aab4bf22295a8b7fba3
Ion Olaru <ion.olaru@gmail.com>
2011-01-17 22:24:32 +0000

    CAAERS-3876 - The system should support user / role centric dashboards
    SVN-Revision: 14473

--- caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/dashboard/SubjectDashboardController.java
+++ caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/dashboard/SubjectDashboardController.java
@@ -1,9 +1,13 @@
 package gov.nih.nci.cabig.caaers.web.dashboard;
 
 import org.springframework.web.servlet.ModelAndView;
+import gov.nih.nci.cabig.caaers.domain.repository.StudyParticipantAssignmentRepository;
+import gov.nih.nci.cabig.caaers.domain.StudyParticipantAssignment;
+
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * @author Ion C. Olaru
@@ -11,9 +15,20 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class SubjectDashboardController extends DashboardController {
 
+     StudyParticipantAssignmentRepository studyParticipantAssignmentRepository;
+
     public ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
         ModelAndView mv = new ModelAndView("dashboard/dashboard_Subject");
+        List<StudyParticipantAssignment> subjectList = studyParticipantAssignmentRepository.getAllAssignments();
+        mv.addObject("subjectList", subjectList);
         return mv;
     }
 
+    public StudyParticipantAssignmentRepository getStudyParticipantAssignmentRepository() {
+        return studyParticipantAssignmentRepository;
+    }
+
+    public void setStudyParticipantAssignmentRepository(StudyParticipantAssignmentRepository studyParticipantAssignmentRepository) {
+        this.studyParticipantAssignmentRepository = studyParticipantAssignmentRepository;
+    }
 }
---
+++ caAERS/software/web/src/main/webapp/WEB-INF/views/dashboard/dashboard_Subject.jsp
@@ -0,0 +1,32 @@
+<%@include file="/WEB-INF/views/taglibs.jsp" %>
+<html>
+<head>
+</head>
+  
+  <chrome:boxIPhone title="Subjects" style="width:700px;">
+  	<jsp:body>
+  		<form action = "dummy">
+                <div id="subjects" class="scrollerTask">
+                    <table border="0" cellpadding="0" cellspacing="0" class="dashboard_table" width="99%">
+                        <tr class="taskTitleRow">
+                            <th>Primary Id
+                            <th width="50%">Name
+                            <th>Study Id
+                            <th>Last Updated
+                            <th>Action
+                        </tr>
+                         <c:forEach var ="subject" items="${subjectList}" varStatus = "loopStatus">
+                         	<tr class="${loopStatus.index % 2 == 0 ? 'alt' : ''}">
+                         		<td>${subject.studySubjectIdentifier}</td>
+
+
+                           </tr>
+                         </c:forEach>
+                     </table>
+                </div>
+          </form>      
+  	</jsp:body>               
+  </chrome:boxIPhone>
+</html>
+  
+         
