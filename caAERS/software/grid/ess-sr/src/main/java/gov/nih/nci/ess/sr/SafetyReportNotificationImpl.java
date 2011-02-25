9a3aa17b3d09751ec19c555065bd3d2a5d14f626
Srini Akkala <akkalas@gmail.com>
2011-02-25 04:24:29 +0000

    SVN-Revision: 14799

---
+++ caAERS/software/grid/ess-sr/src/main/java/gov/nih/nci/ess/sr/SafetyReportNotificationImpl.java
@@ -0,0 +1,70 @@
+package gov.nih.nci.ess.sr;
+
+import ess.caaers.nci.nih.gov.Id;
+import gov.nih.nci.cabig.caaers.dao.report.ReportDefinitionDao;
+import gov.nih.nci.cabig.caaers.domain.report.PlannedNotification;
+import gov.nih.nci.cabig.caaers.domain.report.ReportDefinition;
+import gov.nih.nci.ess.safetyreporting.management.stubs.types.SafetyReportingServiceException;
+import gov.nih.nci.ess.safetyreporting.notification.common.SafetyReportNotificationI;
+import gov.nih.nci.ess.safetyreporting.types.SafetyReportDefinitionNotification;
+
+import java.rmi.RemoteException;
+
+import org.oasis.wsrf.lifetime.Destroy;
+import org.oasis.wsrf.lifetime.DestroyResponse;
+import org.oasis.wsrf.lifetime.SetTerminationTime;
+import org.oasis.wsrf.lifetime.SetTerminationTimeResponse;
+
+import _21090.org.iso.ST;
+
+public class SafetyReportNotificationImpl implements SafetyReportNotificationI {
+	
+	private ReportDefinitionDao reportDefinitionDao;
+	private SafetyToExpeditedReportConverter safetyToExpeditedReportConverter;
+
+	public void createSafetyReportDefinitionNotification(SafetyReportDefinitionNotification safetyReportDefinitionNotification, Id reportDefinitionId) throws RemoteException, SafetyReportingServiceException {
+		ReportDefinition rd = reportDefinitionDao.getById(ISO21090Helper.value(reportDefinitionId));
+		PlannedNotification pn = safetyToExpeditedReportConverter.convertReportDefinitionNotification(safetyReportDefinitionNotification);
+		rd.addPlannedNotification(pn);
+		reportDefinitionDao.save(rd);
+	}
+
+	public void deactivateSafetyReportDefinitionNotification(Id notificationId, ST reasonForDeactivation) throws RemoteException, SafetyReportingServiceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateSafetyReportDefinitionNotification(SafetyReportDefinitionNotification safetyReportDefinitionNotification) throws RemoteException, SafetyReportingServiceException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public DestroyResponse destroy(Destroy params) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public SetTerminationTimeResponse setTerminationTime(SetTerminationTime params) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ReportDefinitionDao getReportDefinitionDao() {
+		return reportDefinitionDao;
+	}
+
+	public void setReportDefinitionDao(ReportDefinitionDao reportDefinitionDao) {
+		this.reportDefinitionDao = reportDefinitionDao;
+	}
+
+	public SafetyToExpeditedReportConverter getSafetyToExpeditedReportConverter() {
+		return safetyToExpeditedReportConverter;
+	}
+
+	public void setSafetyToExpeditedReportConverter(
+			SafetyToExpeditedReportConverter safetyToExpeditedReportConverter) {
+		this.safetyToExpeditedReportConverter = safetyToExpeditedReportConverter;
+	}
+
+
+}
--- caAERS/software/grid/ess-sr/src/main/java/gov/nih/nci/ess/sr/SafetyToExpeditedReportConverterImpl.java
+++ caAERS/software/grid/ess-sr/src/main/java/gov/nih/nci/ess/sr/SafetyToExpeditedReportConverterImpl.java
@@ -3,12 +3,17 @@ package gov.nih.nci.ess.sr;
 import gov.nih.nci.cabig.caaers.dao.OrganizationDao;
 import gov.nih.nci.cabig.caaers.domain.ExpeditedAdverseEventReport;
 import gov.nih.nci.cabig.caaers.domain.ReportFormatType;
+import gov.nih.nci.cabig.caaers.domain.report.NotificationBodyContent;
+import gov.nih.nci.cabig.caaers.domain.report.Recipient;
 import gov.nih.nci.cabig.caaers.domain.report.ReportFormat;
 import gov.nih.nci.cabig.caaers.domain.report.RequirednessIndicator;
 import gov.nih.nci.cabig.caaers.domain.report.TimeScaleUnit;
 import gov.nih.nci.ess.safetyreporting.types.ReportDeliveryDefinition;
 import gov.nih.nci.ess.safetyreporting.types.ReportMandatoryFieldDefinition;
+import gov.nih.nci.ess.safetyreporting.types.SafetyReportDefinitionNotification;
 import gov.nih.nci.ess.safetyreporting.types.SafetyReportVersion;
+import _21090.org.iso.BAG_TEL;
+import _21090.org.iso.TEL;
 
 public class SafetyToExpeditedReportConverterImpl implements SafetyToExpeditedReportConverter{
 	private OrganizationDao organizationDao;
@@ -125,7 +130,34 @@ public class SafetyToExpeditedReportConverterImpl implements SafetyToExpeditedRe
 		return rmd;
 	}
 	
+	public gov.nih.nci.cabig.caaers.domain.report.PlannedNotification convertReportDefinitionNotification (SafetyReportDefinitionNotification notification) {
+		gov.nih.nci.cabig.caaers.domain.report.PlannedEmailNotification pn = new gov.nih.nci.cabig.caaers.domain.report.PlannedEmailNotification();
+		if (notification.getIndexOnTimeScale()!=null) {
+			pn.setIndexOnTimeScale(ISO21090Helper.value(notification.getIndexOnTimeScale()));
+		}
 
+		if (notification.getRecipients()!=null) {
+			BAG_TEL bagTel = notification.getRecipients();
+			TEL[] tels = bagTel.getItem();
+			for (int i=0;i<tels.length;i++) {
+				TEL tel = tels[0];
+				// parse email from TEL and build Recipient..
+				Recipient rr = null;
+				pn.addRecipient(rr);
+			}
+		}
+		if (notification.getBodyContent()!=null) {
+			NotificationBodyContent nbc = new NotificationBodyContent();
+			nbc.setBody(ISO21090Helper.value(notification.getBodyContent()));
+			pn.setNotificationBodyContent(nbc);
+		}
+		if (notification.getSubjectLIne()!=null) {
+			pn.setSubjectLine(ISO21090Helper.value(notification.getSubjectLIne()));
+		}
+		
+		return pn;
+	}
+	
 	public OrganizationDao getOrganizationDao() {
 		return organizationDao;
 	}
