41b3184c23861d3e24d7a1aed43cb8a2cc87934b
Srini Akkala <akkalas@gmail.com>
2011-02-25 04:23:16 +0000

    SVN-Revision: 14797

--- caAERS/software/grid/ess-sr/src/main/java/gov/nih/nci/ess/sr/SafetyToExpeditedReportConverter.java
+++ caAERS/software/grid/ess-sr/src/main/java/gov/nih/nci/ess/sr/SafetyToExpeditedReportConverter.java
@@ -3,6 +3,7 @@ package gov.nih.nci.ess.sr;
 import gov.nih.nci.cabig.caaers.domain.ExpeditedAdverseEventReport;
 import gov.nih.nci.ess.safetyreporting.types.ReportDeliveryDefinition;
 import gov.nih.nci.ess.safetyreporting.types.ReportMandatoryFieldDefinition;
+import gov.nih.nci.ess.safetyreporting.types.SafetyReportDefinitionNotification;
 import gov.nih.nci.ess.safetyreporting.types.SafetyReportVersion;
 
 public interface SafetyToExpeditedReportConverter {
@@ -17,5 +18,6 @@ public interface SafetyToExpeditedReportConverter {
 	
 	public abstract gov.nih.nci.cabig.caaers.domain.report.ReportMandatoryFieldDefinition convertReportMandatoryFieldDefinition (ReportMandatoryFieldDefinition srmd);
 
+	public abstract gov.nih.nci.cabig.caaers.domain.report.PlannedNotification convertReportDefinitionNotification (SafetyReportDefinitionNotification notification) ;
 
 }
