24ceee3f541096ab8f12176c72708869077f59bc
Ion Olaru <ion.olaru@gmail.com>
2011-04-01 17:08:18 +0000

    testcase
    SVN-Revision: 14935

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/ReportVersionDaoTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/ReportVersionDaoTest.java
@@ -3,6 +3,7 @@ package gov.nih.nci.cabig.caaers.dao;
 import gov.nih.nci.cabig.caaers.DaoTestCase;
 import gov.nih.nci.cabig.caaers.dao.query.ReportVersionDTOQuery;
 import gov.nih.nci.cabig.caaers.dao.report.ReportVersionDao;
+import gov.nih.nci.cabig.caaers.domain.ReportStatus;
 import gov.nih.nci.cabig.caaers.domain.report.ReportVersionDTO;
 
 import java.util.List;
@@ -28,18 +29,19 @@ public class ReportVersionDaoTest extends DaoTestCase<ReportVersionDao> {
 
     public void testSearchReportVersionDTO() {
         ReportVersionDTOQuery q = new ReportVersionDTOQuery();
+        q.filterByReportStatus(ReportStatus.COMPLETED);
     	List<ReportVersionDTO> l = rsDao.searchDTO(q);
         interruptSession();
 
-        assertEquals(4, l.size());
-        assertEquals(-91, l.get(0).getRv().getId().intValue());
-        assertEquals("Label of RCT-223", l.get(0).getReportName());
+        assertEquals(1, l.size());
+        assertEquals(-88, l.get(0).getRv().getId().intValue());
+        assertEquals("Label for RCT-222", l.get(0).getReportName());
         assertEquals("Vasile", l.get(0).getSubjectFirstName());
         assertEquals("Boamba", l.get(0).getSubjectLastName());
         assertEquals("Id-02", l.get(0).getSubjectPrimaryIdentifier());
         assertEquals("Study Short Title", l.get(0).getStudyShortTitle());
         assertEquals(-1, l.get(0).getAeReportID().intValue());
-        assertEquals(-225, l.get(0).getReportID().intValue());
+        assertEquals(-223, l.get(0).getReportID().intValue());
         assertEquals(33, l.get(0).getPeriodCycle().intValue());
         assertEquals("2006-09-30 00:00:00.0", l.get(0).getPeriodStartDate().toString());
         assertEquals("NCI CODE 0190", l.get(0).getStudySiteCode());
