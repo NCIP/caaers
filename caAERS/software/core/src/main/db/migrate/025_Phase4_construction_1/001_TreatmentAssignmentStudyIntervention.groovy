a42f178c8bbf293d9aa851378592fba55ab6ac46
Ion Olaru <ion.olaru@gmail.com>
2012-01-13 20:12:24 +0000

    Treatment Assignment Interventions
    SVN-Revision: 15159

--- caAERS/software/core/src/main/db/migrate/025_Phase4_construction_1/001_TreatmentAssignmentStudyIntervention.groovy
+++ caAERS/software/core/src/main/db/migrate/025_Phase4_construction_1/001_TreatmentAssignmentStudyIntervention.groovy
@@ -41,14 +41,33 @@ class TreatmentAssignmentStudyIntervention extends edu.northwestern.bioinformati
             t.addColumn("grade5frequency", "float");
         };
 
-        // execute('alter table study_devices add constraint fk_stdy_dvc_dvc_id foreign key (device_id) references devices (id)')
+        execute('ALTER TABLE treatment_assignment_study_interventions ADD CONSTRAINT fk_ta_id FOREIGN KEY (treatment_assignment_id) REFERENCES treatment_assignment (id)')
+        execute('ALTER TABLE study_interventions_expected_aes ADD CONSTRAINT fk_tai_id FOREIGN KEY (treatment_assignment_study_intervention_id) REFERENCES treatment_assignment_study_interventions (id)')
+        execute('ALTER TABLE treatment_assignment_agents ADD CONSTRAINT fk_tasi_agent_id FOREIGN KEY (study_agent_id) REFERENCES study_agents (id)')
+        execute('ALTER TABLE treatment_assignment_agents ADD CONSTRAINT fk_tasiagent_id FOREIGN KEY (id) REFERENCES treatment_assignment_study_interventions (id)')
+        execute('ALTER TABLE treatment_assignment_devices ADD CONSTRAINT fk_tasi_device_id FOREIGN KEY (study_device_id) REFERENCES study_devices (id)')
+        execute('ALTER TABLE treatment_assignment_devices ADD CONSTRAINT fk_tasidevice_id FOREIGN KEY (id) REFERENCES treatment_assignment_study_interventions (id)')
+        execute('ALTER TABLE treatment_assignment_other_interventions ADD CONSTRAINT fk_tasi_other_id FOREIGN KEY (study_other_intervention_id) REFERENCES other_interventions (id)')
+        execute('ALTER TABLE treatment_assignment_other_interventions ADD CONSTRAINT fk_tasiother_id FOREIGN KEY (id) REFERENCES treatment_assignment_study_interventions (id)')
+
     }
 
     void down() {
+
+        execute("ALTER TABLE study_interventions_expected_aes DROP CONSTRAINT fk_tai_id");
+        execute("ALTER TABLE treatment_assignment_study_interventions DROP CONSTRAINT fk_ta_id");
+        execute("ALTER TABLE treatment_assignment_agents DROP CONSTRAINT fk_tasi_agent_id");
+        execute("ALTER TABLE treatment_assignment_agents DROP CONSTRAINT fk_tasiagent_id");
+        execute("ALTER TABLE treatment_assignment_devices DROP CONSTRAINT fk_tasi_device_id");
+        execute("ALTER TABLE treatment_assignment_devices DROP CONSTRAINT fk_tasidevice_id");
+        execute("ALTER TABLE treatment_assignment_other_interventions DROP CONSTRAINT fk_tasi_other_id");
+        execute("ALTER TABLE treatment_assignment_other_interventions DROP CONSTRAINT fk_tasiother_id");
+
         dropTable("study_interventions_expected_aes");
         dropTable("treatment_assignment_agents");
         dropTable("treatment_assignment_devices");
         dropTable("treatment_assignment_other_interventions");
         dropTable("treatment_assignment_study_interventions");
+
     }
 }
