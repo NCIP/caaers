350bd78a28352999459a744d2188ad71a820e415
Biju Joseph <biju.joseph@semanticbits.com>
2012-02-01 02:08:08 +0000

    Mandatory field configuration and business rules not enforced on Verbatim field in Capture AE CAAERS-4020
    SVN-Revision: 15189

--- caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/ae/AdverseEventCaptureTab.java
+++ caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/ae/AdverseEventCaptureTab.java
@@ -86,7 +86,7 @@ public class AdverseEventCaptureTab extends AdverseEventTab {
                 
             	//verbatim - Is required when there is no other MedDRA
                 boolean verbatimMandatory = (study.getOtherMeddra() == null) && (ae.getAdverseEventTerm().isOtherRequired());
-                boolean verbatimApplicable = caaersFieldConfigurationManager.isFieldApplicable(TAB_NAME, "aeReport.adverseEvents.detailsForOther");
+                boolean verbatimApplicable = caaersFieldConfigurationManager.isFieldApplicable(TAB_NAME, "adverseEvents.detailsForOther");
                 boolean verbatimRequired =  verbatimApplicable && (verbatimMandatory || isFieldRequired(ae, "adverseEvents[].detailsForOther"));
             	InputField verbatimField = InputFieldFactory.createTextField("detailsForOther", "Verbatim", "aeReport.adverseEvents.detailsForOther", verbatimRequired);
                 InputFieldAttributes.setSize(verbatimField, 25);
@@ -163,13 +163,6 @@ public class AdverseEventCaptureTab extends AdverseEventTab {
     	else{
     		return caaersFieldConfigurationManager.isFieldMandatory(TAB_NAME, fieldPath);
     	}
-    }
-
-    @Override
-    public void beforeBind(HttpServletRequest request, CaptureAdverseEventInputCommand command) {
-        super.beforeBind(request, command);
-        
-        command.reassociate();
     }
 
     @Override
