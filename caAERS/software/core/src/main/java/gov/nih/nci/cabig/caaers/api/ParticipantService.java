5ad6d63b65b0ec9c83e2a5c851b2127b25bfea85
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2012-04-03 20:19:17 +0000

    removed JAXB web service annotations
    SVN-Revision: 15479

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/api/ParticipantService.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/api/ParticipantService.java
@@ -2,44 +2,26 @@ package gov.nih.nci.cabig.caaers.api;
 
 import gov.nih.nci.cabig.caaers.webservice.participant.Participants;
 
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-import javax.jws.WebService;
-
-/**
- * Webservice Interface for Participant Creation and Updation.
- * @author Monish Dombla
- *
- */
-@WebService(name="ParticipantServiceIntf",targetNamespace="http://webservice.caaers.cabig.nci.nih.gov/participant")
 public interface ParticipantService {
-	
 	
 	/**
 	 * This operation will accept a Participant which is a jaxb study and creates it.
 	 * @param xmlParticipants
 	 */
-	@WebMethod
-	public gov.nih.nci.cabig.caaers.webservice.participant.CaaersServiceResponse createParticipant(@WebParam(name="Participants",
-            targetNamespace="http://webservice.caaers.cabig.nci.nih.gov/participant") Participants xmlParticipants);
-	
+	public gov.nih.nci.cabig.caaers.webservice.participant.CaaersServiceResponse createParticipant(Participants xmlParticipants);
 	
 	
 	/**
 	 * This operation will accept a Participant which is a jaxb Participant and updates it.
 	 * @param xmlParticipants
 	 */
-	@WebMethod
-	public gov.nih.nci.cabig.caaers.webservice.participant.CaaersServiceResponse updateParticipant(@WebParam(name="Participants",
-            targetNamespace="http://webservice.caaers.cabig.nci.nih.gov/participant") Participants xmlParticipants);
+	public gov.nih.nci.cabig.caaers.webservice.participant.CaaersServiceResponse updateParticipant(Participants xmlParticipants);
 	
 	/**
 	 * This operation will accept a Participant which is a jaxb Participant and deletes it provided there are no reporting periods.
 	 * @param xmlParticipants
 	 */
-	@WebMethod
-	public gov.nih.nci.cabig.caaers.webservice.participant.CaaersServiceResponse deleteParticipant(@WebParam(name="Participants",
-            targetNamespace="http://webservice.caaers.cabig.nci.nih.gov/participant") Participants xmlParticipants);
+	public gov.nih.nci.cabig.caaers.webservice.participant.CaaersServiceResponse deleteParticipant(Participants xmlParticipants);
 
 
 }
