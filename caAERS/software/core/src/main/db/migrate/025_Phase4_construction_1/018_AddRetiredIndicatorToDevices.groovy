0b0397e3757e4ae7f1f57c8dcdb7320d14a490ee
Ion Olaru <ion.olaru@gmail.com>
2012-04-05 20:34:38 +0000

    https://tracker.nci.nih.gov/browse/CAAERS-5246
    SVN-Revision: 15489

---
+++ caAERS/software/core/src/main/db/migrate/025_Phase4_construction_1/018_AddRetiredIndicatorToDevices.groovy
@@ -0,0 +1,9 @@
+class AddRetiredIndicatorToOrganizations extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+        addColumn("devices","retired_indicator", "boolean", defaultValue: 0)
+    }
+
+    void down() {
+        dropColumn("devices", "retired_indicator")
+    }
+}
