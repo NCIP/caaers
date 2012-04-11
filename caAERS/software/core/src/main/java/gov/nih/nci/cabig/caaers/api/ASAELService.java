ae3c2c53ae2aa80d4467ee61d1c5c14f20a2a2d2
Ion Olaru <ion.olaru@gmail.com>
2012-04-11 15:21:39 +0000

    https://tracker.nci.nih.gov/browse/CAAERS-5252
    SVN-Revision: 15521

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/api/ASAELService.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/api/ASAELService.java
@@ -3,12 +3,17 @@ package gov.nih.nci.cabig.caaers.api;
 import gov.nih.nci.cabig.caaers.integration.schema.common.CaaersServiceResponse;
 import gov.nih.nci.cabig.caaers.webservice.asael.ASAELType;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
 
 /**
  * @author Ion C. Olaru
  *         Date: 4/2/12 -10:03 AM
  */
+@WebService(name = "ASAELServiceInterface", targetNamespace = "http://webservice.caaers.cabig.nci.nih.gov")
 public interface ASAELService {
 
-    public CaaersServiceResponse createOrUpdateASAEL(ASAELType inputMessage);
+    @WebMethod
+    public CaaersServiceResponse createOrUpdateASAEL(@WebParam(name = "asael", targetNamespace = "http://webservice.caaers.cabig.nci.nih.gov") ASAELType inputMessage);
 }
