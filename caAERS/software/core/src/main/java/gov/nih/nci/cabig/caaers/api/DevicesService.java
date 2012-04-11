bf61dddce4498c30c07c778004e5b33ca4bcc358
Ion Olaru <ion.olaru@gmail.com>
2012-04-11 15:21:20 +0000

    https://tracker.nci.nih.gov/browse/CAAERS-5252
    SVN-Revision: 15520

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/api/DevicesService.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/api/DevicesService.java
@@ -3,13 +3,18 @@ package gov.nih.nci.cabig.caaers.api;
 import gov.nih.nci.cabig.caaers.integration.schema.common.CaaersServiceResponse;
 import gov.nih.nci.cabig.caaers.integration.schema.devices.DevicesType;
 
-
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
 
 
 /**
  * @author Ion C. Olaru
  *         Date: 4/2/12 -10:03 AM
  */
+@WebService(name = "DevicesServiceInterface", targetNamespace = "http://webservice.caaers.cabig.nci.nih.gov")
 public interface DevicesService {
-    public CaaersServiceResponse createOrUpdateDevices(DevicesType inputMessage);
+
+    @WebMethod
+    public CaaersServiceResponse createOrUpdateDevices(@WebParam(name = "devices", targetNamespace = "http://webservice.caaers.cabig.nci.nih.gov") DevicesType inputMessage);
 }
