f942923401bc91cdf7a9fde6ad7ac8b739f3adfc
Biju Joseph <biju.joseph@semanticbits.com>
2012-04-18 14:46:29 +0000

    CAAERS-5228 -Invoke ASAELLOV CTEPESYS web service from ServiceMix on a nightly basis to retrieve newly created records and modified records.
    SVN-Revision: 15613

---
+++ caAERS/software/jbi/caaers2adeers-sync/core-camel-su/src/main/java/gov/nih/nci/cabig/caaers2adeers/test/MockMessageGenerator.java
@@ -0,0 +1,39 @@
+package gov.nih.nci.cabig.caaers2adeers.test;
+
+
+public class MockMessageGenerator {
+
+
+    public static String getAgentsRequest() {
+
+        return "<payload>" +
+                "<system>adeers</system>" +
+                "<request>" +
+                "<entity>agent</entity>" +
+                "<operation mode=\"async\" name=\"getAgentsLOV\">" +
+                "<criteria>" +
+                "<criterion  name=\"createdDate\">04-10-2001 14:17:38</criterion>" +
+                "<criterion name=\"lastUpdatedDate\">04-10-2001 14:17:38</criterion>" +
+                "</criteria>" +
+                "</operation>" +
+                "</request>" +
+                "</payload>";
+    }
+
+    public static String getStudySearchRequest() {
+
+        return "<payload>" +
+                "<system>adeers</system>" +
+                "<request>" +
+                "<entity>study</entity>" +
+                "<operation mode=\"sync\" name=\"searchStudy\">" +
+                "<criteria>" +
+                "<criterion  name=\"documentTitle\">Adjuvant Chemotherapy</criterion>" +
+                "<criterion  name=\"createdDate\">04-10-2001 14:17:38</criterion>" +
+                "<criterion name=\"lastUpdatedDate\">04-10-2001 14:17:38</criterion>" +
+                "</criteria>" +
+                "</operation>" +
+                "</request>" +
+                "</payload>";
+    }
+}
