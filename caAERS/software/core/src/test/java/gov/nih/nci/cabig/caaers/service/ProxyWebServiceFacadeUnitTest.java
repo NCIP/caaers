75807f4b5e76547ca57db21b2aabeafdd4252ff4
Kruttik Aggarwal <kruttikagarwal@gmail.com>
2012-04-21 06:23:48 +0000

    web service facade
    SVN-Revision: 15646

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/service/ProxyWebServiceFacadeUnitTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/service/ProxyWebServiceFacadeUnitTest.java
@@ -26,25 +26,25 @@ public class ProxyWebServiceFacadeUnitTest extends AbstractTestCase {
 		
 	}
 
-//	public void testSimpleSendAndReceive() throws Exception {
-//		StringBuffer sb = new StringBuffer();
-//		sb.append("<gen:GenericRequest xmlns:gen=\"http://webservice.caaers.cabig.nci.nih.gov/GenericProcessor/\">");
-//		sb.append("<payload correlationId=\"KRUTTIK_02\">");
-//		sb.append("<system>adeers</system>");
-//		sb.append("<request>");
-//		sb.append("<!--Optional:-->");
-//		sb.append("<entity>agent</entity>");
-//		sb.append("<operation name=\"getAgentsLOV\" mode=\"async\">");
-//		sb.append("<criteria>");
-//		sb.append("<!--1 or more repetitions:-->");
-//		sb.append("<criterion name=\"createdDate\">12-02-2002</criterion>");
-//		sb.append("</criteria>");
-//		sb.append("</operation>");
-//		sb.append("</request>");
-//		sb.append("</payload>");
-//		sb.append("</gen:GenericRequest>");
-//		System.out.println(proxyWebServiceFacade.simpleSendAndReceive(sb.toString()));
-//	}
+	public void testSimpleSendAndReceive() throws Exception {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<gen:GenericRequest xmlns:gen=\"http://webservice.caaers.cabig.nci.nih.gov/GenericProcessor/\">");
+		sb.append("<payload correlationId=\"KRUTTIK_02\">");
+		sb.append("<system>adeers</system>");
+		sb.append("<request>");
+		sb.append("<!--Optional:-->");
+		sb.append("<entity>agent</entity>");
+		sb.append("<operation name=\"getAgentsLOV\" mode=\"async\">");
+		sb.append("<criteria>");
+		sb.append("<!--1 or more repetitions:-->");
+		sb.append("<criterion name=\"createdDate\">12-02-2002</criterion>");
+		sb.append("</criteria>");
+		sb.append("</operation>");
+		sb.append("</request>");
+		sb.append("</payload>");
+		sb.append("</gen:GenericRequest>");
+		System.out.println(proxyWebServiceFacade.simpleSendAndReceive(sb.toString()));
+	}
 	
 	public void testSyncAgents() throws Exception{
 		System.out.println(proxyWebServiceFacade.syncAgents());
