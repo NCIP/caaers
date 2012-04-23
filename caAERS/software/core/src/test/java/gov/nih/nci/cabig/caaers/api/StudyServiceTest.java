5b8d24f54d58a2a50108e36163a9b3afa42bc85c
Ion Olaru <ion.olaru@gmail.com>
2012-04-23 15:43:13 +0000

    https://tracker.nci.nih.gov/browse/CAAERS-5298
    SVN-Revision: 15671

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/api/StudyServiceTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/api/StudyServiceTest.java
@@ -6,12 +6,10 @@ package gov.nih.nci.cabig.caaers.api;
 import gov.nih.nci.cabig.caaers.CaaersDbNoSecurityTestCase;
 import gov.nih.nci.cabig.caaers.dao.OrganizationDao;
 import gov.nih.nci.cabig.caaers.dao.ParticipantDao;
-import gov.nih.nci.cabig.caaers.domain.DateValue;
-import gov.nih.nci.cabig.caaers.domain.Organization;
-import gov.nih.nci.cabig.caaers.domain.Participant;
-import gov.nih.nci.cabig.caaers.domain.Study;
-import gov.nih.nci.cabig.caaers.domain.StudyParticipantAssignment;
-import gov.nih.nci.cabig.caaers.domain.StudySite;
+import gov.nih.nci.cabig.caaers.domain.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com>Joshua Phillips</a>
@@ -77,8 +75,50 @@ public class StudyServiceTest extends CaaersDbNoSecurityTestCase {
 
     }
 
+    private static Study createStudy(final String shortTitle) {
+        Study s = new LocalStudy();
+        s.setShortTitle(shortTitle);
+        s.setLongTitle(shortTitle);
+
+        OrganizationAssignedIdentifier i = new OrganizationAssignedIdentifier();
+        i.setType(OrganizationAssignedIdentifier.SPONSOR_IDENTIFIER_TYPE);
+        i.setValue("ABC-01");
+        s.addIdentifier(i);
+
+        Organization o = new LocalOrganization();
+        o.setNciInstituteCode("CTEP");
+        StudyFundingSponsor so = new StudyFundingSponsor();
+        so.setOrganization(o);
+        so.setStudy(s);
+
+        i.setOrganization(o);
+        s.addStudyFundingSponsor(so);
+
+        return s;
+    }
+
     public void testSearchAdEERSStudyInCaAERS() {
         StudyService svc = (StudyService) getApplicationContext().getBean("studyServiceAPI");
+
+        List<Study> studies = new ArrayList<Study>();
+        studies.add(createStudy("Short Title - 01"));
+        studies.add(createStudy("Short Title - 02"));
+        studies.get(0).getIdentifiers().get(0).setValue("ABC-01");
+        studies.get(0).setId(90);
+        studies.get(1).getIdentifiers().get(0).setValue("ABC-99");
+
+        List<Study> studiesCaAERS = new ArrayList<Study>();
+        studiesCaAERS.add(createStudy("Short Title caAERS - 01"));
+        studiesCaAERS.add(createStudy("Short Title caAERS - 02"));
+        studiesCaAERS.get(0).setId(90);
+        studiesCaAERS.get(0).getIdentifiers().get(0).setValue("ABC-01");
+        studiesCaAERS.get(0).setId(90);
+
+        studiesCaAERS.get(1).getIdentifiers().get(0).setValue("ABC-990");
+
+        svc.searchAdEERSStudiesInCaAERS(studies, studiesCaAERS);
+
+        assertEquals("UPDATED", studies.get(1).getStatus());
 
     }
 
