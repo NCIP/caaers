53dd7bb9e736c794a317a13fb6f5223f02021c69
Ion Olaru <ion.olaru@gmail.com>
2012-05-03 21:08:26 +0000

    https://tracker.nci.nih.gov/browse/CAAERS-5252
    SVN-Revision: 15826

---
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/service/synchronizer/StudyInterventionsSynchronizerTest.java
@@ -0,0 +1,50 @@
+package gov.nih.nci.cabig.caaers.service.synchronizer;
+
+import gov.nih.nci.cabig.caaers.AbstractTestCase;
+import gov.nih.nci.cabig.caaers.domain.Fixtures;
+import gov.nih.nci.cabig.caaers.domain.OtherIntervention;
+import gov.nih.nci.cabig.caaers.domain.Study;
+import gov.nih.nci.cabig.caaers.domain.StudyTherapyType;
+
+import java.util.ArrayList;
+
+/**
+ * @author Ion C. Olaru
+ *         Date: 5/3/12 -4:56 PM
+ */
+public class StudyInterventionsSynchronizerTest extends AbstractTestCase {
+
+    Study dbStudy;
+    Study xmlStudy;
+    StudyInterventionSynchronizer studyInterventionSynchronizer;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        xmlStudy = Fixtures.createStudy("xmlStudy");
+        dbStudy = Fixtures.createStudy("dbStudy");
+        studyInterventionSynchronizer = new StudyInterventionSynchronizer();
+
+    }
+
+    public void testSyncOtherInterventions() {
+        xmlStudy.setOtherInterventions(new ArrayList<OtherIntervention>());
+        xmlStudy.getOtherInterventions().add(new OtherIntervention());
+        xmlStudy.getOtherInterventions().get(0).setName("N1");
+        xmlStudy.getOtherInterventions().get(0).setDescription("D1");
+        xmlStudy.getOtherInterventions().get(0).setStudyTherapyType(StudyTherapyType.OTHER);
+
+        assertEquals(0, dbStudy.getOtherInterventions().size());
+        assertEquals(1, xmlStudy.getOtherInterventions().size());
+
+        studyInterventionSynchronizer.migrate(dbStudy, xmlStudy, null);
+
+        assertEquals(1, dbStudy.getOtherInterventions().size());
+        assertEquals(1, xmlStudy.getOtherInterventions().size());
+
+        assertEquals("N1", dbStudy.getOtherInterventions().get(0).getName());
+        assertEquals("D1", dbStudy.getOtherInterventions().get(0).getDescription());
+        assertEquals("OTHER", dbStudy.getOtherInterventions().get(0).getStudyTherapyType().getName());
+
+    }
+}
