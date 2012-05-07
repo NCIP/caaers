c41449604255da3099fcc71e4ed8538fea85c902
Kruttik Aggarwal <kruttikagarwal@gmail.com>
2012-05-07 15:15:15 +0000

    Added a cron job for garbage collection.
    SVN-Revision: 15863

--- caAERS/software/jbi/caaers2adeers-sync/core-camel-su/src/main/java/gov/nih/nci/cabig/caaers2adeers/CronJobRouteBuilder.java
+++ caAERS/software/jbi/caaers2adeers-sync/core-camel-su/src/main/java/gov/nih/nci/cabig/caaers2adeers/CronJobRouteBuilder.java
@@ -5,8 +5,12 @@ import static gov.nih.nci.cabig.caaers2adeers.track.Tracker.track;
 import gov.nih.nci.cabig.caaers2adeers.cronjob.EntityOperation;
 import gov.nih.nci.cabig.caaers2adeers.track.FileTracker;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.xml.XPathBuilder;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +23,9 @@ public class CronJobRouteBuilder implements InitializingBean {
     @Autowired
     private FileTracker fileTracker;
 
-
+    private static final String DEFAULT_GARBAGE_COLL_CRON_EXPR_VAL="0+0/2+*+*+*+?";
+    private String garbageCollectionCronExpression;
+    
     Map<EntityOperation, String> cronExpressions;
 
     public void afterPropertiesSet() throws Exception {
@@ -28,6 +34,9 @@ public class CronJobRouteBuilder implements InitializingBean {
     			cronExpressions.put(entityOperation, entityOperation.getCronJobExpression());
     		}
     	}
+    	if(garbageCollectionCronExpression == null){
+    		garbageCollectionCronExpression=DEFAULT_GARBAGE_COLL_CRON_EXPR_VAL;
+		}
     }
 
     public void configure(Caaers2AdeersRouteBuilder routeBuilder){
@@ -43,10 +52,22 @@ public class CronJobRouteBuilder implements InitializingBean {
                     .to(fileTracker.fileURI(REQUEST_RECEIVED))
                     .to("direct:adEERSRequestSink");
     	}
+    	
+    	routeBuilder.from("quartz://c2a/gc?cron="+garbageCollectionCronExpression)
+			        .process(new Processor() {
+						public void process(Exchange exchange) throws Exception {
+							System.gc();
+						}
+					}).to("log:"+new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(new Date())+"gc run complete");
     }
 
 	public void setCronExpressions(Map<EntityOperation, String> cronExpressions) {
 		this.cronExpressions = cronExpressions;
+	}
+
+	public void setGarbageCollectionCronExpression(
+			String garbageCollectionCronExpression) {
+		this.garbageCollectionCronExpression = garbageCollectionCronExpression;
 	}
 
 }
