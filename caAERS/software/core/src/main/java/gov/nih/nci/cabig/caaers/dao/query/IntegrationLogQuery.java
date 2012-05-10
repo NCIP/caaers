69f04a575e1a3bec55b4637ad9838cf127e1f2ef
Biju Joseph <biju.joseph@semanticbits.com>
2012-05-10 13:21:57 +0000

    fixing tests
    SVN-Revision: 15926

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/dao/query/IntegrationLogQuery.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/dao/query/IntegrationLogQuery.java
@@ -9,63 +9,48 @@ public class IntegrationLogQuery extends AbstractQuery {
 
     private static final String queryString = "SELECT distinct il from IntegrationLog il ";
 
-    private static final String ENTITY = "entity";
-
-    private static final String CORRELATION_ID = "correlationId";
-
-    private static final String NOTES = "notes";
-
-    private static final String SYNCH_STATUS = "synchStatus";
-    
-    private static final String OPERATION = "operation";
-    
-    private static final String LOGGED_ON = "loggedOn";
-    
-    private static final String INTEGRATION_LOG_ALIAS = "il";
-    
-    private static final String STATUS = "status";
-    
     public IntegrationLogQuery() {
         super(queryString);
         orderBy("il.loggedOn desc");
     }
     
     public void joinIntegrationLogDetail() {
-        join(INTEGRATION_LOG_ALIAS+".integrationLogDetails as ild");
+        join("il.integrationLogDetails as ild");
     }
     
     public void filterByCorrelationId(String correlationId) {
-    	andWhere("lower(correlationId) LIKE :" + ":CORRELATION_ID");
-    	setParameter("CORRELATION_ID", correlationId.toLowerCase());
+    	andWhere("lower(correlationId) LIKE :cid");
+    	setParameter("cid", correlationId.toLowerCase());
     }
 
     public void filterByEntity(final String value) {
     	if(!StringUtilities.isBlank(value)) {
 	        String searchString = "%" + value.toLowerCase() + "%";
-	        andWhere("lower(entity) LIKE :" + ENTITY);
-	        setParameter(ENTITY, searchString);
+	        andWhere("lower(entity) LIKE :et");
+	        setParameter("et", searchString);
     	}
     }
 
     public void filterBySynchStatus(final SynchStatus synchStatus) {
-        andWhere("synchStatus LIKE :" + SYNCH_STATUS);
-        setParameter(SYNCH_STATUS, synchStatus);
+        andWhere("synchStatus LIKE :sys");
+        setParameter("sys", synchStatus);
     }
     
     public void filterByOperation(final String value) {
     	if(!StringUtilities.isBlank(value)) {
 	        String searchString = "%" + value.toLowerCase() + "%";
-	        andWhere("lower(operation) LIKE :" + OPERATION);
-	        setParameter(OPERATION, searchString);
+	        andWhere("lower(operation) LIKE :oper" );
+	        setParameter("oper", searchString);
     	}
     }
     
     public void filterByLoggedOn(Date loggedOn, String operator) {
-    	andWhere("loggedOn "+operator+" :LOGGED_ON");
-    		setParameter("LOGGED_ON", loggedOn);
+    	andWhere("loggedOn "+operator+" :lgdon");
+    		setParameter("lgdon", loggedOn);
     }
     
     public void filterByLoggedOnStartDateAndEndDate(Date startDate, Date endDate) {
+
     	if(startDate != null){
     		andWhere("loggedOn >= " + " :START_DATE");
     		setParameter("START_DATE", startDate);
@@ -86,8 +71,8 @@ public class IntegrationLogQuery extends AbstractQuery {
     }
     
     public void excludeHavingSynchStatus(final SynchStatus synchStatus) {
-        andWhere("synchStatus != :" + SYNCH_STATUS);
-        setParameter(SYNCH_STATUS, synchStatus);
+        andWhere("synchStatus != :synst" );
+        setParameter("synst", synchStatus);
     }
     
 }
