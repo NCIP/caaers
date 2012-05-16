e3d746d35c7300ff42d8709071fb38b9ddf52d30
Ion Olaru <ion.olaru@gmail.com>
2012-05-16 18:19:25 +0000

    https://tracker.nci.nih.gov/browse/CAAERS-5371
    SVN-Revision: 15958

---
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/domain/repository/PriorTherapyRepositoryTest.java
@@ -0,0 +1,39 @@
+package gov.nih.nci.cabig.caaers.domain.repository;
+
+import gov.nih.nci.cabig.caaers.DaoTestCase;
+
+import java.util.List;
+
+/**
+ * @author Ion C. Olaru
+ *         Date: 5/16/12 -2:05 PM
+ */
+public class PriorTherapyRepositoryTest extends DaoTestCase {
+    PriorTherapyRepository priorTherapyRepository;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        priorTherapyRepository = (PriorTherapyRepository)getDeployedApplicationContext().getBean("priorTherapyRepository");
+    }
+
+    public void testGetAll() {
+        List l = priorTherapyRepository.getAll(false, false);
+        assertEquals(4, l.size());
+    }
+
+    public void testGetAllActive() {
+        List l = priorTherapyRepository.getAll(false, true);
+        assertEquals(2, l.size());
+    }
+
+    public void testGetAllValid() {
+        List l = priorTherapyRepository.getAll(true, false);
+        assertEquals(3, l.size());
+    }
+
+    public void testGetAllActiveAndValid() {
+        List l = priorTherapyRepository.getAll(true, true);
+        assertEquals(1, l.size());
+    }
+}
