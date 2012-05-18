118ca897e8a39cb3489a9fd8eeb80bef0e727d1a
Biju Joseph <biju.joseph@semanticbits.com>
2012-05-18 21:40:45 +0000

    CAAERS-5381 - OOPS error on solicited AE page after adding an additional column to the evalutation period types
    SVN-Revision: 16011

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/api/impl/OrganizationManagementServiceImpl.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/api/impl/OrganizationManagementServiceImpl.java
@@ -9,7 +9,9 @@ import gov.nih.nci.cabig.caaers.domain.repository.OrganizationRepository;
 import gov.nih.nci.cabig.caaers.service.migrator.OrganizationMigrator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -90,20 +92,58 @@ public class OrganizationManagementServiceImpl implements OrganizationManagement
 	}
 
 	public List<ProcessingOutcome> createOrUpdateOrganizations(List<Organization> organizations) {
-		List<ProcessingOutcome> outcomes = new ArrayList<ProcessingOutcome>();
-        int flushSize = 1000;
+        long start = System.currentTimeMillis();
+        
+        List<ProcessingOutcome> outcomes = new ArrayList<ProcessingOutcome>();
+
+        List<Organization> allOrgs = new ArrayList<Organization>();
+        allOrgs.addAll(organizationDao.getAll());
+        if(logger.isInfoEnabled()) {
+            logger.info("Time taken by  createOrUpdateOrganizations to load all orgs : " + ( ( System.currentTimeMillis() - start)/1000) + " seconds" );
+        }
+        Map<String, Organization> orgMap = new HashMap<String, Organization>(allOrgs.size());
+        for(Organization o: allOrgs){
+            orgMap.put(o.getNciInstituteCode(), o);
+        }
+        
+        for(Organization o : organizations){
+           Organization dbOrganization = orgMap.get(o.getNciInstituteCode());
+           if(dbOrganization == null){
+               dbOrganization = new LocalOrganization(); 
+           }
+           organizationMigrator.migrate(o, dbOrganization, null);
+        }
+        
+        long afterMigrate = System.currentTimeMillis();
+
         int i = 0;
-		for (Organization organization:organizations) {
-            outcomes.add(createOrUpdateOrganization(organization));
-            if(i > flushSize){
-                i = 0;
-                organizationDao.flush();
-                organizationDao.clearSession();
-            }
-            i++;
-		}
+        int flushSize = 1000;
+        for(Organization organization : allOrgs){
+           organizationDao.save(organization);
+           ProcessingOutcome outcome =  Helper.createOutcome(Organization.class, organization.getNciInstituteCode(), false,
+                    "Organization with NCI Code : " +
+                    organization.getNciInstituteCode() + ", id : " + organization.getId() + " modified/created" );
+
+           outcomes.add(outcome);
+
+           if(i > flushSize){
+               organizationDao.flush();
+               organizationDao.clearSession();
+               i = 0;
+           }
+           i++;
+        }
+
         organizationDao.flush();
         organizationDao.clearSession();
+        
+        long end = System.currentTimeMillis();
+        
+        if(logger.isInfoEnabled()) {
+            logger.info("Time taken by  createOrUpdateOrganizations : to save all orgs " + ( ( end - afterMigrate)/1000) + " seconds" );
+            logger.info("Total Time taken by  createOrUpdateOrganizations : " + ( ( end - start)/1000) + " seconds" );
+        }
+        
 		return outcomes;
 	}
 
