f6b5b644029311da14337ea6daad317d7d627cc1
Biju Joseph <biju.joseph@semanticbits.com>
2012-05-31 17:10:27 +0000

    CIP and CTEP removed from the list of pre defined IND orgs, as they will come in as part of org import.
    SVN-Revision: 16185

--- caAERS/software/core/src/main/db/migrate/026_Phase4_construction_3/012_AddAdeersIndHolderNames.groovy
+++ caAERS/software/core/src/main/db/migrate/026_Phase4_construction_3/012_AddAdeersIndHolderNames.groovy
@@ -2,8 +2,6 @@ class PopulateAdeersIndHolderNames extends edu.northwestern.bioinformatics.berin
 
     void up() {
         insert('organizations', [ id: -101, nci_institute_code: "ADEERS-101", name: "Company"], primaryKey: false)
-        insert('organizations', [ id: -102, nci_institute_code: "ADEERS-102", name: "CIP"], primaryKey: false)
-        insert('organizations', [ id: -103, nci_institute_code: "ADEERS-103", name: "CTEP"], primaryKey: false)
         insert('organizations', [ id: -104, nci_institute_code: "ADEERS-104", name: "Investigator"], primaryKey: false)
         insert('organizations', [ id: -105, nci_institute_code: "ADEERS-104", name: "Site"], primaryKey: false)
     }
