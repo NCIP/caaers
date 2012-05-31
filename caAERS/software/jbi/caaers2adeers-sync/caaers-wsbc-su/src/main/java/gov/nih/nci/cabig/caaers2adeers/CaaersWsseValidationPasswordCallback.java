0419ac897a67e9406bed52e5e300eaf1b405184c
Kruttik Aggarwal <kruttikagarwal@gmail.com>
2012-05-31 15:09:26 +0000

    Fixed username-password check. Related to CAAERS-5417
    SVN-Revision: 16178

---
+++ caAERS/software/jbi/caaers2adeers-sync/caaers-wsbc-su/src/main/java/gov/nih/nci/cabig/caaers2adeers/CaaersWsseValidationPasswordCallback.java
@@ -0,0 +1,42 @@
+package gov.nih.nci.cabig.caaers2adeers;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import java.io.IOException;
+
+/**
+ * Will provide the password for AdEERSWebservice and caAERS Webservice
+ * 
+ * @author Biju Joseph
+ */
+public class CaaersWsseValidationPasswordCallback implements CallbackHandler {
+	private String caaersWSUser;
+	private String caaersWSPassword;
+
+	public void handle(Callback[] callbacks) throws IOException,
+			UnsupportedCallbackException {
+		org.apache.ws.security.WSPasswordCallback pc = (org.apache.ws.security.WSPasswordCallback) callbacks[0];
+		if (equals(pc.getIdentifier(), caaersWSUser)) {
+			if (!equals(pc.getPassword(), caaersWSPassword)) {
+				throw new IOException("invalid password");
+			}
+		}else{
+			throw new IOException("invalid username");
+		}
+	}
+
+	private boolean equals(String a, String b) {
+		if (a == null || b == null)
+			return false;
+		return a.equals(b);
+	}
+
+	public void setCaaersWSUser(String caaersWSUser) {
+		this.caaersWSUser = caaersWSUser;
+	}
+
+	public void setCaaersWSPassword(String caaersWSPassword) {
+		this.caaersWSPassword = caaersWSPassword;
+	}
+}
