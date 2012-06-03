c6694fd39520bc8119594f8bd7a13324addfa171
Kruttik Aggarwal <kruttikagarwal@gmail.com>
2012-06-03 16:33:19 +0000

    CAAERS-5498, CAAERS-5497
    SVN-Revision: 16246

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/service/synchronizer/StudyInterventionSynchronizer.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/service/synchronizer/StudyInterventionSynchronizer.java
@@ -1,11 +1,16 @@
 package gov.nih.nci.cabig.caaers.service.synchronizer;
 
+import gov.nih.nci.cabig.caaers.domain.AbstractMutableRetireableDomainObject;
 import gov.nih.nci.cabig.caaers.domain.OtherIntervention;
 import gov.nih.nci.cabig.caaers.domain.Study;
+import gov.nih.nci.cabig.caaers.domain.StudyAgent;
+import gov.nih.nci.cabig.caaers.domain.StudyAgentINDAssociation;
+import gov.nih.nci.cabig.caaers.domain.StudyDevice;
 import gov.nih.nci.cabig.caaers.service.DomainObjectImportOutcome;
 import gov.nih.nci.cabig.caaers.service.migrator.Migrator;
 import org.apache.commons.collections.CollectionUtils;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,19 +22,39 @@ import java.util.Set;
 public class StudyInterventionSynchronizer implements Migrator<Study> {
 
     public void migrate(Study dest, Study src, DomainObjectImportOutcome<Study> studyDomainObjectImportOutcome) {
-        List<OtherIntervention> otherInterventions = src.getOtherInterventions();
-        if (CollectionUtils.isEmpty(otherInterventions)) return;
-        Set<String> destInterventionsSet = new HashSet<String>();
-
-        for (OtherIntervention otherIntervention : dest.getOtherInterventions()) {
-            destInterventionsSet.add(otherIntervention.getHashKey());
-        }
-
-        for (OtherIntervention otherIntervention : otherInterventions) {
-            if (destInterventionsSet.add(otherIntervention.getHashKey())) {
-                dest.addOtherIntervention(otherIntervention);
-            }
-        }
+    	HashMap<String, OtherIntervention> map = new HashMap<String, OtherIntervention>();
+		for(OtherIntervention otherIntervention : dest.getActiveOtherInterventions()) {
+			map.put(otherIntervention.getName(), otherIntervention);
+		}
+		
+		for(OtherIntervention xmlOtherIntervention : src.getOtherInterventions()){
+			OtherIntervention otherIntervention = map.remove(xmlOtherIntervention.getName());
+			if(otherIntervention == null){
+				//newly added one, so add it to study
+				dest.addOtherIntervention(xmlOtherIntervention);
+				continue;
+			}
+			//Update- do nothing
+			
+		}
+		
+		//now soft delete, all the ones not present in XML Study
+		AbstractMutableRetireableDomainObject.retire(map.values());
+		
+		
+//        List<OtherIntervention> otherInterventions = src.getOtherInterventions();
+//        if (CollectionUtils.isEmpty(otherInterventions)) return;
+//        Set<String> destInterventionsSet = new HashSet<String>();
+//
+//        for (OtherIntervention otherIntervention : dest.getOtherInterventions()) {
+//            destInterventionsSet.add(otherIntervention.getHashKey());
+//        }
+//
+//        for (OtherIntervention otherIntervention : otherInterventions) {
+//            if (destInterventionsSet.add(otherIntervention.getHashKey())) {
+//                dest.addOtherIntervention(otherIntervention);
+//            }
+//        }
     }
 
 }
