cf6bb43eed7f35fc2a88c4c9126a3fc2a19317ff
Ion Olaru <ion.olaru@gmail.com>
2012-06-04 21:05:43 +0000

    SVN-Revision: 16271

--- caAERS/software/web/src/main/webapp/js/dropDownActions.js
+++ caAERS/software/web/src/main/webapp/js/dropDownActions.js
@@ -2,12 +2,35 @@
 // STUDY ACTIONS
 
 function showDashboardStudiesMenuOptions(_element, _roles, _ssi, _id, _complete) {
+    var rolesSize = _roles.length;
+
+    var _optionDetails = "";
+    if (hasRole('supplemental_study_information_manager') || (_complete == 'true' && hasRole('study_qa_manager')) || (_complete == 'false' && hasRole('study_creator')))
+        _optionDetails = "<li><a class='submitter-blue' href='#' onclick='doEdit(\"" + _id + "\")'>Edit study details</a></li>";
+
+    var _optionSites = "";
+    if (hasRole('study_site_participation_administrator'))
+        _optionSites = "<li><a class='submitter-blue' href='#' onclick='addStudySite(\"" + _id + "\", " + _complete + ")'>Add study site</a></li>";
+
+    var _optionInvestigators = "";
+    if (hasRole('study_team_administrator'))
+        _optionInvestigators = "<li><a class='submitter-blue' href='#' onclick='addStudyInvestigators(\"" + _id + "\")'>Manage investigators</a></li>";
+
+    var _optionPersonnel = "";
+    if (hasRole('study_team_administrator'))
+        _optionPersonnel = "<li><a class='submitter-blue' href='#' onclick='addStudyPersonnel(\"" + _id + "\")'>Manage personnel</a></li>";
+
+    var _optionRegisterSubject = "";
+    if (hasRole('registrar') || hasRole('subject_manager'))
+        _optionRegisterSubject = "<li><a class='submitter-blue' href='#' onclick='doRegisterSubject(\"" + _id + "\")'>Register subject</a></li>";
+
     var html = "<div><ul style='font-family:tahoma;'>" +
-            "<li><a class='submitter-blue' href='#' onclick='doEdit(\"" + _id + "\")'>Edit study details</a></li>" +
-            "<li><a class='submitter-blue' href='#' onclick='addStudySite(\"" + _id + "\", " + _complete + ")'>Add Study Site</a></li>" +
-            "<li><a class='submitter-blue' href='#' onclick='addStudyInvestigators(\"" + _id + "\")'>Manage Investigators</a></li>" +
-            "<li><a class='submitter-blue' href='#' onclick='addStudyPersonnel(\"" + _id + "\")'>Manage Personnel</a></li>" +
-            "<li><a class='submitter-blue' href='#' onclick='doRegisterSubject(\"" + _id + "\")'>Register Subject</a></li>" +
+            // "<li><a class='submitter-blue' href='#'>" + rolesSize + "</a></li>" +
+            _optionDetails +
+            _optionSites +
+            _optionInvestigators +
+            _optionPersonnel +
+            _optionRegisterSubject +
             "<li><a class='submitter-blue' href='#' onclick='doUpdate(\"" + _ssi + "\", \"" + _id + "\")'>Synchronize with CTEP</a></li>" +
             "</ul></div>";
     jQuery(_element).menu({
