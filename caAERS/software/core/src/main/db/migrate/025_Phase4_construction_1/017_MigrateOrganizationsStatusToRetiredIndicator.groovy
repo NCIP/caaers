134b2f9906fe042231f9fa2f2ceb8943257bd419
Biju Joseph <biju.joseph@semanticbits.com>
2012-06-15 18:45:39 +0000

    fixing bugs in oracle-groovy
    SVN-Revision: 16513

--- caAERS/software/core/src/main/db/migrate/025_Phase4_construction_1/017_MigrateOrganizationsStatusToRetiredIndicator.groovy
+++ caAERS/software/core/src/main/db/migrate/025_Phase4_construction_1/017_MigrateOrganizationsStatusToRetiredIndicator.groovy
@@ -1,7 +1,15 @@
 class MigrateOrganizationsStatusToRetiredIndicator extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-    	 execute("update organizations set retired_indicator = 0 where status = 'AC'");
-    	  execute("update organizations set retired_indicator = 1 where status = 'IN'");
+
+        if (databaseMatches('oracle')) {
+            execute("update organizations set retired_indicator = 0 where status = 'AC'");
+            execute("update organizations set retired_indicator = 1 where status = 'IN'");
+        }
+        if (databaseMatches('postgresql')){
+            execute("update organizations set retired_indicator = false where status = 'AC'");
+            execute("update organizations set retired_indicator = true where status = 'IN'");
+        }
+
         dropColumn("organizations","status")
 		
     }
