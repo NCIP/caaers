e208468747f3e616740cf0ad9290176e082df3a4
Biju Joseph <biju.joseph@semanticbits.com>
2012-06-26 17:42:24 +0000

    wrong groovy fixing
    SVN-Revision: 16598

--- caAERS/software/core/src/main/db/migrate/027_Phase4_construction_4/005_UpdateOrganizationType.groovy
+++ caAERS/software/core/src/main/db/migrate/027_Phase4_construction_4/005_UpdateOrganizationType.groovy
@@ -1,10 +1,10 @@
 class UpdateOrganizationType extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-        execute("update organizations  set org_type = 'ITN';");
+        execute("update organizations  set org_type = 'ITN' where org_type = 'ITN';");
     }
 
     void down() {
-        execute("update organizations  set org_type = null;");
+        execute("update organizations  set org_type = null where org_type is null';");
     }
 }
 
