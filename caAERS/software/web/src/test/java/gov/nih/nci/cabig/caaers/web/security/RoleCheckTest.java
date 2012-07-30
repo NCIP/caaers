32b24153b2adcb156331acd2d954cb9a0cb3a7be
Biju Joseph <biju.joseph@semanticbits.com>
2012-07-30 20:29:11 +0000

    Impoving Code Coverage
    SVN-Revision: 16877

---
+++ caAERS/software/web/src/test/java/gov/nih/nci/cabig/caaers/web/security/RoleCheckTest.java
@@ -0,0 +1,31 @@
+package gov.nih.nci.cabig.caaers.web.security;
+
+import gov.nih.nci.cabig.caaers.AbstractTestCase;
+import gov.nih.nci.cabig.caaers.security.SecurityUtils;
+import org.acegisecurity.Authentication;
+import org.acegisecurity.GrantedAuthority;
+import org.easymock.EasyMock;
+
+/**
+ * @author: Biju Joseph
+ */
+public class RoleCheckTest extends AbstractTestCase {
+    public void setUp() throws Exception {
+
+    }
+
+    public void testCheckAuthorizationForObjectId() throws Exception {
+        RoleCheck rc = new RoleCheck();
+        GrantedAuthority ga1 = registerMockFor(GrantedAuthority.class);
+        EasyMock.expect(ga1.getAuthority()).andReturn("r1").anyTimes();
+
+        replayMocks();
+        Authentication a =  SecurityUtils.createAuthentication("x", "y", ga1);
+        boolean authorized =  rc.checkAuthorizationForObjectId(a, "r1",  null);
+        assertTrue(authorized);
+        authorized =  rc.checkAuthorizationForObjectId(a, "rz1,rc2",  null);
+        assertFalse(authorized);
+        verifyMocks();
+
+    }
+}
