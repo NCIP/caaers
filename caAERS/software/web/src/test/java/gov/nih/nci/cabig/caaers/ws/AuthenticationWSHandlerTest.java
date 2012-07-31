6a4099fa41f90b513b124ac79ff55f621a698792
Biju Joseph <biju.joseph@semanticbits.com>
2012-07-31 21:13:18 +0000

    Impoving Code Coverage
    SVN-Revision: 16896

---
+++ caAERS/software/web/src/test/java/gov/nih/nci/cabig/caaers/ws/AuthenticationWSHandlerTest.java
@@ -0,0 +1,14 @@
+package gov.nih.nci.cabig.caaers.ws;
+
+import junit.framework.TestCase;
+
+/**
+ * @author: Biju Joseph
+ */
+public class AuthenticationWSHandlerTest extends TestCase {
+
+    public void testHandle() throws Exception {
+          assertTrue(true);
+    }
+
+}
