c7ee0b9d12370ce499cc18b8e17b2fe3614af06f
Kruttik Aggarwal <kruttikagarwal@gmail.com>
2012-07-31 15:12:06 +0000

    Test case improvement.
    SVN-Revision: 16892

---
+++ caAERS/software/web/src/test/java/gov/nih/nci/cabig/caaers/web/TaskResolverControllerTest.java
@@ -0,0 +1,59 @@
+package gov.nih.nci.cabig.caaers.web;
+
+
+import gov.nih.nci.cabig.caaers.AbstractTestCase;
+import gov.nih.nci.cabig.caaers.security.CaaersSecurityFacade;
+import gov.nih.nci.cabig.caaers.security.SecurityTestUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.acegisecurity.Authentication;
+import org.easymock.classextension.EasyMock;
+import org.springframework.web.servlet.ModelAndView;
+
+public class TaskResolverControllerTest extends AbstractTestCase {
+	
+	private TaskResolverController taskResolverController;
+	private CaaersSecurityFacade caaersSecurityFacade;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		SecurityTestUtils.switchToSuperuser();
+		taskResolverController = new TaskResolverController();
+		caaersSecurityFacade = registerMockFor(CaaersSecurityFacade.class);
+		taskResolverController.setCaaersSecurityFacade(caaersSecurityFacade);
+	}
+	
+	public void testHandleRequestInternalNull() {
+		taskResolverController.setUrls(new ArrayList<String>());
+		assertNull(taskResolverController.handleRequestInternal(null, null));
+		
+		taskResolverController.setUrls(Arrays.asList(new String[]{"abc"}));
+		taskResolverController.setObjectPrivilegeMap(new HashMap<String, String>());
+		assertNull(taskResolverController.handleRequestInternal(null, null));
+		
+		taskResolverController.setObjectPrivilegeMap(new HashMap<String, String>() {{
+			   put("abc", "pqr:xyz");
+			}});
+		EasyMock.expect(caaersSecurityFacade.checkAuthorization(EasyMock.isA(Authentication.class), EasyMock.isA(String.class), EasyMock.isA(String.class))).andReturn(false);
+		replayMocks();
+		assertNull(taskResolverController.handleRequestInternal(null, null));
+		verifyMocks();
+	}
+	
+	public void testHandleRequestInternal() {
+		taskResolverController.setUrls(Arrays.asList(new String[]{"abc"}));
+		taskResolverController.setObjectPrivilegeMap(new HashMap<String, String>() {{
+			   put("abc", "pqr:xyz");
+			}});
+		EasyMock.expect(caaersSecurityFacade.checkAuthorization(EasyMock.isA(Authentication.class), EasyMock.isA(String.class), EasyMock.isA(String.class))).andReturn(true);
+		replayMocks();
+		ModelAndView mv = taskResolverController.handleRequestInternal(null, null);
+		assertEquals("redirect:abc", mv.getViewName());
+		verifyMocks();
+	}
+
+}
