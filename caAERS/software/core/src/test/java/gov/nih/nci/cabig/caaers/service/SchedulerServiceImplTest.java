f2c2cfc941c8a4d2c3c37d936d94614d5a6e580a
Biju Joseph <biju.joseph@semanticbits.com>
2012-08-01 01:17:00 +0000

    Not a good testcase - so disabling it.
    SVN-Revision: 16900

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/service/SchedulerServiceImplTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/service/SchedulerServiceImplTest.java
@@ -2,13 +2,13 @@ package gov.nih.nci.cabig.caaers.service;
 
 import gov.nih.nci.cabig.caaers.CaaersTestCase;
 /**
- * 
+ *
  * @author Biju Joseph
  *
  */
 public class SchedulerServiceImplTest extends CaaersTestCase {
-	
-	
+
+
 	SchedulerServiceImpl scheduler;
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -16,9 +16,9 @@ public class SchedulerServiceImplTest extends CaaersTestCase {
 	}
 
 	public void testGetScheduler() throws Exception{
-		if(true){
-			//no point of running this from Hudson, 
-			//as loading of scheduler is tested otherwise itself. 
+		if(false){
+			//no point of running this from Hudson,
+			//as loading of scheduler is tested otherwise itself.
 		assertNotNull(scheduler);
 		assertNotNull(scheduler.getScheduler());
 		System.out.println("sleeping...");
