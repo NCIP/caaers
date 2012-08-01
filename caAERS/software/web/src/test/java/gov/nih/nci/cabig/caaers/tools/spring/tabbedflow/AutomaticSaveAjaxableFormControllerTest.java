5e3ab54d938c984b7a7fb66df805221bd80e7f6e
Kruttik Aggarwal <kruttikagarwal@gmail.com>
2012-08-01 15:21:09 +0000

    Test case improvement.
    SVN-Revision: 16902

--- caAERS/software/web/src/test/java/gov/nih/nci/cabig/caaers/tools/spring/tabbedflow/AutomaticSaveAjaxableFormControllerTest.java
+++ caAERS/software/web/src/test/java/gov/nih/nci/cabig/caaers/tools/spring/tabbedflow/AutomaticSaveAjaxableFormControllerTest.java
@@ -3,6 +3,9 @@ package gov.nih.nci.cabig.caaers.tools.spring.tabbedflow;
 import gov.nih.nci.cabig.caaers.AbstractTestCase;
 import gov.nih.nci.cabig.ctms.dao.MutableDomainObjectDao;
 import gov.nih.nci.cabig.ctms.domain.MutableDomainObject;
+import gov.nih.nci.cabig.ctms.web.tabs.Flow;
+import gov.nih.nci.cabig.ctms.web.tabs.FlowFactory;
+import gov.nih.nci.cabig.ctms.web.tabs.Tab;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,13 +22,41 @@ import org.springframework.web.servlet.ModelAndView;
 
 public class AutomaticSaveAjaxableFormControllerTest extends AbstractTestCase {
 	
-	AutomaticSaveAjaxableFormController automaticSaveAjaxableFormController;
-	MockHttpServletRequest request;
-	MockHttpServletResponse response;
+	private AutomaticSaveAjaxableFormController automaticSaveAjaxableFormController;
+	private MockHttpServletRequest request;
+	private MockHttpServletResponse response;
+	private Errors errors;
+	private WorkFlowTab firstTab;
+	private WorkFlowTab secondTab;
+	private Object command;
 	
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		errors = registerMockFor(Errors.class);
+		firstTab = new WorkFlowTab(){
+			@Override
+			public Map referenceData(Object command) {
+				return new HashMap(){{put("first tab", "test");}};
+			}
+			
+			@Override
+			public ModelAndView postProcessAsynchronous(
+					HttpServletRequest request, Object command, Errors error)
+					throws Exception {
+				ModelAndView mv = new ModelAndView("some_name");
+				mv.getModel().put("ajax", "test");
+				return mv;
+			}
+		};
+		secondTab = new WorkFlowTab(){
+			@Override
+			public Map referenceData(Object command) {
+				return new HashMap(){{put("second tab", "test");}};
+			}
+			
+		};
+		command = new Object();
 		request = new MockHttpServletRequest();
 		response = new MockHttpServletResponse();
 		automaticSaveAjaxableFormController = new AutomaticSaveAjaxableFormController() {
@@ -49,20 +80,54 @@ public class AutomaticSaveAjaxableFormControllerTest extends AbstractTestCase {
 				// TODO Auto-generated method stub
 				return null;
 			}
+			
+			@Override
+			public FlowFactory getFlowFactory() {
+				return new FlowFactory() {
+					public Flow createFlow(Object cmd) {
+						
+		            	/**
+		            	 * Third level tabs are secured now , Any changes in this flow needs to reflect in 
+		            	 * applicationContext-web-security.xml <util:map id="tabObjectPrivilegeMap"> 
+		            	 */
+						Flow flow = new Flow("Test Flow");
+						flow.addTab(firstTab);
+						flow.addTab(secondTab);
+						return flow;
+					}
+				};
+			}
+			
+			@Override
+		    protected boolean shouldSave(HttpServletRequest request, Object command, Tab tab) {
+		        return  false;
+		    }
 		};
+		automaticSaveAjaxableFormController.setCommandClass(Object.class);
 	}
 
-	public void testHandleRequestInternalHttpServletRequestHttpServletResponse() {
-//		fail("Not yet implemented");
+	public void testHandleRequestInternalHttpServletRequestHttpServletResponse() throws Exception{
+		ModelAndView mv = automaticSaveAjaxableFormController.handleRequestInternal(request, response);
+		assertNotNull(mv.getModel().get("command"));
+		request.addParameter("_asynchronous", "true");
+		request.setMethod("POST");
+		request.addParameter("_page", "0");
+		mv = automaticSaveAjaxableFormController.handleRequestInternal(request, response);
+		assertEquals("test", mv.getModel().get("ajax"));
 	}
 
-	public void testReferenceDataHttpServletRequestObjectErrorsInt() {
-//		fail("Not yet implemented");
+	public void testReferenceDataHttpServletRequestObjectErrorsInt() throws Exception {
+		request.setParameter("_page", "1");
+		EasyMock.expect(errors.hasErrors()).andReturn(false);
+		replayMocks();
+		Map refdata = automaticSaveAjaxableFormController.referenceData(request, command, errors, 1);
+		assertEquals("Information saved successfully", refdata.get("flashMessage"));
+		assertEquals("test", refdata.get("second tab"));
+		verifyMocks();
 	}
 
 	public void testPopulateSaveConfirmationMessage1() {
 		Map refdata = new HashMap();
-		Errors errors = registerMockFor(Errors.class);
 		EasyMock.expect(errors.hasErrors()).andReturn(true);
 		replayMocks();
 		automaticSaveAjaxableFormController.populateSaveConfirmationMessage(refdata, request, new Object(), errors, 1);
@@ -73,7 +138,6 @@ public class AutomaticSaveAjaxableFormControllerTest extends AbstractTestCase {
 	public void testPopulateSaveConfirmationMessage2() {
 		Map refdata = new HashMap();
 		refdata.put("flashMessage", "Something");
-		Errors errors = registerMockFor(Errors.class);
 		EasyMock.expect(errors.hasErrors()).andReturn(false);
 		replayMocks();
 		automaticSaveAjaxableFormController.populateSaveConfirmationMessage(refdata, request, new Object(), errors, 1);
@@ -83,7 +147,6 @@ public class AutomaticSaveAjaxableFormControllerTest extends AbstractTestCase {
 	
 	public void testPopulateSaveConfirmationMessage3() {
 		Map refdata = new HashMap();
-		Errors errors = registerMockFor(Errors.class);
 		EasyMock.expect(errors.hasErrors()).andReturn(false);
 		replayMocks();
 		automaticSaveAjaxableFormController.populateSaveConfirmationMessage(refdata, request, new Object(), errors, 1);
@@ -94,7 +157,6 @@ public class AutomaticSaveAjaxableFormControllerTest extends AbstractTestCase {
 	public void testPopulateSaveConfirmationMessage4() {
 		request.setParameter("_page", "0");
 		Map refdata = new HashMap();
-		Errors errors = registerMockFor(Errors.class);
 		EasyMock.expect(errors.hasErrors()).andReturn(false);
 		replayMocks();
 		automaticSaveAjaxableFormController.populateSaveConfirmationMessage(refdata, request, new Object(), errors, 0);
@@ -105,7 +167,6 @@ public class AutomaticSaveAjaxableFormControllerTest extends AbstractTestCase {
 	public void testPopulateSaveConfirmationMessage5() {
 		request.setParameter("_page", "1");
 		Map refdata = new HashMap();
-		Errors errors = registerMockFor(Errors.class);
 		EasyMock.expect(errors.hasErrors()).andReturn(false);
 		replayMocks();
 		automaticSaveAjaxableFormController.populateSaveConfirmationMessage(refdata, request, new Object(), errors, 1);
@@ -117,7 +178,6 @@ public class AutomaticSaveAjaxableFormControllerTest extends AbstractTestCase {
 		request.setParameter("_page", "1");
 		request.setAttribute("tabFlashMessage", "Another test");
 		Map refdata = new HashMap();
-		Errors errors = registerMockFor(Errors.class);
 		EasyMock.expect(errors.hasErrors()).andReturn(false);
 		replayMocks();
 		automaticSaveAjaxableFormController.populateSaveConfirmationMessage(refdata, request, new Object(), errors, 1);
