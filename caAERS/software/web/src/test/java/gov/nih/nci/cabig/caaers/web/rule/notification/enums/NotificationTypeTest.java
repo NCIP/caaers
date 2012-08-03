b94ddf7b54239c5597721d41792c0bc10c3d2c62
Biju Joseph <biju.joseph@semanticbits.com>
2012-08-03 14:16:40 +0000

    Impoving Code Coverage
    SVN-Revision: 16933

---
+++ caAERS/software/web/src/test/java/gov/nih/nci/cabig/caaers/web/rule/notification/enums/NotificationTypeTest.java
@@ -0,0 +1,28 @@
+package gov.nih.nci.cabig.caaers.web.rule.notification.enums;
+
+import junit.framework.TestCase;
+
+/**
+ * @author: Biju Joseph
+ */
+public class NotificationTypeTest extends TestCase {
+
+
+    public void testGetCode() throws Exception {
+       assertEquals(new Integer(1),  NotificationType.EMAIL_NOTIFICATION.getCode());
+       assertEquals(new Integer(2),  NotificationType.FAX_NOTIFICATION.getCode());
+       assertEquals(new Integer(3),  NotificationType.PAGER_NOTIFICATION.getCode());
+       assertEquals(new Integer(4),  NotificationType.XML_NOTIFICATION.getCode());
+    }
+
+    public void testGetName() throws Exception {
+        assertEquals("EMAIL_NOTIFICATION",  NotificationType.EMAIL_NOTIFICATION.getName());
+        assertEquals("FAX_NOTIFICATION",  NotificationType.FAX_NOTIFICATION.getName());
+        assertEquals("PAGER_NOTIFICATION",  NotificationType.PAGER_NOTIFICATION.getName());
+        assertEquals("XML_NOTIFICATION",  NotificationType.XML_NOTIFICATION.getName());
+    }
+
+    public void testGetDisplayName() throws Exception {
+        assertEquals("Email Notification",  NotificationType.EMAIL_NOTIFICATION.getDisplayName());
+    }
+}
