d7b52e743b05bc1905e6fafc4c9eb598d7a99cec
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2012-08-09 17:52:26 +0000

    test case coverage - removed unused methods
    SVN-Revision: 16985

--- caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/search/ExportSearchResultsController.java
+++ caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/search/ExportSearchResultsController.java
@@ -1,18 +1,8 @@
 package gov.nih.nci.cabig.caaers.web.search;
 
 import gov.nih.nci.cabig.caaers.dao.AdvancedSearchDao;
-import gov.nih.nci.cabig.caaers.dao.ParticipantDao;
 import gov.nih.nci.cabig.caaers.dao.SearchDao;
-import gov.nih.nci.cabig.caaers.dao.query.AbstractQuery;
-import gov.nih.nci.cabig.caaers.dao.query.HQLQuery;
-import gov.nih.nci.cabig.caaers.domain.Search;
-import gov.nih.nci.cabig.caaers.security.SecurityUtils;
 import gov.nih.nci.cabig.caaers.web.search.ui.AdvancedSearchUi;
-import gov.nih.nci.cabig.caaers.web.search.ui.CriteriaParameter;
-import gov.nih.nci.cabig.caaers.web.search.ui.DependentObject;
-import gov.nih.nci.cabig.caaers.web.search.ui.SaveSearch;
-import gov.nih.nci.cabig.caaers.web.search.ui.SearchTargetObject;
-import gov.nih.nci.cabig.caaers.web.search.ui.SelectedColumn;
 import gov.nih.nci.cabig.caaers.web.search.ui.ViewColumn;
 
 import java.io.File;
@@ -20,8 +10,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.StringReader;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -32,8 +20,6 @@ import javax.xml.bind.Unmarshaller;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractCommandController;
@@ -94,49 +80,6 @@ public class ExportSearchResultsController extends AbstractCommandController {
 		
 	}
 	
-	private void initializeCommandObject(AdvancedSearchCommand command) throws Exception{
-		String loginId = SecurityUtils.getUserLoginName();
-		List<Search> searchList = searchDao.getByLoginAndName(loginId, "exportSearch");
-		
-		Unmarshaller unmarshaller;
-		SaveSearch savedSearch = null;
-		try {
-			unmarshaller = JAXBContext.newInstance("gov.nih.nci.cabig.caaers.web.search.ui").createUnmarshaller();
-			//advancedSearchUi = (AdvancedSearchUi) unmarshaller.unmarshal(inputStream);
-			StringReader reader = new StringReader(searchList.get(0).getCriteriaXml()); 
-			savedSearch = (SaveSearch) unmarshaller.unmarshal(reader);
-			for(SearchTargetObject targetObject: advancedSearchUi.getSearchTargetObject()){
-				if(targetObject.getClassName().equals(savedSearch.getTargetClassName()))
-					command.setSearchTargetObject(targetObject);
-			}
-			for(CriteriaParameter criteriaParameter: savedSearch.getCriteriaParameter()){
-				AdvancedSearchCriteriaParameter parameter = new AdvancedSearchCriteriaParameter();
-				parameter.setObjectName(criteriaParameter.getObjectName());
-				parameter.setDependentObjectName(criteriaParameter.getDependentObjectName());
-				parameter.setAttributeName(criteriaParameter.getAttributeName());
-				parameter.setPredicate(criteriaParameter.getPredicate());
-				parameter.setValue(criteriaParameter.getValue());
-				parameter.setDisplayValue(criteriaParameter.getDisplayValue());
-				command.getCriteriaParameters().add(parameter);
-			}
-			// Setup the view selected.
-			for(SelectedColumn selectedColumn: savedSearch.getSelectedColumn()){
-				DependentObject dObject = AdvancedSearchUiUtil.getDependentObjectByName(command.getSearchTargetObject(), 
-						selectedColumn.getDependentObjectClassName());
-				dObject.setInView(true);
-				for(ViewColumn vCol: dObject.getViewColumn()){
-					if(selectedColumn.getColumnAttribute().equals(vCol.getColumnAttribute()))
-						vCol.setSelected(true);
-				}
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		// Delete the "exportSearch" saved in the database
-		searchDao.deleteByLoginIdAndName(loginId, "exportSearch");
-	}
 	
 	@Override
 	protected ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object arg2, BindException arg3) throws Exception {
@@ -184,129 +127,6 @@ public class ExportSearchResultsController extends AbstractCommandController {
 	    FileOutputStream fileOut = new FileOutputStream(tempDir + File.separator + XLS_SEARCH_RESULTS_FILENAME);
 	    wb.write(fileOut);
 	    fileOut.close();
-	}
-	
-	private void createQueryAndFetchData(AdvancedSearchCommand command) throws Exception{
-		List<ViewColumn> resultsViewColumnList = new ArrayList<ViewColumn>();
-		
-		// Setup up the isInView attribute of the dependentObject if any of the attribute is selected.
-		for(DependentObject dObject: command.getSearchTargetObject().getDependentObject()){
-			dObject.setInView(false);
-			for(ViewColumn viewColumn: dObject.getViewColumn()){
-				if(viewColumn.isSelected())
-					dObject.setInView(true);
-			}
-		}
-		List<AdvancedSearchCriteriaParameter> parameters = new ArrayList<AdvancedSearchCriteriaParameter>();
-		for(AdvancedSearchCriteriaParameter p: command.getCriteriaParameters()){
-			if(p.getAttributeName()!= null && !p.getAttributeName().equals("") && !p.getAttributeName().equals("none") && !p.isDeleted())
-					parameters.add(p);
-		}
-		
-		List<AbstractQuery> multipleQueries = new ArrayList<AbstractQuery>();
-		if (command.getAliasList().size()>0) {
-			for (String str:command.getAliasList()) {
-				AbstractQuery query = CommandToSQL.transform(command.getSearchTargetObject(), parameters,str);
-				
-				multipleQueries.add(query);
-			}
-		} else {
-			AbstractQuery query = CommandToSQL.transform(command.getSearchTargetObject(), parameters,null);
-			
-			multipleQueries.add(query);
-		}
-		command.setHql(multipleQueries);
-
-		
-		List<Object> singleObjectList = new ArrayList<Object>();
-		List<Object[]> multipleObjectList = new ArrayList<Object[]>();
-		if(CommandToSQL.isMultipleViewQuery(command.getSearchTargetObject())){
-			
-			for (AbstractQuery qry:multipleQueries){
-				List<Object[]> result = (List<Object[]>) advancedSearchDao.search(qry);
-				multipleObjectList.addAll(result);
-			}
-			
-			processMultipleObjectsList(multipleObjectList, command);
-			//command.setNumberOfResults(singleObjectList.size());
-			for(DependentObject dObject: command.getSearchTargetObject().getDependentObject())
-				if(dObject.isInView()){
-					for(ViewColumn vColumn: dObject.getViewColumn())
-						if(vColumn.isSelected())
-							resultsViewColumnList.add(vColumn);
-				}
-		}
-		else{
-			for (AbstractQuery qry:multipleQueries){
-				List<Object[]> result = (List<Object[]>) advancedSearchDao.search(qry);
-				singleObjectList.addAll(result);
-			}
-			//command.setNumberOfResults(singleObjectList.size());
-			command.setAdvancedSearchRowList(processSingleObjectList(singleObjectList, command.getSearchTargetObject().getDependentObject().get(0)));
-			for(DependentObject dObject: command.getSearchTargetObject().getDependentObject())
-				if(dObject.isInView()){
-					for(ViewColumn vColumn: dObject.getViewColumn())
-						if(vColumn.isSelected())
-							resultsViewColumnList.add(vColumn);
-				}
-		}
-		command.setResultsViewColumnList(resultsViewColumnList);
-		command.setNumberOfResults(command.getAdvancedSearchRowList().size());
-	}
-	
-	public List<AdvancedSearchRow> processSingleObjectList(List<Object> objectList, DependentObject dObject){
-		List<AdvancedSearchRow> rowList = new ArrayList<AdvancedSearchRow>();
-		for(Object object: objectList){
-			AdvancedSearchRow row = new AdvancedSearchRow();
-			AdvancedSearchColumn column = null;
-			BeanWrapper wrapper = new BeanWrapperImpl(object);
-			
-			for(ViewColumn viewColumn: dObject.getViewColumn()){
-				if(viewColumn.isSelected()){
-					column = new AdvancedSearchColumn();
-					column.setColumnHeader(viewColumn.getColumnTitle());
-					if(wrapper.getPropertyValue(viewColumn.getColumnAttribute()) != null)
-						column.setValue(wrapper.getPropertyValue(viewColumn.getColumnAttribute()).toString());
-					else
-						column.setValue(wrapper.getPropertyValue(viewColumn.getColumnAttribute()));
-					row.getColumnList().add(column);
-				}
-			}
-			rowList.add(row);
-		}
-		return rowList;
-	}
-	
-	/**
-	 * This method processes the result object list when there are multiple objects in the view
-	 */
-	public void processMultipleObjectsList(List<Object[]> objectList, AdvancedSearchCommand command){
-		command.setNumberOfResults(objectList.size());
-		List<AdvancedSearchRow> rowList = new ArrayList<AdvancedSearchRow>();
-		for(Object[] objectArr: objectList){
-			AdvancedSearchRow row = new AdvancedSearchRow();
-			AdvancedSearchColumn column = null;
-			int i = 0;
-			for(DependentObject dObject: command.getSearchTargetObject().getDependentObject()){
-				if(dObject.isInView()){
-					Object obj = objectArr[i++];
-					BeanWrapper wrapper = new BeanWrapperImpl(obj);
-					for(ViewColumn viewColumn: dObject.getViewColumn()){
-						if(viewColumn.isSelected()){
-							column = new AdvancedSearchColumn();
-							column.setColumnHeader(viewColumn.getColumnTitle());
-							if(wrapper.getPropertyValue(viewColumn.getColumnAttribute()) != null)
-								column.setValue(wrapper.getPropertyValue(viewColumn.getColumnAttribute()).toString());
-							else
-								column.setValue(wrapper.getPropertyValue(viewColumn.getColumnAttribute()));
-							row.getColumnList().add(column);
-						}
-					}
-				}
-			}
-			rowList.add(row);
-		}
-		command.setAdvancedSearchRowList(rowList);
 	}
 	
 	public void setSearchDao(SearchDao searchDao){
