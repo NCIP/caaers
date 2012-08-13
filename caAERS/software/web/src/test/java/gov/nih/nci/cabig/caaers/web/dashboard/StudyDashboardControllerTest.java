6d1f58fc2e8d6502c9fcb3adf0048b4823b4b386
Biju Joseph <biju.joseph@semanticbits.com>
2012-08-13 19:41:58 +0000

    Impoving Code Coverage
    SVN-Revision: 17001

---
+++ caAERS/software/web/src/test/java/gov/nih/nci/cabig/caaers/web/dashboard/StudyDashboardControllerTest.java
@@ -0,0 +1,49 @@
+package gov.nih.nci.cabig.caaers.web.dashboard;
+
+import gov.nih.nci.cabig.caaers.domain.Study;
+import gov.nih.nci.cabig.caaers.domain.repository.StudyRepository;
+import gov.nih.nci.cabig.caaers.web.WebTestCase;
+import org.easymock.EasyMock;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: Biju Joseph
+ */
+public class StudyDashboardControllerTest extends WebTestCase {
+
+    StudyRepository studyRepository;
+    StudyDashboardController controller;
+    List<Study> studyList = new ArrayList<Study>();
+    public void setUp() throws Exception {
+        super.setUp();
+        controller = new StudyDashboardController();
+        studyRepository = registerMockFor(StudyRepository.class);
+        controller.setStudyRepository(studyRepository);
+
+
+
+    }
+
+    public void testHandleRequestInternal() throws Exception {
+        request.setParameter("loadAll", "true");
+        EasyMock.expect(studyRepository.getAllStudies()).andReturn(studyList) ;
+        replayMocks();
+        ModelAndView mv = controller.handleRequestInternal(request, response);
+        assertSame(studyList,mv.getModel().get("studyList"));
+        verifyMocks();
+    }
+
+
+
+    public void testHandleRequestInternal_Paginated() throws Exception {
+
+        EasyMock.expect(studyRepository.getAllStudies(0, 20)).andReturn(studyList) ;
+        replayMocks();
+        ModelAndView mv = controller.handleRequestInternal(request, response);
+        assertSame(studyList,mv.getModel().get("studyList"));
+        verifyMocks();
+    }
+}
