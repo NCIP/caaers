0711a9361686ef1fc12e5ee1f32a30cc0fa9f5d6
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2012-09-20 16:41:01 +0000

    CAAERS-5860: Implement saving provisional adverse event reporting period and adverse events from Force.com -dropping not null constraint on start date
    SVN-Revision: 17111

--- caAERS/software/core/src/main/db/migrate/030_Phase4_construction_7/007_alter_table_ext_ae_reporting_periods_drop_not_null_constraints.groovy
+++ caAERS/software/core/src/main/db/migrate/030_Phase4_construction_7/007_alter_table_ext_ae_reporting_periods_drop_not_null_constraints.groovy
@@ -4,12 +4,14 @@ class AlterTableExternalAEReportingPeriodsDropNotNullConstraints extends edu.nor
     		execute("alter table ext_ae_reporting_prds alter column other_description drop not null");
     		execute("alter table ext_ae_reporting_prds alter column treatment_assignment_code drop not null");
     		execute("alter table ext_ae_reporting_prds alter column name drop not null");
+    		execute("alter table ext_ae_reporting_prds alter column start_date drop not null");
     	}
     	
     	if(databaseMatches('oracle')){
     	 	execute("alter table ext_ae_reporting_prds modify other_description null");
     		execute("alter table ext_ae_reporting_prds modify treatment_assignment_code null");
     		execute("alter table ext_ae_reporting_prds modify name null");
+    		execute("alter table ext_ae_reporting_prds modify start_date null");
     	}
     }
 
