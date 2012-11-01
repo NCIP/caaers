2caa4605943455d6000a9244c3aae36ec0d66b49
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2012-11-01 22:09:02 +0000

    CAAERS-5857: The system is not keeping the CTCAE version in synch with AdEERS
    SVN-Revision: 17313

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/service/synchronizer/StudySynchronizer.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/service/synchronizer/StudySynchronizer.java
@@ -1,6 +1,12 @@
 package gov.nih.nci.cabig.caaers.service.synchronizer;
 
+import gov.nih.nci.cabig.caaers.domain.AeTerminology;
+import gov.nih.nci.cabig.caaers.domain.Ctc;
+import gov.nih.nci.cabig.caaers.domain.DiseaseCodeTerm;
+import gov.nih.nci.cabig.caaers.domain.DiseaseTerminology;
+import gov.nih.nci.cabig.caaers.domain.MeddraVersion;
 import gov.nih.nci.cabig.caaers.domain.Study;
+import gov.nih.nci.cabig.caaers.integration.schema.study.DiseaseCodeType;
 import gov.nih.nci.cabig.caaers.service.DomainObjectImportOutcome;
 import gov.nih.nci.cabig.caaers.service.migrator.CompositeMigrator;
 import gov.nih.nci.cabig.caaers.service.migrator.Migrator;
@@ -33,6 +39,10 @@ public class StudySynchronizer extends CompositeMigrator<Study>{
         dbStudy.setStudyPurpose(xmlStudy.getStudyPurpose());
         dbStudy.setPhaseCode(xmlStudy.getPhaseCode());
         dbStudy.setLastSynchedDate(new Date());
+        
+        populateAeTerminology(dbStudy, xmlStudy, outcome);
+        
+        populateDiseaseTerminology(dbStudy, xmlStudy, outcome);
 
 		dbStudy.setLongTitle(xmlStudy.getLongTitle());
 		if(xmlStudy.getDescription() != null &&  StringUtils.isNotEmpty(xmlStudy.getDescription())){
@@ -47,6 +57,46 @@ public class StudySynchronizer extends CompositeMigrator<Study>{
 		dbStudy.setAdeersReporting(xmlStudy.getAdeersReporting());
 		if(xmlStudy.getDesign() != null){
 			dbStudy.setDesign(xmlStudy.getDesign());
+		}
+
+	}
+	
+	private void populateAeTerminology(Study dbStudy,Study xmlStudy,
+			DomainObjectImportOutcome<Study> outcome) {
+
+		if(xmlStudy.getAeTerminology() != null){
+			AeTerminology aeTerminology = null;
+
+			if(xmlStudy.getAeTerminology().getCtcVersion() != null){
+				aeTerminology = new AeTerminology();
+				Ctc ctcVersion = new Ctc();
+				ctcVersion.setName(xmlStudy.getAeTerminology().getCtcVersion().getName());
+				aeTerminology.setCtcVersion(ctcVersion);
+				dbStudy.setAeTerminology(aeTerminology);
+			}
+			if (xmlStudy.getAeTerminology().getMeddraVersion() != null) {
+				aeTerminology = new AeTerminology();
+				MeddraVersion meddraVersion = new MeddraVersion();
+				meddraVersion.setName(xmlStudy.getAeTerminology().getMeddraVersion().getName());
+				aeTerminology.setMeddraVersion(meddraVersion);
+				dbStudy.setAeTerminology(aeTerminology);
+            }
+		}
+
+	}
+	
+	private void populateDiseaseTerminology(Study dbStudy,Study xmlStudy,
+			DomainObjectImportOutcome<Study> outcome) {
+
+		if(xmlStudy.getDiseaseTerminology() != null){
+			DiseaseTerminology diseaseTerminology = dbStudy.getDiseaseTerminology();
+
+			if(DiseaseCodeType.CTEP.equals(xmlStudy.getDiseaseTerminology().getDiseaseCodeTerm())){
+				diseaseTerminology.setDiseaseCodeTerm(DiseaseCodeTerm.CTEP);
+			}
+			if(DiseaseCodeType.MEDDRA.equals(xmlStudy.getDiseaseTerminology().getDiseaseCodeTerm())){
+				diseaseTerminology.setDiseaseCodeTerm(DiseaseCodeTerm.MEDDRA);
+			}
 		}
 
 	}
