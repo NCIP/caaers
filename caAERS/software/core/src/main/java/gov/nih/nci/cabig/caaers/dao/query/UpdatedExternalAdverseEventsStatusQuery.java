61928369ebc8331e719e3e207f2aa267177a4698
Biju Joseph <biju.joseph@semanticbits.com>
2012-11-05 19:35:02 +0000

    CAAERS-5860 - external id constranted to reporting period.
    SVN-Revision: 17351

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/dao/query/UpdatedExternalAdverseEventsStatusQuery.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/dao/query/UpdatedExternalAdverseEventsStatusQuery.java
@@ -3,7 +3,7 @@ package gov.nih.nci.cabig.caaers.dao.query;
 
 public class UpdatedExternalAdverseEventsStatusQuery extends HQLQuery {
     public static final String UPDATE_AE_STATUS_HQL = "update ExternalAdverseEvent eae set eae.status = :newStatus where " +
-            "eae.status = :oldStatus and eae.externalId in (:externalIds) and eae.id in (select eaa1 from ExternalAdverseEvent eaa1 " +
+            "eae.status = :oldStatus and eae.externalId in (:externalIds) and eae.id in (select eaa1.id from ExternalAdverseEvent eaa1 " +
             "join eaa1.externalAdverseEventReportingPeriod earp join earp.domainReportingPeriod drp where drp.id = :domainReportingPeriodId)";
 
     public UpdatedExternalAdverseEventsStatusQuery(String updateQuery) {
