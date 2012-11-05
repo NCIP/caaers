2f4c2fff6186a4e4f244f3764f77fa57089a595b
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2012-11-05 00:22:30 +0000

    https://tracker.nci.nih.gov/browse/CAAERS-5936
    SVN-Revision: 17336

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/domain/repository/AdverseEventRoutingAndReviewRepositoryImpl.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/domain/repository/AdverseEventRoutingAndReviewRepositoryImpl.java
@@ -631,6 +631,17 @@ public class AdverseEventRoutingAndReviewRepositoryImpl implements AdverseEventR
             }
 
         }
+        Collections.sort(dtos, new Comparator<TaskNotificationDTO>() {
+        	public int compare(TaskNotificationDTO o1, TaskNotificationDTO o2) {
+        		if(o1.getDate().after(o2.getDate())){
+        			return -1;
+        		} else if(o1.getDate().before(o2.getDate())){
+        			return 1;
+        		}
+        		return 0;
+        	}
+		});
+        
         return dtos;
     }
 
