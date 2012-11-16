d7edf2a9967d2e7768b8eb72f56737cf7b3e1b29
Vijendhar Meda <vijendhar.meda@semanticbits.com>
2012-11-16 15:41:58 +0000

    CAAERS-6033: Updated the logic all study items and updated the test cases.
    SVN-Revision: 17401

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/query/impl/CaaersStudyIdFetcherImpl.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/accesscontrol/query/impl/CaaersStudyIdFetcherImpl.java
@@ -1,16 +1,20 @@
 package gov.nih.nci.cabig.caaers.accesscontrol.query.impl;
 
-import gov.nih.nci.cabig.caaers.dao.query.HQLQuery;
+import gov.nih.nci.cabig.caaers.dao.query.NativeSQLQuery;
 import gov.nih.nci.cabig.caaers.domain.UserGroupType;
 import gov.nih.nci.cabig.caaers.domain.index.IndexEntry;
-import gov.nih.nci.cabig.caaers.security.SecurityUtils;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.semanticbits.security.contentfilter.IdFetcher;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.type.StandardBasicTypes;
 
 
 /**
@@ -26,14 +30,83 @@ public class CaaersStudyIdFetcherImpl extends AbstractIdFetcher implements IdFet
 	public List fetch(String loginId) {
 
 		List<IndexEntry> resultList = getCaaersSecurityFacade().getAccessibleStudyIds(loginId);
-        for(IndexEntry entry : resultList){
-            if(entry.isAllSiteOrAllStudy()) continue;
-        }
-        
+		if(CollectionUtils.isEmpty(resultList)) return resultList;
+		
         if(log.isInfoEnabled()){
             log.info("Study Fetcher fetched : " + String.valueOf(resultList) );
         }
         
-		return resultList;
+        String all_studies_results = "select distinct so.study_id as study_id from " +
+                " study_organizations so " +
+                " join organization_index oi on  oi.organization_id = so.site_id  " +
+                " where retired_indicator <> :ri";
+        
+        Map<Integer, IndexEntry> studiesMap = new LinkedHashMap<Integer, IndexEntry>();
+        
+        for(IndexEntry entry : resultList){
+            if(entry.isAllSiteOrAllStudy()) {
+            	
+            	// Prepare the condition
+                StringBuffer roleCond = new StringBuffer("(");
+                int size = entry.getRoles().size();
+                for ( UserGroupType role: entry.getRoles()) {                	
+                	roleCond.append( role.dbAlias() +  "=:" + role.dbAlias());
+                	size--;
+                	if ( size > 0 )
+                		roleCond.append(" OR ");
+                }
+                roleCond.append(")");
+                all_studies_results = all_studies_results + " and " + roleCond.toString();    
+                
+            	//now pull out all study organization details from database.
+                NativeSQLQuery studySiteQuery = new NativeSQLQuery(all_studies_results);
+                studySiteQuery.setScalar("study_id", StandardBasicTypes.INTEGER);
+                studySiteQuery.setParameter("ri", true);
+                
+                for ( UserGroupType role: entry.getRoles()) {
+                	studySiteQuery.setParameter(role.dbAlias(),true);
+                }
+                
+                
+                
+                // Update the all site query with roles condition.
+                
+                List<Object[]> studyOrgDataList = (List<Object[]>) search(studySiteQuery);
+                if(studyOrgDataList == null || studyOrgDataList.isEmpty()) break;
+              
+                constructIndexEntriesForExtrapolatedStudies(studyOrgDataList, entry.getRoles(), studiesMap);
+                	
+            } else {
+            	updateStudiesMap(studiesMap, entry);
+            }
+        }
+        
+		return  new ArrayList<IndexEntry>(studiesMap.values());
 	}
+	
+	private void constructIndexEntriesForExtrapolatedStudies( List<Object[]> studyOrgDataList,List<UserGroupType> roles, Map<Integer, IndexEntry> studiesMap) {
+		
+		for ( Object val : studyOrgDataList) {
+			Integer entityId = (Integer)val;
+			IndexEntry e = new IndexEntry(entityId);
+			e.addRole(roles);
+			updateStudiesMap(studiesMap, e);
+		}	
+	}
+	
+	private void updateStudiesMap( Map<Integer, IndexEntry> studiesMap, IndexEntry entry) {
+		Integer entityId = entry.getEntityId();
+		
+		if ( studiesMap.containsKey(entityId)) {
+			// update the roles.
+				IndexEntry e = studiesMap.get(entityId);
+				e.addRole(entry.getRoles());
+				studiesMap.put(entityId, e);
+				
+		} else {
+			studiesMap.put(entityId, entry);
+		}
+		
+	}
+	
 }
---
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/accesscontrol/query/impl/CaaersStudyIdFetcherImplTest.java
@@ -0,0 +1,61 @@
+package gov.nih.nci.cabig.caaers.accesscontrol.query.impl;
+
+import gov.nih.nci.cabig.caaers.CaaersDbNoSecurityTestCase;
+import gov.nih.nci.cabig.caaers.domain.UserGroupType;
+import gov.nih.nci.cabig.caaers.domain.index.IndexEntry;
+import gov.nih.nci.cabig.caaers.security.CaaersSecurityFacade;
+import gov.nih.nci.cabig.caaers.security.CaaersSecurityFacadeTestAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * CaaersStudyIdFetcherImpl Tester.
+ *
+ * @author medav
+ * 
+ */
+public class CaaersStudyIdFetcherImplTest extends CaaersDbNoSecurityTestCase {
+
+    CaaersStudyIdFetcherImpl fetcher;
+
+    CaaersSecurityFacade facade;
+    
+    public void setUp() throws Exception {
+        super.setUp();
+        fetcher = (CaaersStudyIdFetcherImpl) getDeployedApplicationContext().getBean("studyIdFetcher");
+        facade = fetcher.getCaaersSecurityFacade();
+
+        fetcher.setCaaersSecurityFacade(new CaaersSecurityFacadeTestAdapter(){
+            @Override
+            public List<IndexEntry> getAccessibleStudyIds(String loginId) {
+                List<IndexEntry> entries =  super.getAccessibleStudyIds(loginId);
+                
+                IndexEntry e1 = new IndexEntry(Integer.MIN_VALUE);
+                e1.addRole(UserGroupType.ae_reporter);
+                e1.addRole(UserGroupType.data_analyst);
+                entries.add(e1);
+                
+                IndexEntry e = new IndexEntry(-2);
+                e.addRole(UserGroupType.ae_reporter);
+                e.addRole(UserGroupType.data_analyst);
+                
+                
+                entries.add(e);
+                return entries;
+            }
+        });
+
+    }
+
+    public void testFetch(){
+        List<IndexEntry> entries = fetcher.fetch("1000@def.com");
+        assertEquals(3, entries.size());
+    }
+
+    public void tearDown() throws Exception {
+        fetcher.setCaaersSecurityFacade(facade);
+        super.tearDown();
+    }
+
+}
