1999cfc317eee0b4d581df675e215c8fbf3e3517
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2013-01-17 18:12:39 +0000

    CAAERS-6216: SafetyReportManagementService - Update it to consume caAERS-XML datacollection
    SVN-Revision: 17612

--- caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/ws/SafetyReportManagementService.java
+++ caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/ws/SafetyReportManagementService.java
@@ -1,13 +1,16 @@
 package gov.nih.nci.cabig.caaers.ws;
 
+import gov.nih.nci.cabig.caaers.integration.schema.aereport.AdverseEventReport;
+import gov.nih.nci.cabig.caaers.integration.schema.common.CaaersServiceResponse;
+
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
 /**
  * @author: Biju Joseph
  */
-@WebService(name="SafetyReportManagementServiceInterface", targetNamespace = "http://schema.integration.caaers.cabig.nci.nih.gov/icsr")
+@WebService(name="SafetyReportManagementServiceInterface", targetNamespace = "http://schema.integration.caaers.cabig.nci.nih.gov/aereport")
 public interface SafetyReportManagementService {
 
-    public Object submitSafetyReport(@WebParam(name = "report",targetNamespace = "http://schema.integration.caaers.cabig.nci.nih.gov/icsr") Object report);
+    public CaaersServiceResponse submitSafetyReport(@WebParam(name = "AdverseEventReport",targetNamespace = "http://schema.integration.caaers.cabig.nci.nih.gov/aereport") AdverseEventReport xmlAdverseEventReport);
 }
--- caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/ws/impl/SafetyReportManagementServiceImpl.java
+++ caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/ws/impl/SafetyReportManagementServiceImpl.java
@@ -1,5 +1,8 @@
 package gov.nih.nci.cabig.caaers.ws.impl;
 
+import gov.nih.nci.cabig.caaers.api.impl.Helper;
+import gov.nih.nci.cabig.caaers.integration.schema.aereport.AdverseEventReport;
+import gov.nih.nci.cabig.caaers.integration.schema.common.CaaersServiceResponse;
 import gov.nih.nci.cabig.caaers.ws.SafetyReportManagementService;
 
 import javax.jws.WebMethod;
@@ -7,18 +10,37 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * @author: Biju Joseph
  */
 @WebService(endpointInterface="gov.nih.nci.cabig.caaers.ws.SafetyReportManagementService",
-        serviceName="SafetyReportManagementService", targetNamespace = "http://schema.integration.caaers.cabig.nci.nih.gov/icsr")
+        serviceName="SafetyReportManagementService", targetNamespace = "http://schema.integration.caaers.cabig.nci.nih.gov/aereport")
 @SOAPBinding(parameterStyle=SOAPBinding.ParameterStyle.BARE)
 
 public class SafetyReportManagementServiceImpl implements SafetyReportManagementService {
+	
+	private gov.nih.nci.cabig.caaers.api.impl.SafetyReportManagementServiceImpl safetyReportManagementServiceImpl;
+	private static Log logger = LogFactory.getLog(SafetyReportManagementServiceImpl.class);
 
-    @WebMethod
-    public Object submitSafetyReport(@WebParam(name = "report", targetNamespace = "http://schema.integration.caaers.cabig.nci.nih.gov/icsr") Object report){
-        return null;
+    public void setSafetyReportManagementServiceImpl(
+			gov.nih.nci.cabig.caaers.api.impl.SafetyReportManagementServiceImpl safetyReportManagementServiceImpl) {
+		this.safetyReportManagementServiceImpl = safetyReportManagementServiceImpl;
+	}
+
+	@WebMethod
+    public CaaersServiceResponse submitSafetyReport(@WebParam(name = "AdverseEventReport", targetNamespace = "http://schema.integration.caaers.cabig.nci.nih.gov/aereport") AdverseEventReport xmlAdverseEventReport){
+		try {
+			return safetyReportManagementServiceImpl.submitSafetyReport(xmlAdverseEventReport);
+		} catch (Exception e) {
+            logger.error(e);
+            CaaersServiceResponse caaersResponse = Helper.createResponse();
+            Helper.populateError(caaersResponse, "WS_GEN_000", "Unable to process the request :" + e.getMessage());
+            return caaersResponse;
+		}
+		
     }
 
 }
