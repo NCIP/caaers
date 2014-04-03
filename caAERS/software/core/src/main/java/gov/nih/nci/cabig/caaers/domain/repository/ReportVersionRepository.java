490bb0a41407a441eaf9c56146c05183e0d2d120
bijujoseph <biju.joseph@semanticbits.com>
2014-04-03 10:51:19 -0400

    CAAERS-6749 The system does not return the E2B ACK file when adeers submission is in progress status for longer than the set threshold

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/domain/repository/ReportVersionRepository.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/domain/repository/ReportVersionRepository.java
@@ -9,9 +9,12 @@ package gov.nih.nci.cabig.caaers.domain.repository;
 import gov.nih.nci.cabig.caaers.dao.query.ReportVersionDTOQuery;
 import gov.nih.nci.cabig.caaers.dao.report.ReportVersionDao;
 import gov.nih.nci.cabig.caaers.domain.ReportStatus;
+import gov.nih.nci.cabig.caaers.domain.report.Report;
 import gov.nih.nci.cabig.caaers.domain.report.ReportVersion;
 import gov.nih.nci.cabig.caaers.domain.report.ReportVersionDTO;
+import gov.nih.nci.cabig.caaers.esb.client.ResponseMessageProcessor;
 import gov.nih.nci.cabig.ctms.lang.NowFactory;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
@@ -30,6 +33,7 @@ public class ReportVersionRepository {
     /** The report version dao. */
     private ReportVersionDao reportVersionDao;
 
+    private ResponseMessageProcessor responseMessageProcessor;
     /**
      * Update in process reports.
      */
@@ -50,10 +54,46 @@ public class ReportVersionRepository {
                     rv.setReportStatus(ReportStatus.FAILED);
                     rv.setSubmissionMessage("Submission failed for unknown reason , Please resubmit");
                     reportVersionDao.save(rv);
+                    sendFailureXMLMessage(rv);
                 }
 
             }
         }
+    }
+
+
+    /**
+     * Will send a fake AdEERS failure message so that the post process like E2B ack etc will be sent out successfully.
+     * @param reportVersion
+     */
+    private void sendFailureXMLMessage(ReportVersion reportVersion) {
+
+        if(responseMessageProcessor == null) return;
+
+        String responseXMLTemplate = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" " +
+                "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body>" +
+                "<submitAEDataXMLAsAttachmentResponse xmlns=\"http://types.ws.adeers.ctep.nci.nih.gov\">" +
+                "<ns1:AEReportJobInfo xmlns:ns1=\"http://localhost:8080/AdEERSWSMap/services/AEReportXMLService\"><jobID xsi:type=\"xsd:string\" xmlns=\"\">0000</jobID>" +
+                "<patientID xsi:type=\"xsd:string\" xmlns=\"\">${subjectId}</patientID><protocolNumber xsi:type=\"xsd:string\" xmlns=\"\">${protocolId}</protocolNumber>" +
+                "<jobExceptions xmlns=\"\"><code>Error</code><description>Report is stuck at submission, so forcefully marking it as failed</description>" +
+                "</jobExceptions><reportStatus xsi:nil=\"true\" xmlns=\"\"/><comments xsi:type=\"xsd:string\" xmlns=\"\">Report is stuck at submission, so forcefully marking it as failed</comments>" +
+                "<CAEERS_AEREPORT_ID>${aeReportId}</CAEERS_AEREPORT_ID><REPORT_ID>${reportId}</REPORT_ID><SUBMITTER_EMAIL>${submitterEmail}</SUBMITTER_EMAIL><MESSAGE_COMBO_ID>${messageComboId}</MESSAGE_COMBO_ID>" +
+                "</ns1:AEReportJobInfo></submitAEDataXMLAsAttachmentResponse>" +
+                "</soapenv:Body></soapenv:Envelope>";
+        Report report = reportVersion.getReport();
+        Map<String, String> replacementMap = new HashMap<String, String>();
+        replacementMap.put("${reportId}",  "" + report.getId());
+        replacementMap.put("${aeReportId}",  "" + report.getAeReport().getId());
+        replacementMap.put("${protocolId}",  report.getAeReport().getStudy().getPrimaryIdentifierValue());
+        replacementMap.put("${subjectId}",   report.getAeReport().getParticipant().getPrimaryIdentifierValue());
+        replacementMap.put("${messageComboId}",    report.getCaseNumber() + "::" + report.getCreatedOn().getTime());
+        replacementMap.put("${submitterEmail}", report.getSubmitter().getEmailAddress());
+
+        String xmlMessage =  responseXMLTemplate;
+        for(Map.Entry<String, String> entry : replacementMap.entrySet()) {
+           xmlMessage =  StringUtils.replace(xmlMessage, entry.getKey(), entry.getValue());
+        }
+        responseMessageProcessor.processMessage(xmlMessage);
     }
 
 
@@ -117,4 +157,11 @@ public class ReportVersionRepository {
         this.reportVersionDao = reportVersionDao;
     }
 
+    /**
+     * An AdeersSubmissionResponseMessageProcessor
+     * @param responseMessageProcessor
+     */
+    public void setResponseMessageProcessor(ResponseMessageProcessor responseMessageProcessor) {
+        this.responseMessageProcessor = responseMessageProcessor;
+    }
 }
