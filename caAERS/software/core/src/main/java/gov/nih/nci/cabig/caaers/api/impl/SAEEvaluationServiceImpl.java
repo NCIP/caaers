529ac0846d58c3832a6d52204380c212b022f214
DirkCWalter <dirk.walter@semanticbits.com>
2014-12-02 16:48:27 -0500

    CAAERS-7043, more useful error message.

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/api/impl/SAEEvaluationServiceImpl.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/api/impl/SAEEvaluationServiceImpl.java
@@ -351,7 +351,7 @@ public class SAEEvaluationServiceImpl implements ApplicationContextAware {
 
 
 		} catch (Exception e) {
-			logger.error(" Exception Occured when processing rules; " + e.toString());
+			logger.error(" Exception Occured when processing rules; ", e);
 			throw Helper.createCaaersFault(DEF_ERR_MSG, "WS_SAE_001",
 					messageSource.getMessage("WS_SAE_001", new String[]{},  "", Locale.getDefault())
 					);			
