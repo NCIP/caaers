91a420ebd27c3e9f865feaae2546ecb70925da86
Biju Joseph <biju.joseph@semanticbits.com>
2014-12-23 06:51:49 -0500

    CAAERS-7078 - give preference to inprogress reports for reported AES

--- caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/rules/business/service/EvaluationServiceImpl.java
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/rules/business/service/EvaluationServiceImpl.java
@@ -38,6 +38,7 @@ import java.util.TreeSet;
 import org.apache.commons.collections15.Closure;
 import org.apache.commons.collections15.CollectionUtils;
 import org.apache.commons.collections15.ListUtils;
+import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -376,7 +377,7 @@ public class EvaluationServiceImpl implements EvaluationService {
             	log.debug("Rules Engine Result for : " + aeReportId + ", " + String.valueOf(map));
             }
 
-            
+            //  - If inprogress report is always preferred over rules suggestions (CAAERS-7078)
             //  - If child report is active, select that instead of parent. 
             // - If there is a manual selection, ignore rules engine suggestions from the same group
             // - If the manual selection is always a preferred one (ie. by default add active manual selected reports). 
@@ -388,8 +389,27 @@ public class EvaluationServiceImpl implements EvaluationService {
             	
             	//a temporary list
             	List<ReportDefinition> tmplist = new ArrayList<ReportDefinition>(defList);
-            	
-            	//keep active child report instead of parent.
+
+                //inprogress report is preferred for reported AEs
+                for(AdverseEvent adverseEvent : evaluatableAeList) {
+                    if(adverseEvent.getReport() == null || BooleanUtils.isFalse(adverseEvent.getReported()))  continue; //unreported AE
+                    for(Report report : activeReports) {
+                        ReportDefinition rdActive = report.getReportDefinition();
+                        List<ReportDefinition> conflictingReportDefList = ReportDefinition.findBySameOrganizationAndGroup(tmplist, rdActive);
+                        for(ReportDefinition rdSuggested : conflictingReportDefList) {
+                            if(StringUtils.equals(rdActive.getName(), rdSuggested.getName())) continue;
+                            defList.remove(rdSuggested);
+                            evaluationResult.removeReportDefinitionName(aeReportId, adverseEvent, rdSuggested.getName());
+                            evaluationResult.addProcessingStep(aeReportId, String.format("caAERS: removing suggested %s in favor of in-progress %s",
+                                    rdSuggested.getName(), rdActive.getName()),null);
+                        }
+                    }
+                }
+
+                //recreate temporary list
+                tmplist = new ArrayList<ReportDefinition>(defList);
+
+                //keep active child report instead of parent.
             	for(Report activeReport : activeReports){
             		ReportDefinition rdParent = activeReport.getReportDefinition().getParent();
             		ReportDefinition rdFound = findReportDefinition(tmplist, rdParent);
