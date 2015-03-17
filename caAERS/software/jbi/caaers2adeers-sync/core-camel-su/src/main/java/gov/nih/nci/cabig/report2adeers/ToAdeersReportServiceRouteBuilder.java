517134cb88967719a97902edee04653fac990eee
DirkCWalter <dirk.walter@semanticbits.com>
2015-03-17 17:48:03 -0400

    CAAERS-7103, tweaking jms queue

--- caAERS/software/jbi/caaers2adeers-sync/core-camel-su/src/main/java/gov/nih/nci/cabig/report2adeers/ToAdeersReportServiceRouteBuilder.java
+++ caAERS/software/jbi/caaers2adeers-sync/core-camel-su/src/main/java/gov/nih/nci/cabig/report2adeers/ToAdeersReportServiceRouteBuilder.java
@@ -15,7 +15,7 @@ public class ToAdeersReportServiceRouteBuilder {
 
 
     public void configure(Caaers2AdeersRouteBuilder rb){
-        rb.from("activemq:adeers-ae-message.inputQueue")
+        rb.from("activemq:adeers-ae-message.inputQueue?concurrentConsumers=5&asyncConsumer=true")
             .streamCaching()
             .to("log:gov.nih.nci.cabig.report2adeers.first-request?showHeaders=true&multiline=true&level=ERROR")
             .setProperty(OPERATION_NAME, rb.constant("sendReportToAdeers"))
