3c2401d778ac3d92021b7b1ddd829bc4ec128e74
DirkCWalter <dirk.walter@semanticbits.com>
2015-04-16 17:17:35 -0400

    CAAERS-7123, Added more detailed fault logging through a custom fault resolver.

---
+++ caAERS/software/core/src/main/java/gov/nih/nci/cabig/caaers/service/util/SoapFaultResolver.java
@@ -0,0 +1,30 @@
+package gov.nih.nci.cabig.caaers.service.util;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.ws.WebServiceMessage;
+import org.springframework.ws.soap.client.SoapFaultClientException;
+import org.springframework.ws.soap.client.core.SoapFaultMessageResolver;
+
+public class SoapFaultResolver extends SoapFaultMessageResolver {
+
+	protected final Log log = LogFactory.getLog(getClass());
+	
+	public SoapFaultResolver() {
+		
+	}
+	
+	
+	@Override
+	public void resolveFault(WebServiceMessage message) throws IOException {
+		try {
+			super.resolveFault(message);
+		} catch (SoapFaultClientException sfce) {
+			log.error("Got a client Webservice fault, for reason: '" + sfce.getFaultStringOrReason() + "' throwing it up the stack.", sfce);
+			throw sfce;
+		}
+	}
+
+}
