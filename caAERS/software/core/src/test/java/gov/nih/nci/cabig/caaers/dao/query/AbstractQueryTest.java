01efa34180da5e7c0d839c4c4924c40efe3c9550
gjanakiram <janakiram.gollapudi@semanticbits.com>
2015-04-29 16:16:34 -0400

    CAAERS-6058: add less than for dates in advanced search

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/AbstractQueryTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/query/AbstractQueryTest.java
@@ -14,12 +14,12 @@ import junit.framework.TestCase;
  */
 public class AbstractQueryTest extends TestCase {
 	AbstractQuery query;
-	
-	
+
+
 	protected void setUp() throws Exception {
 		super.setUp();
 		query = new AbstractQuery("select * from emp"){
-			
+
 		};
 	}
 	public void testGetQueryString() {
@@ -28,29 +28,50 @@ public class AbstractQueryTest extends TestCase {
 		query.orWhere("z = 3");
 		assertEquals("select * from emp WHERE x = 1 AND ( y = 2 OR z = 3 )",query.getQueryString());
 	}
-	
+
 	public void testGetQueryString_WithoutAnd(){
 		query.orWhere("y = 2");
 		query.orWhere("z = 3");
 		assertEquals("select * from emp WHERE y = 2 OR z = 3",query.getQueryString());
 	}
-	
+
 	public void test2(){
 		query.orWhere("y = 3");
-        assertEquals("select * from emp WHERE y = 3", query.getQueryString());
+		assertEquals("select * from emp WHERE y = 3", query.getQueryString());
 	}
 
 	public void testOneAnd(){
 		query.andWhere("dept = 'ABC'");
-        assertEquals("select * from emp WHERE dept = 'ABC'", query.getQueryString());
+		assertEquals("select * from emp WHERE dept = 'ABC'", query.getQueryString());
 	}
 
-    public void testWithMultipleAndAndOr(){
-        query.join("dept on dept.id = emp.id");
-        query.andWhere("j = 1");
-        query.andWhere("m = 5");
+	public void testWithMultipleAndAndOr(){
+		query.join("dept on dept.id = emp.id");
+		query.andWhere("j = 1");
+		query.andWhere("m = 5");
 		query.orWhere("y = 3");
-        query.orWhere("y = 4");
-        assertEquals("select * from emp join dept on dept.id = emp.id WHERE j = 1 AND m = 5 AND ( y = 3 OR y = 4 )", query.getQueryString());
+		query.orWhere("y = 4");
+		assertEquals("select * from emp join dept on dept.id = emp.id WHERE j = 1 AND m = 5 AND ( y = 3 OR y = 4 )", query.getQueryString());
 	}
+
+	public void testCreateDateQueryForLessThan() {
+		String resQuery = "";
+		try {
+			resQuery = query.createDateQuery("startDate", "04/01/2015", "<");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(resQuery, "(year(startDate) < '2015' OR (year(startDate) = '2015' AND month(startDate) < '4') OR (year(startDate) = '2015' AND month(startDate) = '4' AND day(startDate) < '1'))");
+	}
+
+	public void testCreateDateQueryForLessThanOrEqualTo() {
+		String resQuery = "";
+		try {
+			resQuery = query.createDateQuery("startDate", "04/01/2015", "<=");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(resQuery, "(year(startDate) < '2015' OR (year(startDate) = '2015' AND month(startDate) < '4') OR (year(startDate) = '2015' AND month(startDate) = '4' AND day(startDate) <= '1'))");
+	}
+
 }
