d4805d618186ba51010d0b6ea94f28be05d16020
DirkCWalter <dirk.walter@semanticbits.com>
2015-05-01 18:03:19 -0400

    CAAERS-7089, error message improvements.

--- caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/ae/CreateAdverseEventAjaxFacade.java
+++ caAERS/software/web/src/main/java/gov/nih/nci/cabig/caaers/web/ae/CreateAdverseEventAjaxFacade.java
@@ -708,7 +708,7 @@ public class CreateAdverseEventAjaxFacade {
         try {
 			return ParticipantHistory.bodySuraceArea(ht, htUOM, wt, wtUOM);
 		} catch (Exception e) {
-			log.error(e);
+			log.error("error while calcutaling body surface area.", e);
 		}
 		return 0.0;
     }
