4054b72639622f441c2ee1e83f30268aa93a0a52
DirkCWalter <dirk.walter@semanticbits.com>
2015-05-26 17:05:55 -0400

    bon issue debug

--- caAERS/software/jbi/caaers2adeers-sync/core-camel-su/src/main/java/gov/nih/nci/cabig/rave2caaers/exchange/CRLFFixProcessor.java
+++ caAERS/software/jbi/caaers2adeers-sync/core-camel-su/src/main/java/gov/nih/nci/cabig/rave2caaers/exchange/CRLFFixProcessor.java
@@ -1,21 +1,22 @@
 package gov.nih.nci.cabig.rave2caaers.exchange;
 
 
-import java.io.BufferedInputStream;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.ByteOrderMark;
-import org.apache.commons.io.input.BOMInputStream;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.commons.io.ByteOrderMark;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.input.BOMInputStream;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Will remove the DOS specific line separators & extra whitespaces before the XML input
  */
-public class CRLFFixProcessor  implements Processor {
+public class CRLFFixProcessor implements Processor {
+	protected static final Log log = LogFactory.getLog(CRLFFixProcessor.class);
 	final static String defaultEncoding = "UTF-8";
 	
     public void process(Exchange exchange) throws Exception {
@@ -25,11 +26,13 @@ public class CRLFFixProcessor  implements Processor {
     	    BOMInputStream bOMInputStream = new BOMInputStream(inputStream);
     	    ByteOrderMark bom = bOMInputStream.getBOM();
     	    String encoding = bom == null ? defaultEncoding : bom.getCharsetName();
+    	    log.debug("Processing XML message with encoding; " + encoding);
     	    body = IOUtils.toString(bOMInputStream, encoding); 
     	    bOMInputStream.close();
     	} finally {
     	    inputStream.close();
     	}
+    	log.debug("xml body before linefeed fix; " + body);
         String fixedBody = StringUtils.chomp(body);
         fixedBody = StringUtils.remove(fixedBody, '\r');
         fixedBody = StringUtils.trim(fixedBody);
