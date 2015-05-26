c4b1d6e7454bbe722ebfac692ee7a5ef0765255a
DirkCWalter <dirk.walter@semanticbits.com>
2015-05-26 15:45:14 -0400

    Quick fix for the byte order mark.

--- caAERS/software/jbi/caaers2adeers-sync/core-camel-su/src/main/java/gov/nih/nci/cabig/rave2caaers/exchange/CRLFFixProcessor.java
+++ caAERS/software/jbi/caaers2adeers-sync/core-camel-su/src/main/java/gov/nih/nci/cabig/rave2caaers/exchange/CRLFFixProcessor.java
@@ -1,6 +1,13 @@
 package gov.nih.nci.cabig.rave2caaers.exchange;
 
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.ByteOrderMark;
+import org.apache.commons.io.input.BOMInputStream;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.commons.lang.StringUtils;
@@ -9,8 +16,20 @@ import org.apache.commons.lang.StringUtils;
  * Will remove the DOS specific line separators & extra whitespaces before the XML input
  */
 public class CRLFFixProcessor  implements Processor {
+	final static String defaultEncoding = "UTF-8";
+	
     public void process(Exchange exchange) throws Exception {
-        String body = exchange.getIn().getBody(String.class);
+    	String body = null;
+    	InputStream inputStream = exchange.getIn().getBody(InputStream.class);
+    	try {
+    	    BOMInputStream bOMInputStream = new BOMInputStream(inputStream);
+    	    ByteOrderMark bom = bOMInputStream.getBOM();
+    	    String encoding = bom == null ? defaultEncoding : bom.getCharsetName();
+    	    body = IOUtils.toString(inputStream, encoding); 
+    	    bOMInputStream.close();
+    	} finally {
+    	    inputStream.close();
+    	}
         String fixedBody = StringUtils.chomp(body);
         fixedBody = StringUtils.remove(fixedBody, '\r');
         fixedBody = StringUtils.trim(fixedBody);
