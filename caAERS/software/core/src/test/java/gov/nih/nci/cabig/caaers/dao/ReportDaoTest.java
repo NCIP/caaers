545e11b7775c1aa50fdef641210460e8c3e18a51
gjanakiram <janakiram.gollapudi@semanticbits.com>
2015-06-03 15:42:26 -0400

    Un commented failing test cases and fixed them

--- caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/ReportDaoTest.java
+++ caAERS/software/core/src/test/java/gov/nih/nci/cabig/caaers/dao/ReportDaoTest.java
@@ -558,8 +558,7 @@ public class ReportDaoTest extends DaoTestCase<ReportDao> {
         }
     }
    
-    //TODO: Commented to test caaers-web test cases
-   /* public void testSearchByStudyParticipantReportStatusSearchIdentifier(){
+    public void testSearchByStudyParticipantReportStatusSearchIdentifier(){
     	Study study = studyDao.getById(-2);
     	Participant participant = participantDao.getById(-100);
     	List<Report> reports = rsDao.search(study, participant, null, null , 3);
@@ -582,7 +581,7 @@ public class ReportDaoTest extends DaoTestCase<ReportDao> {
     	List<Report> reports = rsDao.search(study, participant, null, null , 1);
     	assertNotNull(reports);
     	assertEquals("wrong number of reports ", 1, reports.size());
-    }*/
+    }
     
     public void testSearchByStudyParticipantReportNoCriteria(){
     	List<Report> reports = rsDao.search(null, null, null, null , 15);
