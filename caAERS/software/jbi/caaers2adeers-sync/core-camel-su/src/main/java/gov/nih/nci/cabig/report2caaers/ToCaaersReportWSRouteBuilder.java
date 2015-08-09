5926a1dce84ef4daf2b17f38e75933af7ce541a6
Biju Joseph <biju.joseph@semanticbits.com>
2015-08-09 12:07:39 -0400

    CAAERS-7373 - 5day is getting submitted twice, when uploaded via filezilla

--- caAERS/software/jbi/caaers2adeers-sync/core-camel-su/src/main/java/gov/nih/nci/cabig/report2caaers/ToCaaersReportWSRouteBuilder.java
+++ caAERS/software/jbi/caaers2adeers-sync/core-camel-su/src/main/java/gov/nih/nci/cabig/report2caaers/ToCaaersReportWSRouteBuilder.java
@@ -38,7 +38,7 @@ public class ToCaaersReportWSRouteBuilder {
         Map<String, String> nss = new HashMap<String, String>();
         nss.put("svrl", "http://purl.oclc.org/dsdl/svrl");
         
-        routeBuilder.from("file://"+inputEDIDir+"?preMove=inprogress&move=done&moveFailed=movefailed")
+        routeBuilder.from("file://"+inputEDIDir+"?preMove=inprogress&move=done&moveFailed=movefailed&delay=20000&readLock=markerFile&maxMessagesPerPoll=1")
             .streamCaching()
             .setExchangePattern(ExchangePattern.InOnly)
             .setProperty(NEEDS_ACK, rb.constant(Boolean.TRUE.toString()))
